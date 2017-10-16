5742bf50097731f06cdfbc8a9232000f9b5f0bc2
Abose Ukhun <bexis17@gmail.com>
2017-10-16 11:23:46 +0100

    add to bubble sort information about big O complexity and efficiency

--- Sorts/bubblesort.js
+++ Sorts/bubblesort.js
@@ -1,5 +1,7 @@
 /*Bubble Sort is a algorithm to sort an array. It
 * compares adjacent element and swaps thier position
+* The big O on bubble sort in worst and best case is O(N^2). 
+ * Not efficient.
 */
 function bubbleSort(items) {
     var length = items.length;
@@ -25,3 +27,5 @@ console.log(ar);
 bubbleSort(ar);
 //Array after sort
 console.log(ar);
+
+
