b6fd393b906511146551f4505f3a8926f0a86531
Luke Schlangen <lukeschlangen@gmail.com>
2017-10-19 16:32:06 -0500

    Create bubbleSort.js

---
+++ Algorithms/bubbleSort.js
@@ -0,0 +1,20 @@
+var a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+
+function bubbleSort(a)
+{
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
+bubbleSort(a);
+console.log(a);
