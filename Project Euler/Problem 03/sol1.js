c4c1f549d89f32bebe914a321523d022838a85b7
PalmPTSJ <palm_bubui@hotmail.com>
2017-10-27 09:23:38 +0700

    Fixed typo in sol1.js of Project Euler Problem 03

--- Project Euler/Problem 03/sol1.js
+++ Project Euler/Problem 03/sol1.js
@@ -1,8 +1,9 @@
 /*
+Problem statement:
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number N ?
 */
-var n = 600851475143; // For problem 2 in projecteuler.net, N=600851475143
+var n = 600851475143; // For problem 3 in projecteuler.net, N=600851475143
 var i = 2;
 var ans = 1;
 while(i*i <= n) {
