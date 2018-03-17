255a9cfacb289f46c232ac8f58a3b39e57c8333a
Kulshreshtha Goyal <kulshreshthagoyal@gmail.com>
2018-03-18 01:16:50 +0530

    added bubble sorting algorithm

---
+++ Algorithms/bubblesort.js
@@ -0,0 +1,23 @@
+function bubble_Sort(a)
+{
+    var swapp;
+    var n = a.length-1;
+    var x=a;
+    do {
+        swapp = false;
+        for (var i=0; i < n; i++)
+        {
+            if (x[i] < x[i+1])
+            {
+               var temp = x[i];
+               x[i] = x[i+1];
+               x[i+1] = temp;
+               swapp = true;
+            }
+        }
+        n--;
+    } while (swapp);
+ return x; 
+}
+
+console.log(bubble_Sort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
