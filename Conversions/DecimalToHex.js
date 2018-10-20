31d5467ada845dca245a6c49e0f9330d8a6f0dd5
Alex Brown <opti.jawsome@gmail.com>
2018-10-20 15:35:49 -0500

    added DecimalToHex

---
+++ Conversions/DecimalToHex.js
@@ -0,0 +1,19 @@
+function intToHex(num){
+    switch(num){
+        case 10: return "A";
+        case 11: return "B";
+        case 12: return "C";
+        case 13: return "D";
+        case 14: return "E";
+        case 15: return "F";
+    }
+    return num;
+}
+function decimalToHex(num){
+    let hex_out = [];
+    while(num > 15) {
+        hex_out.push(intToHex(num%16));
+        num = Math.floor(num / 16);
+    }
+    return hex_out.join("");
+}
