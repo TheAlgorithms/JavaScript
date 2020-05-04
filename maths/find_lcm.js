a90b487ae7e25e1b8835d78ab4f71af42667a12a
Christian Clauss <cclauss@me.com>
2020-05-04 16:38:28 +0200

    find_lcm.js: Standardjs fixes

--- maths/find_lcm.js
+++ maths/find_lcm.js
@@ -12,19 +12,19 @@
 'use strict'
 
 // Find the LCM of two numbers.
-function find_lcm (num_1, num_2) {
+function findLcm (num1, num2) {
   var max_num
   var lcm
-  // Check to see whether num_1 or num_2 is larger.
-  if (num_1 > num_2) {
-    max_num = num_1
+  // Check to see whether num1 or num2 is larger.
+  if (num1 > num2) {
+    max_num = num1
   } else {
-    max_num = num_2
+    max_num = num2
   }
   lcm = max_num
 
   while (true) {
-    if ((lcm % num_1 === 0) && (lcm % num_2 === 0)) {
+    if ((lcm % num1 === 0) && (lcm % num2 === 0)) {
       break
     }
     lcm += max_num
@@ -32,7 +32,7 @@ function find_lcm (num_1, num_2) {
   return lcm
 }
 
-// Run `find_lcm` Function
-var num_1 = 12
-var num_2 = 76
-console.log(find_lcm(num_1, num_2))
+// Run `findLcm` Function
+var num1 = 12
+var num2 = 76
+console.log(findLcm(num1, num2))
