6507cbf7d476dcf1df1cac122ab2d11047b919e4
itsvinayak <itssvinayak@gmail.com>
2020-05-05 17:41:32 +0530

    sort/

--- Sorts/heapSort.js
+++ Sorts/heapSort.js
@@ -1,11 +1,11 @@
 /*
-* Build a max heap out of the array. A heap is a specialized tree like
-* data structure that satisfies the heap property. The heap property
-* for max heap is the following: "if P is a parent node of C, then the
-* key (the value) of node P is greater than the key of node C"
-* Source: https://en.wikipedia.org/wiki/Heap_(data_structure)
-*/
-
+ * Build a max heap out of the array. A heap is a specialized tree like
+ * data structure that satisfies the heap property. The heap property
+ * for max heap is the following: "if P is a parent node of C, then the
+ * key (the value) of node P is greater than the key of node C"
+ * Source: https://en.wikipedia.org/wiki/Heap_(data_structure)
+ */
+/* eslint no-extend-native: ["off", { "exceptions": ["Object"] }] */
 Array.prototype.heapify = function (index, heapSize) {
   let largest = index
   const leftIndex = 2 * index + 1
@@ -29,10 +29,10 @@ Array.prototype.heapify = function (index, heapSize) {
 }
 
 /*
-* Heap sort sorts an array by building a heap from the array and
-* utilizing the heap property.
-* For more information see: https://en.wikipedia.org/wiki/Heapsort
-*/
+ * Heap sort sorts an array by building a heap from the array and
+ * utilizing the heap property.
+ * For more information see: https://en.wikipedia.org/wiki/Heapsort
+ */
 function heapSort (items) {
   const length = items.length
 
