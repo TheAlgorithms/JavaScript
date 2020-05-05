34ee21d64bcf447e59e02ce8567c73934299d71f
vinayak <itssvinayak@gmail.com>
2020-05-05 17:45:54 +0530

    Update pascalTriangle.js

--- maths/pascalTriangle.js
+++ maths/pascalTriangle.js
@@ -1,33 +1,31 @@
-let numRows = 5
+const numRows = 5                                                                                                                                      
 
-var generate = function(numRows) {
-    let triangle = [[1], [1,1]]
-    
-    if(numRows === 0){
-        return []
-    } else if(numRows == 1){
-        return [[1]]       
-    } else if(numRows == 2){
-        return  [[1], [1,1]]
-    } else {      
-        for(let i = 2; i < numRows; i++){       
-            addRow(triangle)
-        }
+var generate = function (numRows) {
+  const triangle = [[1], [1, 1]]
+
+  if (numRows === 0) {
+    return []
+  } else if (numRows === 1) {
+    return [[1]]
+  } else if (numRows === 2) {
+    return [[1], [1, 1]]
+  } else {
+    for (let i = 2; i < numRows; i++) {
+      addRow(triangle)
     }
-    return triangle
-    
-};
-var addRow = function(triangle){
-    let previous = triangle[triangle.length - 1]
-    let newRow = [1]
-    for(let i = 0; i < previous.length - 1; i++){
-        let current = previous[i]
-        let next = previous[i+1] 
-        newRow.push(current + next)
-    }
-    newRow.push(1)
-    return triangle.push(newRow)
-    
+  }
+  return triangle
+}
+var addRow = function (triangle) {
+  const previous = triangle[triangle.length - 1]
+  const newRow = [1]
+  for (let i = 0; i < previous.length - 1; i++) {
+    const current = previous[i]
+    const next = previous[i + 1]
+    newRow.push(current + next)
+  }
+  newRow.push(1)
+  return triangle.push(newRow)
 }
 
 generate(numRows)
