c124a0cdec3629bbcab8c097309d5cd32987a985
vinayak <itssvinayak@gmail.com>
2020-05-06 14:49:47 +0530

    Update Palindrome.js

--- maths/Palindrome.js
+++ maths/Palindrome.js
@@ -13,33 +13,39 @@
  * @flow
  * @complexity: O(n)
  */
-export default function PalindromeRecursive(string: string): boolean {
+
+function PalindromeRecursive (string) {
   // Base case
-  if (string.length < 2) return true;
+  if (string.length < 2) return true
 
   // Check outermost keys
   if (string[0] !== string[string.length - 1]) {
-    return false;
+    return false
   }
 
-  return PalindromeRecursive(string.slice(1, string.length - 1));
+  return PalindromeRecursive(string.slice(1, string.length - 1))
 }
 
-export function PalindromeIterative(string: string): boolean {
+function PalindromeIterative (string) {
   const _string = string
     .toLowerCase()
     .replace(/ /g, '')
     .replace(/,/g, '')
     .replace(/'.'/g, '')
     .replace(/:/g, '')
-    .split('');
+    .split('')
 
   // A word of only 1 character is already a palindrome, so we skip to check it
   while (_string.length > 1) {
     if (_string.shift() !== _string.pop()) {
-      return false;
+      return false
     }
   }
 
-  return true;
+  return true
 }
+
+// testing
+
+console.log(PalindromeRecursive('Javascript Community'))
+console.log(PalindromeIterative('mom'))
