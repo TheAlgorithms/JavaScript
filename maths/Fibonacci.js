bcead93f1182447319e35df4bee78c2870f2a098
vinayak <itssvinayak@gmail.com>
2020-05-06 18:10:43 +0530

    Update Fibonacci.js

--- maths/Fibonacci.js
+++ maths/Fibonacci.js
@@ -1,52 +1,59 @@
-// @flow
-type num = number;
+const list = []
 
-const list = [];
+const FibonacciIterative = (nth) => {
+  const sequence = []
 
-export default function FibonacciIterative(nth: number): num[] {
-  const sequence = [];
-
-  if (nth >= 1) sequence.push(1);
-  if (nth >= 2) sequence.push(1);
+  if (nth >= 1) sequence.push(1)
+  if (nth >= 2) sequence.push(1)
 
   for (let i = 2; i < nth; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
+    sequence.push(sequence[i - 1] + sequence[i - 2])
   }
 
-  return sequence;
+  return sequence
 }
 
-export function FibonacciRecursive(number: num): num[] {
-  return ((): num[] => {
+const FibonacciRecursive = (number) => {
+  return (() => {
     switch (list.length) {
       case 0:
-        list.push(1);
-        return FibonacciRecursive(number);
+        list.push(1)
+        return FibonacciRecursive(number)
       case 1:
-        list.push(1);
-        return FibonacciRecursive(number);
+        list.push(1)
+        return FibonacciRecursive(number)
       case number:
-        return list;
+        return list
       default:
-        list.push(list[list.length - 1] + list[list.length - 2]);
-        return FibonacciRecursive(number);
+        list.push(list[list.length - 1] + list[list.length - 2])
+        return FibonacciRecursive(number)
     }
-  })();
+  })()
 }
 
-const dict: Map<num, num> = new Map();
+const dict = new Map()
 
-export function FibonacciRecursiveDP(stairs: num): num {
-  if (stairs <= 0) return 0;
-  if (stairs === 1) return 1;
+const FibonacciRecursiveDP = (stairs) => {
+  if (stairs <= 0) return 0
+  if (stairs === 1) return 1
 
   // Memoize stair count
-  if (dict.has(stairs)) return dict.get(stairs);
+  if (dict.has(stairs)) return dict.get(stairs)
 
   const res =
-    FibonacciRecursiveDP(stairs - 1) + FibonacciRecursiveDP(stairs - 2);
+    FibonacciRecursiveDP(stairs - 1) + FibonacciRecursiveDP(stairs - 2)
 
-  dict.set(stairs, res);
+  dict.set(stairs, res)
 
-  return res;
+  return res
 }
+
+// testing
+
+console.log(FibonacciIterative(5))
+// Output: [ 1, 1, 2, 3, 5 ]
+console.log(FibonacciRecursive(5))
+// Output: [ 1, 1, 2, 3, 5 ]
+
+console.log(FibonacciRecursiveDP(5))
+// Output: 5
