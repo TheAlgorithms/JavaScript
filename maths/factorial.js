14b8fb53573ab05500f4d4f4a41a6a8dbdd4f165
vinayak <itssvinayak@gmail.com>
2020-05-06 18:17:19 +0530

    Update factorial.js

--- maths/factorial.js
+++ maths/factorial.js
@@ -11,26 +11,28 @@
         https://en.wikipedia.org/wiki/factorial
 */
 
-"use strict";
+const list = []
 
-var f = [];
-function calc_factorial(num) {
-    if (num < 0) {
-        return "Sorry, factorial does not exist for negative numbers.";
-    }
-    if (num === null || num === undefined) {
-        return "Sorry, factorial does not exist for null or undefined numbers.";
-    }
-  if (num === 0 || num === 1)
-    return 1;
-    
-  if (f[num] > 0)
-    return f[num];
-    
-  return f[num] = factorial(num-1) * num;
-} 
+function calcFactorial (num) {
+  if (num < 0) {
+    return 'Sorry, factorial does not exist for negative numbers.'
+  }
+  if (num === null || num === undefined) {
+    return 'Sorry, factorial does not exist for null or undefined numbers.'
+  }
+  if (num === 0 || num === 1) {
+    return 1
+  }
 
+  if (list[num] > 0) {
+    return list[num]
+  }
+
+  list[num] = calcFactorial(num - 1) * num
+  return list[num]
+}
 
 // Run `factorial` Function to find average of a list of numbers.
-var num = prompt("Enter a number: ");
-alert(calc_factorial(num));
+// testing
+const num = 5
+console.log(calcFactorial(num))
