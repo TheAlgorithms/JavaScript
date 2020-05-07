9c50d755264a2e15583ffd199fc46afc3a468555
vinayak <itssvinayak@gmail.com>
2020-05-07 20:37:21 +0530

    Update ReverseString.js

--- maths/ReverseString.js
+++ maths/ReverseString.js
@@ -7,15 +7,16 @@
  * Create a new string and append
  * @complexity O(n)
  */
-export default function ReverseStringIterative(string: string): string {
-  let reversedString = '';
-  let index;
+
+function ReverseStringIterative (string) {
+  let reversedString = ''
+  let index
 
   for (index = string.length - 1; index >= 0; index--) {
-    reversedString += string[index];
+    reversedString += string[index]
   }
 
-  return reversedString;
+  return reversedString
 }
 
 /**
@@ -25,15 +26,20 @@ export default function ReverseStringIterative(string: string): string {
  *
  * 'some' -> 'eoms' -> 'emos'
  */
-export function ReverseStringIterativeInplace(string: string): string {
-  const _string = string.split('');
+
+function ReverseStringIterativeInplace (string) {
+  const _string = string.split('')
 
   for (let i = 0; i < Math.floor(_string.length / 2); i++) {
-    const first = _string[i];
-    const second = _string[_string.length - 1 - i];
-    _string[i] = second;
-    _string[_string.length - 1 - i] = first;
+    const first = _string[i]
+    const second = _string[_string.length - 1 - i]
+    _string[i] = second
+    _string[_string.length - 1 - i] = first
   }
 
-  return _string.join('');
+  return _string.join('')
 }
+
+// testing
+console.log(ReverseStringIterative('Javascript'))
+console.log(ReverseStringIterativeInplace('Javascript'))
