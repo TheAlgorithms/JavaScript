ba2c3652bc7a079325be5ee059635ea1ccb84329
vinayak <itssvinayak@gmail.com>
2020-05-07 20:44:52 +0530

    Update Heapsort.js

--- Sorts/Heapsort.js
+++ Sorts/Heapsort.js
@@ -1,48 +1,48 @@
- var array_length;
-/* to create MAX  array */  
-function heap_root(input, i) {
-    var left = 2 * i + 1;
-    var right = 2 * i + 2;
-    var max = i;
+let arrayLength = 0
 
-    if (left < array_length && input[left] > input[max]) {
-        max = left;
-    }
+/* to create MAX  array */
 
-    if (right < array_length && input[right] > input[max])     {
-        max = right;
-    }
+function heapRoot (input, i) {
+  const left = 2 * i + 1
+  const right = 2 * i + 2
+  let max = i
 
-    if (max != i) {
-        swap(input, i, max);
-        heap_root(input, max);
-    }
+  if (left < arrayLength && input[left] > input[max]) {
+    max = left
+  }
+
+  if (right < arrayLength && input[right] > input[max]) {
+    max = right
+  }
+
+  if (max !== i) {
+    swap(input, i, max)
+    heapRoot(input, max)
+  }
 }
 
-function swap(input, index_A, index_B) {
-    var temp = input[index_A];
+function swap (input, indexA, indexB) {
+  const temp = input[indexA]
 
-    input[index_A] = input[index_B];
-    input[index_B] = temp;
+  input[indexA] = input[indexB]
+  input[indexB] = temp
 }
 
-function heapSort(input) {
-    
-    array_length = input.length;
+function heapSort (input) {
+  arrayLength = input.length
 
-    for (var i = Math.floor(array_length / 2); i >= 0; i -= 1)      {
-        heap_root(input, i);
-      }
+  for (let i = Math.floor(arrayLength / 2); i >= 0; i -= 1) {
+    heapRoot(input, i)
+  }
 
-    for (i = input.length - 1; i > 0; i--) {
-        swap(input, 0, i);
-        array_length--;
-      
-      
-        heap_root(input, 0);
-    }
+  for (let i = input.length - 1; i > 0; i--) {
+    swap(input, 0, i)
+    arrayLength--
+
+    heapRoot(input, 0)
+  }
 }
 
-var arr = [3, 0, 2, 5, -1, 4, 1];
-heapSort(arr);
-console.log(arr);
+const arr = [3, 0, 2, 5, -1, 4, 1]
+heapSort(arr)
+console.log(arr)
