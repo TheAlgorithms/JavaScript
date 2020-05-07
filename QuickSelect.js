da9ded5d2fb1a4c01c033954d1e47059468b625e
vinayak <itssvinayak@gmail.com>
2020-05-07 20:52:37 +0530

    Update QuickSelect.js

--- QuickSelect.js
+++ QuickSelect.js
@@ -10,57 +10,59 @@
  * @complexity: O(n^2) (worst case)
  * @flow
  */
-type num = number;
 
-export default function QuickSelect(items: num[], kth: num): num {
-  return RandomizedSelect(items, 0, items.length - 1, kth);
+function QuickSelect (items, kth) {
+  return RandomizedSelect(items, 0, items.length - 1, kth)
 }
 
-export function RandomizedSelect(
-  items: num[],
-  left: num,
-  right: num,
-  i: num
-): any {
-  if (left === right) return items[left];
+function RandomizedSelect (
+  items,
+  left,
+  right,
+  i
+) {
+  if (left === right) return items[left]
 
-  const pivotIndex = RandomizedPartition(items, left, right);
-  const k = pivotIndex - left + 1;
+  const pivotIndex = RandomizedPartition(items, left, right)
+  const k = pivotIndex - left + 1
 
-  if (i === k) return items[pivotIndex];
-  if (i < k) return RandomizedSelect(items, left, pivotIndex - 1, i);
+  if (i === k) return items[pivotIndex]
+  if (i < k) return RandomizedSelect(items, left, pivotIndex - 1, i)
 
-  return RandomizedSelect(items, pivotIndex + 1, right, i - k);
+  return RandomizedSelect(items, pivotIndex + 1, right, i - k)
 }
 
-export function RandomizedPartition(items: num[], left: num, right: num): num {
-  const rand = getRandomInt(left, right);
-  Swap(items, rand, right);
-  return Partition(items, left, right);
+function RandomizedPartition (items, left, right) {
+  const rand = getRandomInt(left, right)
+  Swap(items, rand, right)
+  return Partition(items, left, right)
 }
 
-function Partition(items: num[], left: num, right: num): num {
-  const x = items[right];
-  let pivotIndex = left - 1;
+function Partition (items, left, right) {
+  const x = items[right]
+  let pivotIndex = left - 1
 
   for (let j = left; j < right; j++) {
     if (items[j] <= x) {
-      pivotIndex++;
-      Swap(items, pivotIndex, j);
+      pivotIndex++
+      Swap(items, pivotIndex, j)
     }
   }
 
-  Swap(items, pivotIndex + 1, right);
+  Swap(items, pivotIndex + 1, right)
 
-  return pivotIndex + 1;
+  return pivotIndex + 1
 }
 
-function getRandomInt(min: num, max: num): num {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function Swap(arr: any[], x: any, y: any) {
-  const temp = arr[x];
-  arr[x] = arr[y];
-  arr[y] = temp;
+function Swap (arr, x, y) {
+  const temp = arr[x]
+  arr[x] = arr[y]
+  arr[y] = temp
 }
+
+// testing
+console.log(QuickSelect([1, 4, 2, -2, 4, 5]))
