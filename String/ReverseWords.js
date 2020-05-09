88374c32044c2883faa2cf8464cdd6dd648e1e70
vinayak <itssvinayak@gmail.com>
2020-05-10 00:20:56 +0530

    Update ReverseWords.js

--- String/ReverseWords.js
+++ String/ReverseWords.js
@@ -1,12 +1,10 @@
-const reverseWords = (str) => {                                                                                                                        
+const reverseWords = (str) => {
   // Split string into words
   // Ex. "I Love JS" => ["I", "Love", "JS"]
   const words = str.split(' ')
-
   // reverse words
   // ["I", "Love", "JS"] => ["JS", "Love", "I"]
   const reversedWords = words.reverse()
-
   // join reversed words with space and return
   // ["JS", "Love", "I"] => "JS Love I"
   return reversedWords.join(' ')
