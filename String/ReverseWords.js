ac4f199ccc23cafc96e2a29eb96ba41ad51d0ee8
vinayak <itssvinayak@gmail.com>
2020-05-10 00:08:37 +0530

    Update ReverseWords.js

--- String/ReverseWords.js
+++ String/ReverseWords.js
@@ -1,17 +1,17 @@
-const reverseWords = (str) => {
-    // Split string into words
-    // Ex. "I Love JS" => ["I", "Love", "JS"]
-    const words = str.split(" ");
+const reverseWords = (str) => {                                                                                                                        
+  // Split string into words
+  // Ex. "I Love JS" => ["I", "Love", "JS"]
+  const words = str.split(' ')
 
-    // reverse words
-    // ["I", "Love", "JS"] => ["JS", "Love", "I"]
-    const reversedWords = words.reverse();
+  // reverse words
+  // ["I", "Love", "JS"] => ["JS", "Love", "I"]
+  const reversedWords = words.reverse()
 
-    // join reversed words with space and return
-    // ["JS", "Love", "I"] => "JS Love I"
-    return reversedWords.join(" ");
+  // join reversed words with space and return
+  // ["JS", "Love", "I"] => "JS Love I"
+  return reversedWords.join(' ')
 }
 
 // testing
-console.log(reverseWords("I Love JS"));
-console.log(reverseWords("My Name Is JavaScript"));
+console.log(reverseWords('I Love JS'))
+console.log(reverseWords('My Name Is JavaScript'))
