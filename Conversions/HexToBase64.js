45289392388d76c2dcf1436f90b76872c8227530
arfbacem <61031657+arfbacem@users.noreply.github.com>
2020-05-25 01:38:16 +0100

    Update HexToBase64.js

--- Conversions/HexToBase64.js
+++ Conversions/HexToBase64.js
@@ -1,9 +1,7 @@
-//Hex Encoding to base64
-//Example: Hex=12c13f1d313c315cf313513d13; =====> Base64=EsE/HTE8MVzzE1E9Ew==;
-
+// Hex Encoding to base64
+// Example: Hex=12c13f1d313c315cf313513d13; =====> Base64=EsE/HTE8MVzzE1E9Ew==;
 
 function hexToBase64 (hex) {
-  const hexToBase64 = hex => Buffer.from(hex, 'hex').toString('base64');
-  }
-  console.log('the result is ' + hexToBase64);
+  const hexToBase64 = hex => Buffer.from(hex, 'hex').toString('base64')
 }
+console.log('the result is ' + hexToBase64)
