955e083af1e2f99222f0c110f312f1e2d2728eb5
vinayak <itssvinayak@gmail.com>
2020-06-23 10:13:12 +0530

    Update XORCipher.js

--- Ciphers/XORCipher.js
+++ Ciphers/XORCipher.js
@@ -11,15 +11,16 @@
  * @param {Number} key - key for encryption
  * @return {String} encrypted string
  */
-function XOR(str, key) {
-    let result = "";
-    for (const elem of str) {
-        result += String.fromCharCode(elem.charCodeAt(0) ^ key);
-    }
-    return result;
+
+function XOR (str, key) {
+  let result = ''
+  for (const elem of str) {
+    result += String.fromCharCode(elem.charCodeAt(0) ^ key)
+  }
+  return result
 }
 
-const encryptedString =  XOR("test string", 32);
-console.log("Encrypted: ", encryptedString);
-const decryptedString = XOR(encryptedString, 32);
-console.log("Decrypted: ", decryptedString);
+const encryptedString = XOR('test string', 32)
+console.log('Encrypted: ', encryptedString)
+const decryptedString = XOR(encryptedString, 32)
+console.log('Decrypted: ', decryptedString)
