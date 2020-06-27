ee9a03d48a85f7c7f66bec148569468d7510a2da
Askanders <47815058+Askanders@users.noreply.github.com>
2020-06-27 17:11:02 +0200

    Update BinarySearch.js
    Style change

--- Search/BinarySearch.js
+++ Search/BinarySearch.js
@@ -7,25 +7,25 @@
  * value is found or the interval is empty.
  */
 
-function binarySearch(arr, x, low = 0, high = arr.length - 1) {
-  let mid = Math.floor(low + (high - low) / 2);
+function binarySearch( arr, x, low = 0, high = arr.length - 1 ) {
+  const mid = Math.floor(low + (high - low) / 2)
 
   if (high >= low) {
     if (arr[mid] === x) {
       // item found => return its index
-      return mid;
+      return mid
     }
 
     if (x < arr[mid]) {
       // arr[mid] is an upper bound for x, so if x is in arr => low <= x < mid
-      return binarySearch(arr, x, low, mid - 1);
+      return binarySearch(arr, x, low, mid - 1)
     } else {
       // arr[mid] is a lower bound for x, so if x is in arr => mid < x <= high
-      return binarySearch(arr, x, mid + 1, high);
+      return binarySearch(arr, x, mid + 1, high)
     }
   } else {
     // if low > high => we have searched the whole array without finding the item
-    return -1;
+    return -1
   }
 }
 
@@ -33,38 +33,38 @@ function binarySearch(arr, x, low = 0, high = arr.length - 1) {
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const stringArr = [
-  "Alpha",
-  "Bravo",
-  "Charlie",
-  "Delta",
-  "Echo",
-  "Foxtrot",
-  "Golf",
-  "Hotel",
-  "India",
-  "Juliet",
-  "Kilo",
-  "Lima",
-  "Mike",
-  "November",
-  "Oscar",
-  "Papa",
-  "Quebec",
-  "Romeo",
-  "Sierra",
-  "Tango",
-  "Uniform",
-  "Victor",
-  "Whiskey",
-  "X-Ray",
-  "Yankee",
-  "Zulu",
+  'Alpha',
+  'Bravo',
+  'Charlie',
+  'Delta',
+  'Echo',
+  'Foxtrot',
+  'Golf',
+  'Hotel',
+  'India',
+  'Juliet',
+  'Kilo',
+  'Lima',
+  'Mike',
+  'November',
+  'Oscar',
+  'Papa',
+  'Quebec',
+  'Romeo',
+  'Sierra',
+  'Tango',
+  'Uniform',
+  'Victor',
+  'Whiskey',
+  'X-Ray',
+  'Yankee',
+  'Zulu',
 ];
 
-console.log(binarySearch(arr, 3));
-console.log(binarySearch(arr, 7));
-console.log(binarySearch(arr, 13));
+console.log(binarySearch(arr, 3))
+console.log(binarySearch(arr, 7))
+console.log(binarySearch(arr, 13))
 
-console.log(binarySearch(stringArr, "Charlie"));
-console.log(binarySearch(stringArr, "Zulu"));
-console.log(binarySearch(stringArr, "Sierra"));
+console.log(binarySearch(stringArr, "Charlie"))
+console.log(binarySearch(stringArr, "Zulu"))
+console.log(binarySearch(stringArr, "Sierra"))
