48c0ad969dc9128ba6704b3a2b705b7fa7ceb228
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-06-29 11:15:01 +0530

    Standardized program

--- back-tracking/KnightTour.js
+++ back-tracking/KnightTour.js
@@ -1,78 +1,78 @@
 // Wikipedia: https://en.wikipedia.org/wiki/Knight%27s_tour
 
 class OpenKnightTour {
-    constructor(size) {
-        this.board = new Array(size).fill(0).map(() => new Array(size).fill(0))
-        this.size = size
+  constructor (size) {
+    this.board = new Array(size).fill(0).map(() => new Array(size).fill(0))
+    this.size = size
+  }
+
+  getMoves ([i, j]) {
+    // helper function to get the valid moves of the knight from the current position
+    const moves = [
+      [i + 2, j - 1],
+      [i + 2, j + 1],
+      [i - 2, j - 1],
+      [i - 2, j + 1],
+      [i + 1, j - 2],
+      [i + 1, j + 2],
+      [i - 1, j - 2],
+      [i - 1, j + 2]
+    ]
+
+    return moves.filter(([y, x]) => y >= 0 && y < this.size && x >= 0 && x < this.size)
+  }
+
+  isComplete () {
+    // helper function to check if the board is complete
+    return !this.board.map(row => row.includes(0)).includes(true)
+  }
+
+  solve () {
+    // function to find the solution for the given board
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (this.solveHelper([i, j], 0)) return true
+      }
     }
+    return false
+  }
 
-    getMoves([i, j]) {
-        // helper function to get the valid moves of the knight from the current position
-        const moves = [
-            [i + 2, j - 1],
-            [i + 2, j + 1],
-            [i - 2, j - 1],
-            [i - 2, j + 1],
-            [i + 1, j - 2],
-            [i + 1, j + 2],
-            [i - 1, j - 2],
-            [i - 1, j + 2],
-        ]
+  solveHelper ([i, j], curr) {
+    // helper function for the main computation
+    if (this.isComplete()) return true
 
-        return moves.filter(([y, x]) => y >= 0 && y < this.size && x >= 0 && x < this.size)
+    for (const [y, x] of this.getMoves([i, j])) {
+      if (this.board[y][x] === 0) {
+        this.board[y][x] = curr + 1
+        if (this.solveHelper([y, x], curr + 1)) return true
+        // backtracking
+        this.board[y][x] = 0
+      }
     }
+    return false
+  }
 
-    isComplete() {
-        // helper function to check if the board is complete
-        return !this.board.map(row => row.includes(0)).includes(true)
+  printBoard () {
+    // utility function to display the board
+    for (const row of this.board) {
+      let string = ''
+      for (const elem of row) {
+        string += elem + '\t'
+      }
+      console.log(string)
     }
-
-    solve() {
-        // function to find the solution for the given board
-        for (let i = 0; i < this.size; i++) {
-            for (let j = 0; j < this.size; j++) {
-                if (this.solve_helper([i, j], 0)) return true
-            }
-        }
-        return false
-    }
-
-    solve_helper([i, j], curr) {
-        // helper function for the main computation
-        if (this.isComplete()) return true
-
-        for (const [y, x] of this.getMoves([i, j])) {
-            if (this.board[y][x] === 0) {
-                this.board[y][x] = curr + 1
-                if (this.solve_helper([y, x], curr + 1)) return true
-                // backtracking
-                this.board[y][x] = 0
-            }
-        }
-        return false
-    }
-
-    printBoard() {
-        // utility function to display the board
-        for (const row of this.board) {
-            let string = ""
-            for (const elem of row) {
-                string += elem + '\t'
-            }
-            console.log(string)
-        }
-    }
+  }
 }
 
-function main() {
-    const board = new OpenKnightTour(5)
+function main () {
+  const board = new OpenKnightTour(5)
 
-    board.printBoard()
-    console.log('\n')
+  board.printBoard()
+  console.log('\n')
 
-    board.solve()
+  board.solve()
 
-    board.printBoard()
+  board.printBoard()
 }
 
 main()
