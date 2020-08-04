d6f75695d555739691911f504c74c464e5cb4199
Utsav1999 <utsavakash123@gmail.com>
2020-08-04 22:55:35 +0530

    Updated

--- Maths/CountDigit.js
+++ Maths/CountDigit.js
@@ -1,14 +1,12 @@
 /*
-
-    1. This program will ask a user to enter a number
-    2. Then it will count the number of digits present in the given number
-    3. Will print the number of digits in console
-
+1. This program will ask a user to enter a number
+2. Then it will count the number of digits present in the given number
+3. Will print the number of digits in console
 */
 
 function CountDigit (number) {
-    var digits = Math.floor(Math.log10(number) + 1);
-    return digits;
+  var digits = Math.floor(Math.log10(number) + 1)
+  return digits
 }
 
-console.log('The number of digits in the number: ' + CountDigit(parseInt(prompt('Enter number: '))));
+console.log('The number of digits in the number: ' + CountDigit(parseInt(window.prompt('Enter number: '))))
