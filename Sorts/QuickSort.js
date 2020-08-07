3688b0c9abb8f677671df15c197385d83b474be7
thomasJs8 <thomas18thx@gmail.com>
2020-08-07 23:20:17 +0900

    fix: run standard

--- Sorts/QuickSort.js
+++ Sorts/QuickSort.js
@@ -2,39 +2,39 @@
  * Quick sort is a comparison sorting algorithm that uses a divide and conquer strategy.
  * For more information see here: https://en.wikipedia.org/wiki/Quicksort
  */
-function quickSort(items) {
-  var length = items.length;
+function quickSort (items) {
+  var length = items.length
 
   if (length <= 1) {
-    return items;
+    return items
   }
-  var PIVOT = items[0];
-  var GREATER = [];
-  var LESSER = [];
+  var PIVOT = items[0]
+  var GREATER = []
+  var LESSER = []
 
   for (var i = 1; i < length; i++) {
     if (items[i] > PIVOT) {
-      GREATER.push(items[i]);
+      GREATER.push(items[i])
     } else {
-      LESSER.push(items[i]);
+      LESSER.push(items[i])
     }
   }
 
-  var sorted = quickSort(LESSER);
-  sorted.push(PIVOT);
-  sorted = sorted.concat(quickSort(GREATER));
+  var sorted = quickSort(LESSER)
+  sorted.push(PIVOT)
+  sorted = sorted.concat(quickSort(GREATER))
 
-  return sorted;
+  return sorted
 }
 
 // Implementation of quick sort
 
-var ar = [0, 5, 3, 2, 2];
+var ar = [0, 5, 3, 2, 2]
 // Array before Sort
-console.log(ar);
-ar = quickSort(ar);
+console.log(ar)
+ar = quickSort(ar)
 // Array after sort
-console.log(ar);
+console.log(ar)
 
 /**
  * Implementation of Quick sort in Functional Programming.
@@ -50,24 +50,26 @@ console.log(ar);
  *
  * If the smaller-value and the larget-value both get an empty array, sorting ends.
  */
-function functionalQuickSort(items) {
+function functionalQuickSort (items) {
   if (!items.length) {
-    return [];
+    return []
   }
-  const [x, ...rest] = items;
-  const smaller = rest.filter((item) => x > item);
-  const larger = rest.filter((item) => x <= item);
-  return quickSort(smaller).concat([x]).concat(quickSort(larger));
+  const [x, ...rest] = items
+  const smaller = rest.filter((item) => x > item)
+  const larger = rest.filter((item) => x <= item)
+  return functionalQuickSort(smaller)
+    .concat([x])
+    .concat(functionalQuickSort(larger))
 }
 
 // Sort an array having numbers
-const items = [3, 5, 1, 4, 2];
+const items = [3, 5, 1, 4, 2]
 // Array before Sort
-console.log(quickSort(items));
-const returnedItems = quickSort(items);
+console.log(functionalQuickSort(items))
+const returnedItems = functionalQuickSort(items)
 // Array after Sord
-console.log(returnedItems);
+console.log(returnedItems)
 
 // Sort an array having literals
-const anotherItems = ["b", "z", "a", "c", "t", "o"];
-console.log(quickSort(anotherItems));
+const anotherItems = ['b', 'z', 'a', 'c', 't', 'o']
+console.log(functionalQuickSort(anotherItems))
