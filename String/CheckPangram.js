b0c104f3737e0cd41a0654e0150ba1c0e42d9296
Jaden <68265256+arcane2099@users.noreply.github.com>
2020-08-14 14:14:21 +0800

    Update CheckPangram.js
    Moved the const 'ALPHABETS' out of the function body

--- String/CheckPangram.js
+++ String/CheckPangram.js
@@ -1,13 +1,14 @@
 // Pangram is a uniques sentence that contains every letter of the alphabet
 // The alphabet has to appear at least once
 // Pangram Check is case sensitve
-function checkPangram (sentence) {
-  // Create an array of 26 characters
-  const alphabets = 'abcdefghijklmnopqrstuvwxyz'
 
+// Create an array of 26 characters
+const ALPHABETS = 'abcdefghijklmnopqrstuvwxyz'
+
+function checkPangram (sentence) {
   // Check if all 26 alphabets characters exists in the sentence
-  for (let i = 0; i < alphabets.length; i++) {
-    if (sentence.indexOf(alphabets[i]) === -1) {
+  for (let i = 0; i < ALPHABETS.length; i++) {
+    if (sentence.indexOf(ALPHABETS[i]) === -1) {
       return 'This sentence is not a pangram'
     }
   }
