1d938dad99e9266928305aae3d02698c9e82d5c6
vinayak <itssvinayak@gmail.com>
2020-08-19 23:52:01 +0530

    Update Conversions/DecimalToOctal.js
    Co-authored-by: Tapajyoti Bose <44058757+ruppysuppy@users.noreply.github.com>

--- Conversions/DecimalToOctal.js
+++ Conversions/DecimalToOctal.js
@@ -2,7 +2,7 @@ function decimalToOctal (num) {
   let oct = 0
   let c = 0
   while (num > 0) {
-    var r = num % 8
+    const r = num % 8
     oct = oct + (r * Math.pow(10, c++))
     num = Math.floor(num / 8) // basically /= 8 without remainder if any
   }
