10ebd4aafab0791827365b40b4850a7ffa207e91
Neha Saggam <neha@technogise.com>
2020-08-20 22:33:27 +0530

    Add comments for understanding

--- Conversions/BinaryToDecimal.js
+++ Conversions/BinaryToDecimal.js
@@ -1,8 +1,8 @@
 function binaryToDeicmal (binaryNumber) {
   let decimalNumber = 0
-  const binaryDigits = binaryNumber.split('').reverse()
+  const binaryDigits = binaryNumber.split('').reverse() // Splits the bnary number in revered single digits
   binaryDigits.forEach((binaryDigit, index) => {
-    decimalNumber += binaryDigit * (Math.pow(2, index))
+    decimalNumber += binaryDigit * (Math.pow(2, index)) // Summation of all the decimal converted digits
   })
   console.log(`Decimal of ${binaryNumber} is ${decimalNumber}`)
 }
