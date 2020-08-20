26aacc1e02c3b3d55bec325e265a85d94dad4036
Neha Saggam <nehasaggam@gmail.com>
2020-08-20 10:00:17 +0530

    Add missing end of line

--- Conversions/BinaryToDecimal.js
+++ Conversions/BinaryToDecimal.js
@@ -1,12 +1,11 @@
 function binaryToDeicmal (binaryNumber) {
-    let decimalNumber = 0
-    const binaryDigits = binaryNumber.split('').reverse()
-    binaryDigits.forEach((binaryDigit, index) => {
-      decimalNumber += binaryDigit * (Math.pow(2, index))
-    })
-    console.log(`Decimal of ${binaryNumber} is ${decimalNumber}`)
-  }
-  
-  binaryToDeicmal('111001')
-  binaryToDeicmal('101')
-  
+  let decimalNumber = 0
+  const binaryDigits = binaryNumber.split('').reverse()
+  binaryDigits.forEach((binaryDigit, index) => {
+    decimalNumber += binaryDigit * (Math.pow(2, index))
+  })
+  console.log(`Decimal of ${binaryNumber} is ${decimalNumber}`)
+}
+
+binaryToDeicmal('111001')
+binaryToDeicmal('101')
