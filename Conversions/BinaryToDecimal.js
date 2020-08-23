b9c6491c7969973e9ed53adf5e625efae6b3b029
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-08-23 15:49:51 +0530

    Update BinaryToDecimal.js

--- Conversions/BinaryToDecimal.js
+++ Conversions/BinaryToDecimal.js
@@ -1,6 +1,6 @@
 function binaryToDeicmal (binaryNumber) {
   let decimalNumber = 0
-  const binaryDigits = binaryNumber.split('').reverse() // Splits the bnary number in revered single digits
+  const binaryDigits = binaryNumber.split('').reverse() // Splits the binary number into reversed single digits
   binaryDigits.forEach((binaryDigit, index) => {
     decimalNumber += binaryDigit * (Math.pow(2, index)) // Summation of all the decimal converted digits
   })
