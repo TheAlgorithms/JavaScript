3f151d44697608a204c70876a25ef7978fabff64
AMAN123956 <56161073+AMAN123956@users.noreply.github.com>
2020-09-24 16:28:06 +0530

    Updated

--- Maths/SumOfNumbersInRange.js
+++ Maths/SumOfNumbersInRange.js
@@ -1,24 +1,24 @@
-*Program to find sum of all numbers between a range */
+/*Program to find sum of all numbers between a range */
 /* Since the numbers might not be always in order, using max() and min() will help organize.*/
 
 function sumAll(arr) {
   /*  First create a variable to store the max number between two.
    The same as before for the Smallest number. */
 
-  let max = Math.max(arr[0], arr[1]);
-  let min = Math.min(arr[0], arr[1]);
+   let max = Math.max(arr[0], arr[1]);
+   let min = Math.min(arr[0], arr[1]);
 
    /* We create a temporary variable to add the numbers. */
 
-   let temp = 0;
-   for (var i = min; i <= max; i++) {
+    let temp = 0;
+    for (var i = min; i <= max; i++) {
     temp += i;
   }
-  return temp;
+   return temp;
 }
 
 /* function call */
 
 sumAll([1, 4]);
 
-console.log(sumAll[1,4])  // results in 10
+console.log(sumAll[1,4])  // result is 10
