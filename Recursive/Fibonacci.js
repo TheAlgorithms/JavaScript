2431133a863302c14c27c691a713142f4dc26eef
Aditya <dewanganaditya362@gmail.com>
2020-09-26 15:53:53 +0530

    Fibonacci.js file added with no error

--- Recursive/Fibonacci.js
+++ Recursive/Fibonacci.js
@@ -1,23 +1,22 @@
-
-//  Just enter the postion of the number you want to find in the fibonacci sequence and the below code will print the number.
-//   Fibonacci sequence - 1 1 2 3 5 8 13 21 34 55 ............
-//  This code find the answer to that term recursively.
+/*  Just enter the postion of the number you want to find in the fibonacci sequence and the below code will print the number.
+   Fibonacci sequence - 1 1 2 3 5 8 13 21 34 55 ............
+  This code find the answer to that term recursively.
+  Fibonacci sequence link - https://en.wikipedia.org/wiki/Fibonacci_number
+*/
 
 function fibonacci (n) {
-  if (n < 2)
-  {
+  if (n < 2) {
     return n
   }
-
-  return fibonacci(n-1) + fibonacci(n-2)
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
-console.log( fibonacci(3) )  //  prints the 3rd term in the sequence
-console.log( fibonacci(10) )  //  prints the 10th term in the sequence
-console.log( fibonacci(20) )  // prints the 20th term in the sequence
+console.log(fibonacci(3))
+console.log(fibonacci(10))
+console.log(fibonacci(20))
 
-
-// OUTPUT
-//  2
-//  55
-//  6765
+/* OUTPUT
+   2
+   55
+   6765
+*/
