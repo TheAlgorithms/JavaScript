0a234ccc8993f81de2eaf129dd01a82dba1de2e3
Sombit Bose <sombit.bose15@gmail.com>
2020-09-30 20:54:01 +0530

    Update Problem1.js

--- Project Euler/Problem1.js
+++ Project Euler/Problem1.js
@@ -3,12 +3,11 @@
 Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 */
 
-
 function multiplesThreeAndFive (num) {
   let total = 0;
   // total for calculating the sum
-  for (let i = 0; i <= number; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
+  for (let i = 0; i <= num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
       total += i;
     }
   }
