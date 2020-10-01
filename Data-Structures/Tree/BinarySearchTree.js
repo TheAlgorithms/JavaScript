1ec65ef6d442e128dc76500586b20571a9358c1b
JLambertazzo <jbertazzolambert@gmail.com>
2020-10-01 02:56:39 -0500

    Update BinarySearchTree.js
    Comments referenced the binary tree data structure instead of the binary search tree data structure

--- Data-Structures/Tree/BinarySearchTree.js
+++ Data-Structures/Tree/BinarySearchTree.js
@@ -1,10 +1,10 @@
 /* Binary Search Tree!!
 *
-* Nodes that will go on the Binary Tree.
+* Nodes that will go on the Binary Search Tree.
 * They consist of the data in them, the node to the left, the node
 * to the right, and the parent from which they came from.
 *
-* A binary tree is a data structure in which an element
+* A binary search tree is a data structure in which an element
 * has two successors(children). The left child is usually
 * smaller than the parent, and the right child is usually
 * bigger.
