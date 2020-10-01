6d8349cbda913de3f96136cb0712e2d51beecc55
Prerna Aggarwal <32161545+prerna-agg@users.noreply.github.com>
2020-10-01 09:58:06 +0530

    Update LowercaseToUppercase.js

--- String/LowercaseToUppercase.js
+++ String/LowercaseToUppercase.js
@@ -1,25 +1,25 @@
 // upper converts lowercase sting to upper case
-// inputs is 1 string i.e. str
+// input is 1 string i.e. str
 const upper = (str) => {
   // check that inputs are string
   if (typeof str !== "string") {
-    return "Not string(s)";
+    return "Not string(s)"
   }
 
-  convertedString = "";
+  convertedString = ""
   for (let i = 0; i < str.length; i++) {
     if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
       convertedString =
-        convertedString + String.fromCharCode(65 + str.charCodeAt(i) - 97);
-      continue;
+        convertedString + String.fromCharCode(65 + str.charCodeAt(i) - 97)
+      continue
     }
-    convertedString = convertedString + str.charAt(i);
+    convertedString = convertedString + str.charAt(i)
   }
 
-  return convertedString;
-};
+  return convertedString
+}
 
-console.log(upper("abcd")); // should print ABCD
-console.log(upper("abcd1")); // should print ABCD1
-console.log(upper("*abcd")); // *ABCD
-console.log(upper("aBs")); // should print ABS
+console.log(upper("abcd")) // should print ABCD
+console.log(upper("abcd1")) // should print ABCD1
+console.log(upper("*abcd")) // *ABCD
+console.log(upper("aBs")) // should print ABS
