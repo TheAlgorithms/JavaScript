6cd03db6e2be8056a746b51512230f7fc6c5ed9a
Vishnu <vishnupothan2@gmail.com>
2020-10-01 16:52:37 +0530

    Added the standard javascript style

--- Conversions/OctToDecimal.js
+++ Conversions/OctToDecimal.js
@@ -1,14 +1,14 @@
 function octalToDecimal (num) {
-    let dec = 0
-    let base = 1
-    while (num > 0) {
-      const r = num % 10
-      num = Math.floor(num / 10)
-      dec = dec + (r * base)
-      base = base * 8
-    }
-    return dec
+  let dec = 0
+  let base = 1
+  while (num > 0) {
+    const r = num % 10
+    num = Math.floor(num / 10)
+    dec = dec + (r * base)
+    base = base * 8
   }
+  return dec
+}
 
 // test cases
 console.log(octalToDecimal(56) === 46)
