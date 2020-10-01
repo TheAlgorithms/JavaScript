e69743fa8c270d635723f692685bb3a7e50434c4
Prerna Aggarwal <paruprerna@gmail.com>
2020-10-01 10:04:37 +0530

    var added

--- String/LowercaseToUppercase.js
+++ String/LowercaseToUppercase.js
@@ -6,7 +6,7 @@ const upper = (str) => {
     return "Not string(s)";
   }
 
-  convertedString = "";
+  var convertedString = "";
   for (let i = 0; i < str.length; i++) {
     if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
       convertedString =
