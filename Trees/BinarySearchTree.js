ea5ff02327f7ccdcf8dfabf95b5ce8e5b72968df
Abdelhak Ihadjadene <xenomshox69@gmail.com>
2020-10-01 02:01:36 +0100

    added BinarySearchTree class with: insert, find, contains, BFS, DFSPreOrder, DFSPostOrder and DFSInOrder algorithms methods

---
+++ Trees/BinarySearchTree.js
@@ -0,0 +1,140 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    find(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
+
+    contains(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) current = current.left;
+            else if (value > current.value) current = current.right;
+            else return true;
+        }
+        return false;
+    }
+
+    BreadthFirstSearch() {
+        let node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        return data;
+    }
+
+    DepthFirstSearchPreOrder() {
+        let data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DepthFirstSearchPostOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DepthFirstSearchInOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.value);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+}
+
+//      10
+//   5      13
+// 2  7   11   16
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+
+console.log("node with value 13:", tree.find(13));
+console.log("node with value 19:", tree.find(19));
+
+console.log("tree coontais node with value 2:", tree.contains(2));
+console.log("tree coontais node with value 535:", tree.contains(535));
+
+console.log("BreadthFirstSearch:", tree.BreadthFirstSearch());
+console.log("DepthFirstSearchPreOrder:", tree.DepthFirstSearchPreOrder());
+console.log("DepthFirstSearchPostOrder:", tree.DepthFirstSearchPostOrder());
+console.log("DepthFirstSearchInOrder:", tree.DepthFirstSearchInOrder());
