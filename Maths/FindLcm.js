a7c0c1c88bab17b78fa628089ef4f3d582492cbc
Spardha-hash <65541725+Spardha-hash@users.noreply.github.com>
2020-10-01 18:48:32 +0530

    Update FindLcm.js

--- Maths/FindLcm.js
+++ Maths/FindLcm.js
@@ -12,26 +12,22 @@
 'use strict'
 
 // Find the LCM of two numbers.
-function findLcm (num1, num2) {
-  var maxNum
-  var lcm
-  // Check to see whether num1 or num2 is larger.
-  if (num1 > num2) {
-    maxNum = num1
-  } else {
-    maxNum = num2
-  }
-  lcm = maxNum
-
-  while (true) {
-    if ((lcm % num1 === 0) && (lcm % num2 === 0)) {
-      break
-    }
-    lcm += maxNum
-  }
-  return lcm
+function findLcm(num1, num2) {
+  return (num1/findGcd(num1,num2))*num2
 }
 
+function findGcd(num1,num2){
+    if(num1==0){
+        return num2
+    }
+    else{
+        return findGcd(num2%num1,num1)
+    }
+}
+        
+        
+        
+        
 // Run `findLcm` Function
 var num1 = 12
 var num2 = 76
