9ac7a60c15ee559edcb31bca37ae6d619005a0c9
Francisco Santos <franciscosantos@MacBook-Pro.lan>
2020-10-01 20:10:55 +0100

    Binary Search algorithm

---
+++ BinarySearch/BinarySearch.js
@@ -0,0 +1,44 @@
+// Random Array sorted
+const MyArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// choosing the number to find
+let number_to_find = 2;
+
+
+// Create a function to search throught the Array 
+const Search = (number_to_find, Array) => {
+    
+    // Inicialize two variables
+    // The start of the array and the end
+    let beginning = 0;
+    let end = Array.length - 1;
+
+    // Loop until the beginning <= end
+    while (beginning <= end) {
+        // Inicialize another variable, midle of the array
+        let middle = Math.floor((beginning + end) / 2)
+        
+        // check if the middle is the number we are looking for
+        if (Array[middle] == number_to_find) {
+            // The number was found
+            return true;
+        }
+        
+        // check if the middle is lesser than number we are looking for
+        else if (Array[middle] < number_to_find ) {
+            beginning = middle + 1;
+        }
+        
+        // The number is not equal neither lesser so it gotta be gratter
+        else {
+            end = middle - 1;
+        }
+    }
+    
+    // The number was not found
+    return false;
+}
+
+// Calling the function and seeing the results
+const search_result = Search(number_to_find, MyArray)
+console.log(search_result)
