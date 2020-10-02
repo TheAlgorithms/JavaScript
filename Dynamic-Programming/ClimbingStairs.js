6e83953c3189cb01fefb41280ad1c40d816d25b4
illegalcall <44542765+illegalcall@users.noreply.github.com>
2020-10-03 01:16:00 +0530

    Create ClimbingStairs.js

---
+++ Dynamic-Programming/ClimbingStairs.js
@@ -0,0 +1,23 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+var climbStairs = function (n) {
+  let prev = 0
+  let cur = 1
+  let temp
+
+  for (let i = 0; i < n; i++) {
+    temp = prev
+    prev = cur
+    cur += temp
+  }
+  return cur
+}
+
+function main () {
+  const number = 5
+
+  console.log('Number of ways to climb ' + number + 'stairs is' + climbStairs(5))
+};
+main()
