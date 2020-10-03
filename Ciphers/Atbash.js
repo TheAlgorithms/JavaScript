5023cdd7cc5d2195b9a0b9722d1fdd8405dd6bfd
Bharat Arya <aryabharat33@gmail.com>
2020-10-03 15:23:24 +0530

    Update Atbash.js

--- Ciphers/Atbash.js
+++ Ciphers/Atbash.js
@@ -5,30 +5,25 @@ so that the first letter becomes the last letter,
 the second letter becomes the second to last letter, and so on. 
 */
 
-
 /**
  * Decrypt a Atbash cipher
  * @param {String} str - string to be decrypted/encrypt
  * @return {String} decrypted/encrypted string
  */
-
-const Atbash = (message) => {
-    let decodedString = ''
-
-    for (let i = 0; i < message.length; i++) {
-        if (/[^a-zA-Z]/.test(message[i])) {
-            decodedString += message[i]
-        } else if (message[i] === message[i].toUpperCase()) {
-            decodedString += String.fromCharCode(90 + 65 - message.charCodeAt(i))
-        } else {
-            decodedString += String.fromCharCode(122 + 97 - message.charCodeAt(i))
-        }
-    }
-    return decodedString
+function Atbash(message) {
+	let decodedString = ''
+	for(let i = 0; i < message.length; i++) {
+		if(/[^a-zA-Z]/.test(message[i])) {
+			decodedString += message[i]
+		} else if(message[i] === message[i].toUpperCase()) {
+			decodedString += String.fromCharCode(90 + 65 - message.charCodeAt(i))
+		} else {
+			decodedString += String.fromCharCode(122 + 97 - message.charCodeAt(i))
+		}
+	}
+	return decodedString
 }
-
 // Atbash Example
 const encryptedString = 'HELLO WORLD'
 const decryptedString = Atbash(encryptedString)
-
 console.log(decryptedString) // SVOOL DLIOW
