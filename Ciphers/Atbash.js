985e193ab794d24d87a890f26afb2c0346b51467
Bharat Arya <aryabharat33@gmail.com>
2020-10-03 15:36:23 +0530

    Update Atbash.js

--- Ciphers/Atbash.js
+++ Ciphers/Atbash.js
@@ -1,8 +1,8 @@
 /*
-The Atbash cipher is a particular type of monoalphabetic cipher 
-formed by taking the alphabet and mapping it to its reverse, 
-so that the first letter becomes the last letter, 
-the second letter becomes the second to last letter, and so on. 
+The Atbash cipher is a particular type of monoalphabetic cipher
+formed by taking the alphabet and mapping it to its reverse,
+so that the first letter becomes the last letter,
+the second letter becomes the second to last letter, and so on.
 */
 
 /**
@@ -10,18 +10,18 @@ the second letter becomes the second to last letter, and so on.
  * @param {String} str - string to be decrypted/encrypt
  * @return {String} decrypted/encrypted string
  */
-function Atbash(message) {
-	let decodedString = ''
-	for(let i = 0; i < message.length; i++) {
-		if(/[^a-zA-Z]/.test(message[i])) {
-			decodedString += message[i]
-		} else if(message[i] === message[i].toUpperCase()) {
-			decodedString += String.fromCharCode(90 + 65 - message.charCodeAt(i))
-		} else {
-			decodedString += String.fromCharCode(122 + 97 - message.charCodeAt(i))
-		}
-	}
-	return decodedString
+function Atbash (message) {
+  let decodedString = ''
+  for (let i = 0; i < message.length; i++) {
+    if (/[^a-zA-Z]/.test(message[i])) {
+      decodedString += message[i]
+    } else if (message[i] === message[i].toUpperCase()) {
+      decodedString += String.fromCharCode(90 + 65 - message.charCodeAt(i))
+    } else {
+      decodedString += String.fromCharCode(122 + 97 - message.charCodeAt(i))
+    }
+  }
+  return decodedString
 }
 // Atbash Example
 const encryptedString = 'HELLO WORLD'
