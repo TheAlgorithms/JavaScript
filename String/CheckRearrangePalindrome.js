4867185d24bdd43214ba1955886110f297b589be
Maily Santos <mailysantos@gmail.com>
2020-10-04 13:07:40 -0300

    Fixes: #155 - palindromeRearranging

--- String/CheckRearrangePalindrome.js
+++ String/CheckRearrangePalindrome.js
@@ -3,7 +3,7 @@
 // The string can only be a palindrome if the count of ALL characters is even or if the ONLY ONE character count is odd
 // Input is a string
 
-function palindromeRearranging(str) {
+const palindromeRearranging = (str) => {
   // check that input is a string
   if (typeof str !== 'string') {
     return 'Not a string'
@@ -16,13 +16,13 @@ function palindromeRearranging(str) {
   // First obtain the character count for each character in the string and store it in an object.
   // Filter the object's values to only the odd character counts.
   const charCounts = [...str].reduce((counts, char) => {
-      counts[char] = counts[char] ? counts[char] + 1 : 1;
-      return counts;
-  }, {});
+      counts[char] = counts[char] ? counts[char] + 1 : 1
+      return counts
+  }, {})
   
   // If the length of the resulting array is 0 or 1, the string can be a palindrome.
-  return Object.values(charCounts).filter(count => count % 2 !== 0).length <= 1;
+  return Object.values(charCounts).filter(count => count % 2 !== 0).length <= 1
 }
 
 console.log(palindromeRearranging('aaeccrr')) // true
-console.log(palindromeRearranging('leve')) // false 
+console.log(palindromeRearranging('leve')) // false
