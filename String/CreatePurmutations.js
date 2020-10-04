a55f00a0798c50baf1bf6693829e66e9bb71c7d6
rubiin <roobin.bhandari@gmail.com>
2020-10-04 20:45:18 +0545

    ran standard

--- String/CreatePurmutations.js
+++ String/CreatePurmutations.js
@@ -1,37 +1,31 @@
-function permutations(str){
-
+const createPermutations = (str) => {
 // convert string to array
-let arr = str.split(''),
+  const arr = str.split('')
 
-// get array length 
-    len = arr.length, 
-// this will hold all the permutations
-    perms = [],
-    rest,
-    picked,
-    restPerms,
-    next;
-    
-// if len is zero, return the same string
-    if (len === 0)
-        return [str];
-// loop to the length to get all permutations
-    for (let i=0; i<len; i++)
-    {
-        rest = Object.create(arr);
-        picked = rest.splice(i, 1);
+  // get array length
+  const strLen = arr.length
+  // this will hold all the permutations
+  const perms = []
+  let rest
+  let picked
+  let restPerms
+  let next
 
-        restPerms = permutations(rest.join(''));
+  // if strLen is zero, return the same string
+  if (strLen === 0) { return [str] }
+  // loop to the length to get all permutations
+  for (let i = 0; i < strLen; i++) {
+    rest = Object.create(arr)
+    picked = rest.splice(i, 1)
 
-       for (var j=0, jLen = restPerms.length; j< jLen; j++)
-       {
-           next = picked.concat(restPerms[j]);
-           perms.push(next.join(''));
-       }
+    restPerms = createPermutations(rest.join(''))
+
+    for (let j = 0, jLen = restPerms.length; j < jLen; j++) {
+      next = picked.concat(restPerms[j])
+      perms.push(next.join(''))
     }
-   return perms;
+  }
+  return perms
 }
 
-console.log(permutations('abc')) // should print ["abc", "acb", "bac", "bca", "cab", "cba"]
-
-      
+console.log(createPermutations('abc')) // should print ["abc", "acb", "bac", "bca", "cab", "cba"]
