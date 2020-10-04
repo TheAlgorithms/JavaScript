3ab272da4b2f2185197a78dbd1d8b99073202ac3
Omkarnath Parida <pomkarnath98@gmail.com>
2020-10-05 00:00:45 +0530

    Style Changed

--- Data-Structures/Linked-List/CycleDetection.js
+++ Data-Structures/Linked-List/CycleDetection.js
@@ -22,7 +22,7 @@ function main () {
   while (fast != null && fast.next != null && slow != null) {
     fast = fast.next.next
     slow = slow.next
-    if (fast == slow) {
+    if (fast === slow) {
       return true
     }
   }
