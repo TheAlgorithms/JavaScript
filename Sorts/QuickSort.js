bc40d85f8208f7afe348a02c91b19732a2a9a2ef
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2020-10-05 11:52:18 +0530

    Update QuickSort.js
    Simplified the function. The earlier version uses too many unnecessary variables and steps. The old example also uses "var" everywhere which is deprecated now.

--- Sorts/QuickSort.js
+++ Sorts/QuickSort.js
@@ -1,37 +1,27 @@
 /*
 * Quick sort is a comparison sorting algorithm that uses a divide and conquer strategy.
-* For more information see here: https://en.wikipedia.org/wiki/Quicksort
+* For more information, see here: https://en.wikipedia.org/wiki/Quicksort
 */
-function quickSort (items) {
-  var length = items.length
 
-  if (length <= 1) {
-    return items
-  }
-  var PIVOT = items[0]
-  var GREATER = []
-  var LESSER = []
+function quickSort(items) {
+  if (items.length <= 1) return items;
 
-  for (var i = 1; i < length; i++) {
-    if (items[i] > PIVOT) {
-      GREATER.push(items[i])
-    } else {
-      LESSER.push(items[i])
-    }
+  let LESSER = [];
+  let GREATER = [];
+  const PIVOT = items.shift();
+
+  for (let i of items) {
+    if (i > PIVOT) GREATER.push(i)
+    else LESSER.push(items[i])
   }
 
-  var sorted = quickSort(LESSER)
-  sorted.push(PIVOT)
-  sorted = sorted.concat(quickSort(GREATER))
-
-  return sorted
+  return quickSort(LESSER).concat(PIVOT, quickSort(GREATER));
 }
 
-// Implementation of quick sort
+// testing the algorithm
 
-var ar = [0, 5, 3, 2, 2]
-// Array before Sort
-console.log(ar)
-ar = quickSort(ar)
-// Array after sort
-console.log(ar)
+const testArray = [3, 0, 5, 2, 2, -1, 4, 1];
+console.log("Input:", testArray);
+console.time("Executed in");
+console.log("Output:", quickSort(testArray));
+console.timeEnd("Executed in");
