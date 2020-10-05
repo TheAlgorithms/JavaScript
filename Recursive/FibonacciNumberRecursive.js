950b839108fa7ad42fe75039b40851304441cb62
vinayak <itssvinayak@gmail.com>
2020-10-05 23:52:33 +0530

    Update FibonacciNumberRecursive.js

--- Recursive/FibonacciNumberRecursive.js
+++ Recursive/FibonacciNumberRecursive.js
@@ -1,14 +1,14 @@
 //  https://en.wikipedia.org/wiki/Fibonacci_number
 
-var fib = function (N) {
+const fibonacci = (N) => {
   if (N === 0 || N === 1) return N
 
-  return fib(N - 2) + fib(N - 1)
+  return fibonacci(N - 2) + fibonacci(N - 1)
 }
 
-function main () {
-  const number = 5
 
-  console.log(number + 'th Fibonacci number is' + fib(number))
-};
-main()
+// testing
+(() => {
+  const number = 5
+  console.log(number + 'th Fibonacci number is ' + fibonacci(number))
+})()
