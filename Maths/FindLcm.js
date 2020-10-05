ad49e33f38f0d903bddeff80729df3a1afd90a31
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-10-05 22:45:55 +0000

    test: add test for lcm

--- Maths/FindLcm.js
+++ Maths/FindLcm.js
@@ -13,6 +13,16 @@
 
 // Find the LCM of two numbers.
 const findLcm = (num1, num2) => {
+  // If the input numbers are less than 1 return an error message.
+  if (num1 < 1 || num2 < 1) {
+    return 'Please enter values greater than zero.'
+  }
+
+  // If the input numbers are not integers return an error message.
+  if (num1 !== Math.round(num1) || num2 !== Math.round(num2)) {
+    return 'Please enter whole numbers.'
+  }
+
   let maxNum
   let lcm
   // Check to see whether num1 or num2 is larger.
@@ -24,15 +34,10 @@ const findLcm = (num1, num2) => {
   lcm = maxNum
 
   while (true) {
-    if ((lcm % num1 === 0) && (lcm % num2 === 0)) {
-      break
-    }
+    if (lcm % num1 === 0 && lcm % num2 === 0) break
     lcm += maxNum
   }
   return lcm
 }
 
-// Run `findLcm` Function
-var num1 = 12
-var num2 = 76
-console.log(findLcm(num1, num2))
+export { findLcm }
