96e0b5ef5f97f64eeb5bc07926e7f066eeb2f2e6
Aman Verma <42008310+vinashak9@users.noreply.github.com>
2020-10-05 11:17:52 +0530

    Updated the file to npm standards

--- Dynamic-Programming/LongestIncreasingSubsequence.js
+++ Dynamic-Programming/LongestIncreasingSubsequence.js
@@ -1,7 +1,7 @@
 /**
  * Algorithm for Longest Increasing Subsequence
  * Given an array consisting of integers,
- * Determine the longest increasing subsequence 
+ * Determine the longest increasing subsequence
  * Time Complexity: O(n * n)
  * Space Complexity : O(n)
  */
@@ -9,31 +9,31 @@
 /* This function returns the length of the LIS */
 
 function LIS (arr) {
-  const lis = Array(arr.length).fill(1);
+  const lis = Array(arr.length).fill(1)
 
   for (let i = 1; i < lis.length; i++) {
-  	for (let j = 0; j < i; j++) {
-  		if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-        lis[i] = lis[j] + 1;
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+        lis[i] = lis[j] + 1
       }
     }
   }
 
-  let lengthOfLIS = 0;
-  
+  let lengthOfLIS = 0
+
   for (let i = 0; i < lis.length; i++) {
     if (lis[i] > lengthOfLIS) {
-      lengthOfLIS = lis[i];
+      lengthOfLIS = lis[i]
     }
   }
 
-  return lengthOfLIS;
+  return lengthOfLIS
 }
 
 function main () {
-  const arr = [1, 3, 2, 6, 7, 10];
-  const result = LIS(arr);
-  console.log(result);
+  const arr = [1, 3, 2, 6, 7, 10]
+  const result = LIS(arr)
+  console.log(result)
 }
 
 main()
