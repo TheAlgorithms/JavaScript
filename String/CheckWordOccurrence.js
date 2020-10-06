fbd3442cf0c5cc69f259b99cea4397721fdf6d2e
Carlos Carvalho <jcarloss.carvalho@gmail.com>
2020-10-05 23:52:21 -0300

    fix ident file

--- String/CheckWordOccurrence.js
+++ String/CheckWordOccurrence.js
@@ -4,22 +4,22 @@
  */
 
 const checkWordOccurrence = (str, isCaseSensitive = false) => {
-    if (typeof str != 'string') {
-        throw new TypeError('The first param should be a string');
-    }
-    if (typeof isCaseSensitive != 'boolean') {
-        throw new TypeError('The second param should be a boolean')
-    }
+	if (typeof str != 'string') {
+		throw new TypeError('The first param should be a string');
+	}
+	if (typeof isCaseSensitive != 'boolean') {
+		throw new TypeError('The second param should be a boolean')
+	}
 
-    let result = {}
-    if (str.length > 0) {
-        for (let i = 0; i < str.length; i++) {
-            const word = isCaseSensitive ? str[i] : str[i].toUpperCase()
-            if(/\s/.test(word)) continue;
-            result[word] = (!result[word]) ? 1 : result[word] + 1
-        }
+	let result = {}
+	if (str.length > 0) {
+		for (let i = 0; i < str.length; i++) {
+			const word = isCaseSensitive ? str[i] : str[i].toUpperCase()
+			if (/\s/.test(word)) continue;
+			result[word] = (!result[word]) ? 1 : result[word] + 1
+		}
 
-    }
-    return result;
+	}
+	return result;
 }
 export { checkWordOccurrence }
