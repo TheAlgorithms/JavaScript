9cb383bccc69e4079a29e8ebe59675e75fdb2b4d
gerardubuntu <gaabantao1@up.edu.ph>
2020-10-07 17:46:03 +0800

    Implement BreadthFirstTreeTraversal in JavaScript

--- Trees/BreadthFirstTreeTraversal.js
+++ Trees/BreadthFirstTreeTraversal.js
@@ -4,59 +4,59 @@
 */
 
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
+  constructor (data) {
+    this.data = data
+    this.left = null
+    this.right = null
   }
 }
 
 class BinaryTree {
   constructor () {
-    this.root = null;
-    this.traversal = [];
+    this.root = null
+    this.traversal = []
   }
 
   breadthFirst () {
-    const h = this.getHeight(this.root);
+    const h = this.getHeight(this.root)
     for (let i = 1; i <= h; i++) {
-      this.traverseLevel(this.root, i);
+      this.traverseLevel(this.root, i)
     }
-    return this.traversal.toLocaleString();
+    return this.traversal.toLocaleString()
   }
   // Compputing the height of the tree
-  getHeight (root) {
-    if (root == null) {
-      return 0;
+  getHeight (node) {
+    if (node == null) {
+      return 0
     } else {
-      let lheight = this.getHeight(root.left);
-      let rheight = this.getHeight(root.right);
-      return lheight > rheight ? lheight + 1 : rheight + 1;
+      const lheight = this.getHeight(node.left)
+      const rheight = this.getHeight(node.right)
+      return lheight > rheight ? lheight + 1 : rheight + 1
     }
   }
 
   traverseLevel (node, level) {
-    if (node == null) {
-      return
-    } else if (level == 1) {
-      this.traversal.push(node.data);
+    if (level === 1 && node !== null) {
+      this.traversal.push(node.data)
     } else {
-      this.traverseLevel(node.left, level - 1);
-      this.traverseLevel(node.right, level - 1);
+      if (node !== null ) {
+        this.traverseLevel(node.left, level - 1)
+        this.traverseLevel(node.right, level - 1)
+      }
     }
   }
 }
 
-let binaryTree = new BinaryTree();
-let root = new Node(7);
-root.left = new Node(5);
-root.right = new Node(8);
-root.left.left = new Node(3);
-root.left.right = new Node(6);
-root.right.right = new Node(9);
+const binaryTree = new BinaryTree()
+const root = new Node(7)
+root.left = new Node(5)
+root.right = new Node(8)
+root.left.left = new Node(3)
+root.left.right = new Node(6)
+root.right.right = new Node(9)
 binaryTree.root = root;
 
-console.log(binaryTree.breadthFirst());
+console.log(binaryTree.breadthFirst())
 
 //            7
 //           / \
