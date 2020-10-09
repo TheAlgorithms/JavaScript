95929c4552a3791cbe4c28f73186efb1296ff869
marsonya <akhil.marsonya27@gmail.com>
2020-10-09 13:27:14 +0530

    BubbleSort | Improved Comments and Formatting

--- Sorts/BubbleSort.js
+++ Sorts/BubbleSort.js
@@ -1,11 +1,20 @@
-/* Bubble Sort is a algorithm to sort an array. It
-* compares adjacent element and swaps thier position
-* The big O on bubble sort in worst and best case is O(N^2).
- * Not efficient.
+/* Bubble Sort is an algorithm to sort an array. It
+*  compares adjacent element and swaps thier position
+*  The big O on bubble sort in worst and best case is O(N^2).
+*  Not efficient.
+*  
+*  In bubble sort, we keep iterating while something was swapped in
+*  the previous inner-loop iteration. By swapped I mean, in the
+*  inner loop iteration, we check each number if the number proceeding
+*  it is greater than itself, if so we swap them.
 */
 
-function bubbleSort (items) {
+/*
+*  Using 2 for loops
+*/
+function bubbleSort(items) {
   const length = items.length
+
   for (let i = (length - 1); i > 0; i--) {
     // Number of passes
     for (let j = (length - i); j > 0; j--) {
@@ -16,10 +25,12 @@ function bubbleSort (items) {
       }
     }
   }
+
 }
 
-// Implementation of bubbleSort
-
+/*
+*  Implementation of 2 for loops method
+*/
 var ar = [5, 6, 7, 8, 1, 2, 12, 14]
 // Array before Sort
 console.log('-----before sorting-----')
@@ -29,18 +40,12 @@ bubbleSort(ar)
 console.log('-----after sorting-----')
 console.log(ar)
 
-/* alternative implementation of bubble sort algorithm.
- Using a while loop instead. For educational purposses only
- */
 /*
-*In bubble sort, we keep iterating while something was swapped in
-*the previous inner-loop iteration. By swapped I mean, in the
-*inner loop iteration, we check each number if the number proceeding
-*it is greater than itself, if so we swap them.
+*  Using a while loop and a for loop
 */
-
-function alternativeBubbleSort (arr) {
+function alternativeBubbleSort(arr) {
   let swapped = true
+
   while (swapped) {
     swapped = false
     for (let i = 0; i < arr.length - 1; i++) {
@@ -50,10 +55,13 @@ function alternativeBubbleSort (arr) {
       }
     }
   }
+
   return arr
 }
 
-// test
+/*
+*  Implementation of a while loop and a for loop method
+*/
 console.log('-----before sorting-----')
 var array = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
 console.log(array)
