10f664d63f9e975f33b873ea2f62851c29a14417
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2020-10-10 18:14:29 +0530

    ESLINT fixes

--- Project-Euler/Problem2.js
+++ Project-Euler/Problem2.js
@@ -1,14 +1,16 @@
-const SQ5 = 5 ** .5; // Square root of 5 
-const PHI = (1 + SQ5) / 2; // definition of PHI
+const SQ5 = 5 ** 0.5
+//Square root of 5
+
+const PHI = (1 + SQ5) / 2
+// definition of PHI
 
 //theoretically it should take O(1) constant amount of time as long
-// arithmetic calculations are considered to be in constant amount of time.
-
+// arithmetic calculations are considered to be in constant amount of time
 function EvenFibonacci(limit) {
-    const highestIndex = Math.floor(Math.log(limit * SQ5) / Math.log(PHI));
-    const n = Math.floor(highestIndex / 3);
-    return ((PHI ** (3 * n + 3) - 1) / (PHI ** 3 - 1)
-        - ((1 - PHI) ** (3 * n + 3) - 1) / ((1 - PHI) ** 3 - 1)) / SQ5;
+  const highestIndex = Math.floor(Math.log(limit * SQ5) / Math.log(PHI))
+  const n = Math.floor(highestIndex / 3)
+  return ((PHI ** (3 * n + 3) - 1) / (PHI ** 3 - 1) -
+    ((1 - PHI) ** (3 * n + 3) - 1) / ((1 - PHI) ** 3 - 1)) / SQ5
 }
 
 console.log(EvenFibonacci(4e6));
