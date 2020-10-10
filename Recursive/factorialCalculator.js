1f269c4bac570a3f5d9c49ef7ec312df36612b99
Croustys <akos.barabas2002@gmail.com>
2020-10-10 11:37:47 +0200

    added tests and converted to normal function

--- Recursive/factorialCalculator.js
+++ Recursive/factorialCalculator.js
@@ -1,5 +1,7 @@
-const factorialRecursion = n => n === 0 ? 1 : n * factorialRecursion(n - 1);
+function factorialRecursion(n) {
+    return n === 0 ? 1 : n * factorialRecursion(n - 1)
+}
 
-//num can be anything you'd like
-const num = 3;
-console.log(factorialRecursion(num));
+console.log(factorialRecursion(4));
+console.log(factorialRecursion(15));
+console.log(factorialRecursion(0));
