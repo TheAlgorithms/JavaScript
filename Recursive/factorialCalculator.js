b3c2b42ea2f52e3de8206fe4b0c894779f0b89dd
Croustys <akos.barabas2002@gmail.com>
2020-10-10 11:43:59 +0200

    Added tests & normal function convertion

--- Recursive/factorialCalculator.js
+++ Recursive/factorialCalculator.js
@@ -2,6 +2,6 @@ function factorialRecursion(n) {
     return n === 0 ? 1 : n * factorialRecursion(n - 1)
 }
 
-console.log(factorialRecursion(4));
-console.log(factorialRecursion(15));
-console.log(factorialRecursion(0));
+console.log(factorialRecursion(4))
+console.log(factorialRecursion(15))
+console.log(factorialRecursion(0))
