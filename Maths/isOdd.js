008162228e59c731a1cbcd5d0bcaf33f34a0df74
Jake Gerber <jake.j.gerber@gmail.com>
2020-10-10 14:02:35 -0700

    Add and fixed isOdd.js

--- Maths/isOdd.js
+++ Maths/isOdd.js
@@ -1,21 +1,17 @@
-//Returns true if a number is odd, returns false if a number is even, and returns null if the number is a decimal.
+// Returns true if a number is odd, returns false if a number is even, and returns null if the number is a decimal.
 function isOdd (num) {
-  
-  if (num < 0)
-  {
+  if (num < 0) {
     num *= -1
   }
 
-  if (Math.floor(num) !== num)
-  {
+  if (Math.floor(num) !== num) {
     console.error('Decimal Value')
     return null
   }
 
-  if (num % 2 === 1)
-  {
+  if (num % 2 === 1) {
     return true
   }
 
-    return false
+  return false
 }
