65e2d08cb0ecf578bbad23b2dad233123d1118f0
Krishna Pal Deora <53469625+Krishnapal4050@users.noreply.github.com>
2020-10-10 20:44:58 +0530

    documentation changes done

--- Search/TernarySearch.js
+++ Search/TernarySearch.js
@@ -1,11 +1,4 @@
-/* Ternary Search: https://en.wikipedia.org/wiki/Ternary_search
- *Ternary Search is a search and divide algorithm that divides the array into three parts. 
- *It’ll search through the first part, then the third part of the array. 
- *If the target element doesn’t exist within those two sections, we check the middle section to see if the element exist.
- *
- */
-
-
+/* Ternary Search: https://en.wikipedia.org/wiki/Ternary_search.Ternary Search is a search and divide algorithm that divides the array into three parts.It’ll search through the first part, then the third part of the array.If the target element doesn’t exist within those two sections, we check the middle section to see if the element exist.*/
 function ternarySearch(givenList, left, right, absolutePrecision) {
   while (true) {
     if (Math.abs(right - left) < absolutePrecision) {
@@ -20,4 +13,3 @@ function ternarySearch(givenList, left, right, absolutePrecision) {
     }
   }
 }
-  
