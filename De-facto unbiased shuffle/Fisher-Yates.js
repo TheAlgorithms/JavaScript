df274ab42eca61ce49e2d417b54afe17f21da554
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2020-10-11 22:37:44 +0530

    added De-facto unbiased shuffle algorithm

---
+++ De-facto unbiased shuffle/Fisher-Yates.js
@@ -0,0 +1,24 @@
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Used like so
+var arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr);
