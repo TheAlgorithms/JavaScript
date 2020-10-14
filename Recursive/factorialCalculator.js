d88c0491ff4e74c8d8eb675f7a644ada187ba821
vinayak <itssvinayak@gmail.com>
2020-10-14 07:25:55 +0530

    Update and rename factorialCalculator.js to factorial.js

--- Recursive/factorialCalculator.js
+++
@@ -1,7 +0,0 @@
-function factorialRecursion (n) {
-  return n === 0 ? 1 : n * factorialRecursion(n - 1)
-}
-
-console.log(factorialRecursion(4))
-console.log(factorialRecursion(15))
-console.log(factorialRecursion(0))
