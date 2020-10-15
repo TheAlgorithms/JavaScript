11dcd8d22b9eb882dfdb9db04421b6cca91ff458
Victoria Lo <victorialo@berkeley.edu>
2020-10-15 04:33:10 -0700

    minor refactor and comments

--- Project-Euler/Problem7.js
+++ Project-Euler/Problem7.js
@@ -2,21 +2,20 @@
 
 const num = 10001;
 
-let primes = [2,3,5,7,11,13];
+let primes = [2,3,5,7,11,13]; // given list of primes you start with
 
 const calculatePrime = (num) => {
-  let count = primes.length;
-  let current = primes[count-1] + 1;
-  while (count < num) {
+  let count = primes.length; // count number of primes calculated
+  let current = primes[count-1] + 1; // current number being assessed if prime
+  while (count < num) { // repeat while we haven't reached goal number of primes
     // go through each prime and see if divisible by the previous primes
     let prime = false;
     primes.some((n, i) => {
-      if (current % n !== 0) {
-        if (i === count-1) {
-          prime = true;
-        }
-      } else {
+      if (current % n === 0) {
         return true;
+      }
+      if (i === count-1) {
+        prime = true;
       }
     })
     if (prime) {
