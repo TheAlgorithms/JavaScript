d65f9290b9aca09e4f92a9e7b561f9e3c4c21fde
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-10-15 23:45:22 +0000

    chore: update comments

--- Project-Euler/Problem6.js
+++ Project-Euler/Problem6.js
@@ -1,4 +1,7 @@
 /**
+ *
+ * Project Euler 6
+ *
  * The sum of the squares of the first ten natural numbers is 385
  * The square of the sum of the first ten natural numbers is 3025
  * Hence the difference between the sum of the
