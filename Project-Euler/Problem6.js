de4e0df58fc4385da5a77d055b700e0466c3f9ec
Abdelhak Ihadjadene <xenomshox69@gmail.com>
2020-10-15 17:26:26 +0100

    fixed standard js issues

--- Project-Euler/Problem6.js
+++ Project-Euler/Problem6.js
@@ -1,13 +1,14 @@
-function Problem6 (){
-    let sumOfSquare = 0, squareOfSum = 0;
+function Problem6 () {
+  let sumOfSquare = 0
+  let squareOfSum = 0
 
-    for(let i = 1; i <= 100; i++){
-        sumOfSquare += Math.pow(i, 2);
-        squareOfSum += i
-    }
-    squareOfSum = Math.pow(squareOfSum, 2)
-    
-    return squareOfSum - sumOfSquare
+  for (let i = 1; i <= 100; i++) {
+    sumOfSquare += Math.pow(i, 2)
+    squareOfSum += i
+  }
+  squareOfSum = Math.pow(squareOfSum, 2)
+
+  return squareOfSum - sumOfSquare
 }
 
 console.log(Problem6())
