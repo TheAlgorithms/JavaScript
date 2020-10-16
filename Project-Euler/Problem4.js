58dc3c00f8134211f116c59cd7bdeb3e09b5ebe0
Fernando Mota <nando.mot@gmail.com>
2020-10-16 15:23:48 -0300

    Added problem 4 in JavaScript

--- Project-Euler/Problem4.js
+++ Project-Euler/Problem4.js
@@ -1,30 +1,28 @@
-function isPalindrome(number) {
+function isPalindrome (number) {
+  // just 2 random names for variables!
+  const look = String(number)
+  let hear = false
 
-//just 2 random names for variables!
-      let look = String(number)
-      let hear = false
-
-    for(let i = 0; i< look.length; i++) {
-    if(look[i] === look[(look.length)-(i+1)]) {
-        hear = true
-      }
-      else 
-        return false
+  for (let i = 0; i < look.length; i++) {
+    if (look[i] === look[(look.length) - (i + 1)]) {
+      hear = true
+    } else {
+      return false
     }
+  }
   return hear
 }
 
-function largestPalindromeProduct(n) {
+function largestPalindromeProduct (n) {
   let product = 0
   let largestPalindrome = 11
 
-for(let i=10**(n-1) ;i < 10**n;i++) {
-  for(let j = 10**(n-1);j<10**n;j++) {
-       product = i*j
-      if(isPalindrome(product) && product>largestPalindrome)
-       largestPalindrome = product
+  for (let i = 10 ** (n - 1); i < 10 ** n; i++) {
+    for (let j = 10 ** (n - 1); j < 10 ** n; j++) {
+      product = i * j
+      if (isPalindrome(product) && product > largestPalindrome) { largestPalindrome = product }
     }
-}
+  }
   return largestPalindrome
 }
 
