c09e15bfe91b53035422ce6b8535f942ea3244f1
Waddah <waddah.ja@gmail.com>
2020-10-17 22:48:54 +0300

    add a 'brute force' solution for Poject Euler problem 14

---
+++ Project-Euler/Problem014.js
@@ -0,0 +1,47 @@
+/*
+Longest Collatz sequence
+
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*/
+
+const getCollatzSequenceLength = (num, seqLength) => {
+  if (num === 1) {
+    return seqLength
+  } else {
+    let newElement
+    if (num % 2 === 0) {
+      newElement = num / 2
+    } else {
+      newElement = (3 * num) + 1
+    }
+    seqLength++
+    return getCollatzSequenceLength(newElement, seqLength)
+  }
+}
+
+const findLongestCollatzSequence = () => {
+  let startingPointForLargestSequence = 1
+  let largestSequnceLength = 1
+  for (let i = 2; i < 1000000; i++) {
+    const currentSequenceLength = getCollatzSequenceLength(i, 1)
+    if (currentSequenceLength > largestSequnceLength) {
+      startingPointForLargestSequence = i
+      largestSequnceLength = currentSequenceLength
+    }
+  }
+  return startingPointForLargestSequence
+}
+
+console.log(findLongestCollatzSequence())
