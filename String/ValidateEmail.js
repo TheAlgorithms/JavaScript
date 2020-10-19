832ee82f06355586fda240671b07b253a59dd2d4
Cristian Baciu <icristianbaciu@gmail.com>
2020-10-19 16:08:17 +0300

    Modify ValidateEmail.js to accept all suffixes

--- String/ValidateEmail.js
+++ String/ValidateEmail.js
@@ -2,6 +2,7 @@
   function that takes a string input and return either it is true of false
   a valid email address
   e.g.: mahfoudh.arous@gmail.com -> true
+  e.g.: mahfoudh.arous@helsinki.edu -> true
   e.g.: mahfoudh.arous.com ->false
 */
 
@@ -9,11 +10,8 @@ const validateEmail = (str) => {
   if (str === '' || str === null) {
     throw new TypeError('Email Address String Null or Empty.')
   }
-  if (str.startsWith('@') === true || !str.includes('@') || !str.endsWith('.com')) {
-    return false
-  }
 
-  return true
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str)
 }
 
 export { validateEmail }
