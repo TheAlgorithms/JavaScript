792abacadaa6282bfedf4705cb3e3209e877ffb3
Arjun Gupta <ag870994@gmail.com>
2020-10-19 22:52:08 +0530

    Update FactorialNumber.js

--- Dynamic-Programming/FactorialNumber.js
+++ Dynamic-Programming/FactorialNumber.js
@@ -1,16 +1,16 @@
 //Factorial of Number 
 
-function factorial(n){
-  let answer = 1;
-  if (n == 0 || n == 1){
-    return answer;
-  }else{
-    for(var i = n; i >= 1; i--){
-      answer = answer * i;
+function factorial (n) {
+  let answer = 1
+  if (n === 0 || n === 1) {
+    return answer
+  } else {
+    for (var i = n; i >= 1; i--) {
+      answer = answer * i
     }
-    return answer;
-  }  
+    return answer
+  }
 }
-let n = 4;
+const n = 4
 const answer = factorial(n)
-console.log('The factorial of ' + n + ' is ' + answer);
+console.log('The factorial of ' + n + ' is ' + answer)
