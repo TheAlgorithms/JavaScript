db029f24df14e6548d9d9685e649adcc9b60a614
Sutthinart Khunvadhana <sutthinart.khunvadhana@refinitiv.com>
2020-10-22 04:19:13 +0700

    Fix indentation

--- Project-Euler/Problem3.js
+++ Project-Euler/Problem3.js
@@ -2,19 +2,19 @@
 const problem = 600851475143
 
 const largestPrime = (num) => {
-    let newnumm = num
-    let largestFact = 0
-    let counter = 2
-    while (counter * counter <= newnumm) {
-        if (newnumm % counter === 0) {
-            newnumm = newnumm / counter
-        } else {
-            counter++
-        }
+  let newnumm = num
+  let largestFact = 0
+  let counter = 2
+  while (counter * counter <= newnumm) {
+    if (newnumm % counter === 0) {
+      newnumm = newnumm / counter
+    } else {
+      counter++
     }
-    if (newnumm > largestFact) {
-        largestFact = newnumm
-    }
-    return largestFact
+  }
+  if (newnumm > largestFact) {
+    largestFact = newnumm
+  }
+  return largestFact
 }
 console.log(largestPrime(problem))
