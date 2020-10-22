d4e4e9bf4ba75f8176e6e63cd750d8f07c2e2477
Shubh Bansal <00shubhbansal@gmail.com>
2020-10-23 00:23:52 +0530

    Binary Tree in JavaScript.
    Functions provided:
    1. Build Tree from preorder traversal.
    2. Print Pre-Order traversal.
    3. Print Post-Order traversal.
    4. Print In-Order traversal.

--- Data-Structures/Tree/BinaryTree.js
+++ Data-Structures/Tree/BinaryTree.js
@@ -1,128 +1,118 @@
-/* Binary Search Tree!!
-*
-* Nodes that will go on the Binary Tree.
-* They consist of the data in them, the node to the left, the node
-* to the right, and the parent from which they came from.
-*
-* A binary tree is a data structure in which an element
-* has two successors(children). The left child is usually
-* smaller than the parent, and the right child is usually
-* bigger.
-*/
+/**  Binary Tree!!
+ *
+ * A binary tree is a data structure in which an element
+ * has two successors(children). It is a non-linear data
+ * structure which is used to store heirarchical information.
+ * bigger.
+ */
 
-// class Node
-var Node = (function () {
-  // Node in the tree
-  function Node (val) {
-    this.value = val
-    this.left = null
-    this.right = null
-  }
+/**  Class Node
+ * Class that will contain data values of any node.
+ * this.data = The data value
+ * this.left = Node children to left side.
+ * this.right = Node children to right side.
+ */
+class Node { 
+    constructor(data) { 
+        this.data = data; 
+        this.left = null; 
+        this.right = null; 
+    } 
+} 
 
-  // Search the tree for a value
-  Node.prototype.search = function (val) {
-    if (this == null ) {
-      return null ;
+/**
+ * 2 Global Variables.
+ * Will be used in building the binary tree.
+ * 
+ * arr: an array containing the preorder traversal of tree to be build. 
+ * -1 will represent null.
+ * 
+ * i: to iterate over elements of array.
+ * 
+ */
+i = 0 ;
+var arr ;
+
+/**
+ * Utility function buildTree() to build the binary tree.
+ * It will iterate over the array through RECURSION and assign values 
+ * to nodes recurively. If at any point arr[i] is -1 then that node 
+ * will be marked as null.
+ * Function will return a node, the root node of tree it builded.
+ */
+function buildTree() {
+    /**  Base Case
+     * If i exceeds the length of continer then every requested node will be null. 
+    */
+    if (i>=arr.length) {
+        return null;
     }
-    if (this.value === val) {
-      return this
+    /** 
+     * If arr[i] is -1. It means a null is supposed reside there in the tree.
+    */
+    if (arr[i]==-1) {
+        ++i ;
+        return null;
     }
-    // Search in left tree recursively
-    var left = this.left.search(val);
     
-    // If found in left return result of left
-    if ( left!==null )
-      return left;
+    const root = new Node(arr[i]); // Create a new node with current arr[i].
+    ++i; // Increment i indicating one value from arrray has been used.
+    root.left = buildTree() ; // Recursively build left tree. 
+    root.right = buildTree() ; // Recursively build right tree.
     
-    // Else return whatever the result is of search on right subtree.
-    return this.right.search(val);
-  }
-
-  // Pre Order Traversal
-  Node.prototype.preOrder = function () {
-
-    // Print out value
-    console.log(this.value);
+    // Left is builded first because preorder traversal is provided in array.
     
-    // Recursively go to left  
-    this.left.preOrder();
-    
-    // Recursively go to right  
-    this.right.preOrder();
-  }
+    return root ; // Return the root node of the tree which is just built.
 
-  // Post Order Traversal
-  Node.prototype.postOrder = function () {
-    
-    // Recursively go to left  
-    this.left.postOrder();
-    
-    // Recursively go to right  
-    this.right.postOrder();
-    
-    // Print out value
-    console.log(this.value);
-  }
+}
 
-  // In Order Traversal
-  Node.prototype.inOrder = function () {
+/** function preorder
+ * Recursive function to print the preorder traversal of given binary 
+ * tree. root is the root node of binary tree provided.
+ * */ 
+function preOrder( root ) {
+  
+    // Preorder - SLR Order - Self->Left->Right
 
-    // Recursively go to left  
-    this.left.inOrder();
+    console.log(root.data) ; // Self
+    if ( root.left !== null )
+        preOrder(root.left); // Left
+    if ( root.right !== null )
+        preOrder(root.right); // Right
+  
+}
+function postOrder( root ) {
+  
+    // Postorder - LRS Order - Left->Right->Self
 
-    // Print out value
-    console.log(this.value);
-    
-    // Recursively go to right  
-    this.right.inOrder();
-  }
+    if ( root.left !== null )
+        postOrder(root.left); // Left
+    if ( root.right !== null )
+        postOrder(root.right); // Right
+    console.log(root.data) ; // Self
+  
+}
+function inOrder( root ) {
+  
+    // Inorder - LSR Order - Left->Self->Right
 
-  // returns the constructor
-  return Node
-}())
+    if ( root.left !== null )
+        preOrder(root.left); // Left
+    console.log(root.data) ;  // Self
+    if ( root.right !== null )
+        preOrder(root.right); // Right
+  
+}
 
-// class Tree
-var Tree = (function () {
-  function Tree ( input ) {
-    // Just store the root
-    this.root = null
-    
-  };
+// Driver Program
 
-  // Preorder traversal
-  Tree.prototype.preOrder = function () {
-    this.root.preOrder();
-  }
-  // Postorder traversal
-  Tree.prototype.postOrder = function () {
-    this.root.postOrder();
-  }
-  // Inorder traversal
-  Tree.prototype.inOrder = function () {
-    this.root.inOrder();
-  }
+i = 0; // Global variable i(helper for buildtree) set to 0.
+arr = [1,2,-1,-1,3,-1,4,-1,-1]; // Sample preorder traversal of a binary tree.
+var root = buildTree() ;
 
-  // Searching for existence of a value.
-  Tree.prototype.search = function (val) {
-    const found = this.root.search(val)
-    if (found === null) {
-      console.log(val + ' not found')
-    } else {
-      console.log('Found:' + found.value)
-    }
-  }
-
-  // returns the constructor
-  return Tree
-}())
-
-// Implementation of BST
-var bst = new Tree()
-bst.addValue(6)
-bst.addValue(3)
-bst.addValue(9)
-bst.addValue(2)
-bst.addValue(8)
-bst.addValue(4)
-bst.traverse()
-bst.search(8)
+console.log('Preorder traversal of given binary tree:');
+preOrder(root);
+console.log('Postorder traversal of given binary tree:');
+postOrder(root);
+console.log('Inorder traversal of given binary tree:');
+inOrder(root);
