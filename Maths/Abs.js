1d871a88bc2a4091c7553ee23d3446065b41a887
Koddi Evangelista <70675656+Koddi-Evangelista@users.noreply.github.com>
2020-10-22 10:23:15 +0800

    Update Abs.js

--- Maths/Abs.js
+++ Maths/Abs.js
@@ -1,24 +1,19 @@
 /*
-    author: PatOnTheBack
-    license: GPL-3.0 or later
-
-    Modified from:
-        https://github.com/TheAlgorithms/Python/blob/master/maths/abs.py
-
+    author: Koddi-Evangelista
+    license: GPL-3.0 or later.
+    
     This script will find the absolute value of a number.
 
     More about absolute values:
         https://en.wikipedia.org/wiki/Absolute_value
+    More about Math.abs() :
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
 */
 
 const absVal = (num) => {
-  // Find absolute value of `num`.
   'use strict'
-  if (num < 0) {
-    return -num
-  }
-  // Executes if condition is not met.
-  return num
+  // Use Math.abs(number) to return the absolute value of number
+  return Math.abs(num)
 }
 
 export { absVal }
