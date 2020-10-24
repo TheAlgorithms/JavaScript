923ca0823cac4bf3bafc4be0d3090b25b50449a7
Koddi Evangelista <70675656+Koddi-Evangelista@users.noreply.github.com>
2020-10-24 15:34:13 +0800

    Update CheckVowels.js

--- String/CheckVowels.js
+++ String/CheckVowels.js
@@ -5,15 +5,15 @@
 
 const checkVowels = (value) => {
   //firts check if the input value is string
-    if(typeof string === 'string'){
-        // next create a regex(regular expression) that says everthing except aeiou,spaces,digits and expresions
-        // we also use global and ignore case flag 
-        let regEx = /[^aeiou\s\W\d]/gi;
-         // then we return the length of matched letter which is vowels using str.match(regex)
-        return string.match(regEx).length;
-    }
-    // if the input value is not a string return this
-    return "not a string";
+  if(typeof value === 'string'){
+    // next create a regex(regular expression) that says everthing except aeiou,spaces,digits and expresions
+    // we also use global and ignore case flag 
+    let regEx = /[^aeiou\s\W\d]/gi
+    // then we return the length of matched letter which is vowels using str.match(regex)
+    return value.match(regEx).length
+  }
+  // if the input value is not a string return this
+  return 'not a string'
 }
 
 export { checkVowels }
