fc60a316a4a26ce9d31b411e5d97b6096c7ed8b6
Arwy Syahputra Siregar <arwysyahputrasiregar@Arwys-MacBook-Pro.local>
2020-10-26 18:25:36 +0700

    repair the test

--- Maths/test/BInaryConvert.test.js
+++ Maths/test/BInaryConvert.test.js
@@ -4,5 +4,8 @@ describe('Binary Convert', () => {
   it('should return the correct value', () => {
     expect(BinaryConvert(12)).toBe(1100)
   })
+  it('should return the correct value of the sum from two number', () => {
+    expect(BinaryConvert(12+2)).toBe(1110)
+  })
 })
 
