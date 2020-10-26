3add0a14578b5b1626494b68528ca6c0e56cab57
Arwy Syahputra Siregar <arwysyahputrasiregar@Arwys-MacBook-Pro.local>
2020-10-26 17:33:49 +0700

    repair and add test

--- Maths/BinaryConvert.js
+++ Maths/BinaryConvert.js
@@ -1,12 +1,10 @@
 // Convert binary
-function convertToBinary(number)
-{
-    let result = []
-    let i
-    for(i = number; i > 0; i = parseInt(i / 2))
-    {
-        result.push(i % 2)
-    }
-    return result.reverse().join(" ")
+const  convertToBinary=(number)=> {
+  let result = []
+  let i
+  for (i = number; i > 0; i = parseInt(i / 2)) {
+    result.push(i % 2)
+  }
+  return result.reverse().join(' ')
 }
-console.log(convertToBinary(12))
+export {convertToBinary} 
---
+++ Maths/test/BInaryConvert.test.js
@@ -0,0 +1,11 @@
+import { convertToBinary } from '../BinaryConvert'
+
+describe('convertBinary', () => {
+  it('should return the length of number', () => {
+    expect(convertToBinary(12)).toBe(7)
+  })
+
+  it('should return the correct value', () => {
+    expect(numberOfDigit(12)).toBe('1 1 0 0')
+  })
+})
