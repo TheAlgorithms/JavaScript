751d03f909fdba5c6e947a78247ebebf0f205416
Arwy Syahputra Siregar <arwysyahputrasiregar@Arwys-MacBook-Pro.local>
2020-10-26 18:20:22 +0700

    change name

--- Maths/test/BInaryConvert.test.js
+++ Maths/test/BInaryConvert.test.js
@@ -1,8 +1,8 @@
 import { BinaryConvert } from '../BinaryConvert'
 
 describe('Binary Convert', () => {
-
   it('should return the correct value', () => {
     expect(BinaryConvert(12)).toBe(1100)
   })
 })
+
