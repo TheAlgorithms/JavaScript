4e680d70879c4fd4f8f9c3d0e0d8e5bea3c48386
vinayak <itssvinayak@gmail.com>
2020-10-31 12:34:54 +0530

    Update WhileLoopFactorial.js

--- Maths/WhileLoopFactorial.js
+++ Maths/WhileLoopFactorial.js
@@ -2,19 +2,18 @@
     author: Theepag
  */
 const factorialize = (num) => {
-    // Step 1. variable result to store num
-    let result = num;
-    // If num = 0 OR 1, the factorial will return 1
-    if (num === 0 || num === 1) 
-      return 1; 
-    // Step 2. WHILE loop 
-    while (num > 1) { 
-      num--; // decrement 1 at each iteration
-      result = result * num; // or result = result * num;
-    }
-    // Step 3. Return the factorial
-    return result;
+  // Step 1. variable result to store num
+  let result = num
+  // If num = 0 OR 1, the factorial will return 1
+  if (num === 0 || num === 1) { return 1 }
+  // Step 2. WHILE loop
+  while (num > 1) {
+    num-- // decrement 1 at each iteration
+    result = result * num // or result = result * num;
+  }
+  // Step 3. Return the factorial
+  return result
 }
-//test
-console.log(factorialize(5));
-console.log(factorialize(4));
+// test
+console.log(factorialize(5))
+console.log(factorialize(4))
