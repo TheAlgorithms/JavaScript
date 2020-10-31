53f4db30a786e69265be43c6669b3b0a02e6813c
Utkarsh Bajpai <64923342+Utkarsh-byte-bot@users.noreply.github.com>
2020-10-31 14:11:44 +0530

    Added Insertion sort in javascript.

---
+++ InsertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
