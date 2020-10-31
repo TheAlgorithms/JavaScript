83b05cde159e863b0bb278116b69f9df3e1ac6da
Andrew Chudinovskyh <andron989@gmail.com>
2020-10-31 17:53:08 +0200

    style fixes

--- Search/QuickSelectSearch.js
+++ Search/QuickSelectSearch.js
@@ -11,45 +11,45 @@
  *
  * [Reference](http://en.wikipedia.org/wiki/Quickselect)
  */
-function quickSelectSearch(array, k) {
+function quickSelectSearch (array, k) {
   if (!array || array.length <= k) {
-    throw new Error('Invalid arguments');
+    throw new Error('Invalid arguments')
   }
 
-  let from = 0;
-  let to = array.length - 1;
+  let from = 0
+  let to = array.length - 1
   while (from < to) {
-    let left = from;
-    let right = to;
-    const pivot = array[Math.ceil((left + right) * 0.5)];
+    let left = from
+    let right = to
+    const pivot = array[Math.ceil((left + right) * 0.5)]
 
     while (left < right) {
       if (array[left] >= pivot) {
-        const tmp = array[left];
-        array[left] = array[right];
-        array[right] = tmp;
-        --right;
+        const tmp = array[left]
+        array[left] = array[right]
+        array[right] = tmp
+        --right
       } else {
-        ++left;
+        ++left
       }
     }
 
     if (array[left] > pivot) {
-      --left;
+      --left
     }
 
     if (k <= left) {
-      to = left;
+      to = left
     } else {
-      from = left + 1;
+      from = left + 1
     }
   }
-  return array;
+  return array
 }
 
 /* ---------------------------------- Test ---------------------------------- */
 
 const arr = [1121111, 21, 333, 41, 5, 66, 7777, 28, 19, 11110]
-console.log(quickSelectSearch(arr, 5)); // [ 19, 21, 28, 41, 5, 66, 333, 11110, 1121111, 7777 ]
-console.log(quickSelectSearch(arr, 2)); // [ 19, 5, 21, 41, 28, 333, 11110, 1121111, 7777, 66 ]
-console.log(quickSelectSearch(arr, 7)); // [ 19, 5, 21, 41, 28, 66, 333, 7777, 11110, 1121111 ]
+console.log(quickSelectSearch(arr, 5)) // [ 19, 21, 28, 41, 5, 66, 333, 11110, 1121111, 7777 ]
+console.log(quickSelectSearch(arr, 2)) // [ 19, 5, 21, 41, 28, 333, 11110, 1121111, 7777, 66 ]
+console.log(quickSelectSearch(arr, 7)) // [ 19, 5, 21, 41, 28, 66, 333, 7777, 11110, 1121111 ]
