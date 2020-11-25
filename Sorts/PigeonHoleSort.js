d4ede60e3cc7a7b2fa498a6a852d9caaf38cf4e2
Chris Kolonas <ckolonas@csd.auth.gr>
2020-11-25 21:13:41 +0200

    added PigeonHoleSort algorithm

--- Sorts/PigeonHoleSort.js
+++ Sorts/PigeonHoleSort.js
@@ -1,4 +1,6 @@
 /*
+https://en.wikipedia.org/wiki/Pigeonhole_sort
+
 *Pigeonhole sorting is a sorting algorithm that is suitable
 * for sorting lists of elements where the number of elements
 * (n) and the length of the range of possible key values (N)
