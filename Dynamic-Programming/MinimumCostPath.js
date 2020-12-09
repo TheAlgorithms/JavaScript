c85c692ebff6aebbf86d1f5483cd9d398c6eb3cf
DhruvGheewala <dhruvgheewala.4g@gmail.com>
2020-12-09 16:21:44 +0530

    Update MinimumCostPath.js

--- Dynamic-Programming/MinimumCostPath.js
+++ Dynamic-Programming/MinimumCostPath.js
@@ -11,17 +11,16 @@ const minCostPath = (matrix) => {
   const m = matrix[0].length
 
   // moves[i][j] => minimum number of moves to reach cell i, j
-  let moves = new Array(n)
-  for (var i = 0; i < moves.length; i++) moves[i] = new Array(m)
+  const moves = new Array(n)
+  for (let i = 0; i < moves.length; i++) moves[i] = new Array(m)
 
   // base conditions
   moves[0][0] = 0 // to reach cell (0, 0) from (0, 0) is of no moves
-  for (var i = 1; i < m; i++) moves[0][i] = moves[0][i - 1] + matrix[0][i]
-  for (var i = 1; i < n; i++) moves[i][0] = moves[i - 1][0] + matrix[i][0]
+  for (let i = 1; i < m; i++) moves[0][i] = moves[0][i - 1] + matrix[0][i]
+  for (let i = 1; i < n; i++) moves[i][0] = moves[i - 1][0] + matrix[i][0]
 
-  for (var i = 1; i < n; i++) {
-    for (var j = 1; j < m; j++)
-      moves[i][j] = Math.min(moves[i - 1][j], moves[i][j - 1]) + matrix[i][j]
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) { moves[i][j] = Math.min(moves[i - 1][j], moves[i][j - 1]) + matrix[i][j] }
   }
 
   return moves[n - 1][m - 1]
