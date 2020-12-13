aa1159236244354a8ba0adde5cdf31d9da4d8daa
Tushar <tushargpt224@gmail.com>
2020-12-13 22:32:02 +0530

    Different Iterations in JS

--- Iterations/Iterations.js
+++ Iterations/Iterations.js
@@ -1,22 +1,22 @@
-//Different types of Iterations in JS
+//	Different types of Iterations in JS
 
-//ForEach loop
+//	ForEach loop
 let colors = ['red', 'yellow', 'green', 'brown'];
 
 colors.forEach(color => {
 	console.log(color)
 })
 
-//For loop
+//	For loop
 for (let i = 0; i < 5; i++) {
-	//this will start from 0 all the way to 5
+	//	this will start from 0 all the way to 5
 	console.log(i)
 }
 
-//While loop
+//	While loop
 let i = 5;
 while (i !== 0) {
-	//This will iterate until the condition given above is wrong
+	//	This will iterate until the condition given above is wrong
 	console.log(i)
 	i = i - 1;
 }
@@ -24,19 +24,17 @@ while (i !== 0) {
 
 let array = ['apple', 'ice-cream', 'orange', 'tea', 'coffee'];
 
-//Filter function 
-//Filter will iterate the array and will return a new array according to the condition applied
+//	Filter function 
+//	Filter will iterate the array and will return a new array according to the condition applied
 let filteredArray = array.filter(value => value != 'coffee');
 console.log(filteredArray)
 
-//Map function
-//Map funtion will iterate the whole array and you can perform some operations in this one or can return some DOM element
+//	Map function
+//	Map funtion will iterate the whole array and you can perform some operations in this one or can return some DOM element
 array.map(value => {
-	//Some Operations
+	//	Some Operations
 	console.log(value)
-	//return value
-	//or
-	//return <li>{value}</li>
+	//	return value
+	//	or
+	//	return <li>{value}</li>
 })
-
-
