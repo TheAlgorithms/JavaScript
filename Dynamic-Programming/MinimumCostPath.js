902b1dfe351d97cd6b9ec5273e7389f6b0ab799c
DhruvGheewala <dhruvgheewala.4g@gmail.com>
2020-12-20 20:08:30 +0530

    Update MinimumCostPath.js

--- Dynamic-Programming/MinimumCostPath.js
+++ Dynamic-Programming/MinimumCostPath.js
@@ -1,37 +1,46 @@
-// youtube Link -> https://www.youtube.com/watch?v=lBRtnuxg-gU
+// Problem Statement => https://www.youtube.com/watch?v=lBRtnuxg-gU
 
 const minCostPath = (matrix) => {
   /*
-  Find the min cost path from top-left to bottom-right in matrix
-    >>> minCostPath([[2, 1], [3, 1], [4, 2]])
-    6
-  */
+        Find the min cost path from top-left to bottom-right in matrix
+        >>> minCostPath([[2, 1], [3, 1], [4, 2]])
+        >>> 6
+    */
+
   const n = matrix.length
   const m = matrix[0].length
 
-  // Preprocessing first row
-  for (let i = 1; i < m; i++) {
-    matrix[0][i] += matrix[0][i - 1]
-  }
+  // moves[i][j] => minimum number of moves to reach cell i, j
+  const moves = new Array(n)
+  for (let i = 0; i < moves.length; i++) moves[i] = new Array(m)
 
-  // Preprocessing first column
+  // base conditions
+  moves[0][0] = matrix[0][0] // to reach cell (0, 0) from (0, 0) is of no moves
+  for (let i = 1; i < m; i++) moves[0][i] = moves[0][i - 1] + matrix[0][i]
+  for (let i = 1; i < n; i++) moves[i][0] = moves[i - 1][0] + matrix[i][0]
+
   for (let i = 1; i < n; i++) {
-    matrix[i][0] += matrix[i - 1][0]
+    for (let j = 1; j < m; j++) { moves[i][j] = Math.min(moves[i - 1][j], moves[i][j - 1]) + matrix[i][j] }
   }
 
-  // Updating cost to current position
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < m; j++) {
-      matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i][j - 1])
-    }
-  }
-
-  return matrix[n - 1][m - 1]
+  return moves[n - 1][m - 1]
 }
 
 const main = () => {
-  console.log(minCostPath([[2, 1], [3, 1], [4, 2]]))
-  console.log(minCostPath([[2, 1, 4], [2, 1, 3], [3, 2, 1]]))
+  console.log(
+    minCostPath([
+      [2, 1],
+      [3, 1],
+      [4, 2]
+    ])
+  )
+  console.log(
+    minCostPath([
+      [2, 1, 4],
+      [2, 1, 3],
+      [3, 2, 1]
+    ])
+  )
 }
 
 main()
