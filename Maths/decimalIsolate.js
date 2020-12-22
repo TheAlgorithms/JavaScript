ef78fce1e24434736b95a19984eac6c659d13e14
Omkarnath Parida <pomkarnath98@gmail.com>
2020-12-22 11:08:56 +0530

    Update decimalIsolate.js
    The previous code was not efficient enough to give the accurate output.

--- Maths/decimalIsolate.js
+++ Maths/decimalIsolate.js
@@ -5,10 +5,13 @@
  */
 
 const decimalIsolate = (number) => {
-  return number - Math.floor(number)
+  const ans = parseFloat((number + '').replace(/^[-\d]+./, '.'))
+  return isNaN(ans) === true ? 0 : ans
 }
 
 // testing
 console.log(decimalIsolate(35.345))
 console.log(decimalIsolate(56.879))
 console.log(decimalIsolate(89.5643))
+console.log(decimalIsolate(38.00))
+console.log(decimalIsolate(33))
