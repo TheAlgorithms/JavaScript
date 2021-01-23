db246025a3fb7381d891df7e666533758da7456c
marsonya <akhil.marsonya27@gmail.com>
2021-01-23 22:25:06 +0530

    Formatted main Comment | CombSort

--- Sorts/CombSort.js
+++ Sorts/CombSort.js
@@ -1,19 +1,18 @@
-/*
-Wikipedia says: Comb sort improves on bubble sort.
-
-The basic idea is to eliminate turtles, or small values
-near the end of the list, since in a bubble sort these slow the sorting
-down tremendously. Rabbits, large values around the beginning of the list,
-do not pose a problem in bubble sort.
-
-In bubble sort, when any two elements are compared, they always have a
-gap (distance from each other) of 1. The basic idea of comb sort is
-that the gap can be much more than 1. The inner loop of bubble sort,
-which does the actual swap, is modified such that gap between swapped
-elements goes down (for each iteration of outer loop) in steps of
-a "shrink factor" k: [ n/k, n/k2, n/k3, ..., 1 ].
-
-*/
+/**
+ * Comb sort improves on bubble sort.
+ * 
+ * The basic idea is to eliminate turtles, or small values
+ * near the end of the list, since in a bubble sort these slow the sorting
+ * down tremendously. Rabbits, large values around the beginning of the list,
+ * do not pose a problem in bubble sort.
+ * 
+ * In bubble sort, when any two elements are compared, they always have a
+ * gap (distance from each other) of 1. The basic idea of comb sort is
+ * that the gap can be much more than 1. The inner loop of bubble sort,
+ * which does the actual swap, is modified such that gap between swapped
+ * elements goes down (for each iteration of outer loop) in steps of
+ * a "shrink factor" k: [ n/k, n/k2, n/k3, ..., 1 ].
+ */
 
 function combSort (list) {
   if (list.length === 0) {
