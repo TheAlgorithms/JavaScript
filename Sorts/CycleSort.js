9decf7bc7f0e36e9915375bf0141f38e7d3c3475
marsonya <akhil.marsonya27@gmail.com>
2021-01-25 23:52:47 +0530

    Standard fixes | Cycle Sort

--- Sorts/CycleSort.js
+++ Sorts/CycleSort.js
@@ -4,7 +4,7 @@
  * number of writes to the original array, unlike any other in-place sorting
  * algorithm. It is based on the idea that the permutation to be sorted can
  * be factored into cycles, which can individually be rotated to give a sorted result.
- * 
+ *
  * Wikipedia: https://en.wikipedia.org/wiki/Cycle_sort
  */
 
@@ -63,5 +63,5 @@ console.log('\n- Before Sort | Implementation of Cycle Sort -')
 console.log(array)
 // After Sort
 console.log('- After Sort | Implementation of Cycle Sort -')
-console.log(countingSort(array))
+console.log(cycleSort(array))
 console.log('\n')
