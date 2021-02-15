b71d49f9e42ea61fbe884502797c5abbb9bbef74
LOkesh <lpatil@rewardhealth.com>
2021-02-15 09:43:21 +0530

    #559

--- String/CheckPalindrome.js
+++ String/CheckPalindrome.js
@@ -10,15 +10,10 @@ const checkPalindrome = (str) => {
   if (length === 0) {
     return 'Empty string'
   }
-  // Iterate through the length of the string
-  // Compare the first character to the last, the second character to the second last, and so on
-  for (let i = 0; i < length / 2; i++) {
-    // at the first instance of a mismatch
-    if (str[i] !== str[length - 1 - i]) {
-      return 'Not a Palindrome'
-    }
-  }
-  return 'Palindrome'
+  // Reverse only works with array, thus conevert the string to array, reverse it and convert back to string
+  // return as palindrome if the reversed string is equal to the input string
+  const reversed = [...str].reverse().join()
+  return str === reversed ? 'Palindrome' : 'Not a Palindrome'
 }
 
 export { checkPalindrome }
