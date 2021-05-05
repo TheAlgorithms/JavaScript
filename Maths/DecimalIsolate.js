f660df973c4988b9881dc87192f0effbb684d349
Masahiko Shin <contact@shin-translation.com>
2021-05-05 16:57:18 +0900

    Add another decimal isolation method

--- Maths/DecimalIsolate.js
+++ Maths/DecimalIsolate.js
@@ -9,9 +9,15 @@ const decimalIsolate = (number) => {
   return isNaN(ans) === true ? 0 : ans
 }
 
+const decimalIsolateByFloor = (number) => {
+  return number - Math.floor(number)
+}
+
 // testing
-console.log(decimalIsolate(35.345))
-console.log(decimalIsolate(56.879))
-console.log(decimalIsolate(89.5643))
-console.log(decimalIsolate(38.00))
-console.log(decimalIsolate(33))
+[decimalIsolate, decimalIsolateByFloor].forEach(func => {
+  console.log(func(35.345))
+  console.log(func(56.879))
+  console.log(func(89.5643))
+  console.log(func(38.00))
+  console.log(func(33))
+})
