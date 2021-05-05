52cb6a809353b9f80d38aaf5237d0d22703d6443
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-05-05 09:32:58 +0530

    Added 2 division check

--- Maths/PrimeCheck.js
+++ Maths/PrimeCheck.js
@@ -12,6 +12,7 @@ const PrimeCheck = (n) => {
   if (n === 1) return false
   if (n === 0) return false
   if (n === 2) return true
+  if (n % 2 === 0) return false
 
   for (let i = 3; i * i <= n; i += 2) {
     if (n % i === 0) {
