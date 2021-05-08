311c86a1cf85cfefe9a23794eb4879ba8c2dec69
Thivagar Manickam <thivagar200@gmail.com>
2021-05-08 10:37:35 +0530

    Updated the problem10 solution

--- Project-Euler/Problem10.js
+++ Project-Euler/Problem10.js
@@ -1,19 +1,21 @@
 // https://projecteuler.net/problem=10
 
 const isPrime = (number) => {
-  let flag = true
-  for (let j = number - 1; j >= 2; j--) {
+  if (number === 2) return true
+  if (number % 2 === 0) return false
+
+  for (let j = 3; j * j <= number; j += 2) {
     if (number % j === 0) {
-      flag = false
+      return false
     }
   }
-  return flag
+  return true
 }
 
 const calculateSumOfPrimeNumbers = (maxNumber) => {
   let sum = 0
   for (let i = maxNumber - 1; i >= 2; i--) {
-    if (isPrime(i) === true) {
+    if (isPrime(parseInt(i)) === true) {
       sum += i
     }
   }
