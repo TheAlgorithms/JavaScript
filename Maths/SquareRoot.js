2c1052da8bedc94cc71016232a5bbb304693b39a
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-05-13 13:07:15 +0530

    feat(SquareRoot.js): Added TypeError

--- Maths/SquareRoot.js
+++ Maths/SquareRoot.js
@@ -7,6 +7,10 @@
 */
 
 function sqrt (num, precision = 10) {
+	if (!Number.isFinite(num))
+		throw new TypeError(`Expected a number, received ${typeof num}`)
+	if (!Number.isFinite(precision))
+		throw new TypeError(`Expected a number, received ${typeof precision}`)
   let sqrt = 1
   for (let i = 0; i < precision; i++) {
     sqrt -= (sqrt * sqrt - num) / (2 * sqrt)
