054622c9994c0123faa94cb056f0ff166c64c391
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-05-13 13:18:51 +0530

    style(SquareRoot.js): Fixed style

--- Maths/SquareRoot.js
+++ Maths/SquareRoot.js
@@ -7,10 +7,8 @@
 */
 
 function sqrt (num, precision = 10) {
-	if (!Number.isFinite(num))
-		throw new TypeError(`Expected a number, received ${typeof num}`)
-	if (!Number.isFinite(precision))
-		throw new TypeError(`Expected a number, received ${typeof precision}`)
+  if (!Number.isFinite(num)) { throw new TypeError(`Expected a number, received ${typeof num}`) }
+  if (!Number.isFinite(precision)) { throw new TypeError(`Expected a number, received ${typeof precision}`) }
   let sqrt = 1
   for (let i = 0; i < precision; i++) {
     sqrt -= (sqrt * sqrt - num) / (2 * sqrt)
