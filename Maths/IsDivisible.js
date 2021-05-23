a14da3d113998876927df2812b0de95d02493441
Masahiko Shin <contact@shin-translation.com>
2021-05-23 17:44:15 +0900

    improve error handling of isDivisible

--- Maths/IsDivisible.js
+++ Maths/IsDivisible.js
@@ -2,7 +2,7 @@
 
 export const isDivisible = (num1, num2) => {
   if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
-    throw new Error('All parameters have to be numbers')
+    throw new TypeError("Expected a number")
   }
   if (num2 === 0) {
     return false
