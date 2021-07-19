6ac0d61185230650ff225340e63b0f4a58f4ca77
Rak Laptudirm <raklaptudirm@gmail.com>
2021-07-19 13:17:06 +0530

    style: update style

--- Maths/test/FareyApproximation.test.js
+++ Maths/test/FareyApproximation.test.js
@@ -1,13 +1,13 @@
-import { fareyApproximation } from "../FareyApproximation"
+import { fareyApproximation } from '../FareyApproximation'
 
 describe('fareyApproximation', () => {
   it('Return Farey Approximation of 0.7538385', () => {
     const approx = fareyApproximation(0.7538385)
     expect(approx).toBe({ numerator: 22, denominator: 29 })
   })
-  
+
   it('Return Farey Approximation of 0.23584936', () => {
     const approx = fareyApproximation(0.23584936)
-    expet(approx).toBe({ numerator: 13, denominator: 55 })
+    expect(approx).toBe({ numerator: 13, denominator: 55 })
   })
 })
