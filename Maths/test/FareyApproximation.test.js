d41b6aade577111436cf0331625b53c782fc2a62
Rak Laptudirm <raklaptudirm@gmail.com>
2021-07-19 13:21:35 +0530

    chore: Update test values

--- Maths/test/FareyApproximation.test.js
+++ Maths/test/FareyApproximation.test.js
@@ -3,11 +3,11 @@ import { fareyApproximation } from '../FareyApproximation'
 describe('fareyApproximation', () => {
   it('Return Farey Approximation of 0.7538385', () => {
     const approx = fareyApproximation(0.7538385)
-    expect(approx).toBe({ numerator: 22, denominator: 29 })
+    expect(approx).toBe({ numerator: 52, denominator: 69 })
   })
 
   it('Return Farey Approximation of 0.23584936', () => {
     const approx = fareyApproximation(0.23584936)
-    expect(approx).toBe({ numerator: 13, denominator: 55 })
+    expect(approx).toBe({ numerator: 196, denominator: 831 })
   })
 })
