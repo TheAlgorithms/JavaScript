e5b7e56f004d682bbeba2cb1a4413ff7c6b0889b
Rak Laptudirm <raklaptudirm@gmail.com>
2021-07-19 13:25:00 +0530

    chore: Strict equal for tests

--- Maths/test/FareyApproximation.test.js
+++ Maths/test/FareyApproximation.test.js
@@ -3,11 +3,11 @@ import { fareyApproximation } from '../FareyApproximation'
 describe('fareyApproximation', () => {
   it('Return Farey Approximation of 0.7538385', () => {
     const approx = fareyApproximation(0.7538385)
-    expect(approx).toBe({ numerator: 52, denominator: 69 })
+    expect(approx).toStrictEqual({ numerator: 52, denominator: 69 })
   })
 
   it('Return Farey Approximation of 0.23584936', () => {
     const approx = fareyApproximation(0.23584936)
-    expect(approx).toBe({ numerator: 196, denominator: 831 })
+    expect(approx).toStrictEqual({ numerator: 196, denominator: 831 })
   })
 })
