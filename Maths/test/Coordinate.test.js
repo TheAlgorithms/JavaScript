320c6e03f985a00af02b1cbbee5ac3bd2f4ba3c1
Rak Laptudirm <raklaptudirm@gmail.com>
2021-07-27 11:33:11 +0530

    chore: grammar update

--- Maths/test/Coordinate.test.js
+++ Maths/test/Coordinate.test.js
@@ -1,14 +1,14 @@
 import * as coordinate from '../Coordinate'
 describe('Testing distance2points calculations', () => {
-  it('should give a numeric output (distance between 2 points) with 4 numeric arguments', () => {
+  it('Should give a numeric output (distance between 2 points) with 4 numeric arguments', () => {
     const distance2points = coordinate.distance2points(2, 2, -10, -7)
     expect(distance2points).toBe(15)
   })
-  it('should not give any output given non-numeric argument', () => {
+  it('Should not give any output given non-numeric argument', () => {
     expect(() => coordinate.distance2pointseAreaCube('ABC')).toThrow()
     expect(() => coordinate.distance2pointseAreaCube('ABC', '123', '', '###')).toThrow()
   })
-  it('should not give any output given any number of numeric arguments other than 4', () => {
+  it('Should not give any output given any number of numeric arguments other than 4', () => {
     expect(() => coordinate.distance2points(2, 2, -10, -7, 9)).toThrow()
     expect(() => coordinate.distance2points(2, 2, -10)).toThrow()
     expect(() => coordinate.distance2points(2, 2)).toThrow()
