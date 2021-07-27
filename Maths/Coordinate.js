d46e467d0b3eca1ca9547fc1e83526582624ef06
Enoch-Moke <86700309+Enoch-Moke@users.noreply.github.com>
2021-07-27 13:28:57 +0800

    Update Coordinate.js
    Style compliance.

--- Maths/Coordinate.js
+++ Maths/Coordinate.js
@@ -1,14 +1,13 @@
 /*
  Calculate the mathematical properties involving coordinates
-
  Calculate the Distance Between 2 Points on a 2 Dimensional Plane
  Example: coorDistance(2,2,14,11) will return 15
  Wikipedia reference: https://en.wikipedia.org/wiki/Geographical_distance#Flat-surface_formulae
 */
 const distance2points = (longitude1, latitude1, longitude2, latitude2) => {
-  const width = longitude2 - longitude1;
-  const height = latitude2 - latitude1;
-  return (Math.sqrt(width * width + height * height));
-};
+  const width = longitude2 - longitude1
+  const height = latitude2 - latitude1
+  return (Math.sqrt(width * width + height * height))
+}
 
-export { distance2points };
+export { distance2points }
