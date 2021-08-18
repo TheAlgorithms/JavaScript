ae74e6b20b4884a2218a8c9f57cb6921bf891196
Suryapratap Singh <suryprtaps@gmail.com>
2021-08-19 00:04:32 +0530

    reformatted the code with the standard.js code style

--- String/AlternativeStringArrange.js
+++ String/AlternativeStringArrange.js
@@ -1,6 +1,6 @@
 // Alternative arrange the two given strings in one string in O(n) time complexity.
 
-// Problem Source & Explanation: https://www.geeksforgeeks.org/alternatively-merge-two-strings-in-java/ 
+// Problem Source & Explanation: https://www.geeksforgeeks.org/alternatively-merge-two-strings-in-java/
 
 /**
  * Alternative arrange the two given strings in one string in O(n) time complexity.
@@ -9,37 +9,36 @@
  * @returns `String` return one alternative arrange string.
  */
 const AlternativeStringArrange = (str1, str2) => {
-
   // firstly, check that both inputs are strings.
   if (typeof str1 !== 'string' || typeof str2 !== 'string') {
     return 'Not string(s)'
   }
 
   // output string vlaue.
-  let out_str = "";
+  let outStr = ''
 
   // get first string length.
-  const firstStringLength = str1.length;
+  const firstStringLength = str1.length
   // get second string length.
-  const secondStringLength = str2.length;
+  const secondStringLength = str2.length
   // absolute length for oparetion.
-  let absLenght = firstStringLength > secondStringLength ? firstStringLength : secondStringLength;
+  const absLenght = firstStringLength > secondStringLength ? firstStringLength : secondStringLength
 
   // Iterate the character count until the absolute count is reached.
   for (let charCount = 0; charCount < absLenght; charCount++) {
     // If firstStringLength is lesser than the charCount it means they are able to re-arange.
     if (charCount < firstStringLength) {
-      out_str += str1[charCount];
+      outStr += str1[charCount]
     }
 
     // If secondStringLength is lesser than the charCount it means they are able to re-arange.
     if (charCount < secondStringLength) {
-      out_str += str2[charCount];
+      outStr += str2[charCount]
     }
   }
 
   // return the output string.
-  return out_str;
+  return outStr
 }
 
-module.exports = AlternativeStringArrange;
+module.exports = AlternativeStringArrange
