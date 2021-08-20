8722d52980a9705c082b1907727a225929bf6248
SURYAPRATAP SINGH SURYAVANSHI <67123991+suryapratapsinghsuryavanshi@users.noreply.github.com>
2021-08-20 13:17:48 +0530

    Update String/CheckCamelCase.js
    Co-authored-by: Rak Laptudirm <raklaptudirm@gmail.com>

--- String/CheckCamelCase.js
+++ String/CheckCamelCase.js
@@ -10,7 +10,7 @@
 const CheckCamelCase = (varName) => {
   // firstly, check that input is a string or not.
   if (typeof varName !== 'string') {
-    return 'Not string(s)'
+    return new TypeError("Argument is not a string.")
   }
 
   const pat = /^[a-z][A-Za-z]*$/
