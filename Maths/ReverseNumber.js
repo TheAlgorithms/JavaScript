c67b0e9f1d031040cf362808e112fc79736c57ad
Suryapratap Singh <suryprtaps@gmail.com>
2021-08-31 03:53:53 +0530

    add ReverseNumber method

---
+++ Maths/ReverseNumber.js
@@ -0,0 +1,25 @@
+/*
+    Problem statement and Explanation : https://medium.com/@ManBearPigCode/how-to-reverse-a-number-mathematically-97c556626ec6
+*/
+
+/**
+ * ReverseNumber return the reversed value of the given number.
+ * @param {Number} n any digit number.
+ * @returns `Number` n reverse in reverse.
+ */
+const ReverseNumber = (number) => {
+  // A variable for storing the reversed number.
+  let reverseNumber = 0
+  // Iterate the process until getting the number is 0.
+  while (number > 0) {
+    // get the last digit of the number
+    const lastDigit = number % 10
+    // add to the last digit to in reverseNumber
+    reverseNumber = reverseNumber * 10 + lastDigit
+    // reduce the actual number.
+    number = Math.floor(number / 10)
+  }
+  return reverseNumber
+}
+
+module.exports = ReverseNumber
