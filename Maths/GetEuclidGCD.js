09ce0c76a66dd207c0ab6c3bdfe134d4194096ff
Suryapratap Singh <suryprtaps@gmail.com>
2021-08-31 13:53:49 +0530

    change the GetGCD method to GetEuclidGCD method

--- Maths/GetEuclidGCD.js
+++ Maths/GetEuclidGCD.js
@@ -6,15 +6,19 @@
 */
 
 /**
- * GetGCD return the gcd of two numbers.
+ * GetEuclidGCD return the gcd of two numbers using Euclidean algorithm.
  * @param {Number} arg1 first argument for gcd
  * @param {Number} arg2 second argument for gcd
  * @returns return a `gcd` value of both number.
  */
-const getGcd = (arg1, arg2) => {
+const GetEuclidGCD = (arg1, arg2) => {
   // firstly, check that input is a number or not.
   if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
     return new TypeError('Argument is not a number.')
+  }
+  // check that the input number is not a negative value.
+  if (arg1 < 1 || arg2 < 1) {
+    return new TypeError('Argument is a negative number.')
   }
   // Find a minimum of both numbers.
   let less = arg1 > arg2 ? arg2 : arg1
@@ -22,8 +26,7 @@ const getGcd = (arg1, arg2) => {
   for (less; less >= 2; less--) {
     if ((arg1 % less === 0) && (arg2 % less === 0)) return (less)
   }
-
   return (less)
 }
 
-module.exports = getGcd
+module.exports = GetEuclidGCD
