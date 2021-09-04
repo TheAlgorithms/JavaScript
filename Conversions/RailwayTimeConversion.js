34b783dd59ae0339ea4420da88889eb4baf834ab
Suryapratap Singh <suryprtaps@gmail.com>
2021-09-04 23:06:12 +0530

    re-formate with standard.js

--- Conversions/RailwayTimeConversion.js
+++ Conversions/RailwayTimeConversion.js
@@ -1,7 +1,7 @@
 /*
-    The time conversion of normalized time to the railway is a simple algorithm 
-    because we know that if the time is in 'AM' value it means they only want 
-    some changes on hours and minutes and if the time in 'PM' it means the only 
+    The time conversion of normalized time to the railway is a simple algorithm
+    because we know that if the time is in 'AM' value it means they only want
+    some changes on hours and minutes and if the time in 'PM' it means the only
     want some changes in hour value.
 
     Input Formate -> 07:05:45PM
@@ -16,20 +16,20 @@
  * @returns {String} Railway time string.
  */
 const RailwayTimeConversion = (timeString) => {
-    // firstly, check that input is a string or not.
-    if (typeof timeString !== 'string') {
-        return new TypeError('Argument is not a string.')
-    }
-    // split the string by ':' character.
-    const [hour, minute, scondWithShift] = timeString.split(':')
-    // split second and shift value.
-    const [second, shift] = [scondWithShift.substr(0, 2), scondWithShift.substr(2)]
-    // convert shifted time to not-shift time(Railway time) by using the above explanation.
-    if (shift === 'PM') {
-        if (parseInt(hour) === 12) { return `${hour}:${minute}:${second}` } else { return `${parseInt(hour) + 12}:${minute}:${second}` }
-    } else {
-        if (parseInt(hour) === 12) { return `00:${minute}:${second}` } else { return `${hour}:${minute}:${second}` }
-    }
+  // firstly, check that input is a string or not.
+  if (typeof timeString !== 'string') {
+    return new TypeError('Argument is not a string.')
+  }
+  // split the string by ':' character.
+  const [hour, minute, scondWithShift] = timeString.split(':')
+  // split second and shift value.
+  const [second, shift] = [scondWithShift.substr(0, 2), scondWithShift.substr(2)]
+  // convert shifted time to not-shift time(Railway time) by using the above explanation.
+  if (shift === 'PM') {
+    if (parseInt(hour) === 12) { return `${hour}:${minute}:${second}` } else { return `${parseInt(hour) + 12}:${minute}:${second}` }
+  } else {
+    if (parseInt(hour) === 12) { return `00:${minute}:${second}` } else { return `${hour}:${minute}:${second}` }
+  }
 }
 
 module.exports = RailwayTimeConversion
