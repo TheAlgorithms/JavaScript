d79e2f71faadcee3f3df40ddcaf951d76393b1d2
Suryapratap Singh <suryprtaps@gmail.com>
2021-09-07 03:10:17 +0530

    add CoPrimeCheck method

---
+++ Maths/CoPrimeCheck.js
@@ -0,0 +1,30 @@
+/*
+    Problem statement and Explanation : https://en.wikipedia.org/wiki/Coprime_integers
+
+    In number theory, two integers a and b are coprime, relatively prime or
+    mutually prime if the only positive integer that is a divisor of both
+    of them is Consequently, any prime number that divides one of a
+    or b does not divide the other. This is equivalent to their greatest
+    common divisor (gcd) being. One says also a is prime to b or a
+    is coprime with b.
+*/
+
+// Here we require an already implemented method.
+const GetEuclidGCD = require('./GetEuclidGCD')
+
+// CoPrimeCheck function return the boolean in respect of the given number is co-prime or not.
+/**
+ * CoPrimeCheck function return the boolean in respect of the given number is co-prime or not.
+ * @param {Number} firstNumber first number for checking is prime or not.
+ * @param {Number} secondNumber second number for checking is prime or not.
+ * @returns return correspond boolean value, if both number are co-prime return `true`, else return `false`.
+ */
+const CoPrimeCheck = (firstNumber, secondNumber) => {
+  /*
+    This is the most efficient algorithm for checking co-primes
+    if the GCD of both the numbers is 1 that means they are co-primes.
+    */
+  return GetEuclidGCD(firstNumber, secondNumber) === 1
+}
+
+module.exports = CoPrimeCheck
