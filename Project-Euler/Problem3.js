ac7d916044177e1ff8d6e9014be055856e7195d0
Cihat Salik <cihatsalik1@gmail.com>
2021-09-09 10:11:22 +0300

    Update while loop condition
    Exponentially replaced `**` because I thought it was easier to read than `counter**2`.

--- Project-Euler/Problem3.js
+++ Project-Euler/Problem3.js
@@ -1,20 +1,20 @@
 // https://projecteuler.net/problem=3
 const problem = 600851475143
 
-const largestPrime = (num) => {
-  let newnumm = num
+const largestPrime = (number) => {
   let largestFact = 0
   let counter = 2
-  while (counter * counter <= newnumm) {
-    if (newnumm % counter === 0) {
-      newnumm = newnumm / counter
+
+  while (counter ** 2 <= number || number > largestFact) {
+    if (number % counter == 0) {
+      number /= counter
     } else {
       counter++
     }
+
+    largestFact = number
   }
-  if (newnumm > largestFact) {
-    largestFact = newnumm
-  }
+
   return largestFact
 }
 console.log(largestPrime(problem))
