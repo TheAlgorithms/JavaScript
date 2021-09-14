da8184a2880a6d9ff8fbb346bf430618b7e390c9
Charlie Moore <moorejchar@gmail.com>
2021-09-13 20:25:04 -0400

    update function documentation and name to match js convention

--- String/CheckFlatCase.js
+++ String/CheckFlatCase.js
@@ -3,11 +3,11 @@
 // Problem Source & Explanation: https://en.wikipedia.org/wiki/Naming_convention_(programming)
 
 /**
- * CheckFlatCase method returns true if the string in flatcase, else return the false.
- * @param {String} varname the name of the variable to check.
- * @returns `Boolean` return true if the string is in flatcase, else return false.
+ * checkFlatCase method returns true if the string in flatcase, else return the false.
+ * @param {string} varname the name of the variable to check.
+ * @returns {boolean} return true if the string is in flatcase, else return false.
  */
-const CheckFlatCase = (varname) => {
+const checkFlatCase = (varname) => {
   // firstly, check that input is a string or not.
   if (typeof varname !== 'string') {
     return new TypeError('Argument is not a string.')
