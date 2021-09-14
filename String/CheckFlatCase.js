e5f5223a7c7c704fa54cc162f10545c583824293
Charlie Moore <moorejchar@gmail.com>
2021-09-13 20:25:41 -0400

    add additional documentation explaining what the function does

--- String/CheckFlatCase.js
+++ String/CheckFlatCase.js
@@ -1,4 +1,6 @@
-// CheckFlatCase method checks the given string is in flatcase or not.
+// checkFlatCase method checks if the given string is in flatcase or not. Flatcase is a convention
+// where all letters are in lowercase, and there are no spaces between words.
+// thisvariable is an example of flatcase. In camelCase it would be thisVariable, snake_case this_variable and so on.
 
 // Problem Source & Explanation: https://en.wikipedia.org/wiki/Naming_convention_(programming)
 
