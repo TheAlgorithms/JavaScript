8cabd551e20c634fe15491d22a91c24d1a8eb9f4
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-09-22 16:42:44 +0300

    update the commit

--- Search/SlidingWindow.js
+++ Search/SlidingWindow.js
@@ -20,27 +20,28 @@
  * @returns {Int / Null} - Returns a total of N consecutive numbers or null
  */
 
-function slidingWindow(arr, num) {
-    // Edge Case:
-    // If the length of the array shorter than the window size (num) return null.
-    if (arr.length < num) return null;
-    // The highest amount of consecutive numbers
-    let maxSum = 0;
-    // Temp amount of consecutive numbers - For comparative purposes
-    let tempSum = 0;
-    // loop over the array {num} times and save their total amount in {maxSum}
-    for (let i = 0; i < num; i++) {
-        maxSum += arr[i];
-    }
-    // initialize {tempSum} to {maxSum}.  
-    tempSum = maxSum;
-    // loop over the array n times
-    for (let i = num; i < arr.length; i++) {
-        // Add the next num in the array and remove the first one
-        tempSum = tempSum - arr[i - num] + arr[i];
-        // save the largest number between {maxNum} and {tempNum} in maxSum.
-        maxSum = Math.max(maxSum, tempSum);
-    }
-    return maxSum;
+function slidingWindow (arr, num) {
+  // Edge Case:
+  // If the length of the array shorter than the window size (num) return null.
+  if (arr.length < num) return null
+  // The highest amount of consecutive numbers
+  let maxSum = 0
+  // Temp amount of consecutive numbers - For comparative purposes
+  let tempSum = 0
+  // loop over the array {num} times and save their total amount in {maxSum}
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i]
+  }
+  // initialize {tempSum} to {maxSum}.
+  tempSum = maxSum
+  // loop over the array n times
+  for (let i = num; i < arr.length; i++) {
+    // Add the next num in the array and remove the first one
+    tempSum = tempSum - arr[i - num] + arr[i]
+    // save the largest number between {maxNum} and {tempNum} in maxSum.
+    maxSum = Math.max(maxSum, tempSum)
+  }
+  return maxSum
 }
 
+export { slidingWindow }
