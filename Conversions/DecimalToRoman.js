167bc103ce4393755410c560b73d559842bd0c1c
Meehoi <beam_baimon@hotmail.com>
2021-10-02 00:52:37 +0700

    add explanation

--- Conversions/DecimalToRoman.js
+++ Conversions/DecimalToRoman.js
@@ -1,3 +1,11 @@
+/*
+    Decimal To Roman
+
+    This algorithm take decimal number and convert to roman numeral according to standard form (https://en.wikipedia.org/wiki/Roman_numerals#Description)
+
+    Algorithm & Explanation : https://www.rapidtables.com/convert/number/how-number-to-roman-numerals.html
+*/
+
 const values = {
   M: 1000,
   CM: 900,
