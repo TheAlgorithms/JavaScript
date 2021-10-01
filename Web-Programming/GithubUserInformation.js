8a00a0c91e7afbe2b59577ddc407f3864290f9d1
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2021-10-01 22:33:43 +0530

    added comments to GithubUserInformation.js file

--- Web-Programming/GithubUserInformation.js
+++ Web-Programming/GithubUserInformation.js
@@ -1,13 +1,19 @@
 const fetch = require('node-fetch')
 
+// Github API base URL
 const BASE_URL = 'https://api.github.com/users/';
 
 const getUserInformation = async function(userName) {
+
+  // fetching account information using Github API
   const fetchAccountInformation = await fetch(`${BASE_URL}${userName}`)
   const accountInformation = await fetchAccountInformation.json()
   
+  // fetching organizations of user
   const fetchOrganizations = await fetch(`${BASE_URL}${userName}/orgs`);
   const organizations = await fetchOrganizations.json()
+
+  // organizing information
   const userData = {
       ...((accountInformation.name !== null) && {name: accountInformation.name}),
       ...((accountInformation.bio !== null) && {bio: accountInformation.bio}),
@@ -24,6 +30,8 @@ const getUserInformation = async function(userName) {
   return userData
 }
 
+// NOTE: Only 60 requests are allowed per hour using the Github API being unauthorized. To increase the limit, please use Github API access token with Authorization header.
+// tests
 getUserInformation('octocat').then(userData => {console.log(userData)})
 getUserInformation('akkaraju-satvik').then(userData => {console.log(userData)})
 getUserInformation('TheAlgorithms').then(userData => {console.log(userData)})
