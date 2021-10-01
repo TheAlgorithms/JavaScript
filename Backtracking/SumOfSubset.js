c77b646fbde0174d5b13e985bba39f61611edc8d
Suaze9 <suazodaniel99@gmail.com>
2021-09-30 22:42:50 -0600

    separate test to different file and format

--- Backtracking/SumOfSubset.js
+++ Backtracking/SumOfSubset.js
@@ -17,61 +17,48 @@
  * @param {targetSum} The target sum on which the subset sum is compared to
  * @returns {number[][]} Subsets whose elements add up to targetSum
  */
-const subsetSum = (set, subset, setindex, sum, targetSum) => {
-    // Base case where the subset sum is equal to target sum
-    // Evaluation of following subsets on this path will always add up to
-    // greater than targetSum, so no need to continue
-    if (sum === targetSum) return [subset];
-  
-    // This and following subsets on this path will always add up to
-    // greater than targetSum, so no need to continue
-    if (sum > targetSum) return [];
-  
-    // Initialize results array. Will contain only valid subsets
-    let results = [];
-  
-    // Slice gets from the set all the elements at the right of the last element
-    // to be evaluated (last element of subset)
-    // forEach iterated on the resulting array
-    set.slice(setindex).forEach((num, index) => {
-      // The next subset to be evaluated, current subset plus next element
-      const nextSubset = [...subset, num];
-      
-      // Next index from the set. Current set index plus iteration index
-      // index starts at 0, so a + 1 is required 
-      const nextSetIndex = setindex + index + 1;
-  
-      // Sum of elements from the next subset to be evaluated
-      const nextSum = sum + num;
-  
-      // Call recursively the subsetSum for the nextSubset
-      const subsetResult = subsetSum(
-        set,
-        nextSubset,
-        nextSetIndex,
-        nextSum,
-        targetSum
-      );
-  
-      // Concat the recursive result with current result arary
-      results = [...results, ...subsetResult];
-    });
-  
-    //Return results
-    return results;
-  };
-  
-  /*
-   * Example with ordered array
-   *   W = [2, 5, 7, 8, 12, 16, 23, 40]
-   *   K = 25
-   *  
-   *   resulting subsets = [ 2, 7, 16 ], [ 2, 23 ], [ 5, 8, 12 ]
-   */
-  
-  const nums = [2, 5, 7, 8, 12, 16, 23, 40];
-  
-  const subsets = subsetSum(nums, [], 0, 0, 25);
-  
-  console.log(subsets);
-  
+const sumOfSubset = (set, subset, setindex, sum, targetSum) => {
+  // Base case where the subset sum is equal to target sum
+  // Evaluation of following subsets on this path will always add up to
+  // greater than targetSum, so no need to continue
+  if (sum === targetSum) return [subset]
+
+  // This and following subsets on this path will always add up to
+  // greater than targetSum, so no need to continue
+  if (sum > targetSum) return []
+
+  // Initialize results array. Will contain only valid subsets
+  let results = []
+
+  // Slice gets from the set all the elements at the right of the last element
+  // to be evaluated (last element of subset)
+  // forEach iterated on the resulting array
+  set.slice(setindex).forEach((num, index) => {
+    // The next subset to be evaluated, current subset plus next element
+    const nextSubset = [...subset, num]
+
+    // Next index from the set. Current set index plus iteration index
+    // index starts at 0, so a + 1 is required
+    const nextSetIndex = setindex + index + 1
+
+    // Sum of elements from the next subset to be evaluated
+    const nextSum = sum + num
+
+    // Call recursively the sumOfSubset for the nextSubset
+    const subsetResult = sumOfSubset(
+      set,
+      nextSubset,
+      nextSetIndex,
+      nextSum,
+      targetSum
+    )
+
+    // Concat the recursive result with current result arary
+    results = [...results, ...subsetResult]
+  })
+
+  // Return results
+  return results
+}
+
+export { sumOfSubset }
