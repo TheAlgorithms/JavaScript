d43b81653c71cda56f2de86581708c7baacc111d
pseudonerd16 <sreejitaroytiu1923@gmail.com>
2021-10-02 09:20:18 +0530

    Complete Implementation of a Tree

---
+++ Trees/TreeImplementation.js
@@ -0,0 +1,112 @@
+//Complete Implementation of a Tree
+/* author Sreejita Roy (https://github.com/pseudonerd16)*/
+
+function Node(data) {
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+}
+
+function Tree(data) {
+    var node = new Node(data);
+    this._root = node;
+}
+
+Tree.prototype.traverseDF = function(callback) {
+
+    // this is a recurse and immediately-invoking function
+    (function recurse(currentNode) {
+        // step 2
+        for (var i = 0, length = currentNode.children.length; i < length; i++) {
+            // step 3
+            recurse(currentNode.children[i]);
+        }
+
+        // step 4
+        callback(currentNode);
+
+        // step 1
+    })(this._root);
+
+};
+
+Tree.prototype.traverseBF = function(callback) {
+    var queue = new Queue();
+
+    queue.enqueue(this._root);
+
+    currentTree = queue.dequeue();
+
+    while(currentTree){
+        for (var i = 0, length = currentTree.children.length; i < length; i++) {
+            queue.enqueue(currentTree.children[i]);
+        }
+
+        callback(currentTree);
+        currentTree = queue.dequeue();
+    }
+};
+
+Tree.prototype.contains = function(callback, traversal) {
+    traversal.call(this, callback);
+};
+
+Tree.prototype.add = function(data, toData, traversal) {
+    var child = new Node(data),
+        parent = null,
+        callback = function(node) {
+            if (node.data === toData) {
+                parent = node;
+            }
+        };
+
+    this.contains(callback, traversal);
+
+    if (parent) {
+        parent.children.push(child);
+        child.parent = parent;
+    } else {
+        throw new Error('Cannot add node to a non-existent parent.');
+    }
+};
+
+Tree.prototype.remove = function(data, fromData, traversal) {
+    var tree = this,
+        parent = null,
+        childToRemove = null,
+        index;
+
+    var callback = function(node) {
+        if (node.data === fromData) {
+            parent = node;
+        }
+    };
+
+    this.contains(callback, traversal);
+
+    if (parent) {
+        index = findIndex(parent.children, data);
+
+        if (index === undefined) {
+            throw new Error('Node to remove does not exist.');
+        } else {
+            childToRemove = parent.children.splice(index, 1);
+        }
+    } else {
+        throw new Error('Parent does not exist.');
+    }
+
+    return childToRemove;
+};
+
+function findIndex(arr, data) {
+    var index;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].data === data) {
+            index = i;
+        }
+    }
+
+    return index;
+}
