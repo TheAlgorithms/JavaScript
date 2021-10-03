acd670bb2eefdf22711826007f0f100f0269a83e
Omkarnath Parida <omkarnath.parida@yocket.in>
2021-10-03 20:38:23 +0530

    Added test cases for LongestPalindromicSubsequence

---
+++ Dynamic-Programming/tests/LongestPalindromicSubsequence.test.js
@@ -0,0 +1,19 @@
+import { longestPalindromeSubsequence } from '../LongestPalindromicSubsequence'
+
+describe('LongestPalindromicSubsequence', () => {
+  it('expects to return 0 as longest pallindromic subsequence', () => {
+    expect(longestPalindromeSubsequence('abcdefgh')).toBe(1)
+  })
+
+  it('expects to return 4 as longest pallindromic subsequence', () => {
+    expect(longestPalindromeSubsequence('bbbab')).toBe(4)
+  })
+
+  it('expects to return 2 as longest pallindromic subsequence', () => {
+    expect(longestPalindromeSubsequence('cbbd')).toBe(2)
+  })
+
+  it('expects to return 7 as longest pallindromic subsequence', () => {
+    expect(longestPalindromeSubsequence('racexyzcxar')).toBe(7)
+  })
+})
