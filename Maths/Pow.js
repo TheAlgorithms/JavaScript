ca26b76c4c34a4081064359f7c656c81ef8e685b
farharamadhan2 <farharamadhan2@gmail.com>
2021-10-04 20:49:30 +0700

    improve power function to receive negative exponents

--- Maths/Pow.js
+++ Maths/Pow.js
@@ -2,8 +2,15 @@
 
 const pow = (x, y) => {
   let result = 1
-  for (let i = 1; i <= y; i++) {
-    result *= x
+  if (y >= 0) {
+    for (let i = 1; i <= y; i++) {
+      result *= x
+    }
+  } else {
+    for (let i = y; i < 0; i++) {
+      result *= x
+    }
+    result = 1 / result
   }
   return result
 }
