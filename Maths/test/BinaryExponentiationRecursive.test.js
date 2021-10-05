25c29b6ff844280e3394081212aa2e51ade8c82c
Roland Hummel <roland.hummel@qaware.de>
2021-10-05 23:26:03 +0200

    Migrate doctest for EulersTotient.js
    (also remove inline "main" test method)

--- Maths/test/BinaryExponentiationRecursive.test.js
+++ Maths/test/BinaryExponentiationRecursive.test.js
@@ -1,7 +1,6 @@
 const { binaryExponentiation } = require('../BinaryExponentiationRecursive')
 
 describe('BinaryExponentiationRecursive', () => {
-
   it('should calculate 2 to the power of 10 correctly', () => {
     expect(binaryExponentiation(2, 10)).toBe(1024)
   })
@@ -9,5 +8,4 @@ describe('BinaryExponentiationRecursive', () => {
   it('should calculate 3 to the power of 9 correctly', () => {
     expect(binaryExponentiation(3, 9)).toBe(19683)
   })
-
 })
