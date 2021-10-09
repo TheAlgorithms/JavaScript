09da5eef179faec4c050819c264696759c44f976
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2021-10-09 14:29:44 +0200

    Fix GeneratePermutations so that it actually returns the permutations instead of logging them  + add Jest test.

--- Backtracking/GeneratePermutations.js
+++ Backtracking/GeneratePermutations.js
@@ -1,8 +1,8 @@
 /*
- * Problem Statement: Generate all distinct permutations of a string/array (all permutations should be in sorted order);
+ * Problem Statement: Generate all distinct permutations of a an array (all permutations should be in sorted order);
  *
  * What is permutations?
- * - Permutation means possible arrangements in a set (here it is string/array);
+ * - Permutation means possible arrangements in a set (here it is an array);
  *
  * Reference to know more about permutations:
  * - https://www.britannica.com/science/permutation
@@ -17,17 +17,21 @@ const swap = (arr, i, j) => {
   return newArray
 }
 
-const permutations = (arr, low, high) => {
-  if (low === high) {
-    console.log(arr.join(' '))
-    return
+const permutations = arr => {
+  let P = []
+  const permute = (arr, low, high) => {
+    if (low === high) {
+      P.push([...arr])
+      // console.log(arr.join(' '))
+      return P
+    }
+    for (let i = low; i <= high; i++) {
+      arr = swap(arr, low, i)
+      permute(arr, low + 1, high)
+    }
+    return P
   }
-  for (let i = low; i <= high; i++) {
-    arr = swap(arr, low, i)
-    permutations(arr, low + 1, high)
-  }
+  return permute(arr, 0, arr.length - 1)
 }
 
-// Driver Code
-const input = [1, 2, 3]
-permutations(input, 0, input.length - 1)
+export { permutations }
