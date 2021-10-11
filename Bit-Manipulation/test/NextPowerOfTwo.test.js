d6364548aa2a1e3e4d58a27dbfb6752eec8d9029
Indre Maksimovaite <imaksimovaite@gmail.com>
2021-10-11 20:24:38 +0300

    Check if number is already power of two

--- Bit-Manipulation/test/NextPowerOfTwo.test.js
+++ Bit-Manipulation/test/NextPowerOfTwo.test.js
@@ -4,12 +4,12 @@ describe('NextPowerOfTwo', () => {
   it.each`
     input    | result
     ${0}     | ${1}
-    ${1}     | ${2}
-    ${2}     | ${4}
+    ${1}     | ${1}
+    ${2}     | ${2}
     ${3}     | ${4}
     ${5}     | ${8}
     ${125}   | ${128}
-    ${1024}  | ${2048}
+    ${1024}  | ${1024}
     ${10000} | ${16384}
   `('returns $result when is given $input', ({ input, result }) => {
     const res = nextPowerOfTwo(input)
