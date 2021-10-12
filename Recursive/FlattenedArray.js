228a5d30c10ddc724abfc7698fd3c57de08dfcd1
Velkumaran AP <38457682+vels10@users.noreply.github.com>
2021-10-12 23:35:13 +0530

    Update FlattenedArray.js

--- Recursive/FlattenedArray.js
+++ Recursive/FlattenedArray.js
@@ -1,5 +1,9 @@
+
+//  https://flexiple.com/flatten-array-javascript/
+//  Flatten an given array to reduce the dimensionality of an array
+
 const flattened = (arr) => {
-  const  = []
+  const = []
   arr.forEach(i => {
     if(Array.isArray(i)){
       res.push(...flattened(i));
