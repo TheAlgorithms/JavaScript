b4de131ed1716f76de134223492d9d9e646ea206
farharamadhan2 <farharamadhan2@gmail.com>
2021-10-13 20:53:22 +0700

    Pow function optimization using exponentiation by squaring

--- Maths/Pow.js
+++ Maths/Pow.js
@@ -1,18 +1,11 @@
 // Returns the value of x to the power of y
 
 const pow = (x, y) => {
-  let result = 1
-  if (y >= 0) {
-    for (let i = 1; i <= y; i++) {
-      result *= x
-    }
-  } else {
-    for (let i = y; i < 0; i++) {
-      result *= x
-    }
-    result = 1 / result
-  }
-  return result
+  if (y < 0) return pow(1 / x, -y)
+  else if (y === 0) return 1
+  else if (y === 1) return x
+  else if (y % 2 === 0) return pow(x * x, y / 2)
+  else if (y % 2 !== 0) return x * pow(x * x, (y - 1) / 2)
 }
 
 export { pow }
