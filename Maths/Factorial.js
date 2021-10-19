0da5aa9ff6eec76f8ae83aebe6bb14da32543b1a
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2021-10-19 19:39:02 +0530

    Reduced and optimized code
    Merged two if statements

--- Maths/Factorial.js
+++ Maths/Factorial.js
@@ -21,11 +21,8 @@ const calcFactorial = (num) => {
   if (num === 0) {
     return 'The factorial of 0 is 1.'
   }
-  if (num < 0) {
-    return 'Sorry, factorial does not exist for negative numbers.'
-  }
-  if (!num) {
-    return 'Sorry, factorial does not exist for null or undefined numbers.'
+  if (!num) || (num < 0) {
+    return 'Sorry, factorial does not exist for null or undefined or negative numbers.'
   }
   if (num > 0) {
     const range = calcRange(num)
