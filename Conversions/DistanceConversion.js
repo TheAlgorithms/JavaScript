1284223ab86c2976a9b196ccadaa363377a8a77a
Gustavo Seiji Kamihara <gustavo.kamihara@gmail.com>
2021-10-19 14:18:03 -0300

    fixed the identation

--- Conversions/DistanceConversion.js
+++ Conversions/DistanceConversion.js
@@ -1,7 +1,8 @@
+//Wikipedia Foot page used https://en.wikipedia.org/wiki/Foot_(unit)
 const feetToMeters = (feet)=>{
-    return feet*0.3048
+  return feet*0.3048
 }
 
 const metersToFeet = (meters)=>{
-    return meters*(1/0.3048)
+  return meters*(1/0.3048)
 }
