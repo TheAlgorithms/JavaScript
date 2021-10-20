db5b4af7305d9fab34d07e2fecc1acb424be25b7
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2021-10-20 18:43:26 +0530

    formatting with standardjs

--- String/test/AlphaNumericPalindrome.test.js
+++ String/test/AlphaNumericPalindrome.test.js
@@ -5,11 +5,11 @@ test('should return true if the given string has alphanumeric characters that ar
 })
 
 test('should return true if the given string has alphanumeric characters that are palindrom irrespective of case and symbols', () => {
-  expect(alphaNumericPlaindrome('0_0 (: /-\ :) 0-0')).toBe(true)
+  expect(alphaNumericPlaindrome('0_0 (: /-:) 0-0')).toBe(true)
 })
 
 test('should return true if the given string has alphanumeric characters that are palindrom irrespective of case and symbols', () => {
-  expect(alphaNumericPlaindrome('five|\_/|four')).toBe(false)
+  expect(alphaNumericPlaindrome('five|_/|four')).toBe(false)
 })
 
 test('should return true if the given string has alphanumeric characters that are palindrom irrespective of case and symbols', () => {
