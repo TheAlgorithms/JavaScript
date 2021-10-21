16692cdde5b5f950a3440cad36471edd1d5641b3
Adhiraj <adhirajd007@gmail.com>
2021-10-21 18:18:32 +0530

    Update Problem025.test.js

--- Project-Euler/test/Problem025.test.js
+++ Project-Euler/test/Problem025.test.js
@@ -2,26 +2,26 @@ import { fibonacciIndex } from '../Problem025'
 
 describe ('Check Problem 25 - 1000 digit Fibonnaci number', () => {
     it('First term of the Fibonnaci sequence containing 3 digits', () => {
-        expect(digitFibonacci(3)).toBe(12)
+        expect(fibonacciIndex(3)).toBe(12)
     })
 
     it('First term of the Fibonnaci sequence containing 10 digits', () => {
-        expect(digitFibonacci(10)).toBe(45)
+        expect(fibonacciIndex(10)).toBe(45)
     })
 
     it('First term of the Fibonnaci sequence containing 50 digits', () => {
-        expect(digitFibonacci(50)).toBe(237)
+        expect(fibonacciIndex(50)).toBe(237)
     })
 
     it('First term of the Fibonnaci sequence containing 100 digits', () => {
-        expect(digitFibonacci(100)).toBe(476)
+        expect(fibonacciIndex(100)).toBe(476)
     })
 
     it('First term of the Fibonnaci sequence containing 1000 digits', () => {
-        expect(digitFibonacci(1000)).toBe(4782)
+        expect(fibonacciIndex(1000)).toBe(4782)
     })
     
     it('First term of the Fibonnaci sequence containing 10000 digits', () => {
-        expect(digitFibonacci(10000)).toBe(47847)
+        expect(fibonacciIndex(10000)).toBe(47847)
     })
 })
