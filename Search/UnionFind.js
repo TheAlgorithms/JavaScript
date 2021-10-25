3fcef59972bef3cb7088317735ea7d93adb32584
Administrator <pi@pglp.noip.me>
2021-10-26 00:48:11 +0200

    add UnionFind.js plus tests

--- Search/UnionFind.js
+++ Search/UnionFind.js
@@ -1,4 +1,4 @@
-function UnionFind(n, key){
+function UnionFind(n, key) {
 	if (!(this instanceof UnionFind)) return new UnionFind(n);
 	if (key && typeof key != 'function') {
 		throw new Error("key has to be a function or else left undefined");
@@ -38,8 +38,12 @@ function UnionFind(n, key){
 		ensureIndexWithinBounds(p, q);
 		var i = this.find(p),	j = this.find(q);
 		if (i === j) return;
-		if (sz[i] < sz[j]) id[i] = j; sz[j] += sz[i];
-		else id[j] = i; sz[i] += sz[j];
+		if (sz[i] < sz[j]) {
+			id[i] = j; sz[j] += sz[i];
+		}
+		else {
+			id[j] = i; sz[i] += sz[j];
+		}
 		cnt--;
 		return;
 	};
--- Search/test/UnionFindTest.js
+++ Search/test/UnionFindTest.js
@@ -1,107 +1,54 @@
 import { UnionFind } from '../UnionFind'
 
-var uf = new UnionFind(5);
-	it("should have a constructor UnionFind(NUM_OF_NODES)", function() {
-		(uf instanceof UnionFind).should.be.truthy;
-	})
-	it("should expose .size()", function() {
-		uf.size().should.equal(5);
-	})
-	it("should do .union(num1, num2)", function() {
-		uf.union(1, 2);
-		uf.union(3, 4);
-		uf.union(0, 4);
+const uf = new UnionFind(5)
 
-		uf.connected(1, 2).should.be.truthy;
-		uf.connected(1, 2).should.be.truthy;
-
-		uf.connected(3, 4).should.be.truthy;
-		uf.connected(3, 0).should.be.truthy;
-		uf.connected(4, 0).should.be.truthy;
-
-		uf.connected(1, 3).should.be.falsy;
-		uf.connected(1, 4).should.be.falsy;
-		uf.connected(1, 0).should.be.falsy;
-		uf.connected(2, 3).should.be.falsy;
-		uf.connected(2, 4).should.be.falsy;
-		uf.connected(2, 0).should.be.falsy;
-	})
-	it(".count(), should return the number of disparate groups", function() {
-		uf.count().should.equal(2);
-	})
-	it("should check if two components are connected, .connected(num1, num2)", function() {
-		uf.connected(1, 2).should.be.truthy;
-		uf.connected(1, 3).should.be.falsy;
-	})
-	it("should find the root of the tree in which the given element lives, .find(num)", function() {
-		uf.find(1).should.equal(1);
-		uf.find(2).should.equal(1);
-		uf.find(3).should.equal(3);
-		uf.find(4).should.equal(3);
-		uf.find(0).should.equal(3);
-	})
-	describe("weighted", function() {
-		it("should always change the id of the smaller tree and preserve the id of the larger one", function() {
-			//We have two sets now s1 = {1, 2} and s2 = {3, 4, 0}
-			uf.union(2, 3);
-			uf.count().should.equal(1);
-			uf.find(0).should.equal(3);
-			uf.find(1).should.equal(3);
-			uf.find(2).should.equal(3);
-			uf.find(3).should.equal(3);
-			uf.find(4).should.equal(3);
-		})
-	})
-})
-describe("Index out of bounds", function() {
-	it("should throw an error on union of a number out of bounds", function() {
-		var uf = new UnionFind(5);
-		(function() {
-			uf.union(1, 5);
-		}).should.throw(Error);
-	})
-})
-describe("key", function() {
-	it("should accept a key that can map to array index", function() {
-		var key = function(a) { return a - 1; };
-		var uf = new UnionFind(5, key);
-		uf.size().should.equal(5);
-		uf.union(4, 5);
-		(function() {
-			uf.union(5, 6);
-		}).should.throw(Error);
-		uf.count().should.equal(4);
-		uf.count().should.equal(4);
-	})
+test('should expose .size():', () => {
+  const size = uf.size()
+  expect(size).toBe(5)
 })
 
-/*
-test('should return the index of a number in an array of numbers:', () => {
-  const indexNumber = ternarySearchRecursive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
-  expect(indexNumber).toBe(2)
+test('should do .union(num1, num2):', () => {
+  uf.union(1, 2)
+  uf.union(3, 4)
+  uf.union(0, 4)
+  expect(uf.connected(1, 2)).toBe(true)
+  expect(uf.connected(1, 2)).toBe(true)
+
+  expect(uf.connected(3, 4)).toBe(true)
+  expect(uf.connected(3, 0)).toBe(true)
+  expect(uf.connected(4, 0)).toBe(true)
+
+  expect(uf.connected(1, 3)).toBe(false)
+  expect(uf.connected(1, 4)).toBe(false)
+  expect(uf.connected(1, 0)).toBe(false)
+  expect(uf.connected(2, 3)).toBe(false)
+  expect(uf.connected(2, 4)).toBe(false)
+  expect(uf.connected(2, 0)).toBe(false)
 })
-test('should return the index of a number in an array of numbers:', () => {
-  const indexNumber = ternarySearchIterative([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
-  expect(indexNumber).toBe(7)
+
+test('.count(), should return the number of disparate groups:', () => {
+  expect(uf.count()).toBe(2)
 })
-test('should return the index of a number in an array of numbers:', () => {
-  const indexNumber = ternarySearchRecursive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
-  expect(indexNumber).toBe(-1)
+
+test('should check if two components are connected, .connected(num1, num2):', () => {
+  expect(uf.connected(1, 2)).toBe(true)
+  expect(uf.connected(1, 3)).toBe(false)
 })
-test('should return the index of a number in an array of numbers:', () => {
-  const indexNumber = ternarySearchIterative([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 12)
-  expect(indexNumber).toBe(-1)
+
+test('should find the root of the tree in which the given element lives, .find(num):', () => {
+  expect(uf.find(1)).toBe(1)
+  expect(uf.find(2)).toBe(1)
+  expect(uf.find(3)).toBe(3)
+  expect(uf.find(4)).toBe(3)
+  expect(uf.find(0)).toBe(3)
 })
-test('should return the index of a string in an array of strings:', () => {
-  const indexNumber = ternarySearchRecursive(['Ali', 'Cathrynli', 'Josuke', 'Thomas'], 'Cathrynli')
-  expect(indexNumber).toBe(1)
+
+test('should always change the id of the smaller tree and preserve the id of the larger one', () => {
+  uf.union(2, 3)
+  expect(uf.count()).toBe(1)
+  expect(uf.find(0)).toBe(3)
+  expect(uf.find(1)).toBe(3)
+  expect(uf.find(2)).toBe(3)
+  expect(uf.find(3)).toBe(3)
+  expect(uf.find(4)).toBe(3)
 })
-test('should return the index of a string in an array of strings:', () => {
-  const indexNumber = ternarySearchRecursive(['Ali', 'Cathrynli', 'Josuke', 'Thomas'], 'Josuke')
-  expect(indexNumber).toBe(2)
-})
-test('should return the index of a string in an array of strings:', () => {
-  const indexNumber = ternarySearchRecursive(['Ali', 'Cathrynli', 'Josuke', 'Thomas'], 'Angela')
-  expect(indexNumber).toBe(-1)
-})
-*/
