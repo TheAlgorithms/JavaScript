2d42923884794b84ea6d56dc00b941f62243b006
jhonDoe15 <62393042+jhonDoe15@users.noreply.github.com>
2021-10-28 00:10:40 +0300

    style changes

--- Data-Structures/Array/LocalMaximomPoint.js
+++ Data-Structures/Array/LocalMaximomPoint.js
@@ -9,26 +9,22 @@
  * @complexity: O(log(n)) (worst case)
  * @flow
  */
-
 const findMaxPointIndex = (array, rangeStartIndex, rangeEndIndex, originalLength) => {
-    
-    // find index range middle point
-    const middleIndex = rangeStartIndex + parseInt((rangeEndIndex - rangeStartIndex) / 2);
+  // find index range middle point
+  const middleIndex = rangeStartIndex + parseInt((rangeEndIndex - rangeStartIndex) / 2)
 
-    // handle array bounds
-    if ((middleIndex === 0 || array[middleIndex - 1] <= array[middleIndex]) &&
+  // handle array bounds
+  if ((middleIndex === 0 || array[middleIndex - 1] <= array[middleIndex]) &&
         (middleIndex === originalLength - 1 || array[middleIndex + 1] <= array[middleIndex])) {
-        return middleIndex;
-    }
-    else if (middleIndex > 0 && array[middleIndex - 1] > array[middleIndex]) {
-        return findMaxPointIndex(array, rangeStartIndex, (middleIndex - 1), originalLength);
-    }
-    else {
-        // regular local max
-        return findMaxPointIndex(array, (middleIndex + 1), rangeEndIndex, originalLength);
-    }
+    return middleIndex
+  } else if (middleIndex > 0 && array[middleIndex - 1] > array[middleIndex]) {
+    return findMaxPointIndex(array, rangeStartIndex, (middleIndex - 1), originalLength)
+  } else {
+    // regular local max
+    return findMaxPointIndex(array, (middleIndex + 1), rangeEndIndex, originalLength)
+  }
 }
 
-const LocalMaximomPoint = (A) => findMaxPointIndex(A, 0, A.length - 1, A.length);
+const LocalMaximomPoint = (A) => findMaxPointIndex(A, 0, A.length - 1, A.length)
 
 export { LocalMaximomPoint }
