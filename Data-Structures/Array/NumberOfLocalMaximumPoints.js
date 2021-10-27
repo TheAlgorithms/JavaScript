cee903803288bd4d348086c3ab09daafa4819ff5
jhonDoe15 <62393042+jhonDoe15@users.noreply.github.com>
2021-10-28 00:06:25 +0300

    style changes

--- Data-Structures/Array/NumberOfLocalMaximumPoints.js
+++ Data-Structures/Array/NumberOfLocalMaximumPoints.js
@@ -12,31 +12,31 @@
 
 // check if returned index is a local maxima
 const IsMaximumPoint = (array, index) => {
-    // handle array bounds
-    // array start
-    if(index == 0) {
-        return array[index] > array[index + 1];
+  // handle array bounds
+  // array start
+  if (index === 0) {
+    return array[index] > array[index + 1]
     // array end
-    } else if(index == array.length - 1) {
-        return array[index] > array[index - 1];
+  } else if (index === array.length - 1) {
+    return array[index] > array[index - 1]
     // handle index inside array bounds
-    } else {
-        return array[index] > array[index + 1] && array[index] > array[index - 1];
-    }
+  } else {
+    return array[index] > array[index + 1] && array[index] > array[index - 1]
+  }
 }
 
 const CountLocalMaximumPoints = (array, startIndex, endIndex) => {
-    // stop check in divide and conquer recursion
-    if(startIndex == endIndex) {
-        return IsMaximumPoint(array, startIndex) ? 1 : 0;
-    }
-    
-    // handle the two halves
-    let middleIndex = parseInt((startIndex + endIndex) / 2);
-    return CountLocalMaximumPoints(array, startIndex, middleIndex) + 
-    CountLocalMaximumPoints(array, middleIndex + 1, endIndex);
+  // stop check in divide and conquer recursion
+  if (startIndex === endIndex) {
+    return IsMaximumPoint(array, startIndex) ? 1 : 0
+  }
+
+  // handle the two halves
+  const middleIndex = parseInt((startIndex + endIndex) / 2)
+  return CountLocalMaximumPoints(array, startIndex, middleIndex) +
+    CountLocalMaximumPoints(array, middleIndex + 1, endIndex)
 }
 
-const NumberOfLocalMaximumPoints = (A) => CountLocalMaximumPoints(A, 0, A.length - 1);
+const NumberOfLocalMaximumPoints = (A) => CountLocalMaximumPoints(A, 0, A.length - 1)
 
-export { NumberOfLocalMaximumPoints } 
+export { NumberOfLocalMaximumPoints }
