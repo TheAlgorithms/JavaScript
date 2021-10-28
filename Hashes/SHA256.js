05d944a9b18b5c0c74c409271da22509e3a7cf56
Rak Laptudirm <raklaptudirm@gmail.com>
2021-10-28 14:58:16 +0530

    chore: fix spelling

--- Hashes/SHA256.js
+++ Hashes/SHA256.js
@@ -45,7 +45,7 @@ function pad (str, bits) {
  * @return {array} - array of original string split into chunks
  *
  * @example
- *      chunkify("this is a test", 2); // ["th", "is", " i", "s ", "a ", "te", "st"]
+ *      chunkify("this is a test", 2)
  */
 function chunkify (str, size) {
   const chunks = []
@@ -76,7 +76,7 @@ function rotateRight (bits, turns) {
  * @return {string} - processed message
  */
 function preProcess (message) {
-  // covert message to binary representation padded to
+  // convert message to binary representation padded to
   // 8 bits, and add 1
   let m = message.split('')
     .map(e => e.charCodeAt(0))
