1f15216d299c72ee0d0bb1df6a9d24ffb86a6502
ggkogkou <ggkogkou@ggkogkou.gr>
2021-10-28 12:02:32 +0300

    Styled with standard.js

--- Maths/SimpsonIntegration.js
+++ Maths/SimpsonIntegration.js
@@ -22,47 +22,47 @@
 */
 
 
-function integralEvaluation(N, a, b, func) {
-    // Check if N is an even integer
-    let isNEven = true;
-    if(N%2 !== 0) isNEven = false;
+function integralEvaluation (N, a, b, func) {
+  // Check if N is an even integer
+  let isNEven = true
+  if (N % 2 !== 0) isNEven = false
 
-    if(!Number.isInteger(N) || Number.isNaN(a) || Number.isNaN(b)) { throw new TypeError("Expected integer N and finite a, b"); }
-    if(!isNEven) { throw "N is not an even number"; }
-    if(N <= 0) { throw "N has to be >= 2"; }
+  if (!Number.isInteger(N) || Number.isNaN(a) || Number.isNaN(b)) { throw new TypeError('Expected integer N and finite a, b') }
+  if (!isNEven) { throw Error('N is not an even number') }
+  if (N <= 0) { throw Error('N has to be >= 2') }
 
-    // Check if a < b
-    if(a > b) { throw "a must be less or equal than b"; }
-    if(a === b) return 0;
+  // Check if a < b
+  if (a > b) { throw Error('a must be less or equal than b') }
+  if (a === b) return 0
 
-    // Calculate the step h 
-    const h = (b - a) / N;
+  // Calculate the step h
+  const h = (b - a) / N
 
-    // Find interpolation points
-    let xi = a; // initialize xi = x0
-    let pointsArray = [];
+  // Find interpolation points
+  let xi = a // initialize xi = x0
+  const pointsArray = []
 
-    // Find the sum {f(x0) + 4*f(x1) + 2*f(x2) + ... + 2*f(xN-2) + 4*f(xN-1) + f(xN)}
-    let temp;
-    for(let i = 0; i < N+1; i++){
-        if(i === 0 || i === N) temp = func(xi);
-        else if(i%2 === 0) temp = 2*func(xi);
-        else temp = 4*func(xi);
+  // Find the sum {f(x0) + 4*f(x1) + 2*f(x2) + ... + 2*f(xN-2) + 4*f(xN-1) + f(xN)}
+  let temp
+  for (let i = 0; i < N + 1; i++) {
+    if (i === 0 || i === N) temp = func(xi)
+    else if (i % 2 === 0) temp = 2 * func(xi)
+    else temp = 4 * func(xi)
 
-        pointsArray.push(temp);
-        xi += h;
-    }
+    pointsArray.push(temp)
+    xi += h
+  }
 
-    // Calculate the integral
-    let result = h/3;
-    temp = 0;
-    for(let i=0; i < pointsArray.length; i++) temp += pointsArray[i];
+  // Calculate the integral
+  let result = h / 3
+  temp = 0
+  for (let i = 0; i < pointsArray.length; i++) temp += pointsArray[i]
 
-    result *= temp;
+  result *= temp
 
-    if (Number.isNaN(result)) { throw "Result is NaN. The input interval doesn't belong to the function's domain"; }
+  if (Number.isNaN(result)) { throw Error('Result is NaN. The input interval doesnt belong to the functions domain') }
 
-    return result;
+  return result
 }
 
 export { integralEvaluation }
