e17e0d0afdfb66b2d382706aaff586a919592e0e
ChickenHawkXP <47929127+ChickenHawkXP@users.noreply.github.com>
2021-10-30 22:10:23 -0400

    Added FindMax to Maths

---
+++ Maths/FindMax.js
@@ -0,0 +1,20 @@
+/**
+ * Function to find the maximum number given an array of integers
+ * Returns the maximum number of the array
+ * If the array is empty it returns the string 'Array is empty'
+ */
+
+export const findMax = (arr) => {
+    if (arr.length === 0)
+        return 'Array is empty'
+
+    let max = arr[0]
+    arr.forEach(element =>{
+      if (element > max){
+        max = element
+      }
+    })
+    return max
+}
+
+export { findMax }
