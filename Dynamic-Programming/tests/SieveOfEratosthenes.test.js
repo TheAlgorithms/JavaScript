02a4cee5c2316cc721883ebfd50336b0511cdbe1
YATIN KATHURIA <47096348+Yatin-kathuria@users.noreply.github.com>
2021-11-20 17:15:36 +0530

    merge: optamization,documantation,return Array[bool] => Array[primes only], test case (#841)

---
+++ Dynamic-Programming/tests/SieveOfEratosthenes.test.js
@@ -0,0 +1,23 @@
+import { sieveOfEratosthenes } from '../SieveOfEratosthenes'
+
+describe('SieveOfEratosthenes', () => {
+  it('Primes till 0', () => {
+    expect(sieveOfEratosthenes(0)).toEqual([])
+  })
+
+  it('Primes till 1', () => {
+    expect(sieveOfEratosthenes(1)).toEqual([])
+  })
+
+  it('Primes till 10', () => {
+    expect(sieveOfEratosthenes(10)).toEqual([2, 3, 5, 7])
+  })
+
+  it('Primes till 23', () => {
+    expect(sieveOfEratosthenes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23])
+  })
+
+  it('Primes till 70', () => {
+    expect(sieveOfEratosthenes(70)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67])
+  })
+})
