dacdd29342e7f860908c595d3e7346d9642a1a60
Yatin <yatinkathuria74@gmail.com>
2021-11-20 16:58:42 +0530

    optamization,documantation,return Array[bool] => Array[primes only], test case

--- Dynamic-Programming/SieveOfEratosthenes.js
+++ Dynamic-Programming/SieveOfEratosthenes.js
@@ -1,21 +1,28 @@
+/**
+ * @function SieveOfEratosthenes
+ * @description Calculates prime numbers till input number n
+ * @param {Number} n - The input integer
+ * @return {Number[]} List of Primes till n.
+ * @see [Sieve_of_Eratosthenes](https://www.geeksforgeeks.org/sieve-of-eratosthenes/)
+ */
 function sieveOfEratosthenes (n) {
-  /*
-     * Calculates prime numbers till a number n
-     * :param n: Number up to which to calculate primes
-     * :return: A boolean list containing only primes
-     */
-  const primes = new Array(n + 1)
-  primes.fill(true) // set all as true initially
+  if (n <= 1) return []
+  const primes = new Array(n + 1).fill(true) // set all as true initially
   primes[0] = primes[1] = false // Handling case for 0 and 1
-  const sqrtn = Math.ceil(Math.sqrt(n))
-  for (let i = 2; i <= sqrtn; i++) {
+  for (let i = 2; i * i <= n; i++) {
     if (primes[i]) {
-      for (let j = 2 * i; j <= n; j += i) {
+      for (let j = i * i; j <= n; j += i) {
         primes[j] = false
       }
     }
   }
-  return primes
+
+  return primes.reduce((result, isPrime, index) => {
+    if (isPrime) {
+      result.push(index)
+    }
+    return result
+  }, [])
 }
 
 // Example
