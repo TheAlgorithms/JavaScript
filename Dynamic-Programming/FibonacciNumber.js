93e57b0de5d9686c5c53ae24613ae89e082ff635
YATIN KATHURIA <47096348+Yatin-kathuria@users.noreply.github.com>
2021-11-20 16:21:25 +0530

    merge: Add FibonacciNumber.js test case and update the decription of function. (#840)

--- Dynamic-Programming/FibonacciNumber.js
+++ Dynamic-Programming/FibonacciNumber.js
@@ -1,5 +1,10 @@
-//  https://en.wikipedia.org/wiki/Fibonacci_number
-
+/**
+ * @function Fibonacci
+ * @description Fibonacci is the sum of previous two fibonacci numbers.
+ * @param {Integer} N - The input integer
+ * @return {Integer} fibonacci of N.
+ * @see [Fibonacci_Numbers](https://en.wikipedia.org/wiki/Fibonacci_number)
+ */
 const fibonacci = (N) => {
   // creating array to store values
   const memo = new Array(N + 1)
