8cec6bb8ca5715ab00ca283ef638e73390d82c38
aditya.kumar <aditya1944@live.com>
2021-11-25 11:06:29 +0530

    formatted code according to standard.js

--- Cache/LRUCache.js
+++ Cache/LRUCache.js
@@ -1,10 +1,10 @@
 class LRUCache {
   // LRU Cache to store a given capacity of data
   constructor (capacity) {
-    this.cache = new Map();
-    this.capacity = capacity;
-    this.hits = 0;
-    this.miss = 0;
+    this.cache = new Map()
+    this.capacity = capacity
+    this.hits = 0
+    this.miss = 0
   }
 
   cacheInfo () {
@@ -14,25 +14,25 @@ class LRUCache {
 
   set (key, value) {
     // Sets the value for the input key and if the key exists it updates the existing key
-    if(this.cache.size===this.capacity){
+    if (this.cache.size === this.capacity) {
       // delete oldest key existing in map
-      this.cache.delete(this.cache.keys().next().value);
+      this.cache.delete(this.cache.keys().next().value)
     }
-    this.cache.set(key,value);
+    this.cache.set(key, value)
   }
 
   get (key) {
     // Returns the value for the input key. Returns null if key is not present in cache
-    if(this.cache.has(key)){
-      const value = this.cache.get(key);
+    if (this.cache.has(key)) {
+      const value = this.cache.get(key)
       // refresh the cache to update the order of key
-      this.cache.delete(key);
-      this.cache.set(key,value);
-      this.hits+=1;
-      return value;
-    }else{
-      this.miss+=1;
-      return null;
+      this.cache.delete(key)
+      this.cache.set(key, value)
+      this.hits += 1
+      return value
+    } else {
+      this.miss += 1
+      return null
     }
   }
 }
