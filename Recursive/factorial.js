3540e285c170bd1d4eb290ee2ffbc317bc7d4d04
Yatin <yatinkathuria74@gmail.com>
2021-11-27 00:37:02 +0530

    minor fix

--- Recursive/factorial.js
+++ Recursive/factorial.js
@@ -1,10 +1,9 @@
 /**
  * @function Factorial
  * @description function to find factorial using recursion.
- * @param {Integer} x - The input integer
  * @param {Integer} n - The input integer
- * @return {Integer} - Returns x^n.
- * @see [Pow-Logarithmic](https://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/)
+ * @return {Integer} - Factorial of n.
+ * @see [Factorial](https://en.wikipedia.org/wiki/Factorial)
  * @example 5! = 1*2*3*4*5 = 120
  * @example 2! = 1*2 = 2
  */
