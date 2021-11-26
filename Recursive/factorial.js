bff1fc9d452ef0236ea5b77467a06b09f7d48d35
Yatin <yatinkathuria74@gmail.com>
2021-11-27 00:32:25 +0530

    Add test case

--- Recursive/factorial.js
+++ Recursive/factorial.js
@@ -1,11 +1,19 @@
-// function to find factorial using recursion
-// example :
-//    5! = 1*2*3*4*5 = 120
-//    2! = 1*2 = 2
+/**
+ * @function Factorial
+ * @description function to find factorial using recursion.
+ * @param {Integer} x - The input integer
+ * @param {Integer} n - The input integer
+ * @return {Integer} - Returns x^n.
+ * @see [Pow-Logarithmic](https://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/)
+ * @example 5! = 1*2*3*4*5 = 120
+ * @example 2! = 1*2 = 2
+ */
 
-export const factorial = (n) => {
+const factorial = (n) => {
   if (n === 0) {
     return 1
   }
   return n * factorial(n - 1)
 }
+
+export { factorial }
