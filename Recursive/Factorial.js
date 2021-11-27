211e1d0c324d08b2ad6a5c47976103599aca50ba
Yatin <yatinkathuria74@gmail.com>
2021-11-27 11:55:32 +0530

    rename file

---
+++ Recursive/Factorial.js
@@ -0,0 +1,18 @@
+/**
+ * @function Factorial
+ * @description function to find factorial using recursion.
+ * @param {Integer} n - The input integer
+ * @return {Integer} - Factorial of n.
+ * @see [Factorial](https://en.wikipedia.org/wiki/Factorial)
+ * @example 5! = 1*2*3*4*5 = 120
+ * @example 2! = 1*2 = 2
+ */
+
+const factorial = (n) => {
+  if (n === 0) {
+    return 1
+  }
+  return n * factorial(n - 1)
+}
+
+export { factorial }
