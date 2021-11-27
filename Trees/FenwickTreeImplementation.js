06f1527760a6171fad8b6e6e533de8b6922d5885
Mohit Kumar <itsmohitmk99@gmail.com>
2021-11-28 00:02:24 +0530

    fixes : ISSUE#795

--- Trees/FenwickTreeImplementation.js
+++ Trees/FenwickTreeImplementation.js
@@ -1,29 +1,35 @@
+/*
+ * Author: Mohit Kumar
+ * Fedwick Tree Implementation in JavaScript
+ *  Fedwick Tree Implementation for finding prefix sum.
+*/
+
 function constructFenwickTree(array, n) {
-    for (let i = 1; i <= n; i++) {
-        FeneickTree[i] = 0;
-    }
-    for (let i = 0; i < n; i++) {
-        updateFenwickTree(n, i, array[i]);
-    }
+  for (let i = 1; i <= n; i++) {
+    FeneickTree[i] = 0;
+  }
+  for (let i = 0; i < n; i++) {
+    updateFenwickTree(n, i, array[i]);
+  }
 }
 
 function updateFenwickTree(n, index, value) {
-    index = index + 1;
-    while (index <= n) {
-        FeneickTree[index] += value;
-        index += index & (-index);
-    }
+  index = index + 1;
+  while (index <= n) {
+    FeneickTree[index] += value;
+    index += index & (-index);
+  }
 }
-
+ 
 function getPrefixSum(index) {
-    let currSum = 0;
-    index = index + 1;
-    while (index > 0) {
-        currSum += FeneickTree[index];
-        index -= index & (-index);
-    }
+  let currSum = 0;
+  index = index + 1;
+  while (index > 0) {
+    currSum += FeneickTree[index];
+    index -= index & (-index);
+  }
     
-    return currSum;
+  return currSum;
 }
 
 let MAX_VALUE = 1000;
@@ -34,7 +40,6 @@ let length = array.length;
 constructFenwickTree(array, length);
 
 getPrefixSum(6);
-console.log(getPrefixSum(6))
 array[2] += 6;
 updateFenwickTree(length, 2, 6);
 getPrefixSum(6);
