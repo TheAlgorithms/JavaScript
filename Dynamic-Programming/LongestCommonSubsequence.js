a3e7dd04d9baf16592d6f0567079fb962fe1721c
Leo Toff <lev.taydakov@gmail.com>
2021-12-06 10:17:46 -0800

    Refactors, adds tests and comments to longest common subsequence algorithm

--- Dynamic-Programming/LongestCommonSubsequence.js
+++ Dynamic-Programming/LongestCommonSubsequence.js
@@ -1,32 +1,47 @@
 /*
-    * Given two sequences, find the length of longest subsequence present in both of them.
-    * A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
-    * For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”
+Problem:
+Given two sequences, find the length of longest subsequence present in both of them.
+A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
+For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”
+
+Solution:
+We use recursion with tabular memoization.
+Time complexity: O(M x N)
+Solving each subproblem has a cost of O(1). Again, there are MxN subproblems,
+and so we get a total time complexity of O(MxN).
+Space complexity: O(M x N)
+We need to store the answer for each of the MxN subproblems.
 */
 
-function longestCommonSubsequence (x, y, str1, str2, dp) {
-  if (x === -1 || y === -1) {
-    return 0
-  } else {
+ /**
+  * Finds the length of the longest common subsequence
+  * @param {string} str1 Input string #1
+  * @param {string} str2 Input string #2
+  * @returns {number} Length of the longest common subsequence
+  */
+function longestCommonSubsequence(str1, str2) {
+  const dp = new Array(str1.length + 1).fill(0)
+    .map(() => new Array(str2.length + 1).fill(0))
+
+  function recursive(x, y) {
+    if (x === -1 || y === -1) {
+      return 0
+    }
+
     if (dp[x][y] !== 0) {
       return dp[x][y]
+    }
+
+    if (str1[x] === str2[y]) {
+      dp[x][y] = 1 + recursive(x - 1, y - 1)
+      return dp[x][y]
     } else {
-      if (str1[x] === str2[y]) {
-        dp[x][y] = 1 + longestCommonSubsequence(x - 1, y - 1, str1, str2, dp)
-        return dp[x][y]
-      } else {
-        dp[x][y] = Math.max(longestCommonSubsequence(x - 1, y, str1, str2, dp), longestCommonSubsequence(x, y - 1, str1, str2, dp))
-        return dp[x][y]
-      }
+      dp[x][y] = Math.max(recursive(x - 1, y),recursive(x, y - 1))
+      return dp[x][y]
     }
   }
+
+  return recursive(str1.length - 1, str2.length - 1);
 }
-
-// Example
-
-// const str1 = 'ABCDGH'
-// const str2 = 'AEDFHR'
-// const dp = new Array(str1.length + 1).fill(0).map(x => new Array(str2.length + 1).fill(0))
-// const res = longestCommonSubsequence(str1.length - 1, str2.length - 1, str1, str2, dp)
 
 export { longestCommonSubsequence }
