e7a84a7dcec65926e7c0b4eb45fd2e59dd46b713
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2021-12-09 03:05:50 +0300

    average fn Improved: working with generators, erorr if length is 0, custom selector fn

--- Maths/AverageMean.js
+++ Maths/AverageMean.js
@@ -1,29 +1,26 @@
-'use strict'
 /*
-    author: PatOnTheBack
-    license: GPL-3.0 or later
-
-    Modified from:
-        https://github.com/TheAlgorithms/Python/blob/master/maths/average.py
-
-    This script will find the average (mean) of an array of numbers.
-
     More about mean:
         https://en.wikipedia.org/wiki/Mean
 */
 
-const mean = (nums) => {
-  // This is a function returns average/mean of array
-  let sum = 0
-
-  // This loop sums all values in the 'nums' array using forEach loop
-  nums.forEach(function (current) {
-    sum += current
-  })
-
-  // Divide sum by the length of the 'nums' array.
-  const avg = sum / nums.length
-  return avg
+const mean = (_iterable, _selector = undefined) => {
+  let sum = 0;
+  let length = 0;
+  if (!_selector) {
+    for (let current of _iterable) {
+      sum += current;
+      length++;
+    }
+  }
+  else {
+    for (let current of _iterable) {
+      sum += _selector(current);
+      length++;
+    }
+  }
+  if(length === 0)
+    return undefined;
+  return sum/length;
 }
 
-export { mean }
+export {mean}
--- Maths/test/AverageMean.test.js
+++ Maths/test/AverageMean.test.js
@@ -1,11 +1,46 @@
 import { mean } from '../AverageMean'
 
-describe('Tests for average mean', () => {
-  it('should be a function', () => {
-    expect(typeof mean).toEqual('function')
-  })
-  it('should return the mean of an array of numbers', () => {
-    const meanFunction = mean([1, 2, 4, 5])
-    expect(meanFunction).toBe(3)
-  })
-})
+describe('math average tests', () => {
+  // ----------------------------------------------------------------------
+  test('should return undefined if given empty object', () => {
+    expect(average([])).toBeUndefined();
+  });
+  // ----------------------------------------------------------------------
+  test('should return single valueif given 1 value object', () => {
+    expect(average([1])).toBe(1);
+    expect(average([-1])).toBe(-1);
+    expect(average([0])).toBe(0);
+  });
+  // ----------------------------------------------------------------------
+  test('should return correct average in some arrays', () => {
+    expect(average([1,2])).toBe(1.5);
+    expect(average([9,1,2])).toBe(4);
+    expect(average([-1,11])).toBe(5);
+    expect(average([0,100])).toBe(50);
+    expect(average([100,0])).toBe(50);
+    expect(average([100,20,80,0])).toBe(50);
+  });
+  // ----------------------------------------------------------------------
+  test('should return undefined if given empty generator', () => {
+    const src = function*(){}
+    expect(average(src())).toBeUndefined();
+  });
+  // ----------------------------------------------------------------------
+  test('should return correct average in generator function', () => {
+    const src = function*(){
+      yield 1;
+      yield -1;
+      yield 0;
+    }
+    expect(average(src())).toBe(0);
+  });
+  // ----------------------------------------------------------------------
+  test('should return correct average in generator function with custom selector function', () => {
+    const src = function*(){
+      yield "abc";
+      yield "de";
+      yield "qwertyq";
+    }
+    expect(average(src(),_x=>_x.length)).toBe(4);
+  });
+});
