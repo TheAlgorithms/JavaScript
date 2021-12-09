32565ede6039ad02728e5767b2dd63412d2e10e4
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2021-12-09 03:09:43 +0300

    average

--- Maths/test/AverageMean.test.js
+++ Maths/test/AverageMean.test.js
@@ -3,27 +3,27 @@ import { mean } from '../AverageMean'
 describe('math average tests', () => {
   // ----------------------------------------------------------------------
   test('should return undefined if given empty object', () => {
-    expect(average([])).toBeUndefined();
+    expect(mean([])).toBeUndefined();
   });
   // ----------------------------------------------------------------------
   test('should return single valueif given 1 value object', () => {
-    expect(average([1])).toBe(1);
-    expect(average([-1])).toBe(-1);
-    expect(average([0])).toBe(0);
+    expect(mean([1])).toBe(1);
+    expect(mean([-1])).toBe(-1);
+    expect(mean([0])).toBe(0);
   });
   // ----------------------------------------------------------------------
   test('should return correct average in some arrays', () => {
-    expect(average([1,2])).toBe(1.5);
-    expect(average([9,1,2])).toBe(4);
-    expect(average([-1,11])).toBe(5);
-    expect(average([0,100])).toBe(50);
-    expect(average([100,0])).toBe(50);
-    expect(average([100,20,80,0])).toBe(50);
+    expect(mean([1,2])).toBe(1.5);
+    expect(mean([9,1,2])).toBe(4);
+    expect(mean([-1,11])).toBe(5);
+    expect(mean([0,100])).toBe(50);
+    expect(mean([100,0])).toBe(50);
+    expect(mean([100,20,80,0])).toBe(50);
   });
   // ----------------------------------------------------------------------
   test('should return undefined if given empty generator', () => {
     const src = function*(){}
-    expect(average(src())).toBeUndefined();
+    expect(mean(src())).toBeUndefined();
   });
   // ----------------------------------------------------------------------
   test('should return correct average in generator function', () => {
@@ -32,7 +32,7 @@ describe('math average tests', () => {
       yield -1;
       yield 0;
     }
-    expect(average(src())).toBe(0);
+    expect(mean(src())).toBe(0);
   });
   // ----------------------------------------------------------------------
   test('should return correct average in generator function with custom selector function', () => {
@@ -41,6 +41,6 @@ describe('math average tests', () => {
       yield "de";
       yield "qwertyq";
     }
-    expect(average(src(),_x=>_x.length)).toBe(4);
+    expect(mean(src(),_x=>_x.length)).toBe(4);
   });
 });
