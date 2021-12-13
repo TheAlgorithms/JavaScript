0d7e894c331f23e1ed12618ac5dfaac51a42c802
Yatin <yatinkathuria74@gmail.com>
2021-12-13 23:34:00 +0530

    Improve Mean method

--- Maths/test/AverageMean.test.js
+++ Maths/test/AverageMean.test.js
@@ -4,8 +4,18 @@ describe('Tests for average mean', () => {
   it('should be a function', () => {
     expect(typeof mean).toEqual('function')
   })
+
+  it('should throw error for invalid input', () => {
+    expect(() => mean(123)).toThrow()
+  })
+
   it('should return the mean of an array of numbers', () => {
     const meanFunction = mean([1, 2, 4, 5])
     expect(meanFunction).toBe(3)
+  })
+
+  it('should return the mean of an array of numbers', () => {
+    const meanFunction = mean([10, 40, 100, 20])
+    expect(meanFunction).toBe(42.5)
   })
 })
