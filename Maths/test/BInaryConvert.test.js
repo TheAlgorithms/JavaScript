b3502993ad60e4e23256bbeb60a62f56c983e64d
Yatin <yatinkathuria74@gmail.com>
2021-12-13 23:18:37 +0530

    add more test cases

--- Maths/test/BInaryConvert.test.js
+++ Maths/test/BInaryConvert.test.js
@@ -2,9 +2,21 @@ import { BinaryConvert } from '../BinaryConvert'
 
 describe('BinaryConvert', () => {
   it('should return the correct value', () => {
+    expect(BinaryConvert(4)).toBe(100)
+  })
+  it('should return the correct value', () => {
     expect(BinaryConvert(12)).toBe(1100)
   })
   it('should return the correct value of the sum from two number', () => {
     expect(BinaryConvert(12 + 2)).toBe(1110)
+  })
+  it('should return the correct value of the subtract from two number', () => {
+    expect(BinaryConvert(245 - 56)).toBe(10111101)
+  })
+  it('should return the correct value', () => {
+    expect(BinaryConvert(254)).toBe(11111110)
+  })
+  it('should return the correct value', () => {
+    expect(BinaryConvert(63483)).toBe(1111011111111011)
   })
 })
