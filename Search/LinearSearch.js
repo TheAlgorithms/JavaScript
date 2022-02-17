05a156dadc56a8fd8a7272e25fbb531cab52062f
Md. Masud Rana <mr.hasib45@gmail.com>
2022-02-17 23:42:32 +0600

    Update the linear search example

--- Search/LinearSearch.js
+++ Search/LinearSearch.js
@@ -27,3 +27,16 @@ export { SearchArray, Search }
 // SearchArray(3, ar)
 // SearchArray(4, ar)
 // SearchArray(11, ar)
+
+//Linear search Another example of a linear search 
+const linearSearch = (arr, val) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  const notFound = "Not Found";
+  return notFound;
+};
+const result = linearSearch([1, 2, 3, 4, 5], 10);
+// console.log(result);
