743b3179f798905f937af554d4a3d9a41f1b20a1
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-02-21 06:58:40 -0400

    merge: Corrected JSdoc typo (#898)
    The URL in the Wikipedia link had an extra "h" in the URI Scheme (the part that specifies the protocol, usually `https`). I removed the duplicate "h" and made it into a single "h", now the link works

--- Maths/AverageMean.js
+++ Maths/AverageMean.js
@@ -3,7 +3,7 @@
  * @description This script will find the mean value of a array of numbers.
  * @param {Integer[]} nums - Array of integer
  * @return {Integer} - mean of nums.
- * @see [Mean](hhttps://en.wikipedia.org/wiki/Mean)
+ * @see [Mean](https://en.wikipedia.org/wiki/Mean)
  * @example mean([1, 2, 4, 5]) = 3
  * @example mean([10, 40, 100, 20]) = 42.5
  */
