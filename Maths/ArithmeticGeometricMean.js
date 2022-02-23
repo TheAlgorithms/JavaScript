1762ac93437159e135c03b2d9a03e94418f3587a
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-02-23 18:13:59 -0400

    Patched bugs

--- Maths/ArithmeticGeometricMean.js
+++ Maths/ArithmeticGeometricMean.js
@@ -9,6 +9,8 @@
 
  export const agm = (a, g) => {
   if (a === g) return a; //avoid rounding errors, and increase efficiency
+  if (a === Infinity && g === 0) return NaN;
+  if (Object.is(a, -0) && !Object.is(g, -0)) return 0;
   let x; //temp var
   do {
     [a, g, x] = [(a + g) / 2, Math.sqrt(a * g), a]
