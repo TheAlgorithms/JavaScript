769a3889fff2e46dcfd4746226300a498b0a0dba
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-23 14:31:02 +0600

    pref: optimize the algo via reduce & replace method

--- String/CheckAnagram.js
+++ String/CheckAnagram.js
@@ -12,36 +12,10 @@ const checkAnagram = (str1, str2) => {
     return false
   }
 
-  // Use hashmap to keep count of characters in str1
-
-  const str1CharCount = new Map()
-
-  for (let i = 0; i < str1.length; i++) {
-    let previousCount = 0
-    if (str1CharCount.has(str1[i])) {
-      previousCount = str1CharCount.get(str1[i])
-    }
-    str1CharCount.set(str1[i], previousCount + 1)
-  }
-
-  // Now check if second string has same characters?
-
-  for (let i = 0; i < str2.length; i++) {
-    let previousCount = 0
-    // if str1CharCount has no key for str2[i] then not anagram.
-    if (!str1CharCount.has(str2[i])) return false
-
-    previousCount = str1CharCount.get(str2[i])
-    str1CharCount.set(str2[i], previousCount - 1)
-  }
-
-  // Now check if all entries in hashmap has zeros.
-
-  for (const key in str1CharCount) {
-    if (str1CharCount[key] !== 0) return false
-  }
-
-  return true
+  return ![...str1].reduce(
+    (str2Acc, cur) => str2Acc.replace(cur, ''), // remove the similar letter from str2Acc
+    str2
+  )
 }
 
 export { checkAnagram }
