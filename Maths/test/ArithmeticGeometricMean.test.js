6ab3f4bb7ad7a93e961aae21433c83d654fd2fc3
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-02-22 23:06:44 -0400

    Finally added the test script for AGM

---
+++ Maths/test/ArithmeticGeometricMean.test.js
@@ -0,0 +1,54 @@
+import { agm } from '../ArithmeticGeometricMean.js'
+
+describe('Tests for AGM', () => {
+  it('should be a function', () => {
+    expect(typeof agm).toEqual('function')
+  })
+
+  it('number of parameters should be 2', () => {
+    expect(agm.length).toEqual(2)
+  })
+
+  const m = 0x100; //scale for rand
+
+  it('should return NaN if any or all params has a negative argument', () => {
+    //I multiplied by minus one, because the sign inversion is more clearly visible
+    expect(agm(-1 * Math.random() * m, Math.random() * m)).toBe(NaN)
+    expect(agm(Math.random() * m, -1 * Math.random() * m)).toBe(NaN)
+    expect(agm(-1 * Math.random() * m, -1 * Math.random() * m)).toBe(NaN)
+  })
+
+  it('should return Infinity if any arg is Infinity and the other is not 0', () => {
+    expect(agm(Math.random() * m + 1, Infinity)).toEqual(Infinity)
+    expect(agm(Infinity, Math.random() * m + 1)).toEqual(Infinity)
+    expect(agm(Infinity, Infinity).toEqual(Infinity))
+  })
+
+  it('should return NaN if some arg is Infinity and the other is 0', () => {
+    expect(agm(0, Infinity).toBe(NaN))
+    expect(agm(Infinity, 0).toBe(NaN))
+  })
+
+  it('should return +0 if any or all args are +0 or -0, and return -0 if all are -0', () => {
+    expect(agm(Math.random() * m, -0)).toBe(0)
+    expect(agm(0, Math.random() * m)).toBe(0)
+    expect(agm(0, -0).toBe(0))
+    expect(agm(-0, 0).toBe(0))
+    expect(agm(-0, -0).toBe(-0))
+  })
+
+  it('should return NaN if any or all args are NaN', () => {
+    expect(agm(Math.random() * m, NaN)).toBe(NaN)
+    expect(agm(NaN, Math.random() * m)).toBe(NaN)
+    expect(agm(NaN, NaN).toBe(NaN))
+  })
+
+  it('should return an accurate approximation of the AGM between 2 valid input args', () => {
+    //all the constants are provided by WolframAlpha
+    expect(agm(1, 2)).toBeCloseTo(1.4567910310469068691864323832650819749738639432213055907941723832)
+    expect(agm(2, 256)).toBeCloseTo(64.4594071943866695986665434983250898480227029006463800294306182)
+    expect(agm(55555, 34)).toBeCloseTo(9933.40472395519953051873484897963370925448369441581220656590)
+    //test "unsafe" numbers
+    expect(agm(2 ** 48, 3 ** 27).toBeCloseTo(88506556379265.712723873253375677194780))
+  })
+})
