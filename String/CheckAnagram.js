5d097a51416158388a9f91f9ed495cb0c726a2d1
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-24 21:02:04 +0600

    docs: rename function name & add comments

--- String/CheckAnagram.js
+++ String/CheckAnagram.js
@@ -1,13 +1,13 @@
 /**
- * @function checkAnagram
+ * @function checkAnagramViaRegex
  * @param {string} str1
  * @param {string} str2
  * @returns {boolean}
  * @description An [Anagram](https://en.wikipedia.org/wiki/Anagram) is a string that is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. Anagram check is not case-sensitive;
- * @example - checkAnagram('node', 'deno') => true
- * @example - checkAnagram('Eleven plus two', 'Twelve plus one') => true
+ * @example - checkAnagramViaRegex('node', 'deno') => true
+ * @example - checkAnagramViaRegex('Eleven plus two', 'Twelve plus one') => true
  */
-const checkAnagram = (str1, str2) => {
+const checkAnagramViaRegex = (str1, str2) => {
   // check that inputs are strings.
   if (typeof str1 !== 'string' || typeof str2 !== 'string') {
     throw new TypeError('Arguments should be string both')
@@ -18,11 +18,15 @@ const checkAnagram = (str1, str2) => {
     return false
   }
 
-  // str1 converted to array and traverse the each letter of str1 by reduce method
+  /**
+   * str1 converted to an array and traverse each letter of str1 by reduce method
+   * reduce method return string which is empty or not.
+   * if it returns empty string '' -> falsy, with Logical !(NOT) Operator, it's will be converted to boolean and return true else false
+   */
   return ![...str1].reduce(
     (str2Acc, cur) => str2Acc.replace(new RegExp(cur, 'i'), ''), // remove the similar letter from str2Acc in case-insensitive
     str2
   )
 }
 
-export { checkAnagram }
+export { checkAnagramViaRegex }
