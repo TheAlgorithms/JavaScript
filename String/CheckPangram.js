21a49a47457969c19070b1766db70267a0abcd51
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-26 20:39:37 +0600

    feat: used regex instead of Set

--- String/CheckPangram.js
+++ String/CheckPangram.js
@@ -8,15 +8,7 @@ const checkPangram = (string) => {
     throw new TypeError('The given value is not a string')
   }
 
-  const frequency = new Set()
-
-  for (const letter of string.toLowerCase()) {
-    if (letter >= 'a' && letter <= 'z') {
-      frequency.add(letter)
-    }
-  }
-
-  return frequency.size === 26
+  return string.match(/([a-z])(?!.*\1)/gi).length === 26
 }
 
 export { checkPangram }
