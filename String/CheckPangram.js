b5727b8693c059e77a994e6b178de740cd12c21b
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-27 12:43:56 +0600

    docs: add js doc

--- String/CheckPangram.js
+++ String/CheckPangram.js
@@ -1,8 +1,9 @@
-/*
-  Pangram is a sentence that contains all the letters in the alphabet
-  https://en.wikipedia.org/wiki/Pangram
+/**
+ * @function checkPangram
+ * @description - Pangram is a sentence that contains all the letters in the alphabet https://en.wikipedia.org/wiki/Pangram
+ * @param {string} string 
+ * @returns {boolean}
  */
-
 const checkPangram = (string) => {
   if (typeof string !== 'string') {
     throw new TypeError('The given value is not a string')
