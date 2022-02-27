42cc71e0e7a9fc54d418372e641bcb513d6c952f
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-27 15:47:57 +0600

    docs: add details explanation of pangram regex

--- String/CheckPangram.js
+++ String/CheckPangram.js
@@ -10,12 +10,13 @@ const checkPangram = (string) => {
   }
 
   /**
-   * match all 26 alphabets using regex, with the help of:
-   * Capturing Group
-   * Character set
-   * Negative lookahead
-   * Dot & star
-   * Numeric reference
+   * Match all 26 alphabets using regex, with the help of:
+   * Capturing group - () -> Groups multiple tokens together and creates a capture group for extracting a substring or using a backreference.
+   * Character set - [a-z] -> Matches a char in the range a to z in case-insensitive for the 'i' flag
+   * Negative lookahead - (?!) -> Specifies a group that can not match after the main expression (if it matches, the result is discarded).
+   * Dot - . -> Matches any character except linebreaks. Equivalent to 
+   * Star - * -> Matches 0 or more of the preceding token.
+   * Numeric reference - \{$n} -> Matches the results of a capture group. E.g. - \1  matches the results of the first capture group & \3 matches the third.
  */
   return string.match(/([a-z])(?!.*\1)/gi).length === 26
 }
