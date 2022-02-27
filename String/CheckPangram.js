4c7fdbe40e4b35551aca27b481d6f63d8826509b
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-27 16:11:03 +0600

    style: format with standardJs

--- String/CheckPangram.js
+++ String/CheckPangram.js
@@ -1,5 +1,5 @@
 /**
- * What is Pangram? 
+ * What is Pangram?
  * Ans: Pangram is a sentence that contains all the letters in the alphabet https://en.wikipedia.org/wiki/Pangram
  */
 
@@ -21,18 +21,17 @@ const checkPangramRegex = (string) => {
    * Capturing group - () -> Groups multiple tokens together and creates a capture group for extracting a substring or using a backreference.
    * Character set - [a-z] -> Matches a char in the range a to z in case-insensitive for the 'i' flag
    * Negative lookahead - (?!) -> Specifies a group that can not match after the main expression (if it matches, the result is discarded).
-   * Dot - . -> Matches any character except linebreaks. Equivalent to 
+   * Dot - . -> Matches any character except linebreaks. Equivalent to
    * Star - * -> Matches 0 or more of the preceding token.
    * Numeric reference - \{$n} -> Matches the results of a capture group. E.g. - \1  matches the results of the first capture group & \3 matches the third.
  */
   return string.match(/([a-z])(?!.*\1)/gi).length === 26
 }
 
-
 /**
  * @function checkPangramSet
  * @description - This function detect the pangram sentence by HashSet
- * @param {string} string 
+ * @param {string} string
  * @returns {boolean}
  */
 const checkPangramSet = (string) => {
@@ -52,4 +51,4 @@ const checkPangramSet = (string) => {
   return lettersSet.size === 26
 }
 
-export { checkPangramRegex, checkPangramSet}
+export { checkPangramRegex, checkPangramSet }
