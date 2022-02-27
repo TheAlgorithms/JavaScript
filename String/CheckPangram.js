cd448cad8b2e67d8f1eb97785e5d47c834cf2b99
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-27 12:45:51 +0600

    docs: add comments of workable code

--- String/CheckPangram.js
+++ String/CheckPangram.js
@@ -9,6 +9,14 @@ const checkPangram = (string) => {
     throw new TypeError('The given value is not a string')
   }
 
+  /**
+   * match all 26 alphabets using regex, with the help of: 
+   * Capturing Group
+   * Character set
+   * Negative lookahead
+   * Dot & star
+   * Numeric reference
+ */
   return string.match(/([a-z])(?!.*\1)/gi).length === 26
 }
 
