b593735302b193ef022231b6c0f90b79b39bc87b
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-03-02 07:17:51 +0600

    docs: add js doc for powOn function

--- Maths/Pow.js
+++ Maths/Pow.js
@@ -1,3 +1,12 @@
+/**
+ * @function powOn
+ * @description - The powOn function is a power function with Linear O(n) complexity
+ * @param {number} base 
+ * @param {number} power 
+ * @returns {number}
+ * @example - powOn(2, 2) => 4 --> 2 * 2
+ * @example - powOn(3, 3) => 27 --> 3 * 3 *
+ */
 const powOn = (base, power) => {
   if (power < 0) {
     base = 1 / base
