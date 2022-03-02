40db24eb268c652109101bb538d216f4968c5b72
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-03-02 07:56:00 +0600

    chore: rename to exponent

--- Maths/Pow.js
+++ Maths/Pow.js
@@ -2,20 +2,20 @@
  * @function powLinear
  * @description - The powLinear function is a power function with Linear O(n) complexity
  * @param {number} base 
- * @param {number} power 
+ * @param {number} exponent 
  * @returns {number}
  * @example - powLinear(2, 2) => 4 --> 2 * 2
  * @example - powLinear(3, 3) => 27 --> 3 * 3
  */
-const powLinear = (base, power) => {
-  if (power < 0) {
+const powLinear = (base, exponent) => {
+  if (exponent < 0) {
     base = 1 / base
-    power = -power
+    exponent = -exponent
   }
   
   let result = 1
 
-  while (power--) { // Break the execution while the power will 0 
+  while (exponent--) { // Break the execution while the exponent will 0 
     result *= base
   }
 
@@ -26,29 +26,29 @@ const powLinear = (base, power) => {
  * @function powFaster
  * @description - The powFaster function is a power function with Linear O(n) complexity
  * @param {number} base 
- * @param {number} power 
+ * @param {number} exponent 
  * @returns {number}
  * @example - powFaster(2, 2) => 4 --> 2 * 2
  * @example - powFaster(3, 3) => 27 --> 3 * 3
  */
-const powFaster = (base, power) => {
-  if (power < 2) { // explanation below - 1
-    return base && ([1, base][power] || powFaster(1 / base, -power))
+const powFaster = (base, exponent) => {
+  if (exponent < 2) { // explanation below - 1
+    return base && ([1, base][exponent] || powFaster(1 / base, -exponent))
   }
 
-  if (power & 1) { // if the existing power is odd
-    return base * powFaster(base * base, power >> 1) // explanation below - 2
+  if (exponent & 1) { // if the existing exponent is odd
+    return base * powFaster(base * base, exponent >> 1) // explanation below - 2
   }
 
-  return powFaster(base * base, power / 2)
+  return powFaster(base * base, exponent / 2)
 }
 
 /**
  * 1 - Magic of short circuit evaluation (&&, ||)
  * if the base is 0 then it returns 0 cause 0 is falsy
  * if the base is not 0 then it's must be truthy. after that, it will be executed the right portion of the && (AND) operator
- * Now it checks the power by the help array index, is it 0 or 1.
- * if the power is not 0 or 1 it's definitely less than 0, and a negative number is not a valid index number so it returns "undefined"
+ * Now it checks the exponent by the help array index, is it 0 or 1.
+ * if the exponent is not 0 or 1 it's definitely less than 0, and a negative number is not a valid index number so it returns "undefined"
  * if the expression is undefined mean -> falsy, the || (OR) operator evaluates the right portion that is a recursive function.
  */
 
