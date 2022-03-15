0924f1c8c9eb52e9a241cdd7c0c1138d83b16b48
Fahim Faisaal <57553028+fahimfaisaal@users.noreply.github.com>
2022-03-15 18:11:50 +0600

    merge: Improved `abs` function (#923)
    * feat: added validation, test codes
    
    * chore: remove useless words

--- Maths/test/Abs.test.js
+++ Maths/test/Abs.test.js
@@ -1,18 +1,39 @@
-import { absVal } from '../Abs'
+import { abs } from '../Abs'
 
-describe('absVal', () => {
+describe('Testing abs function', () => {
+  it('Testing for invalid types', () => {
+    expect(() => abs('234a')).toThrow()
+    expect(() => abs({})).toThrow()
+    expect(() => abs([12, -32, -60])).toThrow()
+  })
+
+  it('Testing for number of string type', () => {
+    expect(abs('-345')).toBe(345)
+    expect(abs('-345.455645')).toBe(345.455645)
+  })
+
+  it('Testing for a boolean type', () => {
+    expect(abs(true)).toBe(1)
+    expect(abs(false)).toBe(0)
+  })
+
   it('should return an absolute value of a negative number', () => {
-    const absOfNegativeNumber = absVal(-34)
+    const absOfNegativeNumber = abs(-34)
     expect(absOfNegativeNumber).toBe(34)
   })
 
   it('should return an absolute value of a positive number', () => {
-    const absOfPositiveNumber = absVal(50)
+    const absOfPositiveNumber = abs(50)
     expect(absOfPositiveNumber).toBe(50)
   })
 
   it('should return an absolute value of a zero number', () => {
-    const absOfPositiveNumber = absVal(0)
+    const absOfPositiveNumber = abs(0)
     expect(absOfPositiveNumber).toBe(0)
+  })
+
+  it('should return an absolute value of any floating number', () => {
+    const absOfPositiveNumber = abs(-20.2034)
+    expect(absOfPositiveNumber).toBe(20.2034)
   })
 })
