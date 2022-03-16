de1522d5a273ef2aa166b8a4f6fc78192674ab6d
TrasherDK <trasherdk@fumlersoft.dk>
2022-03-16 12:52:02 +0100

    Remove these separator comments.

--- Maths/test/FindMinIterator.test.js
+++ Maths/test/FindMinIterator.test.js
@@ -1,17 +1,16 @@
 import { FindMinIterator } from '../FindMinIterator'
 
 describe('FindMinIterator', () => {
-  // ----------------------------------------------------------------------
   test('given empty array then min is undefined', () => {
     expect(FindMinIterator([])).toBeUndefined()
   })
-  // ----------------------------------------------------------------------
+
   test('given single value array then min is found', () => {
     expect(FindMinIterator([1])).toBe(1)
     expect(FindMinIterator([-1])).toBe(-1)
     expect(FindMinIterator([0])).toBe(0)
   })
-  // ----------------------------------------------------------------------
+
   test('given array then min is found', () => {
     expect(FindMinIterator([1, 2])).toBe(1)
     expect(FindMinIterator([-1, 10])).toBe(-1)
@@ -19,12 +18,12 @@ describe('FindMinIterator', () => {
     expect(FindMinIterator([100, 0])).toBe(0)
     expect(FindMinIterator([100, 50, 20, 0, -100, 0, 2, 30, 45, 99, 104, 23])).toBe(-100)
   })
-  // ----------------------------------------------------------------------
+
   test('given empty generator then min is undefined', () => {
     const src = function* () { } // eslint-disable-line
     expect(FindMinIterator(src())).toBeUndefined()
   })
-  // ----------------------------------------------------------------------
+
   test('given generator then min is found', () => {
     const src = function* () { // eslint-disable-line
       yield 1
@@ -33,7 +32,7 @@ describe('FindMinIterator', () => {
     }
     expect(FindMinIterator(src())).toBe(-1)
   })
-  // ----------------------------------------------------------------------
+
   test('given string generator then min string length is found', () => {
     const src = function* () { // eslint-disable-line
       yield 'abc'
@@ -42,7 +41,7 @@ describe('FindMinIterator', () => {
     }
     expect(FindMinIterator(src(), _x => _x.length)).toBe(2)
   })
-  // ----------------------------------------------------------------------
+
   test('given array of objects then min accessor is found', () => {
     const array = [
       { name: 'Item #1', price: 1.0 },
