5136f7dbb4b65de6913af7d838401b6acf053911
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2022-03-18 16:58:22 +0600

    chore: move union function

--- Cache/test/TesterFunction.js
+++ Cache/test/TesterFunction.js
@@ -1,19 +1,37 @@
-export const fibonacciCache = (num, cache = null) => {
+/**
+ * @function fibonacciCache
+ * @description - this is a cached variant of fib number
+ * @param {number} n  - Real number (n > -1)
+ * @param {Object} cache
+ * @returns {number}
+ */
+export const fibonacciCache = (n, cache = null) => {
   if (cache) {
-    const value = cache.get(num)
+    const value = cache.get(n)
 
     if (value !== null) {
       return value
     }
   }
 
-  if (num === 1 || num === 2) {
+  if (n === 1 || n === 2) {
     return 1
   }
 
-  const result = fibonacciCache(num - 1, cache) + fibonacciCache(num - 2, cache)
+  const result = fibonacciCache(n - 1, cache) + fibonacciCache(n - 2, cache)
 
-  cache && cache.set(num, result)
+  cache && cache.set(n, result)
 
   return result
+}
+
+/**
+ * @title implementation of union function
+ * @param {Set} sets
+ * @return {new Set}
+ */
+export const union = (...sets) => {
+  return new Set(
+    sets.reduce((flatArray, set) => [...flatArray, ...set], [])
+  )
 }
