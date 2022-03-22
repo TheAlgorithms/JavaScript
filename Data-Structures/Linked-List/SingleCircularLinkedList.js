b9cd7cdac6b595ded29165ade77f27896c875b58
Abdullah Mortada <abdullahmortada@gmail.com>
2022-03-22 11:17:06 +0200

    Made comment styles more consistent

--- Data-Structures/Linked-List/SingleCircularLinkedList.js
+++ Data-Structures/Linked-List/SingleCircularLinkedList.js
@@ -1,4 +1,4 @@
-//Defining Node class
+// Defining Node class
 class Node {
   constructor (data, next = null) {
     this.data = data
@@ -11,7 +11,7 @@ class SinglyCircularLinkedList {
     this.head = null
     this.size = 0
   }
-  //Constructor, starts list with an empty head node and size 0
+  // Constructor, starts list with an empty head node and size 0
   insert (data) {
     const node = new Node(data)
 
@@ -19,7 +19,7 @@ class SinglyCircularLinkedList {
       node.next = node
       this.head = node
       this.size++
-      //If list is empty, head is set to new node with data, and size is incremented
+      // If list is empty, head is set to new node with data, and size is incremented
     } else {
       node.next = this.head
 
@@ -31,8 +31,8 @@ class SinglyCircularLinkedList {
 
       current.next = node
       this.size++
-      /*If list is not empty, make the new node point at the head, 
-      and loop until you reach the last node, and make it point at the new node*/
+      // If list is not empty, make the new node point at the head, 
+      // and loop until you reach the last node, and make it point at the new node
     }
   }
 
@@ -40,13 +40,13 @@ class SinglyCircularLinkedList {
     const node = new Node(data)
 
     if (index < 0 || index > this.size) return
-    //If index is invalid, return
+    // If index is invalid, return
     if (index === 0) {
       this.head = node
       this.size = 1
       return
     }
-    //If index is 0, set the new node as the head of the list
+    // If index is 0, set the new node as the head of the list
 
     let previous
     let count = 0
@@ -57,16 +57,16 @@ class SinglyCircularLinkedList {
       current = current.next
       count++
     }
-    //Loop over the nodes until required index is reached
+    // Loop over the nodes until required index is reached
     node.next = current
     previous.next = node
     this.size++
-    //Insert the node
+    // Insert the node
   }
 
   remove () {
     if (!this.head) return
-    //If list is empty, return
+    // If list is empty, return
     let prev
     let current = this.head
 
@@ -74,10 +74,10 @@ class SinglyCircularLinkedList {
       prev = current
       current = current.next
     }
-    //Loop until the last node is reached
+    // Loop until the last node is reached
     prev.next = this.head
     this.size--
-    //Unlink the last node and decrement size
+    // Unlink the last node and decrement size
   }
 
   printData (output = value => console.log(value)) {
