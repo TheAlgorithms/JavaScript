9be30a8f5b9a742b3ddeabe86274d2d68504a1d2
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-03-28 12:28:07 +0600

    feat: added `reduceRight` & `trim` method

--- String/test/ReverseWords.test.js
+++ String/test/ReverseWords.test.js
@@ -1,12 +1,6 @@
-import { reverseWords } from '../ReverseWords'
+import reverseWords from '../ReverseWords'
 
-describe('reverseWords', () => {
-  it('expects to reverse words to return a joined word', () => {
-    expect(reverseWords('I Love JS')).toBe('JS Love I')
-    expect(reverseWords('Hello World')).toBe('World Hello')
-    expect(reverseWords('The Algorithms Javascript')).toBe('Javascript Algorithms The')
-  })
-
+describe('Testing the reverseWords function', () => {
   it.each`
     input
     ${123456}
@@ -21,4 +15,10 @@ describe('reverseWords', () => {
       }).toThrow('The given value is not a string')
     }
   )
+
+  it('expects to reverse words to return a joined word', () => {
+    expect(reverseWords('I Love JS')).toBe('JS Love I')
+    expect(reverseWords('Hello World')).toBe('World Hello')
+    expect(reverseWords('The Algorithms Javascript')).toBe('Javascript Algorithms The')
+  })
 })
