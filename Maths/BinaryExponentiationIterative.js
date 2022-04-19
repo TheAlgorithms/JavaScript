123e2c1993cf2517578817fce782d7a3997e32c9
CarlosZoft <carlosk2gunn@hotmail.com>
2022-04-19 15:58:30 -0300

    fix: improving code readability

--- Maths/BinaryExponentiationIterative.js
+++ Maths/BinaryExponentiationIterative.js
@@ -14,11 +14,10 @@
 const exponent = (x, n) => {
   let answer = 1
   while (n > 0) {
-    if (n % 2 !== 0) answer *= x
+    if (n % 2) answer *= x
     n = Math.floor(n / 2)
     if (n > 0) x *= x
   }
   return answer
 }
-
 export { exponent }
