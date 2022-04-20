ea67fc62382a05f3ef6d5fe6d31a09ad1c415a6d
CarlosZoft <carlosk2gunn@hotmail.com>
2022-04-20 17:00:07 -0300

    fix: refactoring duplicated code of tests

--- Search/test/BinarySearch.test.js
+++ Search/test/BinarySearch.test.js
@@ -30,32 +30,20 @@ const stringArr = [
   'Zulu'
 ]
 
-describe('Binary Search Iterative test', () => {
-  test('expect to return the index of the item in the array', () => {
-    expect(binarySearchIterative(arr, 3)).toBe(2)
-  })
-  test('expect to return -1 if not in array', () => {
-    expect(binarySearchIterative(arr, 11)).toBe(-1)
-  })
-  test('expect to return the index of the item in the array', () => {
-    expect(binarySearchIterative(stringArr, 'Charlie')).toBe(2)
-  })
-  test('expect to return -1 if not in array', () => {
-    expect(binarySearchIterative(stringArr, 'Zoft')).toBe(-1)
-  })
-})
-
-describe('Binary Search Recursive test', () => {
-  test('expect to return the index of the item in the array', () => {
-    expect(binarySearchRecursive(arr, 3)).toBe(2)
-  })
-  test('expect to return -1 if not in array', () => {
-    expect(binarySearchRecursive(arr, 11)).toBe(-1)
-  })
-  test('expect to return the index of the item in the array', () => {
-    expect(binarySearchRecursive(stringArr, 'Charlie')).toBe(2)
-  })
-  test('expect to return -1 if not in array', () => {
-    expect(binarySearchRecursive(stringArr, 'Zoft')).toBe(-1)
-  })
+describe('Binary Search', () => {
+  const funcs = [binarySearchIterative, binarySearchRecursive]
+  for (const func of funcs) {
+    test('expect to return the index of the item in the array', () => {
+      expect(func(arr, 3)).toBe(2)
+    })
+    test('expect to return -1 if not in array', () => {
+      expect(func(arr, 11)).toBe(-1)
+    })
+    test('expect to return the index of the item in the array', () => {
+      expect(func(stringArr, 'Charlie')).toBe(2)
+    })
+    test('expect to return -1 if not in array', () => {
+      expect(func(stringArr, 'Zoft')).toBe(-1)
+    })
+  }
 })
