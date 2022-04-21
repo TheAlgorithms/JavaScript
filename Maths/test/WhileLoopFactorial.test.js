acce146169e5b484740cde1f08df0021bdbdbee7
merelymyself <88221256+merelymyself@users.noreply.github.com>
2022-04-21 19:01:36 +0800

    Update WhileLoopFactorial.test.js

--- Maths/test/WhileLoopFactorial.test.js
+++ Maths/test/WhileLoopFactorial.test.js
@@ -1,21 +1,17 @@
 import { factorialize } from '../WhileLoopFactorial'
 
 test('Testing on 3!', () => {
-  const three_factorial = factorialize(3)
-  expect(three_factorial).toBe(6)
+  expect(factorialize(3)).toBe(6)
 })
 
 test('Testing on 7!', () => {
-  const seven_factorial = factorialize(7)
-  expect(seven_factorial).toBe(5040)
+  expect(factorialize(7)).toBe(5040)
 })
 
 test('Testing on 0!', () => {
-  const zero_factorial = factorialize(0)
-  expect(zero_factorial).toBe(1)
+  expect(factorialize(0)).toBe(1)
 })
 
 test('Testing on 12!', () => {
-  const twelve_factorial = factorialize(12)
-  expect(twelve_factorial).toBe(479001600)
+  expect(factorialize(12)).toBe(479001600)
 })
