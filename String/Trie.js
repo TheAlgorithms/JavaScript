a13bf472201682d1aab1fb730d51f537c263dcbc
Ayoade David <adewoleayoade057@gmail.com>
2022-04-29 02:12:34 +0100

    Update Trie.js
    Codestyle fix

--- String/Trie.js
+++ String/Trie.js
@@ -4,14 +4,14 @@ It is implemented using a nested object where each level has its direct children
 A trie node can be formed by using an object to store the children.
 The trie has a root node that is instantiated in the constructor of the Trie class, as shown.
 */
-const TrieNode = function(){
+const TrieNode = function () {
   this.children = {}
   this.endOfWord = false
 }
-const Trie = function(){
+const Trie = function () {
   this.root = new TrieNode()
 }
-Trie.prototype.insert = function(word) {
+Trie.prototype.insert = function (word) {
   let current = this.root
   for (let i = 0; i < word.length; i++) {
     const ch = word.charAt(i)
@@ -29,11 +29,11 @@ To search inside a trie, each character of the word must be checked.
 This is done by setting a temporary variable of current on the root.
 The current variable is updated as each character in the word is checked.
 */
-Trie.prototype.search = function(word) {
+Trie.prototype.search = function (word) {
   let current = this.root
   for (let i = 0; i < word.length; i++) {
     const ch = word.charAt(i)
-    let node = current.children[ch]
+    const node = current.children[ch]
     if (node === null) {
       return false // node doesn't exist
     }
@@ -46,17 +46,17 @@ To delete an element from a trie, the algorithm should traverse the root node un
 Then, for each node that does not have any other children, the node should be deleted.
 The recursive implementation in the following code block implements this algorithm
 */
-Trie.prototype.delete = function(word) {
+Trie.prototype.delete = function (word) {
   this.deleteRecursively(this.root, word, 0)
 }
-Trie.prototype.deleteRecursively = function(current, word, index) {
+Trie.prototype.deleteRecursively = function (current, word, index) {
   if (index === word.length) {
-    //when end of word is reached only delete if currrent.end Of Word is true.
+    // when end of word is reached only delete if current.end Of Word is true.
     if (!current.endOfWord) {
       return false
     }
-    current.endOfWord = false; 
-    //if current has no other mapping then return true
+    current.endOfWord = false
+    // if current has no other mapping then return true
     return Object.keys(current.children).length === 0
   }
   const ch = word.charAt(index)
@@ -67,14 +67,12 @@ Trie.prototype.deleteRecursively = function(current, word, index) {
   const canDeleteCurrentNode = this.deleteRecursively(node, word, index + 1)
   if (canDeleteCurrentNode) {
     delete current.children[ch]
-    //return true if no mappings are left in the map.
+    // return true if no mappings are left in the map.
     return Object.keys(current.children).length === 0
   }
-  return false; 
+  return false
 }
-
-export {Trie}
-
+export { Trie }
 /*
 const trie1 = new Trie()
 trie1.insert("javascript")
