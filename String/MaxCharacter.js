41becaae7515ecd9b26f08ed09c9067b4c9aafcc
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-05-01 01:14:19 +0600

    resolve: added empty validation improved ignore pattern

--- String/MaxCharacter.js
+++ String/MaxCharacter.js
@@ -2,32 +2,38 @@
  * @function maxCharacter
  * @example - Given a string of characters, return the character that appears the most often. Example: input = "Hello World!" return "l"
  * @param {string} str
+ * @param {RegExp} ignorePattern - ignore the char in str that is not required
  * @returns {string} - char
  */
-const maxCharacter = (str, filterPattern = /[^a-z]/gi) => { // initially it's count only alphabets
+const maxCharacter = (str, ignorePattern) => { // initially it's count only alphabets
   if (typeof str !== 'string') {
     throw new TypeError('Argument should be a string')
+  } else if (!str) {
+    throw new Error('The param should be a valid string')
   }
-
-  const filteredStr = filterPattern ? str.replace(filterPattern, '') : str
-
-  const occurrenceMap = new Map()
 
   // store all char in occurrence map
-  for (const char of filteredStr) {
-    occurrenceMap.set(char, occurrenceMap.get(char) + 1 || 1)
-  }
-
-  let max = { char: '', occur: -Infinity }
+  const occurrenceMap = [...str].reduce(
+    (map, char) => (
+      !ignorePattern?.test(char)
+        ? map.set(char, map.get(char) + 1 || 1)
+        : map
+    ),
+    new Map()
+  )
 
   // find the max char from the occurrence map
-  for (const [char, occur] of occurrenceMap) { // map is iterable so we can fire the for of loop
-    if (occur > max.occur) {
-      max = { char, occur } // re-initialize the new max object
-    }
-  }
+  return [...occurrenceMap.entries()].reduce(
+    (acc, [char, occur]) => {
+      if (occur > acc.occur) {
+        return { char, occur }
+      }
 
-  return max.char
+      return acc
+    },
+    { char: '', occur: -Infinity }
+  )
+    .char // return the maximum char
 }
 
 export default maxCharacter
--- String/test/MaxCharacter.test.js
+++ String/test/MaxCharacter.test.js
@@ -3,18 +3,19 @@ import maxCharacter from '../MaxCharacter'
 describe('Testing the maxCharacter function', () => {
   it('Expect throw with wrong arg', () => {
     expect(() => maxCharacter(123)).toThrow()
+    expect(() => maxCharacter('')).toThrow()
   })
 
   it('Check the max character in string', () => {
     const theString = 'I can\'t do that'
-    const maxCharInAllCount = maxCharacter(theString, false)
-    const maxChar = maxCharacter(theString)
+    const maxCharInAllCount = maxCharacter(theString)
+    const maxChar = maxCharacter(theString, /\s/)
 
     expect(maxCharInAllCount).toBe(' ')
     expect(maxChar).toBe('t')
 
-    expect(maxCharacter('!!!Hello, World!!!', /[a-z]/g)).toBe('!')
+    expect(maxCharacter('!!!Hello, World!!!', /[a-z]/)).toBe('!')
 
-    expect(maxCharacter('!!!Hello, World!!!', /[^a-z]/gi)).toBe('l')
+    expect(maxCharacter('!!!Hello, World!!!', /[^a-z]/i)).toBe('l')
   })
 })
