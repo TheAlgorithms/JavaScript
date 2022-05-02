28062533b6b36a72d27b54bd5f4240886c8843d1
Fahim Faisaal <57553028+fahimfaisaal@users.noreply.github.com>
2022-05-02 15:21:16 +0600

    merge: Upgrade max char (#983)
    * feat: improved memoize function
    
    used Map instead of object & used the JSON.stringfy method for generate a valid string as a key
    
    * docs: modified documentation
    
    * style: format with standard
    
    * docs: modified stringify doc
    
    * refactor: remove two repetition implementation
    
    * feat: added validation, test codes
    
    * chore: remove useless words
    
    * feat: added types for jest
    
    * chore: added link box
    
    * feat: added new validation test casses & methods
    
    * style: formated with standard
    
    * feat: added parse method & test cases
    
    * docs: added js docs
    
    * chore: added default import export
    
    * feat: imporved algorithm via replace method
    
    * test: added two test cases
    
    * feat: added jest type for suggestions
    
    * feat: added `reduceRight` & `trim` method
    
    * chore: added helper variable
    
    * feat: added new rotation option
    
    * Revert "chore: added helper variable"
    
    This reverts commit 489544da0a3d479910fbea020d3be3d0d10681bf.
    
    * remove: yarn lock
    
    * chore: fix grammer
    
    * feat: used replace method & added test case
    
    * feat: remove revert
    
    * chore: added new line
    
    * feat: added filter for alphabets only
    
    * resolve: added empty validation improved ignore pattern
    
    * chore: reverted to for of loop

--- String/MaxCharacter.js
+++ String/MaxCharacter.js
@@ -1,29 +1,36 @@
-/*
-  Given a string of characters, return the character that appears the most often.
-  Example: input = "Hello World!" return "l"
-*/
-const maxCharacter = (value) => {
-  if (typeof value !== 'string') {
-    throw new TypeError('The param should be a string')
-  } else if (!value) {
-    throw new Error('The param should be a valid string')
+/**
+ * @function maxCharacter
+ * @example - Given a string of characters, return the character that appears the most often. Example: input = "Hello World!" return "l"
+ * @param {string} str
+ * @param {RegExp} ignorePattern - ignore the char in str that is not required
+ * @returns {string} - char
+ */
+const maxCharacter = (str, ignorePattern) => { // initially it's count only alphabets
+  if (typeof str !== 'string') {
+    throw new TypeError('Argument should be a string')
+  } else if (!str) {
+    throw new Error('The param should be a nonempty string')
   }
 
-  const occurrences = {}
-  for (let i = 0; i < value.length; i++) {
-    const char = value[i]
-    if (/\s/.test(char)) continue
-    occurrences[char] = occurrences[char] + 1 || 1
-  }
-  let maxCharacter = null
-  let maxCount = 0
-  Object.keys(occurrences).forEach(char => {
-    if (occurrences[char] > maxCount) {
-      maxCount = occurrences[char]
-      maxCharacter = char
+  // store all char in occurrence map
+  const occurrenceMap = new Map()
+
+  for (const char of str) {
+    if (!ignorePattern?.test(char)) {
+      occurrenceMap.set(char, occurrenceMap.get(char) + 1 || 1)
     }
-  })
-  return maxCharacter
+  }
+
+  // find the max char from the occurrence map
+  let max = { char: '', occur: -Infinity }
+
+  for (const [char, occur] of occurrenceMap) {
+    if (occur > max.occur) {
+      max = { char, occur }
+    }
+  }
+
+  return max.char
 }
 
-export { maxCharacter }
+export default maxCharacter
--- String/test/MaxCharacter.test.js
+++ String/test/MaxCharacter.test.js
@@ -1,12 +1,21 @@
-import { maxCharacter } from '../MaxCharacter'
+import maxCharacter from '../MaxCharacter'
 
 describe('Testing the maxCharacter function', () => {
   it('Expect throw with wrong arg', () => {
     expect(() => maxCharacter(123)).toThrow()
+    expect(() => maxCharacter('')).toThrow()
   })
+
   it('Check the max character in string', () => {
     const theString = 'I can\'t do that'
-    const maxChar = maxCharacter(theString)
+    const maxCharInAllCount = maxCharacter(theString)
+    const maxChar = maxCharacter(theString, /\s/)
+
+    expect(maxCharInAllCount).toBe(' ')
     expect(maxChar).toBe('t')
+
+    expect(maxCharacter('!!!Hello, World!!!', /[a-z]/)).toBe('!')
+
+    expect(maxCharacter('!!!Hello, World!!!', /[^a-z]/i)).toBe('l')
   })
 })
