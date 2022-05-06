6fb4bfc28dd34dbf7a0e65483a35568252b5b3ef
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-05-06 21:25:38 +0600

    feat: updated the Queue array to linkedlist DS

--- Data-Structures/Queue/test/Queue.test.js
+++ Data-Structures/Queue/test/Queue.test.js
@@ -1,8 +1,17 @@
-import { Queue } from '../Queue'
+import Queue from '../Queue'
 
-describe('Queue', () => {
+describe('Testing the Queue DS', () => {
+  it('Check Capacity', () => {
+    const queue = new Queue(3)
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(8)
+
+    expect(() => queue.enqueue(9)).toThrow()
+  })
+
   it('Check enqueue/dequeue', () => {
-    const queue = new Queue()
+    const queue = new Queue(10)
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(8)
@@ -13,36 +22,46 @@ describe('Queue', () => {
   })
 
   it('Check length', () => {
-    const queue = new Queue()
+    const queue = new Queue(5)
 
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(8)
     queue.enqueue(9)
 
-    expect(queue.length()).toBe(4)
+    expect(queue.toArray()).toEqual([1, 2, 8, 9])
   })
 
-  it('Check peek', () => {
-    const queue = new Queue()
+  it('Check peekFirst & peekLast', () => {
+    const queue = new Queue(4)
 
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(8)
     queue.enqueue(9)
 
-    expect(queue.peek()).toBe(1)
+    expect(queue.peekFirst()).toBe(1)
+    expect(queue.peekLast()).toBe(9)
+  })
+
+  it('Check toArray', () => {
+    const queue = new Queue()
+
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(8)
+    queue.enqueue(9)
   })
 
   it('Check empty', () => {
-    const queue = new Queue()
-    expect(queue.empty()).toBeTruthy()
+    const queue = new Queue(5)
+    expect(queue.isEmpty()).toBeTruthy()
 
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(8)
     queue.enqueue(9)
 
-    expect(queue.empty()).toBeFalsy()
+    expect(queue.isEmpty()).toBeFalsy()
   })
 })
