98526f28b99ac4789e1b468c520c28d3b669bd14
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-05-16 06:09:25 +0600

    pref: used 1 method instead of 5

--- String/test/FormatPhoneNumber.test.js
+++ String/test/FormatPhoneNumber.test.js
@@ -1,23 +1,15 @@
-import { formatPhoneNumber } from '../FormatPhoneNumber'
+import formatPhoneNumber from '../FormatPhoneNumber'
 
-describe('PhoneNumberFormatting', () => {
+describe('Testing the formatPhoneNumber functions', () => {
+  it('expects to throw a type error', () => {
+    expect(() => formatPhoneNumber('1234567')).toThrow('Invalid phone number!')
+    expect(() => formatPhoneNumber('123456text')).toThrow('Invalid phone number!')
+    expect(() => formatPhoneNumber(12345)).toThrow('Invalid phone number!')
+  })
+
   it('expects to return the formatted phone number', () => {
     expect(formatPhoneNumber('1234567890')).toEqual('(123) 456-7890')
-  })
-
-  it('expects to return the formatted phone number', () => {
-    expect(formatPhoneNumber(1234567890)).toEqual('(123) 456-7890')
-  })
-
-  it('expects to throw a type error', () => {
-    expect(() => { formatPhoneNumber('1234567') }).toThrow('Invalid phone number.')
-  })
-
-  it('expects to throw a type error', () => {
-    expect(() => { formatPhoneNumber('123456text') }).toThrow('Invalid phone number.')
-  })
-
-  it('expects to throw a type error', () => {
-    expect(() => { formatPhoneNumber(12345) }).toThrow('Invalid phone number.')
+    expect(formatPhoneNumber('2124323322')).toEqual('(212) 432-3322')
+    expect(formatPhoneNumber('1721543455')).toEqual('(172) 154-3455')
   })
 })
