08672d3005a4821a301ff218df1cf25bf718ba0a
Exortions <75327059+Exortions@users.noreply.github.com>
2022-05-23 13:14:00 -0700

    Update CollatzConjecture.js

--- Maths/CollatzConjecture.js
+++ Maths/CollatzConjecture.js
@@ -1,11 +1,11 @@
 export function collatz (n) {
   while (n > 1) {
     if (n % 2 === 0) {
-      n = n / 2;
+      n = n / 2
     } else {
-      n = 3 * n + 1;
+      n = 3 * n + 1
     }
   }
-  
-  return n;
+
+  return n
 }
