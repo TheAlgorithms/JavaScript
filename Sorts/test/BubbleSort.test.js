a55119d9e5aaa09a524969631b6867a117de4d62
thanhphuonghdhh <19021346@vnu.edu.vn>
2022-05-23 08:19:34 +0700

    re-update test case for bubble sort

--- Sorts/test/BubbleSort.test.js
+++ Sorts/test/BubbleSort.test.js
@@ -7,6 +7,11 @@ describe('bubbleSort', () => {
     expect(bubbleSort([1, 2, 3])).toEqual([1, 2, 3])
     expect(bubbleSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([1, 2, 5, 6, 7, 8, 12, 14])
     expect(bubbleSort([5, 6, 7, 8, 9, 4])).toEqual([4, 5, 6, 7, 8, 9])
+    expect(bubbleSort([2,1,3])).toEqual([1, 2, 3])
+    expect(bubbleSort([10,15,16,100])).toEqual([10,15,16,100])
+    expect(bubbleSort([10,9,11])).toEqual([9,10,11])
+    expect(bubbleSort([3,2,1])).toEqual([1, 2, 3])
+    expect(bubbleSort([10,9,8])).toEqual([8,9,10])
   })
 })
 
@@ -16,23 +21,5 @@ describe('alternativeBubbleSort', () => {
     expect(alternativeBubbleSort([])).toEqual([])
     expect(alternativeBubbleSort([1, 2, 3])).toEqual([1, 2, 3])
     expect(alternativeBubbleSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([1, 2, 5, 6, 7, 8, 12, 14])
-  })
-})
-//add unit test with MCC coverage
-describe('bubbleSort', () => {
-  it('should sort arrays correctly', () => {
-    expect(bubbleSort([1, 2, 3])).toEqual([1, 2, 3])
-    expect(bubbleSort([2,1,3])).toEqual([1, 2, 3])
-    expect(bubbleSort([10,15,16,100])).toEqual([10,15,16,100])
-    expect(bubbleSort([10,9,11])).toEqual([9,10,11])
-  })
-})
-//add unit test with all-du path coverage
-describe('bubbleSort', () => {
-  it('should sort arrays correctly', () => {
-    expect(bubbleSort([1, 2, 3])).toEqual([1, 2, 3])
-    expect(bubbleSort([2,1,3])).toEqual([1, 2, 3])
-    expect(bubbleSort([10,15,16,100])).toEqual([10,15,16,100])
-    expect(bubbleSort([10,9,11])).toEqual([9,10,11])
   })
 })
