08ccb61fea3d80d80fb047ca2f112e75c01dd63a
Exortions <75327059+Exortions@users.noreply.github.com>
2022-05-24 08:23:18 -0700

    Update CollatzSequence.test.js

--- Maths/test/CollatzSequence.test.js
+++ Maths/test/CollatzSequence.test.js
@@ -2,8 +2,7 @@ import { collatz } from '../CollatzSequence'
 
 describe('The Collatz Conjecture', () => {
   it('Should be 1', () => {
-    expect(collatz(1)).toBe(1)
-    expect(collatz(5)).toBe(1)
-    expect(collatz(51351)).toBe(1)
+    expect(collatz(1)).toBe({ result: 1, steps: [] })
+    expect(collatz(5)).toBe({ result: 1, steps: [16, 8, 4, 2, 1] })
   })
 })
