71b45ea1c8585dd5e14395b753b72721d47df2b6
Exortions <75327059+Exortions@users.noreply.github.com>
2022-05-24 08:27:40 -0700

    Fix styling errors

--- Maths/CollatzSequence.js
+++ Maths/CollatzSequence.js
@@ -15,14 +15,14 @@
 */
 export function collatz (n) {
   const steps = []
-  
+
   while (n > 1) {
     if (n % 2 === 0) {
       n = n / 2
     } else {
       n = 3 * n + 1
     }
-    
+
     steps.push(n)
   }
 
