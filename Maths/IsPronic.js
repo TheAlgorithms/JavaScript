a5c413192b0645569a4cc54527a6cd6b69b12bbf
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-25 16:46:41 +0530

    refactor: Change pronic number check condition

--- Maths/IsPronic.js
+++ Maths/IsPronic.js
@@ -18,4 +18,10 @@
  * @returns {boolean}
  */
 
-export const isPronic = (number) => Math.round(Math.sqrt(number + 1)) - Math.round(Math.sqrt(number)) === 1
+export const isPronic = (number) => {
+  if (number === 0){
+    return true
+  }
+  const sqrt = Math.sqrt(number)
+  return sqrt % 1 !== 0 && Math.ceil(sqrt) * Math.floor(sqrt) === number
+}
