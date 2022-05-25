2157d10688ec6da09066f074554afdfa0c6d3fc1
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-25 11:43:25 +0530

    feat: Add pronic number implementation

---
+++ Maths/IsPronic.js
@@ -0,0 +1,34 @@
+/*
+ * Author: Akshay Dubey (https://github.com/itsAkshayDubey)
+ * Pronic Number: https://en.wikipedia.org/wiki/Pronic_number
+ * function to check if number is pronic.
+ * return true if number is pronic.
+ * else false
+ */
+
+/**
+ * @function isPronic
+ * @description -> Checking if number is pronic using product of two consecutive numbers
+ * If number is a product of two consecutive numbers, then it is pronic
+ * therefore, the function will return true
+ *
+ * If number is not a product of two consecutive numbers, then it is not pronic
+ * therefore, the function will return false
+ * @param {number} number
+ * @returns {boolean}
+ */
+
+function isPronic(number) {
+
+	for (var i = 0; i <= parseInt(Math.sqrt(number)); i++)
+	
+		// Checking Pronic Number
+		// by multiplying consecutive
+		// numbers
+		if (number == i * (i + 1))
+			return true;
+
+	return false;
+}
+
+export { isPronic }
