951b584e13b4c4ef49357b9fee0be69489646972
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-25 13:46:44 +0530

    Minor refactoring

--- Maths/test/IsPronic.test.js
+++ Maths/test/IsPronic.test.js
@@ -2,13 +2,13 @@ import { isPronic } from '../IsPronic'
 
 describe('Testing isPronic function', () => {
   it('should return if the number is pronic or not for even number', () => {
-    const isPronicNumber = isPronic(2)
-    expect(isPronicNumber).toBe(true)
+    const isPronicNumber = isPronic(4)
+    expect(isPronicNumber).toBe(false)
   })
 
   it('should return if the number is pronic or not for even number', () => {
-    const isPronicNumber = isPronic(4)
-    expect(isPronicNumber).toBe(false)
+    const isPronicNumber = isPronic(2)
+    expect(isPronicNumber).toBe(true)
   })
 
   it('should return false for odd number', () => {
