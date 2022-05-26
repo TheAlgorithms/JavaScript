c78fdcd01a65b3f57143f0f81633df44abea3ff3
Exortions <75327059+Exortions@users.noreply.github.com>
2022-05-26 14:54:42 -0700

    Create TwinPrime.js

---
+++ Maths/TwinPrime.js
@@ -0,0 +1,33 @@
+function isPrime (n) {
+  let prime = false
+
+  if (n > 1) {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        prime = true
+        break
+      }
+    }
+  }
+
+  return !prime
+}
+
+function twinPrime (n) {
+  const result = []
+  const prime = prime(n)
+
+  if (!prime) {
+    return []
+  }
+  
+  result.push(n)
+  
+  if (!isPrime(n + 2)) {
+    return []
+  }
+  
+  result.push(n + 2)
+  
+  return result
+}
