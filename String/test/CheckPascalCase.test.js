71392bc8ef0b76673ad9656907eca8b27803b93c
Ankush263 <ankushbanik263@gmail.com>
2022-05-31 11:50:00 +0530

    Add test case to CheckPascalCase Algorithm

---
+++ String/test/CheckPascalCase.test.js
@@ -0,0 +1,19 @@
+import { CheckPascalCase } from '../CheckPascalCase'
+
+test('CheckPascalCase(TheAlgorithms) -> true', () => {
+  const word = 'TheAlgorithms'
+  const res = CheckPascalCase(word)
+  expect(res).toBeTruthy()
+})
+
+test('CheckPascalCase(theAlgorithms) -> false', () => {
+  const word = 'theAlgorithms'
+  const res = CheckPascalCase(word)
+  expect(res).toBeFalsy()
+})
+
+test('CheckPascalCase(The Algorithms) -> false', () => {
+  const word = 'The Algorithms'
+  const res = CheckPascalCase(word)
+  expect(res).toBeFalsy()
+})
