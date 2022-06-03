dbffac253d892006a739ff7c79bfab04ff323350
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2022-06-03 19:07:49 +0200

    merge: FindSecondLargestElement: Support negative numbers (#1036)
    * FindSecondLargestElement: Support negative numbers
    
    * Updated Documentation in README.md
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- Sorts/FindSecondLargestElement.js
+++ Sorts/FindSecondLargestElement.js
@@ -11,7 +11,7 @@
 
 const secondLargestElement = (array) => {
   const largestElement = Math.max(...array)
-  let element = 0
+  let element = -Number.MAX_VALUE
 
   for (let i = 0; i < array.length; i++) {
     if (element < array[i] && array[i] !== largestElement) {
