3d865f3bf1d11615a11da8159030b66f249f806c
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-06-21 14:30:15 -0400

    Splitted `BigInt` & `Number` tests

--- Maths/test/Fibonacci.test.js
+++ Maths/test/Fibonacci.test.js
@@ -47,15 +47,7 @@ describe('Fibonacci', () => {
     expect(FibonacciMatrixExpo(4)).toBe(3)
     expect(FibonacciMatrixExpo(5)).toBe(5)
     expect(FibonacciMatrixExpo(6)).toBe(8)
-    
-    expect(FibonacciMatrixExpo(0n)).toBe(0n)
-    expect(FibonacciMatrixExpo(1n)).toBe(1n)
-    expect(FibonacciMatrixExpo(2n)).toBe(1n)
-    expect(FibonacciMatrixExpo(3n)).toBe(2n)
-    expect(FibonacciMatrixExpo(4n)).toBe(3n)
-    expect(FibonacciMatrixExpo(5n)).toBe(5n)
-    expect(FibonacciMatrixExpo(6n)).toBe(8n)
-    
+
     expect(FibonacciMatrixExpo(-0)).toBe(-0)
     expect(FibonacciMatrixExpo(-1)).toBe(1)
     expect(FibonacciMatrixExpo(-2)).toBe(-1)
@@ -63,7 +55,17 @@ describe('Fibonacci', () => {
     expect(FibonacciMatrixExpo(-4)).toBe(-3)
     expect(FibonacciMatrixExpo(-5)).toBe(5)
     expect(FibonacciMatrixExpo(-6)).toBe(-8)
-    
+  })
+  
+  it('should return bigint for FibonacciMatrixExpo', () => {
+    expect(FibonacciMatrixExpo(0n)).toBe(0n)
+    expect(FibonacciMatrixExpo(1n)).toBe(1n)
+    expect(FibonacciMatrixExpo(2n)).toBe(1n)
+    expect(FibonacciMatrixExpo(3n)).toBe(2n)
+    expect(FibonacciMatrixExpo(4n)).toBe(3n)
+    expect(FibonacciMatrixExpo(5n)).toBe(5n)
+    expect(FibonacciMatrixExpo(6n)).toBe(8n)
+
     expect(FibonacciMatrixExpo(-0n)).toBe(0n)
     expect(FibonacciMatrixExpo(-1n)).toBe(1n)
     expect(FibonacciMatrixExpo(-2n)).toBe(-1n)
