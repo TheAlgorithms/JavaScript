5234f12f58d9e1017a0ef23d9ff385d6e23beaa6
IndianBlitz <101599761+IndianBlitz@users.noreply.github.com>
2022-07-01 19:45:30 +0530

    Update OddEven.js

--- Bit-Manipulation/OddEven.js
+++ Bit-Manipulation/OddEven.js
@@ -4,11 +4,8 @@
 /*
     author: IndianBlitz
    
-
     This script will find number is Odd or even 
-    in binary representation of given number
-
-
+   
 */
 //Check even if true then return 1 else return 0 which means number is odd;
 function checkEvenOdd(num){
