bb714275dc02e1e88156cd1cff8bfd93308267ea
IndianBlitz <101599761+IndianBlitz@users.noreply.github.com>
2022-07-01 19:25:02 +0530

    add newfile OddEven to bits

---
+++ Bit-Manipulation/OddEven.js
@@ -0,0 +1,22 @@
+
+
+
+/*
+    author: vivek9patel
+   
+
+    This script will find number is Odd or even 
+    in binary representation of given number
+
+
+*/
+//Check even if true then return 1 else return 0 which means number is odd;
+function checkEvenOdd(num){
+    'use strict';
+    if((num & 1) === 0)
+        return 1;
+    else
+        return 0;
+}
+
+export {CheckEvenOdd}
