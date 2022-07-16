fe5c5f8a13c8b77569beee6ce8d45ac88001499d
Ram Munde <rammunde19@gmail.com>
2022-07-16 10:10:15 +0530

    Update TreeTraversal.js

--- Data-Structures/Tree/TreeTraversal/TreeTraversal.js
+++ Data-Structures/Tree/TreeTraversal/TreeTraversal.js
@@ -1,31 +1,29 @@
 //O(n) Time and O(n) Space Complexity
 
 function inOrderTraverse(tree, array) {
-    if(tree !== null){
-      inOrderTraverse(tree.left, array);
-      array.push(tree.value);
-      inOrderTraverse(tree.right, array);
-    }
-    return array;
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
   }
-  
-  function preOrderTraverse(tree, array) {
-    if(tree != null){
-      array.push(tree.value);
-      preOrderTraverse(tree.left, array);
-      preOrderTraverse(tree.right, array);
-    }
+  return array;
+}
+function preOrderTraverse(tree, array) {
+  if (tree != null) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
   }
-  
-  function postOrderTraverse(tree, array) {
-    if(tree != null){
-      postOrderTraverse(tree.left, array);
-      postOrderTraverse(tree.right, array);
-      array.push(tree.value);
-    }
+}
+
+function postOrderTraverse(tree, array) {
+  if (tree != null) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
   }
-  
-  exports.inOrderTraverse = inOrderTraverse;
-  exports.preOrderTraverse = preOrderTraverse;
-  exports.postOrderTraverse = postOrderTraverse;
-  
+}
+
+exports.inOrderTraverse = inOrderTraverse;
+exports.preOrderTraverse = preOrderTraverse;
+exports.postOrderTraverse = postOrderTraverse;
