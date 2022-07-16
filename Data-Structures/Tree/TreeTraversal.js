c47af1c7675713e86f9a62a6bd5df5a71aebe6ea
Ram Munde <rammunde19@gmail.com>
2022-07-16 18:41:29 +0530

    Standard --fix is done

--- Data-Structures/Tree/TreeTraversal.js
+++ Data-Structures/Tree/TreeTraversal.js
@@ -1,29 +1,30 @@
-//O(n) Time and O(n) Space Complexity
+/* eslint no-use-before-define: 0 */ // --> OFF
+// O(n) Time and O(n) Space Complexity
 
-function inOrderTraverse(tree, array) {
+function inOrderTraverse (tree, array) {
   if (tree !== null) {
-    inOrderTraverse(tree.left, array);
-    array.push(tree.value);
-    inOrderTraverse(tree.right, array);
+    inOrderTraverse(tree.left, array)
+    array.push(tree.value)
+    inOrderTraverse(tree.right, array)
   }
-  return array;
+  return array
 }
-function preOrderTraverse(tree, array) {
+function preOrderTraverse (tree, array) {
   if (tree !== null) {
-    array.push(tree.value);
-    preOrderTraverse(tree.left, array);
-    preOrderTraverse(tree.right, array);
-  }
-}
-
-function postOrderTraverse(tree, array) {
-  if (tree !== null) {
-    postOrderTraverse(tree.left, array);
-    postOrderTraverse(tree.right, array);
-    array.push(tree.value);
+    array.push(tree.value)
+    preOrderTraverse(tree.left, array)
+    preOrderTraverse(tree.right, array)
   }
 }
 
-exports.inOrderTraverse = inOrderTraverse;
-exports.preOrderTraverse = preOrderTraverse;
-exports.postOrderTraverse = postOrderTraverse;
+function postOrderTraverse (tree, array) {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array)
+    postOrderTraverse(tree.right, array)
+    array.push(tree.value)
+  }
+}
+
+exports.inOrderTraverse = inOrderTraverse
+exports.preOrderTraverse = preOrderTraverse
+exports.postOrderTraverse = postOrderTraverse
