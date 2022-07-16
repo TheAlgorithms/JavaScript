5a099d35b9ae26c01054e0ffcb8972ec37750e2b
Ram Munde <rammunde19@gmail.com>
2022-07-16 16:09:22 +0530

    Updated TreeTraversal

--- Data-Structures/Tree/TreeTraversal.js
+++ Data-Structures/Tree/TreeTraversal.js
@@ -9,7 +9,7 @@ function inOrderTraverse(tree, array) {
   return array;
 }
 function preOrderTraverse(tree, array) {
-  if (tree != null) {
+  if (tree !== null) {
     array.push(tree.value);
     preOrderTraverse(tree.left, array);
     preOrderTraverse(tree.right, array);
@@ -17,7 +17,7 @@ function preOrderTraverse(tree, array) {
 }
 
 function postOrderTraverse(tree, array) {
-  if (tree != null) {
+  if (tree !== null) {
     postOrderTraverse(tree.left, array);
     postOrderTraverse(tree.right, array);
     array.push(tree.value);
