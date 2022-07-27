1a089cc49122f558fb23fe0447637db1c9c2a721
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2022-07-27 15:13:07 +0200

    merge: Fix GetEuclidGCD (#1068)
    * Fix GetEuclidGCD
    
    Implement the actual Euclidean Algorithm
    
    * Replace == with ===
    
    * Lua > JS
    
    * Standard sucks
    
    * Oops
    
    * Update GetEuclidGCD.js
    
    * Updated Documentation in README.md
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- Maths/GetEuclidGCD.js
+++ Maths/GetEuclidGCD.js
@@ -1,32 +1,20 @@
-/*
-    Problem statement and Explanation : https://en.wikipedia.org/wiki/Euclidean_algorithm
-
-    In this method, we have followed the iterative approach to first
-    find a minimum of both numbers and go to the next step.
-*/
-
 /**
- * GetEuclidGCD return the gcd of two numbers using Euclidean algorithm.
- * @param {Number} arg1 first argument for gcd
- * @param {Number} arg2 second argument for gcd
- * @returns return a `gcd` value of both number.
+ * GetEuclidGCD Euclidean algorithm to determine the GCD of two numbers
+ * @param {Number} a integer (may be negative)
+ * @param {Number} b integer (may be negative)
+ * @returns {Number} Greatest Common Divisor gcd(a, b)
  */
-const GetEuclidGCD = (arg1, arg2) => {
-  // firstly, check that input is a number or not.
-  if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
-    return new TypeError('Argument is not a number.')
+export function GetEuclidGCD (a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Arguments must be numbers')
   }
-  // check that the input number is not a negative value.
-  if (arg1 < 1 || arg2 < 1) {
-    return new TypeError('Argument is a negative number.')
+  if (a === 0 && b === 0) return undefined // infinitely many numbers divide 0
+  a = Math.abs(a)
+  b = Math.abs(b)
+  while (b !== 0) {
+    const rem = a % b
+    a = b
+    b = rem
   }
-  // Find a minimum of both numbers.
-  let less = arg1 > arg2 ? arg2 : arg1
-  // Iterate the number and find the gcd of the number using the above explanation.
-  for (less; less >= 2; less--) {
-    if ((arg1 % less === 0) && (arg2 % less === 0)) return (less)
-  }
-  return (less)
+  return a
 }
-
-export { GetEuclidGCD }
