b165ca34559a344890f8aa3829b0e113828308da
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2022-07-27 15:10:01 +0200

    Update GetEuclidGCD.js

--- Maths/GetEuclidGCD.js
+++ Maths/GetEuclidGCD.js
@@ -8,11 +8,9 @@ export function GetEuclidGCD (a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError('Arguments must be numbers')
   }
+  if (a === 0 && b === 0) return undefined // infinitely many numbers divide 0
   a = Math.abs(a)
   b = Math.abs(b)
-  if (a === 0 && b === 0) return undefined // infinitely many numbers divide 0
-  if (a < 0) a = -a
-  if (b < 0) b = -b
   while (b !== 0) {
     const rem = a % b
     a = b
