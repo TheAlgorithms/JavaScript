03008e4ad01550c244101afda97b9203670c6cbb
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-07-28 13:24:37 -0400

    Remove space

--- Maths/PerfectCube.js
+++ Maths/PerfectCube.js
@@ -3,7 +3,7 @@
  * License: GPL-3.0 or later
  *
  * The integer check acts as a guard clause, and avoids calculating the root if the input is invalid,
- * it also ensures Infinity is treated as a non-cube. 
+ * it also ensures Infinity is treated as a non-cube.
  * This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors.
  */
 
