205246e1cd9d97d2ab38e0d5857707fbab111b35
tehliang <tehl64@gmail.com>
2022-07-28 18:06:54 +0800

    Update IsCompositeNumber.js

--- Maths/IsCompositeNumber.js
+++ Maths/IsCompositeNumber.js
@@ -9,22 +9,12 @@
  * First few composite numbers are 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, ………
  * Every integer greater than one is either a prime number or a composite number.
  * The number one is a unit – it is neither prime nor composite.
- * Reference: https://www.geeksforgeeks.org/composite-number/
  */
 
+import { PrimeCheck } from './PrimeCheck'
+
 function isCompositeNumber (number) {
-  // Conditions that can immediate gain result:
-  // Corner cases
-  if (number <= 1) return false
-  if (number <= 3) return false
-  // Middle five numbers in below loop
-  if (number % 2 === 0 || number % 3 === 0) return true
-
-  for (let i = 5; i * i <= number; i = i + 6) {
-    if (number % i === 0 || number % (i + 2) === 0) { return true }
-  }
-
-  return false
+  return number > 1 && !PrimeCheck(number)
 }
 
 export { isCompositeNumber }
