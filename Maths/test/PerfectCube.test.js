c788dcbbb32f5d99e6c7186538bbe2093ae31586
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-07-28 12:44:17 -0400

    Add negative and Infinity tests

--- Maths/test/PerfectCube.test.js
+++ Maths/test/PerfectCube.test.js
@@ -3,8 +3,10 @@ import { perfectCube } from '../PerfectCube'
 describe('PerfectCube', () => {
   it('should return true for a perfect cube', () => {
     expect(perfectCube(125)).toBeTruthy()
+    expect(perfectCube(-125)).toBeTruthy()
   })
   it('should return false for a non perfect cube', () => {
     expect(perfectCube(100)).toBeFalsy()
+    expect(perfectCube(Infinity)).toBeFalsy()
   })
 })
