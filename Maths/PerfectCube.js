bbdf16fb700335153f948e3da35bbe81f1672952
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-07-28 13:02:36 -0400

    Update PerfectCube.js

--- Maths/PerfectCube.js
+++ Maths/PerfectCube.js
@@ -1,11 +1,12 @@
 /**
  * Author: dephraiim
  * License: GPL-3.0 or later
- * 
- * Infinity is an undefined cube, so return false for any sign.
- * This uses `round` instead of `floor` or `trunc`, to guard against rounding errors.
+ *
+ * The integer check acts as a guard clause, and avoids calculating the root if the input is invalid,
+ * it also ensures Infinity is treated as a non-cube. 
+ * This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors.
  */
 
-const perfectCube = (num) => Number.isFinite(num) && Math.round(Math.cbrt(num)) ** 3 === num
+const perfectCube = (num) => Number.isInteger(num) && Math.round(Math.cbrt(num)) ** 3 === num
 
 export { perfectCube }
