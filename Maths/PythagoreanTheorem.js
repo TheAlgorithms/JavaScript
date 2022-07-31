978a1003286563bc6689761c2851cc87922e7429
cwh0430 <108912628+cwh0430@users.noreply.github.com>
2022-08-01 03:41:20 +0800

    Update PythagoreanTheorem.js

--- Maths/PythagoreanTheorem.js
+++ Maths/PythagoreanTheorem.js
@@ -1,35 +1,34 @@
 /**
  * @function calcHypothenuse
  * @description Calculate the length of hypothenuse of triangle
- * @param {Integer} base - Integer
- * @param {Integer} adjacent - Integer
+ * @param {Integer} side1 - Integer
+ * @param {Integer} side2 - Integer
  * @return {Integer} - hypothenuse
- * @see [findHypothenuse](https://en.wikipedia.org/wiki/Pythagorean_theorem)
+ * @see [calcHypothenuse](https://en.wikipedia.org/wiki/Pythagorean_theorem)
  * @example calcHypothenuse(2, 3) = 4
  */
 const calcHypothenuse = (side1, side2) => {
   const hypothenuse = Math.sqrt((adjacent ** 2) + (base ** 2))
   return hypothenuse
 }
-
 /**
- * @function calcAdjacent
- * @description Calculate the length of adjacent of triangle
+ * @function calcOtherSides
+ * @description Calculate the length of other sides of triangle
  * @param {Integer} hypothenuse - Integer
- * @param {Integer} base - Integer
- * @return {Integer} - adjacent
- * @see [findHypothenuse](https://en.wikipedia.org/wiki/Pythagorean_theorem)
- * @example calcAdjacent(4, 3) = 2
+ * @param {Integer} side1 - Integer
+ * @return {Integer} - side2
+ * @see [calcOtherSides](https://en.wikipedia.org/wiki/Pythagorean_theorem)
+ * @example calcOtherSides(4, 3) = 2
  */
 const calcOtherSides = (hypothenuse, side1) => {
   if (side1 >= hypothenuse) {
     return 'Length of side1 must be smaller than hypothenuse'
   }
-  const side2 = Math.sqrt((hypothenuse ** 2) - (side1 ** 2))
-  return side2
+    const side2 = Math.sqrt((hypothenuse ** 2) - (side1 ** 2))
+    return side2
 }
 
 export {
-calcHypothenuse, 
-calcOtherSides,
+  calcHypothenuse,
+  calcOtherSides,
 }
