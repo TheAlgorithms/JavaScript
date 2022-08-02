322a67c3fb33412f99792cd13c052704fd38316f
AanthereX <aghahamza000@gmail.com>
2022-08-02 18:06:01 +0500

    add more functions to DecimalToBinary.js file from Conversions directory

--- Conversions/DecimalToBinary.js
+++ Conversions/DecimalToBinary.js
@@ -1,13 +1,35 @@
-function decimalToBinary (num) {
-  const bin = []
-  while (num > 0) {
-    bin.unshift(num % 2)
-    num >>= 1 // basically /= 2 without remainder if any
-  }
-  return bin.join('')
+function decimalToBinary(num) {
+    const bin = []
+    while (num > 0) {
+        bin.unshift(num % 2)
+        num >>= 1 // basically /= 2 without remainder if any
+    }
+    return bin.join('')
 }
 
-export { decimalToBinary }
+// using .toString() method
+
+function decimalToBinary_2(dec) {
+    return dec.toString(2)
+}
+
+// using Remainder Quotient method
+
+function decimalToBinary_3(dec) {
+    let bin = 0;
+    let rem, i = 1,
+        step = 1;
+    while (dec != 0) {
+        rem = dec % 2;
+        dec = parseInt(dec / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    return bin
+}
+
+
+export { decimalToBinary, decimalToBinary_2, decimalToBinary_3 }
 
 // > decimalToBinary(2)
 // '10'
