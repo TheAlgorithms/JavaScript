7d0e2db74723c21acc8b64761ce8b418bfb6d18f
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-08-04 18:32:55 -0400

    Parity between `PerfectSquare` and `PerfectCube`

--- Maths/PerfectSquare.js
+++ Maths/PerfectSquare.js
@@ -2,8 +2,9 @@
  * Author: dephraiim
  * License: GPL-3.0 or later
  *
+ * This uses `round` instead of `floor` or `trunc`, to guard against potential `sqrt` accuracy errors
  */
 
-const perfectSquare = (num) => Math.sqrt(num) ** 2 === num
+const perfectSquare = (num) => Number.isFinite(num) && Math.round(Math.sqrt(num)) ** 2 === num
 
 export { perfectSquare }
