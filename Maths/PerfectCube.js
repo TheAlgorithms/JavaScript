2491d258791a5efe45ccbb08eeea1ab57fa6335e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-08-04 18:30:14 -0400

    Update PerfectCube.js
    Remove dot

--- Maths/PerfectCube.js
+++ Maths/PerfectCube.js
@@ -2,7 +2,7 @@
  * Author: dephraiim
  * License: GPL-3.0 or later
  *
- * This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors.
+ * This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors
  */
 
 const perfectCube = (num) => Number.isFinite(num) && Math.round(Math.cbrt(num)) ** 3 === num
