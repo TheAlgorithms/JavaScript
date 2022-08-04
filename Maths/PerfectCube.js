c1210aaa7ccb913c482e46e2ba26930381cddc5a
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-08-04 18:29:31 -0400

    Update PerfectCube.js
    Now `isInt` is replaced by `isFinite`, because `isInt` is a redundant check

--- Maths/PerfectCube.js
+++ Maths/PerfectCube.js
@@ -2,11 +2,9 @@
  * Author: dephraiim
  * License: GPL-3.0 or later
  *
- * The integer check acts as a guard clause, and avoids calculating the root if the input is invalid,
- * it also ensures Infinity is treated as a non-cube.
  * This uses `round` instead of `floor` or `trunc`, to guard against potential `cbrt` accuracy errors.
  */
 
-const perfectCube = (num) => Number.isInteger(num) && Math.round(Math.cbrt(num)) ** 3 === num
+const perfectCube = (num) => Number.isFinite(num) && Math.round(Math.cbrt(num)) ** 3 === num
 
 export { perfectCube }
