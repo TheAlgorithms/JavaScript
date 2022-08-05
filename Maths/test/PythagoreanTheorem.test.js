153f4053093b5e525b42225d0f087d38256955ba
cwh0430 <108912628+cwh0430@users.noreply.github.com>
2022-08-05 21:02:32 +0800

    Update PythagoreanTheorem.test.js

--- Maths/test/PythagoreanTheorem.test.js
+++ Maths/test/PythagoreanTheorem.test.js
@@ -1,17 +1,17 @@
 import * as pytheorem from "../PythagoreanTheorem"
 
 test('Testing on hypothenuse calculation', () => {
-  const hypothenuse = pytheorem.calcHypothenuse(2.0, 3.0)
-  expect(hypothenuse).toBe(4.0)
+  const hypothenuse = pytheorem.calcHypothenuse(6.0, 8.0)
+  expect(hypothenuse).toBe(10.0)
 })
 
 test('Testing on other sides calculation', () => {
-  const side = pytheorem.calcOtherSide(4.0, 3.0)
-  expect(side).toBe(2.0)
+  const side = pytheorem.calcOtherSide(10.0, 6.0)
+  expect(side).toBe(8.0)
 })
 
 test('Testing on other sides calculation', () => {
-  const side = pytheorem.calcOtherSide(2.0, 4.0)
+  const side = pytheorem.calcOtherSide(6.0, 10.0)
   expect(side).toBe('Length of side1 must be smaller than hypothenuse')
 })
 
