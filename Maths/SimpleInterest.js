d809becfe2905793736f6f40fbfd5beecae25c59
april <hsy.k10@gmail.com>
2022-08-08 04:11:40 +0800

    fix

--- Maths/SimpleInterest.js
+++ Maths/SimpleInterest.js
@@ -2,13 +2,13 @@
  * @function SimpleInterest
  * @description to calculate the amount of interest charged on a sum at a given rate and for a given period of time
  * @param {number} principal - input: the principal amount
- * @param {number} intrestRate - input: the rate of interest in percentage
+ * @param {number} interestRate - input: the rate of interest in percentage
  * @param {number} time - input: the time period in year
  * @return {number} simpleInterest
  */
 
 const SimpleInterest = (principal, interestRate, time) => {
-  const simpleInterest = parseInt((principal * interestRate * time) / 100)
+  const simpleInterest = Math.floor((principal * interestRate * time) / 100)
   return simpleInterest
 }
 
