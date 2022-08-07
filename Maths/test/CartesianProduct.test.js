ee7ef6769c79928b672d094f9b1c11d0b3bdecd0
xinzhe822 <xinzhezhexin@gmail.com>
2022-08-07 22:18:27 +0800

    Added CartesianProduct.test.js

--- Maths/test/CartesianProduct.test.js
+++ Maths/test/CartesianProduct.test.js
@@ -11,9 +11,11 @@ describe('cartesianProduct', () => {
 
   it('should calculate the product of two sets', () => {
     const product1 = cartesianProduct([1], [1])
-    const product2 = cartesianProduct([1, 2], [3, 5])
+    const product2 = cartesianProduct([1,2], [3])
+    const product3 = cartesianProduct([1, 2], [3, 4])
 
-    expect(product1).toEqual([[1, 1]]);
-    expect(product2).toEqual([[1, 3], [1, 5], [2, 3], [2, 5]])
+    expect(product1).toEqual([[1, 1]])
+    expect(product2).toEqual([[1, 3],[2, 3]])
+    expect(product3).toEqual([[1, 3], [1, 4], [2, 3], [2, 4]])
   })
 })
