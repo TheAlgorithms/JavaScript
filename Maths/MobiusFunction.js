47ded10dff4a2035950d313ad72f99a91b465fb8
Akshay Dubey <akshay.dubey055@gmail.com>
2022-09-03 12:20:30 +0530

    fix: Code style fixes

--- Maths/MobiusFunction.js
+++ Maths/MobiusFunction.js
@@ -1,7 +1,7 @@
 /*
  * Author: Akshay Dubey (https://github.com/itsAkshayDubey)
  * Mobius Function: https://en.wikipedia.org/wiki/M%C3%B6bius_function
- * For any positive integer n, define μ(n) as the sum of the primitive nth roots of unity. 
+ * For any positive integer n, define μ(n) as the sum of the primitive nth roots of unity.
  * It has values in {−1, 0, 1} depending on the factorization of n into prime factors:
  *   μ(n) = +1 if n is a square-free positive integer with an even number of prime factors.
  *   μ(n) = −1 if n is a square-free positive integer with an odd number of prime factors.
@@ -11,7 +11,7 @@
 /**
  * @function mobiusFunction
  * @description -> This method returns μ(n) of given number n
- * returns 1 when number is less than or equals 1 
+ * returns 1 when number is less than or equals 1
  * or number has even number of prime factors
  * returns 0 when number has repeated prime factor
  * returns -1 when number has odd number of prime factors
@@ -21,5 +21,5 @@
 
 import { PrimeFactors } from './PrimeFactors.js'
 export const mobiusFunction = (number) => {
-  return number <= 0 ? "Number must be greater than zero." : PrimeFactors(number).length != new Set(PrimeFactors(number)).size ? 0 : PrimeFactors(number).length % 2 === 0 ? 1 : -1
+  return number <= 0 ? 'Number must be greater than zero.' : PrimeFactors(number).length !== new Set(PrimeFactors(number)).size ? 0 : PrimeFactors(number).length % 2 === 0 ? 1 : -1
 }
