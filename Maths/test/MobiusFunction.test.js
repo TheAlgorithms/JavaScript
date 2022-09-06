6f55ed4a1fa07f3e560be94dcc784215bb47fcea
Akshay Dubey <38462415+itsAkshayDubey@users.noreply.github.com>
2022-09-06 15:25:28 +0530

    algorithm: mobius function implementation (#1088)
    * feat: Add mobius function implementation
    
    * test: Add tests for mobius function
    
    * fix: Code style fixes
    
    * fix: Code style fixes
    
    * fix: Store prime factors in a variable &  add throw error
    
    * fix: Fix unit tests for zero and negative numbers
    
    * fix: Minor code style fixes

---
+++ Maths/test/MobiusFunction.test.js
@@ -0,0 +1,19 @@
+import { mobiusFunction } from '../MobiusFunction'
+
+const expectedValuesArray = [1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1, 0, 1, 1, -1, 0, 0, 1, 0, 0, -1, -1, -1, 0, 1, 1, 1, 0, -1, 1, 1, 0, -1, -1, -1, 0, 0, 1, -1, 0, 0, 0, 1, 0, -1, 0, 1, 0, 1, 1, -1, 0, -1, 1, 0, 0, 1, -1, -1, 0, 1, -1, -1, 0, -1, 1, 0, 0, 1, -1, -1, 0, 0, 1, -1, 0, 1, 1, 1, 0, -1, 0, 1, 0, 1, 1, 1, 0, -1, 0, 0, 0]
+
+describe('Testing mobius function', () => {
+  for (let i = 1; i <= 100; i++) {
+    it('Testing for number = ' + i + ', should return ' + expectedValuesArray[i], () => {
+      expect(mobiusFunction(i)).toBe(expectedValuesArray[i - 1])
+    })
+  }
+
+  it('should throw error when supplied negative numbers', () => {
+    expect(() => { mobiusFunction(-1) }).toThrow(Error)
+  })
+
+  it('should throw error when supplied zero', () => {
+    expect(() => { mobiusFunction(0) }).toThrow(Error)
+  })
+})
