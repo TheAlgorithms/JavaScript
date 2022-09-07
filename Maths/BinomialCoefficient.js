b3c013aa9dc6885b20721cd3c11f239237c8b6e0
Akshay Dubey <akshay.dubey055@gmail.com>
2022-09-07 16:40:05 +0530

    fix: Fix zero checks for n and k

--- Maths/BinomialCoefficient.js
+++ Maths/BinomialCoefficient.js
@@ -20,7 +20,7 @@ export const findBinomialCoefficient = (n, k) => {
   if ((typeof n !== 'number') || (typeof k !== 'number')) {
     throw Error('Type of arguments must be number.')
   }
-  if (n <= 0 || k <= 0) {
+  if (n < 0 || k < 0) {
     throw Error('Arguments must be greater than zero.')
   }
   let product = 1
