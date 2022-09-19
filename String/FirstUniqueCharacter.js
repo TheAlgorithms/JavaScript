0bd36044318529e3c075ff2e828f35643c1b8e7d
10kartik <kartik@plgworks.com>
2022-09-20 00:21:41 +0530

    Review changes: if-else logic

--- String/FirstUniqueCharacter.js
+++ String/FirstUniqueCharacter.js
@@ -13,8 +13,13 @@ const firstUniqChar = (str) => {
     throw new TypeError('Argument should be string')
   }
   const count = new Map()
+
   for (const char of str) {
-    count[char] ? count[char]++ : count[char] = 1
+    if (!count[char]) {
+      count[char] = 1
+    } else {
+      count[char]++
+    }
   }
   for (let i = 0; i < str.length; i++) {
     if (count[str[i]] === 1) return i
