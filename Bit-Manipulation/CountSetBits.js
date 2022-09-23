b74962555af1df5457306c385bf0eed1a259d6db
Smit Gabani <gabanismit11@gmail.com>
2022-09-23 03:44:38 -0400

    fixes #1106

---
+++ Bit-Manipulation/CountSetBits.js
@@ -0,0 +1,24 @@
+// author: Smit Gabani
+// https://github.com/smitgabani
+
+/**
+ * explanation link: https://iq.opengenus.org/brian-kernighan-algorithm/
+ * The below algorithm is called as Brian Kernighan's algorithm
+ * @param: num takes number whose number of set bit is to be found
+ * @return: the count of set bits in the binary equivalent
+ * */
+ function CountSetBits(num) {
+	'use strict' 
+	let cnt = 0;
+	while(num > 0) {
+		cnt++;
+		// num = num & (num - 1);
+		num&=(num-1);
+	}
+	return cnt;
+}
+
+// console.log(CountSetBits(10))
+// expected output: 2
+
+export { CountSetBits }
