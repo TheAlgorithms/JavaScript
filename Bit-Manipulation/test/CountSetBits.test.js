fe9555ba9e1d9f3a1fc8a33e562bf31e71276c8d
Smit Gabani <gabanismit11@gmail.com>
2022-09-27 00:48:26 -0400

    fix

--- Bit-Manipulation/test/CountSetBits.test.js
+++ Bit-Manipulation/test/CountSetBits.test.js
@@ -1,15 +1,22 @@
 import { CountSetBits } from '../CountSetBits'
 
-test('CountSetBits', () => {
-  const tests = {
-    // decimal representation: set bits
-    25: 3,
-    36: 2,
-    16: 1,
-    58: 4,
-    0: 0
-  }
-  for (const binary in tests) {
-    expect(CountSetBits(binary)).toBe(tests[binary])
-  }
+test('check CountSetBits of 25 is 3', () => {
+  const res = CountSetBits(25)
+  expect(res).toBe(3)
+})
+test('check CountSetBits of 36 is 2', () => {
+  const res = CountSetBits(36)
+  expect(res).toBe(2)
+})
+test('check CountSetBits of 16 is 1', () => {
+  const res = CountSetBits(16)
+  expect(res).toBe(1)
+})
+test('check CountSetBits of 58 is 4', () => {
+  const res = CountSetBits(58)
+  expect(res).toBe(4)
+})
+test('check CountSetBits of 0 is 0', () => {
+  const res = CountSetBits(0)
+  expect(res).toBe(0)
 })
