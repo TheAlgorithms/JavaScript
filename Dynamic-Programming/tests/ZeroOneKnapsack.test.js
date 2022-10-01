9cdf21a27a889f7ed1f2718c303c664c1a780b1c
Nitin Ramnani <nitinramnani15@gmail.com>
2022-10-01 08:46:13 +0530

    Added testcases for Zero One Knapsack

---
+++ Dynamic-Programming/tests/ZeroOneKnapsack.test.js
@@ -0,0 +1,15 @@
+import { zeroOneKnapsack } from '../ZeroOneKnapsack'
+
+describe('ZeroOneKnapsack', () => {
+  it('zeroOneKnapsack when capacity is 4 and 5 items', () => {
+    expect(zeroOneKnapsack([[1, 8], [2, 4], [3, 0], [2, 5], [2, 3]], 5, 4, [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]])).toBe(13)
+  })
+
+  it('zeroOneKnapsack when capacity is 1 and 1 items', () => {
+    expect(zeroOneKnapsack([[1, 80]], 1, 1, [[-1, -1], [-1, -1]])).toBe(80)
+  })
+
+  it('zeroOneKnapsack when capacity is 0 and 1 items', () => {
+    expect(zeroOneKnapsack([[1, 80]], 1, 0, [[-1], [-1]])).toBe(0)
+  })
+})
