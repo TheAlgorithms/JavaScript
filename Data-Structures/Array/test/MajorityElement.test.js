2d912fb4a4da0a83cee0634a26c8044e74e57609
shivamp1998 <shivampandey98@gmail.com>
2022-10-02 13:13:47 +0530

    add Majority Element

---
+++ Data-Structures/Array/test/MajorityElement.test.js
@@ -0,0 +1,9 @@
+
+import { FindMajority } from "../MajorityElement";
+
+describe('Majority element cases', () => {
+ it('no majority element in array test', () => {
+    const Array = [1,2,3,4,5];
+    expect(FindMajority(Array).toEqual(-1))
+ })
+})
