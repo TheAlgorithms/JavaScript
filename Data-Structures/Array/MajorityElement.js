c3b7b80ed1de9963a9fcef744999dc9574ee0d24
shivamp1998 <shivampandey98@gmail.com>
2022-10-02 15:09:29 +0530

    review fixes

--- Data-Structures/Array/MajorityElement.js
+++ Data-Structures/Array/MajorityElement.js
@@ -5,53 +5,50 @@
  * @flow
  */
 
-
-
 // To find the candidate for majority element
-function Candidate(array) {
-    let indexMajority = 0 ,  count = 1;
-    let size = array.length;
-
-    for (let i = 1 ; i < size ; i++) {
-        if(array[indexMajority] === array[i]) {
-            count += 1;
-        }else {
-            count -= 1;
-            if(count === 0) {
-                indexMajority = i;
-                count = 1;
-            }
-        }
+function Candidate (array) {
+  let indexMajority = 0
+  let count = 1
+  const size = array.length
+  for (let i = 1; i < size; i++) {
+    if (array[indexMajority] === array[i]) {
+      count += 1
+    } else {
+      count -= 1
+      if (count === 0) {
+        indexMajority = i
+        count = 1
+      }
     }
-    return array[indexMajority];
-} 
-
-
-//verifies if candidate occures more than two times in an array
-function isMajority(array, candidate) {
-    let count = 0;
-    let size = array.length;
-    for(let i = 0 ; i < size ; i++) {
-        if(array[i] === candidate) {
-            count += 1;
-        }
-        if(count > size / 2) {
-            return 1;
-        }
-    }
-    return 0;
+  }
+  return array[indexMajority]
 }
 
-function Majority(array) {
-    let size = array.length;
-    //finds the candidate for majority
-    let cand = Candidate(array,size);
-    //checks if its is the majority element
-    if(isMajority(array, cand)) {
-        return cand;
-    }else return -1;
-
+//  verifies if candidate occures more than two times in an array
+function isMajority (array, candidate) {
+  let count = 0
+  const size = array.length
+  for (let i = 0; i < size; i++) {
+    if (array[i] === candidate) {
+      count += 1
+    }
+    if (count > size / 2) {
+      return 1
+    }
+  }
+  return 0
 }
 
-const  FindMajority = (array) => { return Majority(array)};
+function Majority (array) {
+  //    finds the candidate for majority
+  const cand = Candidate(array)
+  //    checks if its is the majority element
+  if (isMajority(array, cand)) {
+    return cand
+  } else return -1
+}
+
+const FindMajority = (array) => {
+  return Majority(array)
+}
 export { FindMajority }
