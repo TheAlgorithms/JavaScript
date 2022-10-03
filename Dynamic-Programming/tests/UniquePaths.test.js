0496b332cf197f553a4cd89f53370eb4c7076e17
Jaiharishan <downdkar@gmail.com>
2022-10-03 20:51:15 +0530

    Fix: Names for test cases are changed

--- Dynamic-Programming/tests/UniquePaths.test.js
+++ Dynamic-Programming/tests/UniquePaths.test.js
@@ -1,18 +1,18 @@
 import { uniquePaths } from '../UniquePaths'
 
-test('Base Case 1', () => {
+test('Test case with rows=3 and cols=7', () => {
   const rows = 3
   const cols = 7
   expect(uniquePaths(rows, cols)).toBe(28)
 })
 
-test('Base Case 2', () => {
+test('Test case with rows=3 and cols=2', () => {
   const rows = 3
   const cols = 2
   expect(uniquePaths(rows, cols)).toBe(3)
 })
 
-test('Base Case 3', () => {
+test('Test case with rows=8 and cols=14', () => {
   const rows = 8
   const cols = 14
   expect(uniquePaths(rows, cols)).toBe(77520)
