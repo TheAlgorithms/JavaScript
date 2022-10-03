0de33fa67d3fccc0d300995620ccae4696a4fe2d
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2022-10-03 21:42:32 +0530

    Updated to follow Javascript Standard Style

--- Maths/CircularArc.js
+++ Maths/CircularArc.js
@@ -6,36 +6,36 @@
  * @see https://en.wikipedia.org/wiki/Degree_(angle)
  * @example degreesToRadians(45) = 0.7853981633974483
  */
- function degreesToRadians(degrees) {
-    return angle = degrees * Math.PI / 180
-}
+ function degreesToRadians (degrees) {
+    return degrees * Math.PI / 180
+  }
 
-/**
- * @function circularArcLength
- * @description calculate the length of a circular arc
- * @param {Integer} radius 
- * @param {Integer} degrees 
- * @returns {Integer} radius * angle_in _adians
- * @see https://en.wikipedia.org/wiki/Circular_arc
- * @example circularArcLength(3, 45) = 2.356194490192345
- */
-function circularArcLength(radius, degrees) {
-    return length = radius * degreesToRadians(degrees)
-}
-/**
- * @function circularArcArea
- * @description calculate the area of the sector formed by an arc
- * @param {Integer} radius 
- * @param {Integer} degrees 
- * @returns {Integer} 0.5 * r * r * angle_in_radians
- * @see https://en.wikipedia.org/wiki/Circular_arc
- * @example circularArcArea(3,45) = 3.5342917352885173
- */
-function circularArcArea(radius, degrees) {
-    return area = Math.pow(radius, 2) * degreesToRadians(degrees) / 2
-}
+  /**
+   * @function circularArcLength
+   * @description calculate the length of a circular arc
+   * @param {Integer} radius
+   * @param {Integer} degrees
+   * @returns {Integer} radius * angle_in _adians
+   * @see https://en.wikipedia.org/wiki/Circular_arc
+   * @example circularArcLength(3, 45) = 2.356194490192345
+   */
+  function circularArcLength (radius, degrees) {
+    return radius * degreesToRadians(degrees)
+  }
+  /**
+   * @function circularArcArea
+   * @description calculate the area of the sector formed by an arc
+   * @param {Integer} radius
+   * @param {Integer} degrees
+   * @returns {Integer} 0.5 * r * r * angle_in_radians
+   * @see https://en.wikipedia.org/wiki/Circular_arc
+   * @example circularArcArea(3,45) = 3.5342917352885173
+   */
+  function circularArcArea (radius, degrees) {
+    return Math.pow(radius, 2) * degreesToRadians(degrees) / 2
+  }
 
-export {
+  export {
     circularArcLength,
     circularArcArea
-}
+  }
