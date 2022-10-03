2be2bce16030dc60c71da9d26d80fdde55969b64
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2022-10-03 23:59:11 +0530

    Update CircularArc.js

--- Maths/CircularArc.js
+++ Maths/CircularArc.js
@@ -15,7 +15,7 @@ function degreesToRadians (degrees) {
    * @description calculate the length of a circular arc
    * @param {Integer} radius
    * @param {Integer} degrees
-   * @returns {Integer} radius * angle_in _adians
+   * @returns {Integer} radius * angle_in_radians
    * @see https://en.wikipedia.org/wiki/Circular_arc
    * @example circularArcLength(3, 45) = 2.356194490192345
    */
