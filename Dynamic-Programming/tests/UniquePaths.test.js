e1c8bd1582c3980d790dd62b2d4238f3a11f8bbc
Jaiharishan <downdkar@gmail.com>
2022-10-03 21:41:48 +0530

    Fix: Made the test cases to come under single Test block

--- Dynamic-Programming/tests/UniquePaths.test.js
+++ Dynamic-Programming/tests/UniquePaths.test.js
@@ -1,15 +1,7 @@
 import { uniquePaths } from '../UniquePaths'
 
-describe('UniquePaths', () => {
-  it('uniquePaths for rows=3 and cols=7', () => {
-    expect(uniquePaths(3, 7)).toBe(28)
-  })
-
-  it('uniquePaths for rows=3 and cols=2', () => {
-    expect(climbStairs(3, 2)).toBe(3)
-  })
-
-  it('uniquePaths for rows=8 and cols=14', () => {
-    expect(climbStairs(8, 14)).toBe(77520)
-  })
+test('Test case for UniquePaths', () => {
+  expect(uniquePaths(3, 7)).toBe(28)
+  expect(uniquePaths(3, 2)).toBe(3)
+  expect(uniquePaths(8, 14)).toBe(77520)
 })
