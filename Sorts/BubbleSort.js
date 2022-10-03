b5d9933a9230faa0c2606134e974b76ca581c145
samar srivastav <90980311+samarsrivastav@users.noreply.github.com>
2022-10-03 14:31:22 +0530

    Update BubbleSort.js

--- Sorts/BubbleSort.js
+++ Sorts/BubbleSort.js
@@ -63,19 +63,15 @@ export function alternativeBubbleSort (arr) {
 /* bubble sort using two for loops and no boolean terms
  */
 // Creating the bblSort function
-
  function bblSort(arr){
-    
- for(var i = 0; i < arr.length; i++){
-    
-   // Last i elements are already in place 
+ 
+     for(var i = 0; i < arr.length; i++){
+       // Last i elements are already in place 
    for(var j = 0; j < ( arr.length - i -1 ); j++){
-      
-     // Checking if the item at present iteration
+           // Checking if the item at present iteration
      // is greater than the next iteration
      if(arr[j] > arr[j+1]){
-        
-       // If the condition is true then swap them
+         // If the condition is true then swap them
        var temp = arr[j]
        arr[j] = arr[j + 1]
        arr[j+1] = temp
