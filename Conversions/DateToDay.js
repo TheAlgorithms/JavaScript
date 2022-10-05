e372c921b3a5f72a7cde936d66cdaa30ac7e328c
SINGH 1258 <sumantpaldm@gmail.com>
2022-10-05 06:47:30 -0400

    refix function of DateToDay and the tests without inbuilt methods

--- Conversions/DateToDay.js
+++ Conversions/DateToDay.js
@@ -1,44 +1,3 @@
-/*
-                    DateToDay Method
-                    ----------------
-    The DateToDay method takes a date in string format and
-    returns the name of a day. The approach behind this method
-    is very simple, we first take a string date and check
-    whether their date is valid or not, if the date is valid
-    then we do this But apply the algorithm shown below. The
-    algorithm shown below gives us the number of the day and
-    finally converts it to the name of the day.
-
-    Algorithm & Explanation : https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html
-*/
-
-// March is taken as the first month of the year.
-const calcMonthList = {
-  1: 11,
-  2: 12,
-  3: 1,
-  4: 2,
-  5: 3,
-  6: 4,
-  7: 5,
-  8: 6,
-  9: 7,
-  10: 8,
-  11: 9,
-  12: 10
-}
-
-// show the week day in a number : Sunday - Saturday => 0 - 6
-const daysNameList = { // weeks-day
-  0: 'Sunday',
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday'
-}
-
 const DateToDay = (date) => {
   // firstly, check that input is a string or not.
   if (typeof date !== 'string') {
@@ -50,14 +9,56 @@ const DateToDay = (date) => {
   if (day < 0 || day > 31 || month > 12 || month < 0) {
     return new TypeError('Date is not valid.')
   }
-  // divide year to century and yearDigit value.
-  const yearDigit = (year % 100)
-  const century = Math.floor(year / 100)
-  // Apply the algorithm shown above
-  const weekDay = Math.abs((day + Math.floor((2.6 * calcMonthList[month]) - 0.2) - (2 * century) + yearDigit + Math.floor(yearDigit / 4) + Math.floor(century / 4)) % 7)
-  // return the weekDay name.
-  return daysNameList[weekDay]
+
+  // JS months start at 0
+  return dayOfTheWeekJS(day, month - 1, year)
 }
+function dayOfTheWeekJS(day, month, year) {
+  const DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+  const DAY_1970_01_01 = 4
+  let days = day - 1
+  while (month - 1 >= 0) {
+    days += daysInMonthJS(month - 1, year)
+    month -= 1
+  }
+  while (year - 1 >= 1970) {
+    days += daysInYear(year - 1)
+    year -= 1
+  }
+  return DAYS[(days + DAY_1970_01_01) % DAYS.length]
+}
+function daysInMonthJS(month, year) {
+  const days = [
+    31, // January
+    28 + (isLeapYear(year) ? 1 : 0), // Feb,
+    31, // March
+    30, // April
+    31, // May
+    30, // June
+    31, // July
+    31, // August
+    30, // September
+    31, // October
+    30, // November
+    31 // December
+  ]
+  return days[month]
+}
+function daysInYear(year) {
+  return 365 + (isLeapYear(year) ? 1 : 0)
+}
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+}
+console.log(DateToDay('19/9/1993'))
 
 // Example : DateToDay("18/12/2020") => Friday
 
