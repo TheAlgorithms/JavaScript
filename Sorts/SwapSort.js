6942ad7f951b149990266a6fc34125f617c9db1a
Chanchal Kumar Mishra <chanchalmishra1947@gmail.com>
2022-10-06 12:25:20 +0530

    adding Swapsort algorithm and corresponding test file

---
+++ Sorts/SwapSort.js
@@ -0,0 +1,30 @@
+/**
+ * @function SwapSort
+ * @description Swap Sort is an algorithm to find the number of swaps required to sort an array.
+    Time complexity of Swap Sort Algorithm is O(nlogn).
+    Auxiliary Space required for Swap Sort Algorithm is O(n).
+ * @param {Integer[]} items - Array of integers
+ * @return {Integer} - Number of swaps required to sort the array.
+ */
+
+export function swapSort (items) {
+  const sortedArray = items.slice()
+  sortedArray.sort()
+  const indexMap = {}
+  for (let i = 0; i < items.length; i++) {
+    indexMap[items[i]] = i
+  }
+  let swaps = 0
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] !== sortedArray[i]) {
+      const temp = items[i]
+      items[i] = items[indexMap[sortedArray[i]]]
+      items[indexMap[sortedArray[i]]] = temp
+
+      indexMap[temp] = indexMap[sortedArray[i]]
+      indexMap[sortedArray[i]] = i
+      swaps++
+    }
+  }
+  return swaps
+}
