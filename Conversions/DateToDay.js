f437c62c3b7d55bdfe6a316863e14e5edd60d0a8
Sharan Sukesh <77352136+sharansukesh1003@users.noreply.github.com>
2022-10-07 10:51:56 +0530

    Update DateToDay.js
    Made year--

--- Conversions/DateToDay.js
+++ Conversions/DateToDay.js
@@ -31,7 +31,7 @@ const DateToDay = (date) => {
     return new TypeError('Date is not valid.')
   }
   // date is resolved based on Zeller's congruence.
-  if (month < 3) { --year; month += 12 }
+  if (month < 3) { year--; month += 12 }
   const century = Math.floor(year / 100)
   year %= 100
   const weekDay = (year + Math.floor(year / 4) + Math.floor(century / 4) - 2 * century + Math.floor((26 * (month + 1)) / 10) + day - 1) % 7
