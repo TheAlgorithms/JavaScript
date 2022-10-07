8461271fc8deafb02c77f39e1ac594c953770d07
Abdul Ahad Khan <aahad6674@gmail.com>
2022-10-07 17:16:28 +0530

    added check for Integer (#1137)

--- Bit-Manipulation/BinaryCountSetBits.js
+++ Bit-Manipulation/BinaryCountSetBits.js
@@ -9,6 +9,11 @@
 
 function BinaryCountSetBits (a) {
   'use strict'
+
+  // check whether input is an integer, some non-integer number like, 21.1 have non-terminating binary expansions and hence their binary expansion will contain infinite ones, thus the handling of non-integers (including strings,objects etc. as it is meaningless) has been omitted
+
+  if (!Number.isInteger(a)) throw new TypeError('Argument not an Integer')
+
   // convert number into binary representation and return number of set bits in binary representation
   return a.toString(2).split('1').length - 1
 }
