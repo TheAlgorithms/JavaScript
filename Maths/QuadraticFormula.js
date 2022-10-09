a74ba8e05b777cf23b9cb1d4aa16713fb9168b9a
YorLecaros <yorginalecaros@outlook.es>
2022-10-09 12:28:51 -0500

    [UPDATE] added new validation

--- Maths/QuadraticFormula.js
+++ Maths/QuadraticFormula.js
@@ -18,10 +18,13 @@ const solveQuadraticEquation = (a, b, c) => {
   const discriminant = (b ** 2) - (4 * a * c)
   const denominator = 2 * a
   let answer = []
-  if (discriminant >= 0) {
+  if (discriminant > 0) {
     const x1 = (-b + Math.sqrt(discriminant)) / denominator
     const x2 = (-b - Math.sqrt(discriminant)) / denominator
     answer = [x1, x2]
+  } else if (discriminant === 0) {
+    const x = (-b) / denominator
+    answer = [x]
   }
   return answer
 }
--- Maths/test/QuadraticFormula.test.js
+++ Maths/test/QuadraticFormula.test.js
@@ -3,6 +3,7 @@ import { solveQuadraticEquation } from '../QuadraticFormula'
 test('Quadratic Equation', () => {
   expect(solveQuadraticEquation(1, -3, -4)).toStrictEqual([4, -1])
   expect(solveQuadraticEquation(1, 5, 6)).toStrictEqual([-2, -3])
+  expect(solveQuadraticEquation(3, 24, 48)).toStrictEqual([-4])
   expect(solveQuadraticEquation(1, -3, 8)).toStrictEqual([])
   expect(solveQuadraticEquation(1, -2, 9)).toStrictEqual([])
 })
