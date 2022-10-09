19691c667f3feee05edc69f20a236689cfa964d2
YorLecaros <yorginalecaros@outlook.es>
2022-10-09 11:44:10 -0500

    [UPDATE] Return type and function name changed, improved test file

--- Maths/QuadraticFormula.js
+++ Maths/QuadraticFormula.js
@@ -4,28 +4,26 @@
  * @param {number} a
  * @param {number} b
  * @param {number} c
- * @return {string}
+ * @return {array}
  * @see https://en.wikipedia.org/wiki/Quadratic_formula
- * @example quadraticFormula(1, -3, -4) = 'the roots are X1=4 and X2=-1'
- * @example quadraticFormula(1, 5, 6) = 'the roots are X1=-2 and X2=-3'
- * @example quadraticFormula(1, -3, 8) = 'the roots do not exist or the roots are imaginary'
+ * @example quadraticFormula(1, -3, -4) = [4, -1]
+ * @example quadraticFormula(1, 5, 6) = [-2, -3]
+ * @example quadraticFormula(1, -3, 8) = []
  */
 
-const quadraticFormula = (a, b, c) => {
+const solveQuadraticEquation = (a, b, c) => {
   if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
     return new TypeError('Some argument is not a number.')
   }
   const discriminant = (b ** 2) - (4 * a * c)
   const denominator = 2 * a
-  let answer = ''
+  let answer = []
   if (discriminant >= 0) {
     const x1 = (-b + Math.sqrt(discriminant)) / denominator
     const x2 = (-b - Math.sqrt(discriminant)) / denominator
-    answer = `the roots are X1=${x1} and X2=${x2}`
-  } else {
-    answer = 'the roots do not exist or the roots are imaginary'
+    answer = [x1, x2]
   }
   return answer
 }
 
-export { quadraticFormula }
+export { solveQuadraticEquation }
--- Maths/test/QuadraticFormula.test.js
+++ Maths/test/QuadraticFormula.test.js
@@ -1,21 +1,8 @@
-import { quadraticFormula } from '../QuadraticFormula'
+import { solveQuadraticEquation } from '../QuadraticFormula'
 
-test('Testing on quadraticFormula', () => {
-  const quadraticForm = quadraticFormula(1, -3, -4)
-  expect(quadraticForm).toBe('the roots are X1=4 and X2=-1')
-})
-
-test('Testing on quadraticFormula', () => {
-  const quadraticForm = quadraticFormula(1, 5, 6)
-  expect(quadraticForm).toBe('the roots are X1=-2 and X2=-3')
-})
-
-test('Testing on quadraticFormula', () => {
-  const quadraticForm = quadraticFormula(1, -3, 8)
-  expect(quadraticForm).toBe('the roots do not exist or the roots are imaginary')
-})
-
-test('Testing on quadraticFormula', () => {
-  const quadraticForm = quadraticFormula(1, -2, 9)
-  expect(quadraticForm).toBe('the roots do not exist or the roots are imaginary')
+test('Quadratic Equation', () => {
+  expect(solveQuadraticEquation(1, -3, -4)).toStrictEqual([4, -1])
+  expect(solveQuadraticEquation(1, 5, 6)).toStrictEqual([-2, -3])
+  expect(solveQuadraticEquation(1, -3, 8)).toStrictEqual([])
+  expect(solveQuadraticEquation(1, -2, 9)).toStrictEqual([])
 })
