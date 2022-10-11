2d395ec5c696bf0ab150a6e689c3639fa238e01b
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-10-11 23:28:25 +0200

    :white_check_mark: tests added for Problem017

---
+++ Project-Euler/test/Problem017.test.js
@@ -0,0 +1,45 @@
+import { countNumberWordLength } from '../Problem017.js'
+
+describe('Number letter count', () => {
+  const textFixture = [
+    {
+      name: 'should throw invalid input (NaN) error',
+      input: 'test',
+      error: 'Invalid input, please provide valid number'
+    },
+    {
+      name: 'should throw error if number < 1',
+      input: 0,
+      error: 'Please provide number greater that 1'
+    },
+    {
+      name: 'if the number provided is 5',
+      input: 5,
+      expected: 19
+    },
+    {
+      name: 'if the number provided is 100',
+      input: 100,
+      expected: 864
+    },
+    {
+      name: 'if the number provided is 1000',
+      input: 1000,
+      expected: 21124
+    }
+  ]
+
+  // it('should be invalid input if number is negative', () => {
+  //   expect(() => nthPrime(-3)).toThrowError('Invalid Input')
+  // })
+
+  for (const tt of textFixture) {
+    test(tt.name, () => {
+      if (tt.error) {
+        expect(() => countNumberWordLength(tt.input)).toThrowError(tt.error)
+      } else {
+        expect(countNumberWordLength(tt.input)).toBe(tt.expected)
+      }
+    })
+  }
+})
