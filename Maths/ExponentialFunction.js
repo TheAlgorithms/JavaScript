7b4ff6eb1f9c97f0a49781045789910d89b2f46b
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2022-10-11 13:53:40 +0530

    Add checks for invalid inputs

--- Maths/ExponentialFunction.js
+++ Maths/ExponentialFunction.js
@@ -5,10 +5,13 @@ let factorial = 1
  * @description Calculates the n+1 th order Taylor series approximation of exponential function e^x given n
  * @param {Integer} power
  * @param {Integer} order - 1
- * @returns exponentialFunction(18,4) = 18.4
+ * @returns exponentialFunction(2,20) = 7.389056098930604
  * @url https://en.wikipedia.org/wiki/Exponential_function
  */
 function exponentialFunction (power, n) {
+  if (isNaN(power) || isNaN(n) || n < 0) {
+    throw new TypeError('Invalid Input')
+  }
   if (n === 0) { return 1 }
   const recursion = exponentialFunction(power, n - 1)
   powerOfX = powerOfX * power
--- Maths/test/ExponentialFunction.test.js
+++ Maths/test/ExponentialFunction.test.js
@@ -1,7 +1,15 @@
 import { exponentialFunction } from '../ExponentialFunction'
 
-describe('exponentialFunction', () => {
-  it('with a power of 5 and order of 21', () => {
+describe('Tests for exponential function', () => {
+  it('should be a function', () => {
+    expect(typeof exponentialFunction).toEqual('function')
+  })
+
+  it('should throw error for invalid input', () => {
+    expect(() => exponentialFunction(2, -34)).toThrow()
+  })
+
+  it('should return the exponential function of power of 5 and order of 21', () => {
     const ex = exponentialFunction(5, 20)
     expect(ex).toBe(148.4131470673818)
   })
