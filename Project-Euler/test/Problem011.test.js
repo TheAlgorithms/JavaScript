3b49f5746b96574881d040a922c0b9cf8c8752e8
Matheus Muriel <matheus.muriel@outlook.com>
2022-10-12 18:31:15 -0300

    Included Project Euler 11 solution

---
+++ Project-Euler/test/Problem011.test.js
@@ -0,0 +1,8 @@
+import { largestProductInAGrid } from '../Problem011.js'
+
+describe('Checking Largest Product In A Grid', () => {
+  // Project Euler Condition Check
+  test('Test Euler Condition', () => {
+    expect(largestProductInAGrid()).toBe(70600674)
+  })
+})
