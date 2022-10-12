141d104234f7c5f8a088fb99a1d9bba4cc6eed52
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-10-12 11:09:47 +0200

    test: refractored

--- Project-Euler/test/Problem017.test.js
+++ Project-Euler/test/Problem017.test.js
@@ -1,41 +1,11 @@
 import { countNumberWordLength } from '../Problem017.js'
 
 describe('Number letter count', () => {
-  const textFixture = [
-    {
-      name: 'should throw invalid input (NaN) error',
-      input: 'test',
-      error: 'Invalid input, please provide valid number'
-    },
-    {
-      name: 'should throw error if number < 1',
-      input: 0,
-      error: 'Please provide number greater that 1'
-    },
-    {
-      name: 'if the number provided is 5',
-      input: 5,
-      expected: 19
-    },
-    {
-      name: 'if the number provided is 100',
-      input: 100,
-      expected: 864
-    },
-    {
-      name: 'if the number provided is 1000',
-      input: 1000,
-      expected: 21124
-    }
-  ]
+  test.each([[5, 19], [100, 864], [1000, 21124]])('%i! = %i', (n, expected) => {
+    expect(countNumberWordLength(n)).toBe(expected)
+  })
 
-  for (const tt of textFixture) {
-    test(tt.name, () => {
-      if (tt.error) {
-        expect(() => countNumberWordLength(tt.input)).toThrowError(tt.error)
-      } else {
-        expect(countNumberWordLength(tt.input)).toBe(tt.expected)
-      }
-    })
-  }
+  test.each([['test', 'Invalid input, please provide valid number'], [0, 'Please provide number greater that 1']])('%i! = %i', (n, expected) => {
+    expect(() => countNumberWordLength(n)).toThrowError(expected)
+  })
 })
