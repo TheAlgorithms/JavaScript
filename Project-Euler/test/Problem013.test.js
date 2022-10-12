3a0d8b899eb58d225362837f21a355f322ad27f7
Matheus Muriel <matheus.muriel@outlook.com>
2022-10-12 18:33:47 -0300

    Included Project Euler 13 solution

---
+++ Project-Euler/test/Problem013.test.js
@@ -0,0 +1,8 @@
+import { largeSum } from '../Problem013.js'
+
+describe('checking Large Sum', () => {
+  // Project Euler Condition Check
+  test('Test Euler Condition', () => {
+    expect(largeSum()).toBe('5537376230')
+  })
+})
