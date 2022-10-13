06af73a388e683345f7bf2d1e04127d45a88cc80
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-10-14 01:41:07 +0200

    Fixes: #1183

--- String/test/ValidateUrl.test.js
+++ String/test/ValidateUrl.test.js
@@ -2,21 +2,24 @@ import { validateURL } from '../ValidateUrl'
 
 describe('ValidateUrl', () => {
   test.each([
-    ['http://www.google.com', true],
-    ['https://www.google.com', true],
-    ['www.google.com', true],
-    ['google.com', true],
-    ['http://wwwdomain.com', true]
+    ['http://www.google.com'],
+    ['https://www.google.com'],
+    ['www.google.com'],
+    ['google.com'],
+    ['http://wwwdomain.com']
 
-  ])('Should return validate=true for URL %s', (n, expected) => {
-    expect(validateURL(n)).toBe(expected)
+  ])('Should return validate=true for URL %s', (n) => {
+    expect(validateURL(n)).toBe(true)
   })
 
   test.each([
-    ['google', false],
-    ['link: https://www.google.com', false]
-
-  ])('Should return validate=false for URL %s', (n, expected) => {
-    expect(validateURL(n)).toBe(expected)
+    ['google'],
+    ['link: https://www.google.com'],
+    ['www.foufos'],
+    ['http://foufos'],
+    ['www.mp3#.com'],
+    ['www.-foufos.gr']
+  ])('Should return validate=false for URL %s', (n) => {
+    expect(validateURL(n)).toBe(false)
   })
 })
