bbb32da4585cec9d0eb3b894bd14b2e88fe9b4f1
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-10-14 01:36:17 +0200

    :white_check_mark: test cases suit updated, more tests added

--- String/test/ValidateUrl.test.js
+++ String/test/ValidateUrl.test.js
@@ -1,14 +1,22 @@
 import { validateURL } from '../ValidateUrl'
 
 describe('ValidateUrl', () => {
-  it('expects to return false', () => {
-    expect(validateURL('google')).toEqual(false)
-    expect(validateURL('link: https://www.google.com')).toEqual(false)
+  test.each([
+    ['http://www.google.com', true],
+    ['https://www.google.com', true],
+    ['www.google.com', true],
+    ['google.com', true],
+    ['http://wwwdomain.com', true]
+
+  ])('Should return validate=true for URL %s', (n, expected) => {
+    expect(validateURL(n)).toBe(expected)
   })
 
-  it('expects to return true', () => {
-    expect(validateURL('http://www.google.com')).toEqual(true)
-    expect(validateURL('https://www.google.com')).toEqual(true)
-    expect(validateURL('www.google.com')).toEqual(true)
+  test.each([
+    ['google', false],
+    ['link: https://www.google.com', false]
+
+  ])('Should return validate=false for URL %s', (n, expected) => {
+    expect(validateURL(n)).toBe(expected)
   })
 })
