810091c7ce60b687e2c7566f90b7bff80362d7ff
EduardoLariosEspinoza <elariosespinoza2@gmail.com>
2022-10-15 13:50:24 -0500

    first knight condicional

--- Backtracking/KnightTour.js
+++ Backtracking/KnightTour.js
@@ -1,12 +1,12 @@
 // Wikipedia: https://en.wikipedia.org/wiki/Knight%27s_tour
 
 class OpenKnightTour {
-  constructor (size) {
+  constructor(size) {
     this.board = new Array(size).fill(0).map(() => new Array(size).fill(0))
     this.size = size
   }
 
-  getMoves ([i, j]) {
+  getMoves([i, j]) {
     // helper function to get the valid moves of the knight from the current position
     const moves = [
       [i + 2, j - 1],
@@ -22,22 +22,22 @@ class OpenKnightTour {
     return moves.filter(([y, x]) => y >= 0 && y < this.size && x >= 0 && x < this.size)
   }
 
-  isComplete () {
+  isComplete() {
     // helper function to check if the board is complete
     return !this.board.map(row => row.includes(0)).includes(true)
   }
 
-  solve () {
+  solve() {
     // function to find the solution for the given board
     for (let i = 0; i < this.size; i++) {
       for (let j = 0; j < this.size; j++) {
-        if (this.solveHelper([i, j], 0)) return true
+        return this.solveHelper([i, j], 0)
       }
     }
     return false
   }
 
-  solveHelper ([i, j], curr) {
+  solveHelper([i, j], curr) {
     // helper function for the main computation
     if (this.isComplete()) return true
 
@@ -45,14 +45,14 @@ class OpenKnightTour {
       if (this.board[y][x] === 0) {
         this.board[y][x] = curr + 1
         if (this.solveHelper([y, x], curr + 1)) return true
-        // backtracking
+          // backtracking
         this.board[y][x] = 0
       }
     }
     return false
   }
 
-  printBoard (output = value => console.log(value)) {
+  printBoard(output = value => console.log(value)) {
     // utility function to display the board
     for (const row of this.board) {
       let string = ''
