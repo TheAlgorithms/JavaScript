0c6163278ebd98ec55311e423cc187cd3b923c0e
Janith Thenuka Herath <janithherath2@gmail.com>
2022-10-15 07:56:34 -0700

    Update ackerman.test.js

--- Recursive/test/ackerman.test.js
+++ Recursive/test/ackerman.test.js
@@ -1,9 +1,8 @@
 import { ackerman } from '../Ackerman'
 
-// More concise, plus we get test name formatting
 test("Ackerman", () => {
-  test.each([[0, 0, 1], [1, 0, 2], [1, 1, 3], [1, 2, 4]])('%i! = %i', (m, n, expected) => {
-    expect(ackerman(m,n)).toBe(expected);
-  });
+  expect(ackerman(0,0)).toBe(1);
+  expect(ackerman(1,0)).toBe(2);
+  expect(ackerman(1,1)).toBe(3);
+  expect(ackerman(1,2)).toBe(4);
 });
-
