ac839d962d83ad9e38f60649bea8da708cc42786
hari <harinath.b.07@gmail.com>
2022-10-15 22:13:58 +0530

    Added test for infix to postfix conversion

---
+++ Data-Structures/Stack/test/ToPostfix.test.js
@@ -0,0 +1,12 @@
+import { ToPostfix } from "../ToPostfix";
+
+describe('ToPostfix', () => {
+    it('Convert the given infix expression to postfix', () => {
+        const infix = "a*b+(c-d/e)";
+        const postfix = ToPostfix(infix);
+        const expected = "ab*cde/-+";
+
+        expect(postfix).toEqual(expected);
+
+    })
+})
