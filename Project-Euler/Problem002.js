ecbb0651c00d8fc00030fac7b53a68693502133e
Omkarnath Parida <omkarnath.parida@yocket.in>
2022-10-16 19:28:51 +0530

    👌 IMPROVE: Added testcases for Project Euler Problem 2

--- Project-Euler/Problem002.js
+++ Project-Euler/Problem002.js
@@ -5,8 +5,10 @@ const PHI = (1 + SQ5) / 2 // definition of PHI
 // theoretically it should take O(1) constant amount of time as long
 // arithmetic calculations are considered to be in constant amount of time
 export const EvenFibonacci = (limit) => {
+  if (limit < 1) throw new Error('Fibonacci sequence limit can\'t be lesser than 1')
+
   const highestIndex = Math.floor(Math.log(limit * SQ5) / Math.log(PHI))
   const n = Math.floor(highestIndex / 3)
-  return ((PHI ** (3 * n + 3) - 1) / (PHI ** 3 - 1) -
-    ((1 - PHI) ** (3 * n + 3) - 1) / ((1 - PHI) ** 3 - 1)) / SQ5
+  return Math.floor(((PHI ** (3 * n + 3) - 1) / (PHI ** 3 - 1) -
+    ((1 - PHI) ** (3 * n + 3) - 1) / ((1 - PHI) ** 3 - 1)) / SQ5)
 }
