7c860d76eb5bbf76baa67cadd6b9bf275c8469eb
hari <harinath.b.07@gmail.com>
2022-10-16 22:11:55 +0530

    code formatted properly

--- Data-Structures/Stack/test/ToPostfix.test.js
+++ Data-Structures/Stack/test/ToPostfix.test.js
@@ -1,12 +1,10 @@
-import { ToPostfix } from "../ToPostfix";
+import { ToPostfix } from "../ToPostfix"
 
 describe('ToPostfix', () => {
-    it('Convert the given infix expression to postfix', () => {
-        const infix = "a*b+(c-d/e)";
-        const postfix = ToPostfix(infix);
-        const expected = "ab*cde/-+";
-
-        expect(postfix).toEqual(expected);
-
-    })
+  it('Convert the given infix expression to postfix', () => {
+    const infix = 'a*b+(c-d/e)'
+    const postfix = ToPostfix(infix)
+    const expected = 'ab*cde/-+'
+    expect(postfix).toEqual(expected)
+  })
 })
