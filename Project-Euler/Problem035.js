f5f8d3695be69dc5f4fbf316cb17c7ce3677d291
Daniel Dorado <danieldorado27@gmail.com>
2022-10-17 11:07:03 -0500

    [UPDATE] Some algorithm changes

--- Project-Euler/Problem035.js
+++ Project-Euler/Problem035.js
@@ -15,14 +15,16 @@
  * This function also discards all numbers that contain an even digit
  * @see {@link https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes}
  */
-function getPrimes (max) {
+function getOddPrimes (max) {
   const sieve = []
   const primes = []
 
   for (let i = 2; i <= max; ++i) {
     if (!sieve[i]) { // If i has not been marked then it is prime
       const hasEven = String(i).split('').some((n) => n % 2 === 0) // Check if i contains an even digit
-      !hasEven && primes.push(i)
+      if (!hasEven) {
+        primes.push(i)
+      }
 
       for (let j = i << 1; j <= max; j += i) { // Mark all multiples of i as non-prime
         sieve[j] = true
@@ -34,17 +36,19 @@ function getPrimes (max) {
 
 function problem35 (n) {
   if (n < 2) {
-    throw new Error('Invalid Input')
+    throw new Error('Invalid input')
   }
-  const list = getPrimes(n)
+  const list = getOddPrimes(n)
 
   const result = list.filter((number, _idx, arr) => {
     const str = String(number)
-    const rotations = []
     for (let i = 0; i < str.length; i++) { // Get all rotations of the number
-      rotations.push(str.slice(i) + str.slice(0, i))
+      const rotation = str.slice(i) + str.slice(0, i)
+      if (!arr.includes(Number(rotation))) { // Check if the rotation is prime
+        return false
+      }
     }
-    return rotations.every((rotation) => arr.includes(Number(rotation))) // Check if all rotations are prime
+    return true // If all rotations are prime, then the number is circular prime
   })
 
   return result.length + 1 // Add +1 to the result because 2 is not included in the list
