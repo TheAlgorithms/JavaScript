6338ad2ac079c2285ea1f3c2c5520c4d2a80c573
SczSca <aalvarez59@ucol.mx>
2022-10-16 23:27:58 -0500

    Invert Array Data Structures

--- Data-Structures/Array/InvertArray.js
+++ Data-Structures/Array/InvertArray.js
@@ -1,12 +1,12 @@
-/* https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/ here explains ways to solve this problem in other languages*/
+/* https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/ here explains ways to solve this problem in other languages */
 const invertArr = (arr) => {
-    let temp = 0;
-    for(let i = 0, j = arr.length - 1; i < arr.length/2; i++, j--){ //LIMIT SPECIFIES THE AMOUNT OF INVERT ACTIONS
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        
+    let temp = 0
+    for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) { // LIMIT SPECIFIES THE AMOUNT OF INVERT ACTIONS
+      temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
     }
-    return arr;
-}
-export {invertArr};
+    return arr
+  }
+  export { invertArr }
+  
