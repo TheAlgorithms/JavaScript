5c3058ee7cb81fad31de5dbd8ce2ef1283af6d37
SczSca <aalvarez59@ucol.mx>
2022-10-16 23:30:04 -0500

    Invert Array Data-Structures

--- Data-Structures/Array/test/InvertArray.test.js
+++ Data-Structures/Array/test/InvertArray.test.js
@@ -5,10 +5,12 @@ describe('invert Array', () => {
     const Array = []
     expect(invertArr(Array)).toEqual([])
   })
+
   it('returns same array when array given has 1 element', () => {
     const Array = [1]
     expect(invertArr(Array)).toEqual([1])
   })
+
   it('returns inverted array when array given has +1 elements', () => {
     const Array = [1, 2, 3, 4, 5, 6]
     expect(invertArr(Array)).toEqual([6, 5, 4, 3, 2, 1])
