8e62fb6c09042b51905c481d6ba89538b0d39a12
SczSca <aalvarez59@ucol.mx>
2022-10-18 17:39:28 -0500

    leaving just jest-each on .test.js

--- Data-Structures/Array/test/InvertArray.test.js
+++ Data-Structures/Array/test/InvertArray.test.js
@@ -10,15 +10,4 @@ describe('reverse elements in an array', () => {
   `.test('returns $expected when given $array', ({ array, expected }) => {
       expect(Reverse(array)).toEqual(expected)
     })
-  it(' returns an empty array when array with 0 elements given', () => {
-    expect(Reverse([])).toEqual([])
-  })
-
-  it('returns same array when array given has 1 element', () => {
-    expect(Reverse([1])).toEqual([1])
-  })
-
-  it('returns inverted array when array given has +1 elements', () => {
-    expect(Reverse([1, 2, 3, 4, 5, 6])).toEqual([6, 5, 4, 3, 2, 1])
-  })
 })
