807bbc03f9f2f4d352f405cf46db5c70e195df79
hiitesh1127 <hiteshkk49@gmail.com>
2022-10-19 03:01:18 -0400

    update Directory.md

--- Dynamic-Programming/UniquePaths.js
+++ Dynamic-Programming/UniquePaths.js
@@ -3,12 +3,12 @@
  *
  *  Unique Paths
  *
- *  There is a robot on an `m x n` grid. 
- *  The robot is initially located at the top-left corner. 
- *  The robot tries to move to the bottom-right corner. 
+ *  There is a robot on an `m x n` grid.
+ *  The robot is initially located at the top-left corner.
+ *  The robot tries to move to the bottom-right corner.
  *  The robot can only move either down or right at any point in time.
- * 
- *  Given the two integers `m` and `n`, 
+ *
+ *  Given the two integers `m` and `n`,
  *  return the number of possible unique paths that the robot can take to reach the bottom-right corner.
  *  More info: https://leetcode.com/problems/unique-paths/
  */
--- Dynamic-Programming/tests/UniquePaths.test.js
+++ Dynamic-Programming/tests/UniquePaths.test.js
@@ -2,12 +2,12 @@ import { uniquePaths } from '../UniquePaths'
 
 describe('Unique Paths', () => {
   it('should return 28 when m is 3 and n is 7', () => {
-    const data = uniquePaths(3, 7);
-    expect(data).toBe(28);
+    const data = uniquePaths(3, 7)
+    expect(data).toBe(28)
   })
 
   it('should return 48620 when m is 10 and n is 10', () => {
-    const data = uniquePaths(10, 10);
-    expect(data).toBe(48620);
+    const data = uniquePaths(10, 10)
+    expect(data).toBe(48620)
   })
 })
