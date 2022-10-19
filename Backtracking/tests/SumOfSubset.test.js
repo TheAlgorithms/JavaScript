d76518219c1bcca683f38a123599a69ada1c99ff
Hitesh Gupta <72217750+hiitesh1127@users.noreply.github.com>
2022-10-19 11:30:51 -0400

    follow standard

--- Backtracking/tests/SumOfSubset.test.js
+++ Backtracking/tests/SumOfSubset.test.js
@@ -5,9 +5,7 @@ describe('SumOfSubset', () => {
     // W = [2, 5, 7, 8, 12, 16, 23, 40]
     // K = 25
 
-    const nums = [2, 5, 7, 8, 12, 16, 23, 40]
-
-    const subsets = sumOfSubset(nums, [], 0, 0, 25)
+    const subsets = sumOfSubset([2, 5, 7, 8, 12, 16, 23, 40], [], 0, 0, 25)
 
     expect(subsets).toEqual([
       [2, 7, 16],
