ec01d3811cc687c9531880c8445f2f42031df8f7
hiitesh1127 <hiteshkk49@gmail.com>
2022-10-19 15:30:38 -0400

    suggested changes

--- Maths/ModularArithmetic.js
+++ Maths/ModularArithmetic.js
@@ -7,46 +7,50 @@ import { extendedEuclideanGCD } from './ExtendedEuclideanGCD'
  * @returns {Number}
  */
 
-const MOD = 10000007
+export class ModRing {
+  constructor (MOD) {
+    this.MOD = MOD
+  }
 
-const isInputValid = (arg1, arg2) => {
-  if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
-    throw new TypeError('Input must be Numbers')
+  isInputValid = (arg1, arg2) => {
+    if (!this.MOD) {
+      throw new Error('Modulus must be initialized in the object constructor')
+    }
+    if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
+      throw new TypeError('Input must be Numbers')
+    }
+  }
+  /**
+   * Modulus is Distributive property,
+   * As a result, we separate it into numbers in order to keep it within MOD's range
+  */
+
+  add = (arg1, arg2) => {
+    this.isInputValid(arg1, arg2)
+    return ((arg1 % this.MOD) + (arg2 % this.MOD)) % this.MOD
+  }
+
+  subtract = (arg1, arg2) => {
+    this.isInputValid(arg1, arg2)
+    // An extra MOD is added to check negative results
+    return ((arg1 % this.MOD) - (arg2 % this.MOD) + this.MOD) % this.MOD
+  }
+
+  multiply = (arg1, arg2) => {
+    this.isInputValid(arg1, arg2)
+    return ((arg1 % this.MOD) * (arg2 % this.MOD)) % this.MOD
+  }
+
+  /**
+   *
+   * It is not Possible to find Division directly like the above methods,
+   * So we have to use the Extended Euclidean Theorem for finding Multiplicative Inverse
+   * https://github.com/TheAlgorithms/JavaScript/blob/master/Maths/ExtendedEuclideanGCD.js
+   */
+
+  divide = (arg1, arg2) => {
+    // 1st Index contains the required result
+    // The theorem may have return Negative value, we need to add MOD to make it Positive
+    return (extendedEuclideanGCD(arg1, arg2)[1] + this.MOD) % this.MOD
   }
 }
-
-/**
- * Modulus is Distributive property,
- * As a result, we separate it into numbers in order to keep it within MOD's range
-*/
-
-const add = (arg1, arg2) => {
-  isInputValid(arg1, arg2)
-  return ((arg1 % MOD) + (arg2 % MOD)) % MOD
-}
-
-const subtract = (arg1, arg2) => {
-  isInputValid(arg1, arg2)
-  // An extra MOD is added to check negative results
-  return ((arg1 % MOD) - (arg2 % MOD) + MOD) % MOD
-}
-
-const multiply = (arg1, arg2) => {
-  isInputValid(arg1, arg2)
-  return ((arg1 % MOD) * (arg2 % MOD)) % MOD
-}
-
-/**
- *
- * It is not Possible to find Division directly like the above methods,
- * So we have to use the Extended Euclidean Theorem for finding Multiplicative Inverse
- * https://github.com/TheAlgorithms/JavaScript/blob/master/Maths/ExtendedEuclideanGCD.js
- */
-
-const divide = (arg1, arg2) => {
-  // 1st Index contains the required result
-  // The theorem may have return Negative value, we need to add MOD to make it Positive
-  return (extendedEuclideanGCD(arg1, arg2)[1] + MOD) % MOD
-}
-
-export { add, subtract, multiply, divide }
--- Maths/test/ModularArithmetic.test.js
+++ Maths/test/ModularArithmetic.test.js
@@ -1,39 +1,45 @@
-import { add, subtract, multiply, divide } from '../ModularArithmetic'
+import { ModRing } from '../ModularArithmetic'
 
 describe('Modular Arithmetic', () => {
+  const MOD = 10000007
+  let ring
+  beforeEach(() => {
+    ring = new ModRing(MOD)
+  })
+
   describe('add', () => {
     it('Should return 9999993 for 10000000 and 10000000', () => {
-      expect(add(10000000, 10000000)).toBe(9999993)
+      expect(ring.add(10000000, 10000000)).toBe(9999993)
     })
     it('Should return 9999986 for 10000000 and 20000000', () => {
-      expect(add(10000000, 20000000)).toBe(9999986)
+      expect(ring.add(10000000, 20000000)).toBe(9999986)
     })
   })
 
   describe('subtract', () => {
     it('Should return 1000000 for 10000000 and 9000000', () => {
-      expect(subtract(10000000, 9000000)).toBe(1000000)
+      expect(ring.subtract(10000000, 9000000)).toBe(1000000)
     })
     it('Should return 7 for 10000000 and 20000000', () => {
-      expect(subtract(10000000, 20000000)).toBe(7)
+      expect(ring.subtract(10000000, 20000000)).toBe(7)
     })
   })
 
   describe('multiply', () => {
     it('Should return 1000000 for 100000 and 10000', () => {
-      expect(multiply(100000, 10000)).toBe(9999307)
+      expect(ring.multiply(100000, 10000)).toBe(9999307)
     })
     it('Should return 7 for 100000 and 10000100', () => {
-      expect(multiply(10000000, 20000000)).toBe(98)
+      expect(ring.multiply(10000000, 20000000)).toBe(98)
     })
   })
 
   describe('divide', () => {
     it('Should return 4 for 3 and 11', () => {
-      expect(divide(3, 11)).toBe(4)
+      expect(ring.divide(3, 11)).toBe(4)
     })
     it('Should return 2 for 18 and 7', () => {
-      expect(divide(18, 7)).toBe(2)
+      expect(ring.divide(18, 7)).toBe(2)
     })
   })
 })
