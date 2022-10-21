97f4bdfb3c0aeb15e5d1bfb1aab94f1db5dc5055
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-10-21 21:57:26 +0000

    Removed unnecessary variable assignment from tests
    Thank you for noticing this unnecessary piece of code. I tried to see how others wrote the tests but in this case, assigning something that immediately gets compared is an extra step that doesn't make a difference.

--- String/test/CheckHeterogram.test.js
+++ String/test/CheckHeterogram.test.js
@@ -8,31 +8,24 @@ describe('Testing checkHeterogram', () => {
     expect(() => checkHeterogram(true)).toThrow()
   })
   it('expects to return true if the argument is a heterogram', () => {
-    const SUT = checkHeterogram('hero')
-    expect(SUT).toBe(true)
+    expect(checkHeterogram('hero')).toBe(true)
   })
   it('expects to return true if the given argument only contains non-alphabetic characters', () => {
-    const SUT = checkHeterogram('!!..&&  ??22')
-    expect(SUT).toBe(true)
+    expect(checkHeterogram('!!..&&  ??22')).toBe(true)
   })
   it('expects to return true if the given string is a heterogram which contains duplicate non-alphabetic characters', () => {
-    const SUT = checkHeterogram('C, o, m, p, u, t, e, r!!')
-    expect(SUT).toBe(true)
+    expect(checkHeterogram('C, o, m, p, u, t, e, r!!')).toBe(true)
   })
   it('expects to return true if the given string is empty', () => {
-    const SUT = checkHeterogram('')
-    expect(SUT).toBe(true)
+    expect(checkHeterogram('')).toBe(true)
   })
   it('expects to return true if the given string contains multiple spaces and is a heterogram', () => {
-    const SUT = checkHeterogram('Cwm fjord bank glyphs vext quiz')
-    expect(SUT).toBe(true)
+    expect(checkHeterogram('Cwm fjord bank glyphs vext quiz')).toBe(true)
   })
   it('expects to return false if the given string contains duplicate letters with differing case (case insensitive)', () => {
-    const SUT = checkHeterogram('Ada')
-    expect(SUT).toBe(false)
+    expect(checkHeterogram('Ada')).toBe(false)
   })
   it('expects to return false if the given string contains duplicate letters in different words', () => {
-    const SUT = checkHeterogram('List duplicates')
-    expect(SUT).toBe(false)
+    expect(checkHeterogram('List duplicates')).toBe(false)
   })
 })
