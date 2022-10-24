e847c45eb08cac469a70056997c9148298366d2d
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-10-24 14:38:08 -0600

    indeces => indices

--- Data-Structures/Array/TwoSum.js
+++ Data-Structures/Array/TwoSum.js
@@ -4,7 +4,7 @@
  * @description Given an array of integers, returns indices of the two numbers such that they add up to a specific target. You may assume that each input would have exactly one solution, and you may not use the same element twice. This is a basic brute force approach.
  * @param {Array} nums - array of integers.
  * @param {number} target - target integer.
- * @returns {Array} Array of the indeces of the two numbers whose sum equals the target
+ * @returns {Array} Array of the indices of the two numbers whose sum equals the target
  * @example Given nums = [2, 7, 11, 15], target = 9; return [0, 1] because nums[0] + nums[1] = 2 + 7 = 9
  * @complexity: O(n^2)
  */
