c7b07ce3f25c8717860a812efbe49b995bc940e0
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-10-24 11:38:46 -0600

    Fix comments

--- Data-Structures/Array/TwoSum.js
+++ Data-Structures/Array/TwoSum.js
@@ -1,17 +1,14 @@
-// https://leetcode.com/problems/two-sum/
-
-// Given an array of integers, return indices of the two numbers such that they add up to
-// a specific target.
-
-// You may assume that each input would have exactly one solution, and you may not use the
-// same element twice.
-
-// Example:
-// Given nums = [2, 7, 11, 15], target = 9,
-
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
-
+/** https://leetcode.com/problems/two-sum/
+* Given an array of integers, return indices of the two numbers such that they add up to
+* a specific target.
+* You may assume that each input would have exactly one solution, and you may not use the
+* same element twice.
+* Example:
+* Given nums = [2, 7, 11, 15], target = 9,
+* Because nums[0] + nums[1] = 2 + 7 = 9,
+* return [0, 1].
+*
+* This is a basic brute force approach */
 const TwoSum = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
