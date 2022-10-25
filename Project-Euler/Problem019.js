b5cfb1e19aadd40ce9e2f2968dddb5246c20e46b
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2022-10-25 11:27:11 +0200

    Commit to trigger CI

--- Project-Euler/Problem019.js
+++ Project-Euler/Problem019.js
@@ -11,7 +11,6 @@ You are given the following information, but you may prefer to do some research 
   * A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
-
 */
 
 export function countingSundays () {
