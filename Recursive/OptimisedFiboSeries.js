d5840445895db913255462f6c7b61ce14f55f7fa
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2022-10-27 23:57:52 +0530

    Added the optimised version of fibonacci series

---
+++ Recursive/OptimisedFiboSeries.js
@@ -0,0 +1,11 @@
+class OptimisedFibonacciSeries {
+    static fiboFormula(n) {
+       return parseInt(((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5)));
+    }
+    static main(args) {
+       for (var i = 0; i <= 10; i++) {
+          console.log(OptimisedFibonacciSeries.fiboFormula(i) + " ");
+       }
+    }
+ }
+ OptimisedFibonacciSeries.main([]);
