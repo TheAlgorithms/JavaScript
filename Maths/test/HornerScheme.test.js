be56ea70e40a9d4d9ba65472dd09308a995574e7
alex_p <aleksborislavskij@gmail.com>
2022-10-29 16:17:52 +0300

    fix: fixed spelling errors in Horner Scheme tests

--- Maths/test/HornerScheme.test.js
+++ Maths/test/HornerScheme.test.js
@@ -1,15 +1,15 @@
 import { hornerScheme } from '../HornerScheme'
 
 describe('Horner Scheme', () => {
-  test('Case 1: testing convertation to the binary system', () => {
+  test('Case 1: testing conversion to the binary system', () => {
     expect(hornerScheme(210, 2)).toEqual('11010010')
     expect(hornerScheme(-210, 2)).toEqual('-11010010')
   })
-  test('Case 2: testing convertation to the system with base 5', () => {
+  test('Case 2: testing conversion to the system with base 5', () => {
     expect(hornerScheme(210, 5)).toEqual('1320')
     expect(hornerScheme(-210, 5)).toEqual('-1320')
   })
-  test('Case 3: testing convertation to the octal system', () => {
+  test('Case 3: testing conversion to the octal system', () => {
     expect(hornerScheme(210, 8)).toEqual('322')
     expect(hornerScheme(-210, 8)).toEqual('-322')
   })
