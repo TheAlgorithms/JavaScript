cde4d94d18eef98a70b586e460895d7510a8faae
alex_p <aleksborislavskij@gmail.com>
2022-10-29 18:12:30 +0300

    refactor: refactor code in UniquePaths2.js

--- Dynamic-Programming/UniquePaths2.js
+++ Dynamic-Programming/UniquePaths2.js
@@ -23,9 +23,9 @@
  */
 const generateMatrix = (rows, columns, filler = 0) => {
   const matrix = []
-  for (let i = 0; i < rows; i += 1) {
+  for (let i = 0; i < rows; i++) {
     const submatrix = []
-    for (let k = 0; k < columns; k += 1) {
+    for (let k = 0; k < columns; k++) {
       submatrix[k] = filler
     }
     matrix[i] = submatrix
@@ -35,11 +35,11 @@ const generateMatrix = (rows, columns, filler = 0) => {
 
 /**
  * @description Return number of unique paths
- * @param {Object []} obstacles Obstacles grid
+ * @param {Array [][]} obstacles Obstacles grid
  * @returns {Number}
  */
 const uniquePaths2 = (obstacles) => {
-  if (!(obstacles instanceof Object)) {
+  if (!Array.isArray(obstacles)) {
     throw new Error('Input data must be type of Array')
   }
   // Create grid for calculating number of unique ways
@@ -48,15 +48,15 @@ const uniquePaths2 = (obstacles) => {
   const grid = generateMatrix(rows, columns)
   // Fill the outermost cell with 1 b/c it has
   // the only way to reach neighbor
-  for (let i = 0; i < rows; i += 1) {
+  for (let i = 0; i < rows; i++) {
     // If robot encounters an obstacle in these cells,
-    // he cannot continue movind in that direction
+    // he cannot continue moving in that direction
     if (obstacles[i][0]) {
       break
     }
     grid[i][0] = 1
   }
-  for (let j = 0; j < columns; j += 1) {
+  for (let j = 0; j < columns; j++) {
     if (obstacles[0][j]) {
       break
     }
@@ -65,13 +65,9 @@ const uniquePaths2 = (obstacles) => {
   // Fill the rest of grid by dynamic programming
   // using following reccurent formula:
   // K[i][j] = K[i - 1][j] + K[i][j - 1]
-  for (let i = 1; i < rows; i += 1) {
-    for (let j = 1; j < columns; j += 1) {
-      if (obstacles[i][j]) {
-        grid[i][j] = 0
-      } else {
-        grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
-      }
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < columns; j++) {
+      grid[i][j] = obstacles[i][j] ? 0 : grid[i - 1][j] + grid[i][j - 1]
     }
   }
   return grid[rows - 1][columns - 1]
--- Dynamic-Programming/tests/UniquePaths2.test.js
+++ Dynamic-Programming/tests/UniquePaths2.test.js
@@ -2,17 +2,17 @@ import { uniquePaths2 } from '../UniquePaths2'
 
 describe('Unique Paths2', () => {
   // Should return number of ways, taken into account the obstacles
-  test('Case 1: there are obstacles in the way', () => {
+  test('There are obstacles in the way', () => {
     expect(uniquePaths2([[0, 0, 0], [0, 1, 0], [0, 0, 0]])).toEqual(2)
     expect(uniquePaths2([[0, 0, 0], [0, 1, 0], [0, 0, 0], [1, 0, 0]])).toEqual(3)
   })
   // Should return number of all possible ways to reach right-bottom corner
-  test('Case 2: there are no obstacles in the way', () => {
+  test('There are no obstacles in the way', () => {
     expect(uniquePaths2([[0, 0, 0], [0, 0, 0], [0, 0, 0]])).toEqual(6)
     expect(uniquePaths2([[0, 0, 0], [0, 0, 0]])).toEqual(3)
   })
   // Should throw an exception b/c input data has wrong type
-  test('Case 3: there are wrong type of input data', () => {
+  test('There are wrong type of input data', () => {
     expect(() => uniquePaths2('wrong input')).toThrow()
     expect(() => uniquePaths2(100)).toThrow()
   })
