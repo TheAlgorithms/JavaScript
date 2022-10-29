f85505182b1bc97705e5655545ef446e103a25c5
Adrito-M <adrito.mukherjee.cse21@itbhu.ac.in>
2022-10-30 01:30:20 +0530

    made requested changes

--- String/test/ZFunction.test.js
+++ String/test/ZFunction.test.js
@@ -1,15 +1,10 @@
-import ZFunction from '../ZFunction'
+import zFunction from '../ZFunction'
 
-test('Test Case 1', () => {
-  const text = 'aabxaayaab'
-  const ZArray = ZFunction(text)
-  expect(ZArray).toEqual([10, 1, 0, 0, 2, 1, 0, 3, 1, 0])
-})
-
-test('Test Case 2', () => {
-  const text = 'aabxaabxcaabxaabxay'
-  const ZArray = ZFunction(text)
-  expect(ZArray).toEqual([
-    19, 1, 0, 0, 4, 1, 0, 0, 0, 8, 1, 0, 0, 5, 1, 0, 0, 1, 0
-  ])
+describe('Testing the zFunction', () => {
+  it('Check the zFunction of a string', () => {
+    expect(zFunction('aabxaayaab')).toEqual([10, 1, 0, 0, 2, 1, 0, 3, 1, 0])
+    expect(zFunction('aabxaabxcaabxaabxay')).toEqual([
+      19, 1, 0, 0, 4, 1, 0, 0, 0, 8, 1, 0, 0, 5, 1, 0, 0, 1, 0
+    ])
+  })
 })
