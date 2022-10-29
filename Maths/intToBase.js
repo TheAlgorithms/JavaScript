1d81d404960b3a6923967c8e06fb175cc0ab51b3
alex_p <aleksborislavskij@gmail.com>
2022-10-29 19:15:32 +0300

    refactor: refactor code in intToBase.js

--- Maths/intToBase.js
+++ Maths/intToBase.js
@@ -1,5 +1,5 @@
 /**
- * @function hornerScheme
+ * @function intToBase
  * @description Convert a number from decimal system to another (till decimal)
  * @param {Number} number Number to be converted
  * @param {Number} base Base of new number system
@@ -12,27 +12,29 @@
  * const num2 = 125 // Needs to be converted to the octal number system
  * gornerScheme(num, 8); // ===> 175
  */
-const hornerScheme = (number, base) => {
+const intToBase = (number, base) => {
   if (typeof number !== 'number' || typeof base !== 'number') {
     throw new Error('Input data must be numbers')
   }
   // Zero in any number system is zero
   if (number === 0) {
-    return `${number}`
+    return '0'
   }
   let absoluteValue = Math.abs(number)
   let convertedNumber = ''
   while (absoluteValue > 0) {
     // Every iteration last digit is taken away
-    // and added to the previois one
+    // and added to the previous one
     const lastDigit = absoluteValue % base
     convertedNumber = lastDigit + convertedNumber
     absoluteValue = Math.trunc(absoluteValue / base)
   }
   // Result is whether negative or positive,
   // depending on the original value
-  const result = number < 0 ? `-${convertedNumber}` : convertedNumber
-  return result
+  if (number < 0) {
+    convertedNumber = '-' + convertedNumber
+  }
+  return convertedNumber
 }
 
-export { hornerScheme }
+export { intToBase }
