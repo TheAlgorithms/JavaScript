3097951da199ede4f13b15f3d1a3dbb583b4c137
Gustavo Kamihara <49957327+Japoncio3k@users.noreply.github.com>
2022-10-30 11:16:07 -0300

    Update Circle.test.js

--- Geometry/Test/Circle.test.js
+++ Geometry/Test/Circle.test.js
@@ -3,11 +3,9 @@ import Circle from '../Circle'
 const circle = new Circle(3)
 
 test('The area of a circle with radius equal to 3', () => {
-  const area = circle.area().toFixed(2)
-  expect(parseFloat(area)).toEqual(28.27)
+  expect(parseFloat(circle.area().toFixed(2))).toEqual(28.27)
 })
 
 test('The perimeter of a circle with radius equal to 3', () => {
-  const perimeter = circle.perimeter().toFixed(2)
-  expect(parseFloat(perimeter)).toEqual(18.85)
+  expect(parseFloat(circle.perimeter().toFixed(2))).toEqual(18.85)
 })
