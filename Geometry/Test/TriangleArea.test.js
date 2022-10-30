6877c3728c9fb61b16e6bd03ed56aa6d78a8a762
japoncio3k <gustavo.kamihara@gmail.com>
2022-10-29 23:30:19 -0300

    Added triangle area

---
+++ Geometry/Test/TriangleArea.test.js
@@ -0,0 +1,7 @@
+import { triangleArea } from '../TriangleArea'
+
+describe('TriangleArea', () => {
+  it('should return 25', () => {
+    expect(triangleArea(10, 5)).toEqual(25)
+  })
+})
---
+++ Geometry/TriangleArea.js
@@ -0,0 +1,6 @@
+const triangleArea = (base, height) => {
+  //  Triangle area is calculated by multiplying base and height and dividing it by 2.
+  return base * height / 2
+}
+
+export { triangleArea }
