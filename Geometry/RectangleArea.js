3f53618ba96ce95329ecfecd8372369a91bcaa04
japoncio3k <gustavo.kamihara@gmail.com>
2022-10-29 23:18:47 -0300

    Added rectangle area

---
+++ Geometry/RectangleArea.js
@@ -0,0 +1,5 @@
+const rectangleArea = (sideA, sideB) => {
+    return sideA*sideB;
+}
+
+export { rectangleArea };
---
+++ Geometry/Test/RectangleArea.test.js
@@ -0,0 +1,8 @@
+import { rectangleArea } from "../RectangleArea";
+
+describe('RectangleArea', () => {
+  it('should return 15', ()=>{
+    expect(rectangleArea(3,5)).toEqual(15);
+  })
+})
+  
