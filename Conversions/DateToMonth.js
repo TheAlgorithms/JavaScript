2038d27a157b9a8226e814f1edd7bef2272a4cd6
japoncio3k <gustavo.kamihara@gmail.com>
2022-10-29 21:10:27 -0300

    Fixed spelling

--- Conversions/DateToMonth.js
+++ Conversions/DateToMonth.js
@@ -2,7 +2,7 @@
 const dateToMonth = (date) => {
   const months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   if (typeof date !== 'string') {
-    return new TypeError('Argument isnt a string.')
+    return new TypeError('Argument is not a string.')
   }
   //  Inputs a date in format DD/MM/YYYY, extracts MM and gets the month name from months const
   return months[parseInt(date.split('/')[1])]
