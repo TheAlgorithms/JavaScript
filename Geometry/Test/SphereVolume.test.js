6abb5f308c300dd1a07fa3b290802f70d7f266fb
lcvieira2001 <lehcvieira@outlook.com>
2022-10-30 12:01:46 -0300

    Fixed indentation

--- Geometry/Test/SphereVolume.test.js
+++
@@ -1,8 +0,0 @@
-import { sphereVolume } from '../SphereVolume'
-
-describe('SphereVolume', () => {
-  it('should return the sphere volume given the radius', () => {
-    const res = sphereVolume(3)
-    expect(res).toEqual(113.1)
-  })
-})
