06992e0021eaa4595b7be330254e07dfd6b4ee35
alex_p <aleksborislavskij@gmail.com>
2022-10-31 22:41:15 +0300

    refactor: remove empty line

--- String/PercentageOfLetters.js
+++ String/PercentageOfLetters.js
@@ -18,10 +18,9 @@ const percentageOfLetter = (str, letter) => {
   // Iterate through the whole given text
   for (let i = 0; i < str.length; i++) {
     // Count how often the letter appears in the word
-    letterCount = str[i].toLowerCase() === letter.toLowerCase() ? letterCount + 1 : letterCount + 0
+    letterCount += str[i].toLowerCase() === letter.toLowerCase() ? 1 : 0
   }
   const percentage = Math.floor((100 * letterCount) / str.length)
-
   return percentage
 }
 
