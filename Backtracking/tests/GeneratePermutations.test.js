e327fad1d6de152276bbf3d0cd1158c24de6cce5
k ho k ho? <6939499+kho-kho-kho@users.noreply.github.com>
2022-11-09 19:31:32 -0800

    Update GeneratePermutations.test.js
    Original test was very limited and also (unnecessarily) strict in that the order of results mattered.
    
    I've tweaked the original test so that results can come in any order. Also added additional tests for smaller arrays of strings/booleans. Finally added some sanity testing for larger input arrays.

--- Backtracking/tests/GeneratePermutations.test.js
+++ Backtracking/tests/GeneratePermutations.test.js
@@ -1,14 +1,32 @@
 import { permutations } from '../GeneratePermutations'
 
 describe('Permutations', () => {
+  it('Permutations of [a]', () => {
+    const perms = permutations(['a'])
+    expect(perms).toHaveLength(1)
+    expect(perms).toContainEqual(['a'])
+  })
+
+  it('Permutations of [true, false]', () => {
+    const perms = permutations([true, false])
+    expect(perms).toHaveLength(2 * 1)
+    expect(perms).toContainEqual([true, false])
+    expect(perms).toContainEqual([false, true])
+  })
+
   it('Permutations of [1, 2, 3]', () => {
-    expect(permutations([1, 2, 3])).toEqual([
-      [1, 2, 3],
-      [1, 3, 2],
-      [2, 1, 3],
-      [2, 3, 1],
-      [3, 1, 2],
-      [3, 2, 1]
-    ])
+    const perms = permutations([1, 2, 3])
+    expect(perms).toHaveLength(3 * 2 * 1)
+    expect(perms).toContainEqual([1, 2, 3])
+    expect(perms).toContainEqual([1, 3, 2])
+    expect(perms).toContainEqual([2, 1, 3])
+    expect(perms).toContainEqual([2, 3, 1])
+    expect(perms).toContainEqual([3, 1, 2])
+    expect(perms).toContainEqual([3, 2, 1])
+  })
+
+  it('Permutation counts across larger input arrays', () => {
+    expect(permutations([1, 2, 3, 4, 5, 6])).toHaveLength(720)
+    expect(permutations([1, 2, 3, 4, 5, 6, 7])).toHaveLength(5040)
   })
 })
