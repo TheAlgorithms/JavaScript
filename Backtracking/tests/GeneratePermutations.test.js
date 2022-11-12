542b0ca82cacf8dfe6c973a65226a4286855224a
k ho k ho? <6939499+kho-kho-kho@users.noreply.github.com>
2022-11-11 19:11:03 -0800

    Update GeneratePermutations.test.js
    Leverage package's existing factorial function.

--- Backtracking/tests/GeneratePermutations.test.js
+++ Backtracking/tests/GeneratePermutations.test.js
@@ -1,22 +1,23 @@
+import { factorial } from '../../Recursive/Factorial'
 import { permutations } from '../GeneratePermutations'
 
 describe('Permutations', () => {
   it('Permutations of [a]', () => {
     const perms = permutations(['a'])
-    expect(perms).toHaveLength(1)
+    expect(perms).toHaveLength(factorial(1))
     expect(perms).toContainEqual(['a'])
   })
 
   it('Permutations of [true, false]', () => {
     const perms = permutations([true, false])
-    expect(perms).toHaveLength(2 * 1)
+    expect(perms).toHaveLength(factorial(2))
     expect(perms).toContainEqual([true, false])
     expect(perms).toContainEqual([false, true])
   })
 
   it('Permutations of [1, 2, 3]', () => {
     const perms = permutations([1, 2, 3])
-    expect(perms).toHaveLength(3 * 2 * 1)
+    expect(perms).toHaveLength(factorial(3))
     expect(perms).toContainEqual([1, 2, 3])
     expect(perms).toContainEqual([1, 3, 2])
     expect(perms).toContainEqual([2, 1, 3])
@@ -26,11 +27,7 @@ describe('Permutations', () => {
   })
 
   it('Permutation counts across larger input arrays', () => {
-    const factorial = (previousVal, currentVal) => previousVal * currentVal
-    const input8 = [1, 2, 3, 4, 5, 6, 7, 8]
-    const input9 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    expect(permutations(input8)).toHaveLength(input8.reduce(factorial, 1))
-    expect(permutations(input9)).toHaveLength(input9.reduce(factorial, 1))
+    expect(permutations([1, 2, 3, 4, 5, 6, 7, 8])).toHaveLength(factorial(8))
+    expect(permutations([1, 2, 3, 4, 5, 6, 7, 8, 9])).toHaveLength(factorial(9))
   })
 })
