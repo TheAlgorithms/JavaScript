3b7b1f18907f2010a7267cfd99d0de50ec385a68
Akshay Dubey <akshay.dubey055@gmail.com>
2022-11-25 10:58:55 +0530

    refactor: Fix build failure

--- Maths/HexagonalNumber.js
+++ Maths/HexagonalNumber.js
@@ -2,7 +2,7 @@
  * Author: Akshay Dubey (https://github.com/itsAkshayDubey)
  * Hexagonal Number: https://en.wikipedia.org/wiki/Hexagonal_number
  * The nth hexagonal number hn is the number of distinct dots in a pattern of dots
- * consisting of the outlines of regular hexagons with sides up to n dots, when the 
+ * consisting of the outlines of regular hexagons with sides up to n dots, when the
  * hexagons are overlaid so that they share one vertex.
  */
 
