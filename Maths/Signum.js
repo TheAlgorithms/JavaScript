cbf8a66fe88c27d0683140151fbcff6c08ee3e10
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-11-27 00:34:07 +0530

    🧩 Added signum implementaion

---
+++ Maths/Signum.js
@@ -0,0 +1,23 @@
+/*
+  A program to demonstrate the implementation of the signum function,
+  also known as the sign function, in JavaScript.
+
+  The signum function is an odd mathematical function, which returns the
+  sign of the provided real number.
+  It can return 3 values: 1 for values greater than zero, 0 for zero itself,
+  and -1 for values less than zero
+
+  Wikipedia: https://en.wikipedia.org/wiki/Sign_function
+*/
+
+/**
+ * @param {Number} input
+ * @returns {-1 | 0 | 1} sign of input
+ */
+function signum (input) {
+  if (input === 0) return 0
+  else if (input < 0) return -1
+  else return 1
+}
+
+export { signum }
---
+++ Maths/test/Signum.test.js
@@ -0,0 +1,15 @@
+import { signum } from '../Signum'
+
+describe('The sign of a number', () => {
+  it('Sign of 10', () => {
+    expect(signum(10)).toBe(1)
+  })
+
+  it('Sign of 0', () => {
+    expect(signum(10)).toBe(0)
+  })
+
+  it('Sign of -420', () => {
+    expect(signum(-420)).toBe(-1)
+  })
+})
