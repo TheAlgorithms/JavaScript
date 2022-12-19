cdcba20902c940e599226b1cb8578c48762e24b6
Yerenzter <yerenzter@yandex.com>
2022-12-19 20:02:55 +0800

    --sum-of-array

---
+++ Maths/SumOfArray.js
@@ -0,0 +1,11 @@
+// It will sum of how much the length of an array.
+
+let sum = (y) => {
+        let sum = 0;
+
+        for(r=0; r<y.length; r++) {
+                sum += y[r];
+        }
+
+        return sum;
+};
