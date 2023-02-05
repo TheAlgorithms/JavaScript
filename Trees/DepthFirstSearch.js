3847b40e5815ac700ea6827c26a5ac852d913daf
Sundaram Kumar Jha <93595231+sundaram2021@users.noreply.github.com>
2023-02-05 08:36:42 +0530

    solves issue #1280
    traverseDFS() is not working properly ,
    
    while logging it says  [ undefined ].
    
    and there is also an issue with searchDFS() function,
    
    the following issue i have faced
    
    The stack array is being used as a queue instead of a stack. A queue follows the first-in, first-out (FIFO) principle, while a stack follows the last-in, first-out (LIFO) principle. For a DFS algorithm, we need to use a stack data structure.
    
    The code is pushing elements to the stack using the tree[node.right] and tree[node.left] expressions, but it should be using node.right and node.left directly. The tree array should not be used here.
    
    The code is using tree[0] as the starting node for the search, but it should be using the entire tree object as the starting node.

--- Trees/DepthFirstSearch.js
+++ Trees/DepthFirstSearch.js
@@ -1,76 +1,213 @@
 /*
- * Author: Surendra Kumar
+ * Author: Sundaram Kumar Jha
  * DFS Algorithm implementation in JavaScript
  * DFS Algorithm for traversing or searching graph data structures.
 */
 
-function traverseDFS (root) {
-  const stack = [root]
-  const res = []
+function dfs(tree) {
+  if (!tree) return;
+  console.log(tree.value);
+  for (const child of tree.children) {
+    dfs(child);
+  }
+}
 
-  while (stack.length) {
-    const curr = stack.pop()
-    res.push(curr.key)
+// This function takes a tree node as an argument, and uses recursion to traverse 
+// the tree in a depth-first manner. The function first logs the value of the current
+//  node and then iterates through its children, calling the function on each child. 
+// The function stops when it encounters a null node.
 
-    if (curr.right) {
-      stack.push(curr.right)
-    }
+function searchDFS(tree, target) {
+  if (tree.value === target) {
+    return true;
+  }
 
-    if (curr.left) {
-      stack.push(curr.left)
+  for (let i = 0; i < tree.children.length; i++) {
+    if (searchDFS(tree.children[i], target)) {
+      return true;
     }
   }
 
-  return res.reverse()
+  return false;
 }
 
-function searchDFS (tree, value) {
-  const stack = []
+// This function takes two arguments: tree, which is the tree to be searched, and target, 
+// which is the value you're searching for. The function uses a depth-first search algorithm to 
+// traverse the tree and look for the target value.
 
-  stack.push(tree[0])
+// The function starts by checking if the current node has the target value. 
+// If it does, it returns true to indicate that the target value has been found. 
+// If not, the function loops over the children of the current node and calls itself recursively on each child. 
+// If any of the recursive calls returns true, the function also returns true to indicate that the target value 
+// has been found. If none of the recursive calls returns true, the function returns false to indicate that the 
+// target value was not found in the tree
 
-  while (stack.length !== 0) {
-    for (let i = 0; i < stack.length; i++) {
-      const node = stack.pop()
+const tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [
+        {
+          value: 4,
+          children: [
+            {
+              value: 8,
+              children: [
+                {
+                  value: 16,
+                  children: [],
+                },
+              ],
+            },
+            {
+              value: 9,
+              children: [
+                {
+                  value: 17,
+                  children: [],
+                },
+                {
+                  value: 18,
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: 5,
+          children: [
+            {
+              value: 10,
+              children: [
+                {
+                  value: 19,
+                  children: [],
+                },
+                {
+                  value: 20,
+                  children: [],
+                },
+              ],
+            },
+            {
+              value: 11,
+              children: [
+                {
+                  value: 21,
+                  children: [],
+                },
+                {
+                  value: 22,
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 3,
+      children: [
+        {
+          value: 6,
+          children: [
+            {
+              value: 12,
+              children: [
+                {
+                  value: 23,
+                  children: [],
+                },
+                {
+                  value: 24,
+                  children: [],
+                },
+              ],
+            },
+            {
+              value: 13,
+              children: [
+                {
+                  value: 25,
+                  children: [],
+                },
+                {
+                  value: 26,
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: 7,
+          children: [
+            {
+              value: 14,
+              children: [
+                {
+                  value: 27,
+                  children: [],
+                },
+                {
+                  value: 28,
+                  children: [],
+                },
+              ],
+            },
+            {
+              value: 15,
+              children: [
+                {
+                  value: 29,
+                  children: [],
+                },
+                {
+                  value: 30,
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
-      if (node.value === value) {
-        return node
-      }
-      if (node.right) {
-        stack.push(tree[node.right])
-      }
-      if (node.left) {
-        stack.push(tree[node.left])
-      }
-    }
-  }
-  return null
-}
+console.log(dfs(tree));
 
-const tree = [
-  { value: 6, left: 1, right: 2 },
-  { value: 5, left: 3, right: 4 },
-  { value: 7, left: null, right: 5 },
-  { value: 3, left: 6, right: null },
-  { value: 4, left: null, right: null },
-  { value: 9, left: 7, right: 8 },
-  { value: 2, left: 9, right: null },
-  { value: 8, left: null, right: null },
-  { value: 10, left: null, right: null },
-  { value: 1, left: null, right: null }
-]
+const target = 275;
+const result = searchDFS(tree, target);
+console.log(result); // true or false
 
-searchDFS(tree, 9)
-searchDFS(tree, 10)
 
-traverseDFS(6)
+// The tree argument is an object that represents a tree data structure, 
+// where each node has a value and an array of child nodes. In this particular example, 
+// the tree has a root node with value 1. The root node has two child nodes, represented by objects with values 2 and 3. 
+// These child nodes each have two child nodes, and so on.
 
-//            6
-//           / \
-//          5   7
-//         / \   \
-//        3   4   9
-//       /       / \
-//      2       8   10
-//     /
-//    1
+// Each node in the tree is represented as an object with two properties: value and children. 
+// The value property holds the value of the node, and the children property is an array of child nodes. 
+// If a node has no children, its children property is an empty array [].
+
+// Here's a visual representation of the tree argument:
+
+//       1
+//       / \
+//      2   3
+//     / \ / \
+//   4  5 6  7
+//   /\  /\ /\ /\
+//  8 9 10 11 12 13 14 15
+//  /\ /\ /\ /\ /\ /\ /\ /\
+// 161718192021 22222324252627282930
+
+// This tree can be traversed using the dfs function provided earlier, 
+// which visits nodes in a depth-first order. The dfs function uses recursion 
+// to traverse the tree, starting from the root node and visiting each child node 
+// in turn before moving on to the next sibling node.
+
