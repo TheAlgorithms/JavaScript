3b7f48933afa2d000d4e9898b55db0de96a8efea
ramzi <ramziinfo2001@gmail.com>
2023-02-12 11:02:29 +0100

    adding generateParenthses algorithm

--- Backtracking/generateParentheses.js
+++ Backtracking/generateParentheses.js
@@ -1,19 +1,26 @@
-const generateParentheses =(n, chres, openParenthese, closedParenthese, res) => {
+/*
+ * Problem Statement: Given a number n pairs of parenthses, try to Generate all combinations of valid parentheses
+ *
+ * link to problem: https://leetcode.com/problems/generate-parentheses/
+ *
+ */
+
+const generateParentheses = (n, chres, openParenthese, closedParenthese, res) => {
   if (openParenthese === n && closedParenthese === n) {
-      res.push(chres);
-      return;
+    res.push(chres)
+    return
   }
 
   if (openParenthese <= n) {
-      generateParentheses(n, chres + "(", openParenthese + 1, closedParenthese, res);
+    generateParentheses(n, chres + '(', openParenthese + 1, closedParenthese, res)
   }
-    
+
   if (closedParenthese < openParenthese) {
-      generateParentheses(n, chres + ")", openParenthese, closedParenthese + 1, res);
+    generateParentheses(n, chres + ')', openParenthese, closedParenthese + 1, res)
   }
 }
 
-let res = [];
-generateParentheses(3, [], 0, 0, res);
+const res = []
+generateParentheses(3, [], 0, 0, res)
 
 export { generateParentheses }
