b4c0075914c73c5547c7259a6edd329224593652
ramzi <ramziinfo2001@gmail.com>
2023-02-14 17:29:42 +0100

    adding sliding window problem(fruitsIntoBasket)

--- Dynamic-Programming/Sliding-Window/test/FruitsIntoBasket.test.js
+++ Dynamic-Programming/Sliding-Window/test/FruitsIntoBasket.test.js
@@ -1,5 +1,5 @@
 import { totalFruit } from '../FruitsIntoBasket'
 
-test('generate all valid parentheses of input n', () => {
+test('result : ', () => {
   expect(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4])).toStrictEqual(5)
 })
