ffdd09a465e627c9af4f405b52eea0588ad5a787
Pauline Garelli <pauline.garelli@hotmail.fr>
2023-02-22 10:21:43 +0100

    feat: add tests for KeyPriorityQueue #1298

---
+++ Data-Structures/Heap/test/KeyPriorityQueue.test.js
@@ -0,0 +1,148 @@
+import { KeyPriorityQueue } from '../KeyPriorityQueue.js'
+
+describe('KeyPriorityQueue', () => {
+
+  test('Check heap correctly ordered', () => {
+    // create queue and fill it
+    const priorities = [5, 2, 4, 1, 7, 6, 3, 8]
+    const queue = new KeyPriorityQueue()
+    for (let i = 0; i < priorities.length; i++) {
+      queue.push(i, priorities[i])
+    }
+
+    // result from popping all elements from the queue
+    let res = []
+    while (!queue.isEmpty()) {
+      res.push(queue.pop())
+    }
+
+    expect(res).toEqual([1, 2, 3, 5, 7, 6, 4, 8])
+  })
+})
+
+describe('Method isEmpty', () => {
+
+  test('Check heap is empty', () => {
+    const queue = new KeyPriorityQueue()
+    let res = queue.isEmpty()
+    expect(res).toEqual(true)
+  })
+
+  test('Check heap is not empty', () => {
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 2)
+    let res = queue.isEmpty()
+    expect(res).toEqual(false)
+  })
+})
+
+describe('Methods push and pop', () => {
+
+  test('Test Case 1', () => {
+    // create queue
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 3)
+    queue.push(1, 7)
+    queue.push(2, 9)
+    queue.push(3, 13)
+    // create expected queue
+    const expectedQueue = new KeyPriorityQueue()
+    expectedQueue.push(1, 7)
+    expectedQueue.push(3, 13)
+    expectedQueue.push(2, 9)
+    // result from popping element from the queue
+    queue.pop()
+    expect(queue).toEqual(expectedQueue)
+  })
+  
+  test('Test Case 2', () => {
+    // create queue
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 3)
+    queue.push(1, 9)
+    queue.push(2, 7)
+    queue.push(3, 13)
+    // create expected queue
+    const expectedQueue = new KeyPriorityQueue()
+    expectedQueue.push(2, 7)
+    expectedQueue.push(1, 9)
+    expectedQueue.push(3, 13)
+    // result from popping element from the queue
+    queue.pop()
+    expect(queue).toEqual(expectedQueue)
+  })
+  
+  test('Test Case 3', () => {
+    // create queue
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 3)
+    queue.push(1, 7)
+    queue.push(2, 9)
+    queue.push(3, 12)
+    queue.push(4, 13)
+    // create expected queue
+    const expectedQueue = new KeyPriorityQueue()
+    expectedQueue.push(1, 7)
+    expectedQueue.push(3, 12)
+    expectedQueue.push(2, 9)
+    expectedQueue.push(4, 13)
+    // result from popping element from the queue
+    queue.pop()
+    expect(queue).toEqual(expectedQueue)
+  })
+})
+
+describe('Method contains', () => {
+
+  test('Check heap does not contain element', () => {
+    const queue = new KeyPriorityQueue()
+    let res = queue.contains(0)
+    expect(res).toEqual(false)
+  })
+
+  test('Check heap contains element', () => {
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 2)
+    let res = queue.contains(0)
+    expect(res).toEqual(true)
+  })
+})
+
+describe('Method update', () => {
+
+  test('Update without change in position', () => {
+    // create queue
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 3)
+    queue.push(1, 5)
+    queue.push(2, 7)
+    queue.push(3, 11)
+    // create expected queue
+    const expectedQueue = new KeyPriorityQueue()
+    expectedQueue.push(0, 3)
+    expectedQueue.push(1, 5)
+    expectedQueue.push(2, 7)
+    expectedQueue.push(3, 11)
+    // result from updating to similar priority
+    queue.update(0, 2)
+    expect(queue).toEqual(expectedQueue)
+  })
+
+  test('Update with change in position', () => {
+    // create queue
+    const queue = new KeyPriorityQueue()
+    queue.push(0, 3)
+    queue.push(1, 5)
+    queue.push(2, 7)
+    queue.push(3, 11)
+    // create expected queue
+    const expectedQueue = new KeyPriorityQueue()
+    expectedQueue.push(1, 5)
+    expectedQueue.push(3, 11)
+    expectedQueue.push(2, 7)
+    expectedQueue.push(0, 9)
+    // result from updating to similar priority
+    queue.update(0, 9)
+    expect(queue).toEqual(expectedQueue)
+  })
+})
