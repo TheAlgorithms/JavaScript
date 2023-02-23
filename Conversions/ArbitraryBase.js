6667e4c227648c3c33f6951195936a695b46c1fb
lionel-rowe <lionel.rowe@gmail.com>
2023-02-23 08:22:19 +0800

    Add BigInt version and push output chars to array

--- Conversions/ArbitraryBase.js
+++ Conversions/ArbitraryBase.js
@@ -12,7 +12,7 @@ const floorDiv = (dividend, divisor) => {
 }
 
 /**
-* Converts a string from one base to other
+* Converts a string from one base to other. Loses accuracy above the value of `Number.MAX_SAFE_INTEGER`.
 * @param {string} stringInBaseOne String in input base
 * @param {string} baseOneCharacters Character set for the input base
 * @param {string} baseTwoCharacters Character set for the output base
@@ -43,14 +43,57 @@ const convertArbitraryBase = (stringInBaseOne, baseOneCharacterString, baseTwoCh
     value += (digitNumber * placeValue)
     placeValue *= stringOneBase
   }
-  let stringInBaseTwo = ''
+  const outputChars = []
   const stringTwoBase = baseTwoCharacters.length
   while (value > 0) {
     const [divisionResult, remainder] = floorDiv(value, stringTwoBase)
-    stringInBaseTwo = baseTwoCharacters[remainder] + stringInBaseTwo
+    outputChars.push(baseTwoCharacters[remainder])
     value = divisionResult
   }
-  return stringInBaseTwo || baseTwoCharacters[0]
+  return outputChars.reverse().join('') || baseTwoCharacters[0]
 }
 
-export { convertArbitraryBase }
+/**
+* Converts a arbitrary-length string from one base to other. Doesn't lose accuracy.
+* @param {string} stringInBaseOne String in input base
+* @param {string} baseOneCharacters Character set for the input base
+* @param {string} baseTwoCharacters Character set for the output base
+* @returns {string}
+*/
+const convertArbitraryBaseBigIntVersion = (stringInBaseOne, baseOneCharacterString, baseTwoCharacterString) => {
+  if ([stringInBaseOne, baseOneCharacterString, baseTwoCharacterString].map(arg => typeof arg).some(type => type !== 'string')) {
+    throw new TypeError('Only string arguments are allowed')
+  }
+
+  const baseOneCharacters = [...baseOneCharacterString]
+  const baseTwoCharacters = [...baseTwoCharacterString]
+
+  for (const charactersInBase of [baseOneCharacters, baseTwoCharacters]) {
+    if (charactersInBase.length !== new Set(charactersInBase).size) {
+      throw new TypeError('Duplicate characters in character set are not allowed')
+    }
+  }
+  const reversedStringOneChars = [...stringInBaseOne].reverse()
+  const stringOneBase = BigInt(baseOneCharacters.length)
+  let value = 0n
+  let placeValue = 1n
+  for (const digit of reversedStringOneChars) {
+    const digitNumber = BigInt(baseOneCharacters.indexOf(digit))
+    if (digitNumber === -1n) {
+      throw new TypeError(`Not a valid character: ${digit}`)
+    }
+    value += (digitNumber * placeValue)
+    placeValue *= stringOneBase
+  }
+  const outputChars = []
+  const stringTwoBase = BigInt(baseTwoCharacters.length)
+  while (value > 0n) {
+    const divisionResult = value / stringTwoBase
+    const remainder = value % stringTwoBase
+    outputChars.push(baseTwoCharacters[remainder])
+    value = divisionResult
+  }
+  return outputChars.reverse().join('') || baseTwoCharacters[0]
+}
+
+export { convertArbitraryBase, convertArbitraryBaseBigIntVersion }
--- Conversions/test/ArbitraryBase.test.js
+++ Conversions/test/ArbitraryBase.test.js
@@ -1,4 +1,4 @@
-import { convertArbitraryBase } from '../ArbitraryBase'
+import { convertArbitraryBase, convertArbitraryBaseBigIntVersion } from '../ArbitraryBase'
 
 test('Check the answer of convertArbitraryBase(98, 0123456789, 01234567) is 142', () => {
   const res = convertArbitraryBase('98', '0123456789', '01234567')
@@ -43,4 +43,14 @@ test('Unicode awareness', () => {
 test('zero', () => {
   const res = convertArbitraryBase('0', '0123456789', 'abc')
   expect(res).toBe('a')
+})
+
+test('BigInt version with input string of arbitrary length', () => {
+  const resBigIntVersion = convertArbitraryBaseBigIntVersion(
+    String(10n ** 100n),
+    '0123456789',
+    '0123456789abcdefghijklmnopqrstuvwxyz'
+  )
+
+  expect(resBigIntVersion).toBe((10n ** 100n).toString(36))
 })
