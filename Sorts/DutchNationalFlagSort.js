69fd9e0989f8d8006f42db683cb36221070ac6ae
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-03 17:50:50 +0200

    fix: add test, fit code style

--- Sorts/DutchNationalFlagSort.js
+++ Sorts/DutchNationalFlagSort.js
@@ -7,27 +7,27 @@
  * @return {Integer[]} - Array of integers sorted in non-decreasing order.
  * @see [Dutch National Flag Sort](https://en.wikipedia.org/wiki/Dutch_national_flag_problem)
  */
-export function dutchNationalFlagSort(nums) {
-  let low = 0;
-  let mid = 0;
-  let high = nums.length - 1;
+export function dutchNationalFlagSort (nums) {
+  let low = 0
+  let mid = 0
+  let high = nums.length - 1
 
   while (mid <= high) {
     switch (nums[mid]) {
       case 0:
-        [nums[low], nums[mid]] = [nums[mid], nums[low]];
-        low++;
-        mid++;
-        break;
+        [nums[low], nums[mid]] = [nums[mid], nums[low]]
+        low++
+        mid++
+        break
       case 1:
-        mid++;
-        break;
+        mid++
+        break
       case 2:
-        [nums[mid], nums[high]] = [nums[high], nums[mid]];
-        high--;
-        break;
+        [nums[mid], nums[high]] = [nums[high], nums[mid]]
+        high--
+        break
     }
   }
 
-  return nums;
+  return nums
 }
