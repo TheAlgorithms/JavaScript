c7eeff2e59cd3606f58826b6898f49cb16f99654
mrmagic2020 <2732298611@qq.com>
2023-04-27 17:24:07 +1000

    [fix] Reset indentation

--- Maths/ParityOutlier.js
+++ Maths/ParityOutlier.js
@@ -7,23 +7,23 @@
  * @example parityOutlier([1, 3, 5, 8, 9]) = 8
  */
 const parityOutlier = (integers) => {
-    // define new containers
-    let odds = []; //store odd number(s)
-    let evens = []; // store even number(s)
+  // define new containers
+  let odds = []; //store odd number(s)
+  let evens = []; // store even number(s)
 
-    for (let i = 0; i < integers.length; i++) {
-        if (!Number.isInteger(integers[i])) { // detect non-integer elements
-            return null;
-        } else if (integers[i] % 2 === 0) { // an even number
-            evens.push(integers[i]);
-        } else { // an odd number
-            odds.push(integers[i]);
-        }
+  for (let i = 0; i < integers.length; i++) {
+    if (!Number.isInteger(integers[i])) { // detect non-integer elements
+      return null;
+    } else if (integers[i] % 2 === 0) { // an even number
+      evens.push(integers[i]);
+    } else { // an odd number
+      odds.push(integers[i]);
     }
+  }
 
-    if (odds.length === 0 || evens.length === 0) return null;
+  if (odds.length === 0 || evens.length === 0) return null;
 
-    return odds.length === 1 ? odds[0] : evens[0];
+  return odds.length === 1 ? odds[0] : evens[0];
 }
 
 export { parityOutlier };
