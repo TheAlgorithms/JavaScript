568763d9fe288d4fdde9b8b3412f22b334fb0dbb
mrmagic2020 <2732298611@qq.com>
2023-04-27 17:33:20 +1000

    [fix] Style changes

--- Maths/ParityOutlier.js
+++ Maths/ParityOutlier.js
@@ -8,22 +8,22 @@
  */
 const parityOutlier = (integers) => {
   // define new containers
-  let odds = []; //store odd number(s)
-  let evens = []; // store even number(s)
+  const odds = [] // store odd number(s)
+  const evens = [] // store even number(s)
 
   for (let i = 0; i < integers.length; i++) {
     if (!Number.isInteger(integers[i])) { // detect non-integer elements
-      return null;
+      return null
     } else if (integers[i] % 2 === 0) { // an even number
-      evens.push(integers[i]);
+      evens.push(integers[i])
     } else { // an odd number
-      odds.push(integers[i]);
+      odds.push(integers[i])
     }
   }
 
-  if (odds.length === 0 || evens.length === 0) return null;
+  if (odds.length === 0 || evens.length === 0) return null
 
-  return odds.length === 1 ? odds[0] : evens[0];
+  return odds.length === 1 ? odds[0] : evens[0]
 }
 
-export { parityOutlier };
+export { parityOutlier }
