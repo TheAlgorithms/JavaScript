2ee7ed96bda4a633ec53786c5a2dd3751cb70388
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2023-04-30 17:04:24 +0530

    Update Guess_the_number.js

--- Games/Guess_the_number.js
+++ Games/Guess_the_number.js
@@ -1,1 +1,23 @@
 
+/*Guess the number game */
+
+/* this function is used to get the random number */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let randomNumber = getRandomInt(1, 100);   // random number is stored in this
+let guessedNumber;
+let count =0;
+
+
+do{
+   guessedNumber = prompt("guess the number");  // used to get the number from user
+   if (guessedNumber < randomNumber)  
+        console.log("The number is greater than you guessed");
+   if (guessedNumber > randomNumber)
+        console.log("The number is Smaller than you guessed");
+  count++;
+} while(guessedNumber != randomNumber);  // loop is continue till the number is not guessed 
+
+console.log("Your Score : ", 100 - count);
