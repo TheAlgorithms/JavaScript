02798e8899d96d46a1a4aef74a2d213d7bd24d60
shamimsikder <shamimsikder138@gmail.com>
2023-06-27 14:04:55 +0600

    Upadated reverse array code

--- Data-Structures/Array/Reverse.js
+++ Data-Structures/Array/Reverse.js
@@ -1,17 +1,15 @@
-/** https://www.geeksforgeeks.org/write-a-program-to-Reverse-an-array-or-string/
- * This function will accept an array and
- * Reverse its elements and returns the inverted array
- * @param {Array} arr array with elements of any data type
- * @returns {Array} array with inverted elements
+/**
+ * This function accepts an array and reverses its elements.
+ * @param {Array} arr - Array with elements of any data type.
+ * @returns {Array} - Array with reversed elements.
  */
-
-const Reverse = (arr) => {
-  // limit specifies the amount of Reverse actions
-  for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
-    const temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+const reverseArray = (arr) => {
+  for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
-  return arr
-}
-export { Reverse }
+  return arr;
+};
+
+export default reverseArray;
