f12616c7a03da92a078b5b8ce16a46207baf7473
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-18 02:32:06 +0530

    Octal to Binary added

--- Conversions/OctToBinary.js
+++ Conversions/OctToBinary.js
@@ -1,19 +1,18 @@
-function octalToBinary(octal) {
-    var binary = ""; // an empty string to store the binary number
-    for (var i = 0; i < octal.length; i++) { // loop through each octal digit
-        var digit = parseInt(octal[i], 8); // convert the octal digit to a decimal digit
-        var binDigit = digit.toString(2); // convert the decimal digit to a binary digit
-        while (binDigit.length < 3) { // pad the binary digit with zeros if needed
-          binDigit = "0" + binDigit;
-        }
-        binary += binDigit; // concatenate the binary digit to the binary number
-      }
-      binary = binary.replace(/^0+/, "");
-      return binary;
+function octalToBinary (octal) {
+  let binary = '' // an empty string to store the binary number
+  for (let i = 0; i < octal.length; i++) { // loop through each octal digit
+    const digit = parseInt(octal[i], 8) // convert the octal digit to a decimal digit
+    let binDigit = digit.toString(2) // convert the decimal digit to a binary digit
+    while (binDigit.length < 3) { // pad the binary digit with zeros if needed
+      binDigit = '0' + binDigit
+    }
+    binary += binDigit // concatenate the binary digit to the binary number
   }
+  binary = binary.replace(/^0+/, '')
+  return binary
+}
 
-export { octalToBinary };
-  
+export { octalToBinary }
+
 // > octalToBinary(46)
 // 100110
-  
--- Conversions/test/OctToBinary.test.js
+++ Conversions/test/OctToBinary.test.js
@@ -1,16 +1,16 @@
 import { octalToBinary } from '../OctToBinary'
 
 test('The Binary representation of Octal number 46 is 100110', () => {
-  const res = octalToBinary("46")
-  expect(res).toBe("100110")
+  const res = octalToBinary('46')
+  expect(res).toBe('100110')
 })
 
 test('The Decimal representation of Octal number 32 is 11010', () => {
-    const res = octalToBinary("32")
-    expect(res).toBe("11010")
+  const res = octalToBinary('32')
+  expect(res).toBe('11010')
 })
-  
+
 test('The Decimal representation of Octal number 17 is 1111', () => {
-    const res = octalToBinary("17")
-    expect(res).toBe("1111")
+  const res = octalToBinary('17')
+  expect(res).toBe('1111')
 })
