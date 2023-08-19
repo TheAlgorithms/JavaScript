d73e24002ac281f10963c0ba24d89938bc85377d
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2023-08-18 22:38:34 -0300

    Update IsDivisible.js

--- Maths/IsDivisible.js
+++ Maths/IsDivisible.js
@@ -1,12 +1,20 @@
 // Checks if a number is divisible by another number.
-
-export const isDivisible = (num1, num2) => {
+function checkFiniteNumbers(num1, num2) {
   if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
-    throw new TypeError('Expected a valid real number')
+    throw new TypeError('Expected valid real numbers');
   }
+  // Se ambos os números forem finitos, você pode fazer alguma operação aqui
+  // ou simplesmente retornar true ou executar qualquer outra ação desejada.
+  return true; // Por exemplo, aqui estamos retornando true se os números forem finitos.
+}
+function isZero(num2){
   if (num2 === 0) {
     return false
   }
+}
+export const isDivisible = (num1, num2) => {
+  checkFiniteNumbers(num1, num2);
+  if(isZero(num2))
   return num1 % num2 === 0
 }
 
