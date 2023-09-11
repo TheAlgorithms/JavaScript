a8118ce9265c83a43b92e392555049350c216cd6
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-11 21:19:34 +0530

    used shorthand

--- Conversions/DecimalToRoman.js
+++ Conversions/DecimalToRoman.js
@@ -11,11 +11,9 @@ const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
 const hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
 const thousands = ['', 'M', 'MM', 'MMM']
 
-function decimalToRoman (num) {
-  return thousands[Math.floor(num / 1000)] +
-          hundreds[Math.floor((num % 1000) / 100)] +
+const decimalToRoman = (num) => thousands[Math.floor(num / 1000)] +
+         hundreds[Math.floor((num % 1000) / 100)] +
          tens[Math.floor((num % 100) / 10)] +
          ones[num % 10]
-}
 
 export { decimalToRoman }
