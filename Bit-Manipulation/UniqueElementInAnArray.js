c221c92a70e5727f48c6c870284a77ac5d07fddb
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-11 20:46:40 +0530

    updated changes and add some explanations

--- Bit-Manipulation/UniqueElementInAnArray.js
+++ Bit-Manipulation/UniqueElementInAnArray.js
@@ -1,7 +1,18 @@
-const UniqueElementInAnArray = (arr) => {
-  let xr = 0
-  for (let i = 0; i < arr.length; i++) { xr ^= arr[i] }
-  return xr
-}
+/*
+  Problem Statement :
+
+    author : @dev-madhurendra
+
+    a) This algo works when interviewer asks if the all the element is repeating
+       twice except one element.
+
+    Explanation :
+      [1,2,1,2,3]
+      a) Basic thing is a^a (a xor a) = 0
+      b) So if we do like
+         1^2^1^2^3 => (1^1)^(2^2)^3 => 0^0^3 => 3
+*/
+
+const UniqueElementInAnArray = (arr) => arr.reduce((acc, val) => acc ^ val, 0)
 
 export { UniqueElementInAnArray }
