4b181b1b8f5c575b5ddba20bfab5f150987e152c
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-11 21:14:37 +0530

    fixed naming convention

--- Conversions/DecimalToRoman.js
+++ Conversions/DecimalToRoman.js
@@ -8,12 +8,12 @@
 
 const ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
 const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
-const hrns = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
-const ths = ['', 'M', 'MM', 'MMM']
+const hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+const thousands = ['', 'M', 'MM', 'MMM']
 
 function decimalToRoman (num) {
-  return ths[Math.floor(num / 1000)] +
-         hrns[Math.floor((num % 1000) / 100)] +
+  return thousands[Math.floor(num / 1000)] +
+          hundreds[Math.floor((num % 1000) / 100)] +
          tens[Math.floor((num % 100) / 10)] +
          ones[num % 10]
 }
