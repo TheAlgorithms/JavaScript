155e9c1ecc37f68835905c1b35120bf48677380a
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-11 21:11:21 +0530

    updated decimal to roman code

--- Conversions/DecimalToRoman.js
+++ Conversions/DecimalToRoman.js
@@ -6,47 +6,16 @@
     Algorithm & Explanation : https://www.rapidtables.com/convert/number/how-number-to-roman-numerals.html
 */
 
-const values = {
-  M: 1000,
-  CM: 900,
-  D: 500,
-  CD: 400,
-  C: 100,
-  XC: 90,
-  L: 50,
-  XL: 40,
-  X: 10,
-  IX: 9,
-  V: 5,
-  IV: 4,
-  I: 1
-}
-
-const orders = [
-  'M',
-  'CM',
-  'D',
-  'CD',
-  'C',
-  'XC',
-  'L',
-  'XL',
-  'X',
-  'IX',
-  'V',
-  'IV',
-  'I'
-]
+const ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+const hrns = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+const ths = ['', 'M', 'MM', 'MMM']
 
 function decimalToRoman (num) {
-  let roman = ''
-  for (const symbol of orders) {
-    while (num >= values[symbol]) {
-      roman += symbol
-      num -= values[symbol]
-    }
-  }
-  return roman
+  return ths[Math.floor(num / 1000)] +
+         hrns[Math.floor((num % 1000) / 100)] +
+         tens[Math.floor((num % 100) / 10)] +
+         ones[num % 10]
 }
 
 export { decimalToRoman }
