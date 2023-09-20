3a4b8facc977a3375126658f96945334af2c700f
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-20 15:58:39 +0530

    added JSDoc comment

--- Maths/NumberOfDigits.js
+++ Maths/NumberOfDigits.js
@@ -10,13 +10,13 @@
 const numberOfDigit = (n) => Math.abs(n).toString().length
 
 /**
+ * Returns the number of digits of a given integer.
  *
- * Author : dev-madhurendra
- * Returns the number of digits of a given integer
- * Resource : https://math.stackexchange.com/questions/2145480/how-does-the-logarithm-returns-the-number-of-digits-of-a-number
- *
+ * @param {number} n - The integer for which to count digits.
+ * @returns {number} The number of digits in the integer.
+ * @see https://math.stackexchange.com/questions/2145480/how-does-the-logarithm-returns-the-number-of-digits-of-a-number
+ * @author dev-madhurendra
  */
-
 const numberOfDigitsUsingLog = (n) => n === 0 ? 1 : Math.floor(Math.log10(Math.abs(n))) + 1
 
 export { numberOfDigit, numberOfDigitsUsingLog }
