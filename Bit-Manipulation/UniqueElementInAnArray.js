d7da55f47958ac91c67d4c5b7defb19ec5903dae
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-21 15:11:30 +0530

    used JSDoc comment

--- Bit-Manipulation/UniqueElementInAnArray.js
+++ Bit-Manipulation/UniqueElementInAnArray.js
@@ -1,20 +1,13 @@
-/*
-  Problem Statement :
+/**
+ * Finds the unique element in an array where all other elements are repeated twice.
+ *
+ * @param {number[]} arr - The input array of integers.
+ * @returns {number} The unique element.
+ *
+ * @example
+ * const arr = [1, 2, 1, 2, 3];
+ * const uniqueElement = findUniqueElement(arr); // Returns 3
+ */
+const findUniqueElement = (arr) => arr.reduce((acc, val) => acc ^ val, 0)
 
-    author : @dev-madhurendra
-
-    Question : https://leetcode.com/problems/unique-number-of-occurrences/
-
-    a) This algo works if all the element is repeating
-       twice except one element.
-
-    Explanation :
-      [1,2,1,2,3]
-      a) Basic thing is a^a (a xor a) = 0
-      b) So if we do like
-         1^2^1^2^3 => (1^1)^(2^2)^3 => 0^0^3 => 3
-*/
-
-const UniqueElementInAnArray = (arr) => arr.reduce((acc, val) => acc ^ val, 0)
-
-export { UniqueElementInAnArray }
+export { findUniqueElement }
--- Bit-Manipulation/test/UniqueElementInAnArray.test.js
+++ Bit-Manipulation/test/UniqueElementInAnArray.test.js
@@ -1,10 +1,10 @@
-import { UniqueElementInAnArray } from '../UniqueElementInAnArray'
+import { findUniqueElement } from '../UniqueElementInAnArray'
 
 describe('UniqueElementInAnArray', () => {
   it.each([
     [[1, 2, 1, 3, 3], 2],
     [[1, 2, 3, 4, 5, 4, 3, 2, 1], 5]
   ])('should return an unique element from an array', (arr, expected) => {
-    expect(UniqueElementInAnArray(arr)).toBe(expected)
+    expect(findUniqueElement(arr)).toBe(expected)
   })
 })
