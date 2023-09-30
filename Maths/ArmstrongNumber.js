4e38ab5331b6cf6f53c0477563ee4a8318ac02b8
shahayush480 <shahayush480@gmail.com>
2023-09-30 14:40:27 +0530

    fix: #758  optimised armstrongNumber code

--- Maths/ArmstrongNumber.js
+++ Maths/ArmstrongNumber.js
@@ -9,16 +9,13 @@
  */
 
 const armstrongNumber = (num) => {
-  if (num < 0 || typeof num !== 'number') return false
-
-  let newSum = 0
-
-  const numArr = num.toString().split('')
-  numArr.forEach((num) => {
-    newSum += parseInt(num) ** numArr.length
-  })
-
-  return newSum === num
+  if (typeof num !== 'number' || num < 0) return false
+  const numStr = num.toString()
+  const sum = [...numStr].reduce(
+    (acc, digit) => acc + parseInt(digit) ** numStr.length,
+    0
+  )
+  return sum === num
 }
 
 export { armstrongNumber }
