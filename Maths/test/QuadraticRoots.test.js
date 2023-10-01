ef24e59409e4f929d128e877edbf86835086a6f5
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-10-01 20:09:04 +0200

    Readd describe block, remove redundant comments

--- Maths/test/QuadraticRoots.test.js
+++ Maths/test/QuadraticRoots.test.js
@@ -1,16 +1,13 @@
 import { quadraticRoots } from '../QuadraticRoots.js'
 
-// Test case 1: Two real roots
-test('returns an array with two real roots when the discriminant is positive', () => {
-  expect(quadraticRoots(1, -3, 2)).toEqual([2, 1])
-})
-
-// Test case 2: One real root
-test('returns an array with one real root when the discriminant is zero', () => {
-  expect(quadraticRoots(1, -2, 1)).toEqual([1])
-})
-
-// Test case 3: No real roots
-test('returns an empty array indicating no real roots when the discriminant is negative', () => {
-  expect(quadraticRoots(1, 2, 5)).toEqual([])
+describe('quadratic roots', () => {
+  it('returns an array with two real roots when the discriminant is positive', () => {
+    expect(quadraticRoots(1, -3, 2)).toEqual([2, 1])
+  })
+  it('returns an array with one real root when the discriminant is zero', () => {
+    expect(quadraticRoots(1, -2, 1)).toEqual([1])
+  })
+  it('returns an empty array indicating no real roots when the discriminant is negative', () => {
+    expect(quadraticRoots(1, 2, 5)).toEqual([])
+  })
 })
