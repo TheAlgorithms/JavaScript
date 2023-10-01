a3cd2f32fb0d49eec597970631bfb1a2c9faac67
Dibya <Dibya.DebayanDash@siriuscom.com>
2023-10-02 00:23:59 +0530

    Changed [1,1] to [1]

--- Maths/QuadraticRoots.js
+++ Maths/QuadraticRoots.js
@@ -12,7 +12,7 @@
  * @example
  * // Find the roots of the quadratic equation: 2x^2 - 4x + 2 = 0
  * const roots = quadraticRoots(2, -4, 2);
- * // Expected output: [1, 1]
+ * // Expected output: [1]
  */
 const quadraticRoots = (a, b, c) => {
   // Calculate the discriminant
