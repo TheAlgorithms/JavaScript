78730998a61f704f857dd4ccbe08bee059ac8338
Abhijit-033 <abhijitpresennang@gmail.com>
2023-10-01 18:43:47 +0530

    fix:#1389 Errors Fixed

--- String/LengthofLongestSubstringWithoutRepetition.js
+++ String/LengthofLongestSubstringWithoutRepetition.js
@@ -9,19 +9,19 @@
 
 const lengthOfLongestSubstring = (s) => {
     if (typeof s !== 'string') {
-      throw new TypeError('Invalid Input Type');
+      throw new TypeError('Invalid Input Type')
     }
-    let maxLength = 0;
-    let start = 0;
-    const charMap = new Map();
+    let maxLength = 0
+    let start = 0
+    const charMap = new Map()
     for (let end = 0; end < s.length; end++) {
       if (charMap.has(s[end])) {
-        start = Math.max(start, charMap.get(s[end]) + 1);
+        start = Math.max(start, charMap.get(s[end]) + 1)
       }
-      charMap.set(s[end], end);
-      maxLength = Math.max(maxLength, end - start + 1);
+      charMap.set(s[end], end)
+      maxLength = Math.max(maxLength, end - start + 1)
     }
-    return maxLength;
+    return maxLength
   }
 
 export {lengthOfLongestSubstring}
