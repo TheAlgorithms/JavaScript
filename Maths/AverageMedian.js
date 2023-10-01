c58f28f9f18e0cd5bc628ec0701392eae3e0d4d9
shahayush480 <shahayush480@gmail.com>
2023-10-01 20:27:33 +0530

    Fix: #758 used ternary operator to make code more optimised

--- Maths/AverageMedian.js
+++ Maths/AverageMedian.js
@@ -12,9 +12,9 @@ const averageMedian = (sourceArrayOfNumbers) => {
   const numbers = [...sourceArrayOfNumbers].sort(sortNumbers)
   const numLength = numbers.length
 
-  if (numLength % 2 === 0) {
-    return (numbers[numLength / 2 - 1] + numbers[numLength / 2]) / 2
-  } else return numbers[Math.floor(numLength / 2)]
+  return numLength % 2 === 0
+    ? (numbers[numLength / 2 - 1] + numbers[numLength / 2]) / 2
+    : numbers[Math.floor(numLength / 2)]
 }
 
 const sortNumbers = (num1, num2) => num1 - num2
