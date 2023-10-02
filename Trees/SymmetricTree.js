07aedea9536c234e9df7f53087bc991752bb0670
Matheus Henrique <39416183+matheusjustino@users.noreply.github.com>
2023-10-02 17:32:07 -0300

    fix lint

--- Trees/SymmetricTree.js
+++ Trees/SymmetricTree.js
@@ -7,25 +7,16 @@ Definition for a binary tree node.
  * }
  */
 /**
- * @param {TreeNode} root
- * @return {boolean}
+ * @param '{TreeNode}' root
+ * @return '{boolean}'
  */
 
-function TreeNode(val, left, right) {
-  this.val = (val === undefined ? 0 : val)
-  this.left = (left === undefined ? null : left)
-  this.right = (right === undefined ? null : right)
-}
-
 const isSymmetric = function (root) {
-  function resolve(n1, n2) {
+  function resolve (n1, n2) {
     if (!n1 && !n2) return true
     if (n1?.val !== n2?.val) return false
-  
     return resolve(n1.left, n2.right) && resolve(n1.right, n2.left)
   }
-  
   return resolve(root?.left, root?.right)
 }
-
 export { isSymmetric }
