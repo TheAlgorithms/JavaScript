515ff8fdb47032072c7e0825359da3dc6765d9ad
Matheus Henrique <39416183+matheusjustino@users.noreply.github.com>
2023-10-02 17:18:30 -0300

    Create LongestPalindromicSubstring.js

---
+++ String/LongestPalindromicSubstring.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = function (s) {
+  let subStr = ""
+  let subStrLen = 0
+  
+  for (let i = 0 i < s.length i++) {
+    for (let j = 0 j <= 1 j++) {
+      let left = i
+      let right = i + j
+  
+      while (left >= 0 && right < s.length && s[left] === s[right]) {
+        let len = right - left + 1
+  
+        if (len > subStrLen) {
+          subStr = s.substring(left, right + 1)
+          subStrLen = len
+        }
+  
+        left--
+        right++
+      }
+    }
+  }
+  
+  return subStr
+}
+
+export { longestPalindrome }
