4949e3e1be68308994bd623e317f52ce04a00d3e
Matheus Henrique <39416183+matheusjustino@users.noreply.github.com>
2023-10-02 17:13:15 -0300

    fix lint

--- Hash-Table/TopKFrequentElements.js
+++ Hash-Table/TopKFrequentElements.js
@@ -3,19 +3,19 @@
  * @param {number} k
  * @return {number[]}
  */
-const topKFrequent = function(nums, k) {
+const topKFrequent = function (nums, k) {
   const mapNums = nums.reduce((prev, curr) => {
     if (prev[curr]) {
       prev[curr] += 1
     } else {
       prev[curr] = 1
     }
-    return prev;
-  }, {});
+    return prev
+  }, {})
 
   const sortedMap = Object.entries(mapNums).sort((a, b) => {
     return b[1] - a[1]
-  });
+  })
 
   return Object.values(sortedMap).slice(0, k).map((v) => v[0])
 }
