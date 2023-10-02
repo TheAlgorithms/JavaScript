8510580310f72b7069444cbd240b491b2394110c
Matheus Henrique <39416183+matheusjustino@users.noreply.github.com>
2023-10-02 16:50:03 -0300

    Update MergeTwoSortedLists.js
    Update space style

--- Recursive/MergeTwoSortedLists.js
+++ Recursive/MergeTwoSortedLists.js
@@ -11,38 +11,38 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(list1, list2) {
-    let result = new ListNode();
+  let result = new ListNode();
 
-    let vl1 = list1?.val;
-    let vl2 = list2?.val;
+  let vl1 = list1?.val;
+  let vl2 = list2?.val;
 
-    if (vl1 === undefined && vl2 === undefined) return list1;
+  if (vl1 === undefined && vl2 === undefined) return list1;
 
-    function recursiveNode(node) {
-        if (node.next) {
-            return recursiveNode(node.next);
-        }
-        return node;
-    }
+  function recursiveNode(node) {
+      if (node.next) {
+          return recursiveNode(node.next);
+      }
+      return node;
+  }
 
-    while(true) {
-        if (!list1 && !list2) break;
+  while(true) {
+      if (!list1 && !list2) break;
 
-        let v1 = (list1?.val ?? Infinity);
-        let v2 = (list2?.val ?? Infinity);
+      let v1 = (list1?.val ?? Infinity);
+      let v2 = (list2?.val ?? Infinity);
 
-        let deepNode = recursiveNode(result);
+      let deepNode = recursiveNode(result);
 
-        if (v1 <= v2) {
-            deepNode.val = v1;
-            list1 = list1?.next;
-        } else {
-            deepNode.val = v2;
-            list2 = list2?.next;
-        }
+      if (v1 <= v2) {
+          deepNode.val = v1;
+          list1 = list1?.next;
+      } else {
+          deepNode.val = v2;
+          list2 = list2?.next;
+      }
 
-        if (list1 || list2) deepNode.next = new ListNode();
-    }
+      if (list1 || list2) deepNode.next = new ListNode();
+  }
 
-    return result;
+  return result;
 };
