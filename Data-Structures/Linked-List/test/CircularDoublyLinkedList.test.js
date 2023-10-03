b1f967a0fbfe925aa5d9d490ad171e19cf533937
Pronay Debnath <pronayd4455@gmail.com>
2023-10-03 18:51:37 +0530

    Update CircularDoublyLinkedList.test.js

--- Data-Structures/Linked-List/test/CircularDoublyLinkedList.test.js
+++ Data-Structures/Linked-List/test/CircularDoublyLinkedList.test.js
@@ -1,55 +1,55 @@
-import { CircularDoublyLinkedList } from '../CircularDoublyLinkedList'; // Adjust the import path as needed
+import { CircularDoublyLinkedList } from '../CircularDoublyLinkedList' // Adjust the import path as needed
 
-describe('CircularDoublyLinkedList', () => {
-  it('Check append', () => {
-    const list = new CircularDoublyLinkedList();
+describe ('CircularDoublyLinkedList', () => {
+  it ('Check append', () => {
+    const list = new CircularDoublyLinkedList()
 
-    list.append(1);
-    expect(list.toString()).toEqual('1');
+    list.append(1)
+    expect(list.toString()).toEqual('1')
 
-    list.append(2);
-    expect(list.toString()).toEqual('1 <-> 2');
-  });
+    list.append(2)
+    expect(list.toString()).toEqual('1 <-> 2')
+  })
 
-  it('Check insert', () => {
-    const list = new CircularDoublyLinkedList();
+  it ('Check insert', () => {
+    const list = new CircularDoublyLinkedList()
 
-    list.insert(0, 1);
-    expect(list.toString()).toEqual('1');
+    list.insert(0, 1)
+    expect(list.toString()).toEqual('1')
 
-    list.insert(1, 20);
-    expect(list.toString()).toEqual('1 <-> 20');
-  });
+    list.insert(1, 20)
+    expect(list.toString()).toEqual('1 <-> 20')
+  })
 
-  it('Check removeAt', () => {
-    const list = new CircularDoublyLinkedList();
+  it ('Check removeAt', () => {
+    const list = new CircularDoublyLinkedList()
 
-    list.append(10);
-    list.append(40);
-    list.append(30);
+    list.append(10)
+    list.append(40)
+    list.append(30)
 
-    list.removeAt(0);
-    expect(list.toString()).toEqual('40 <-> 30');
+    list.removeAt(0)
+    expect(list.toString()).toEqual('40 <-> 30')
 
-    list.removeAt(1);
-    expect(list.toString()).toEqual('40');
-  });
+    list.removeAt(1)
+    expect(list.toString()).toEqual('40')
+  })
 
-  it('Check print', () => {
-    const list = new CircularDoublyLinkedList();
+  it ('Check print', () => {
+    const list = new CircularDoublyLinkedList()
 
-    list.append(20);
+    list.append(20)
     expect(() => {
-      list.print();
-    }).not.toThrow();
-  });
+      list.print()
+    }).not.toThrow()
+  })
 
-  it('Check isEmpty', () => {
-    const list = new CircularDoublyLinkedList();
+  it ('Check isEmpty', () => {
+    const list = new CircularDoublyLinkedList()
 
-    expect(list.isEmpty()).toEqual(true);
+    expect(list.isEmpty()).toEqual(true)
 
-    list.append('Hello');
-    expect(list.isEmpty()).toEqual(false);
-  });
-});
+    list.append('Hello')
+    expect(list.isEmpty()).toEqual(false)
+  })
+})
