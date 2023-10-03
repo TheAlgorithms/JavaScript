72765bd97f3e810144277a1a2ff24e2fa63ac615
Pronay Debnath <pronayd4455@gmail.com>
2023-10-03 16:33:11 +0530

    Update Rectangle.js

--- Geometry/Rectangle.js
+++ Geometry/Rectangle.js
@@ -10,7 +10,7 @@ export default class Rectangle {
    * @param {number} width - The width of the rectangle.
    * @throws {Error} If length or width is less than or equal to 0.
    */
-  constructor(length, width) {
+  constructor (length, width) {
     this.length = length
     this.width = width
   }
@@ -21,9 +21,9 @@ export default class Rectangle {
    * @returns {number} The perimeter of the rectangle.
    * @throws {Error} If length or width is less than or equal to 0.
    */
-  calculatePerimeter() {
+  calculatePerimeter () {
     if (this.length <= 0 || this.width <= 0) {
-      throw new Error("Invalid input. Length and width must be greater than 0.")
+      throw new Error('Invalid input. Length and width must be greater than 0.')
     }
     return 2 * (this.length + this.width)
   }
@@ -34,9 +34,9 @@ export default class Rectangle {
    * @returns {number} The area of the rectangle.
    * @throws {Error} If length or width is less than or equal to 0.
    */
-  calculateArea() {
+  calculateArea () {
     if (this.length <= 0 || this.width <= 0) {
-      throw new Error("Invalid input. Length and width must be greater than 0.")
+      throw new Error('Invalid input. Length and width must be greater than 0.')
     }
     return this.length * this.width
   }
