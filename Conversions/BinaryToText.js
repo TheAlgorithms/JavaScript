3b29a7e11ba529c85667faab4eba986d0fb5baab
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-03 13:56:48 +0530

    Update BinaryToText.js

--- Conversions/BinaryToText.js
+++ Conversions/BinaryToText.js
@@ -4,22 +4,9 @@
 // 3.Get character of ASCII code from ASCII table.
 // 4.Continue with next byte.
   
-  function binaryAgent(str) {
-    function toDecimal(x){
-        let i=0;
-        let sum=0
-        while(x>0){
-        sum+=(x%10)*Math.pow(2,i);
-        x=Math.floor(x/10);
-        i++;
-        }
-        return sum;
-    }
-    let arr=str.split(" ").map(x=>parseFloat(x));
-    let arr1=arr.map(toDecimal)
-    arr1=arr1.map(x=>String.fromCharCode(x))
-    console.log(arr1.join(""))
-    return arr1.join("")
+ function binaryToText(str) {
+    let arr=str.split(" ").map(x=>String.fromCharCode(parseInt(x,2))).join("");
+    return arr;
   }
   
-  export { binaryAgent }
+  export { binaryToText }
