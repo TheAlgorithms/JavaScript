7492d558b57f4ae302921d5237c1363ea0606b7e
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-03 17:21:28 +0530

    Update BinaryToText.test.js

--- Conversions/test/BinaryToText.test.js
+++ Conversions/test/BinaryToText.test.js
@@ -1,13 +1,10 @@
-import { binaryToText } from '../BinaryToText.js';
+import { binaryToText } from './BinaryToText.js';
 
 describe('binaryToText', () => {
   it('should convert binary to text', () => {
-    const binaryInput = '01001000 01100101 01101100 01101100 01101111';
-    const expectedOutput = 'Hello';
-
-    const result = binaryToText(binaryInput);
-
-    expect(result).toEqual(expectedOutput);
+    expect(binaryToText('01001000 01100101 01101100 01101100 01101111')).toBe('Hello');
   });
 });
+
+
 
