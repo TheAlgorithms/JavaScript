5733b78cf66c6fe63b71646cf87ff536b51bf087
shahayush480 <shahayush480@gmail.com>
2023-10-03 07:05:54 +0530

    Resolved comments and changes requests

--- Maths/TwoSum.js
+++ Maths/TwoSum.js
@@ -1,22 +1,23 @@
-/*
-Given an array of integers, return indices of the two numbers such that they add up to
-a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the
-same element twice.
-
-Example
-Given nums = [2, 7, 11, 15], target = 9,
-Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
-return [0, 1].
-*/
+/**
+ * Given an array of integers, find two numbers that add up to a specific target.
+ *
+ * @param {number[]} nums - The array of integers.
+ * @param {number} target - The target sum.
+ * @returns {number[]} - An array containing the indices of the two numbers.
+ *
+ * @example
+ * const nums = [2, 7, 11, 15];
+ * const target = 9;
+ * const result = twoSum(nums, target);
+ * // The function should return [0, 1] because nums[0] + nums[1] = 2 + 7 = 9.
+ */
 
 const TwoSum = (nums, target) => {
-  const numIndicesMap = {}
+  const numIndicesMap = new Map()
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i]
-    if (complement in numIndicesMap) return [numIndicesMap[complement], i]
-    numIndicesMap[nums[i]] = i
+    if (numIndicesMap.has(complement)) return [numIndicesMap.get(complement), i]
+    numIndicesMap.set(nums[i], i)
   }
   return []
 }
