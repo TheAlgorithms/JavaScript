f0982bd28c361a82841a780eff912f757657e8b8
Harsh Dev Pathak <118347330+Harshdev098@users.noreply.github.com>
2023-10-04 22:10:16 +0530

    changes made in test cases

--- Bit-Manipulation/test/GrayCodes.test.js
+++ Bit-Manipulation/test/GrayCodes.test.js
@@ -3,41 +3,17 @@ import { generateGrayCodes } from '../GrayCodes.js'
 /**
  * Test cases for the generateGrayCodes function.
  */
+const testCases = [
+  { n: 0, expected: [0] },
+  { n: 1, expected: [0, 1] },
+  { n: 2, expected: [0, 1, 3, 2] },
+  { n: 3, expected: [0, 1, 3, 2, 6, 7, 5, 4] },
+  { n: 4, expected: [0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8] },
+];
 
-test('Generate Gray codes for n=3', () => {
-  const n = 3
-  const expectedGrayCodes = [0, 1, 3, 2, 6, 7, 5, 4]
-  const grayCodes = generateGrayCodes(n)
-  expect(grayCodes).toEqual(expectedGrayCodes)
-})
-test('Generate Gray codes for n=0', () => {
-    const n = 0;
-    const expectedGrayCodes = [0];
+testCases.forEach(({ n, expected }) => {
+  test(`Generate Gray codes for n=${n}`, () => {
     const grayCodes = generateGrayCodes(n);
-    expect(grayCodes).toEqual(expectedGrayCodes);
+    expect(grayCodes).toEqual(expected);
   });
-  
-  test('Generate Gray codes for n=1', () => {
-    const n = 1;
-    const expectedGrayCodes = [0, 1];
-    const grayCodes = generateGrayCodes(n);
-    expect(grayCodes).toEqual(expectedGrayCodes);
-  });
-  
-  test('Generate Gray codes for n=4', () => {
-    const n = 4;
-    const expectedGrayCodes = [
-      0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8
-    ];
-    const grayCodes = generateGrayCodes(n);
-    expect(grayCodes).toEqual(expectedGrayCodes);
-  });
-  
-  test('Generate Gray codes for n=5', () => {
-    const n = 5;
-    const expectedGrayCodes = [
-      0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8, 24, 25, 27, 26, 30, 31, 29, 28, 20, 21, 23, 22, 18, 19, 17, 16
-    ];
-    const grayCodes = generateGrayCodes(n);
-    expect(grayCodes).toEqual(expectedGrayCodes);
-  });
+});
