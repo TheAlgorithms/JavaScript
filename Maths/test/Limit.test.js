a8323ccce345648a4dfab5627b3e5259187e0f9a
maruf hasan <gamerid703@gmail.com>
2023-10-04 17:36:29 +0600

    Update Limit.test.js

--- Maths/test/Limit.test.js
+++ Maths/test/Limit.test.js
@@ -16,24 +16,17 @@ describe("limit", () => {
   it("should return 1", () => {
     expect(limit(0, (x) => Math.cos(x) / x)).toBe(0);
   });
-  it("should throw error", () => {
-    expect(limit("10", (x) => Math.cos(x) / x)).toThrow(TypeError);
+  it("should return 1", () => {
+    expect(limit(0, (x) => Math.tan(x) / x)).toBe(1);
   });
-  it("should throw error", () => {
-    expect(limit(10, "x")).toThrow(SyntaxError);
+  it("should return -2", () => {
+    expect(limit(-Infinity, (x) => (2 * x) / (1 - x))).toBe(-2);
   });
-  it("should throw error", () => {
-    expect(limit(10, (x) => Math.cos(x) / x, 12)).toThrow(TypeError);
-  });
-  it("should throw error", () => {
-    expect(limit(10, (x) => Math.cos(x) / x, 0)).toThrow(TypeError);
-  });
-  it("should throw error", () => {
+  it("should return 875", () => {
     expect(
-      limit(10, (x) => {
-        console.log(x);
-        return "abc";
+      limit(1, (x) => {
+        return (x ** (7 / 2) - 5 ** (7 / 2)) / (Math.sqrt(x) - Math.sqrt(5));
       })
-    ).toThrow(TypeError);
+    ).toBe(875);
   });
 });
