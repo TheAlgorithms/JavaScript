e08154226e257dde0e7ef01f7e45c8878c459540
ChrDek <cdekatrr@hotmail.com>
2023-10-04 19:22:52 +0300

    Update HammingCode.js
    Changes according to specs.
     - more understandable variable names/ comments etc.
    - usage of let/const
    Note: code refers to bit population count of a binary sequence

--- Bit-Manipulation/HammingCode.js
+++ Bit-Manipulation/HammingCode.js
@@ -2,27 +2,32 @@
  *   github author: chrdek
  *   license: GPL-3.0 or later
  *
- *   @param {number} b
+ *   @param {number} b = integer or, binary representation of it
  *
- *   The following code generates the hamming code for
- *   32-bit integer values (incl. parity bit check)
+ *   The following code generates the hamming code for a binary sequence
+ *   of 32-bit integer values (incl. parity bit check)
+ *
+ *   Returns the overall of bit count population for values up to 65535, inclusive
  *
  **/
 
 function HammingCode(b) {
-  var bytes = new Array(65536);
+  //preallocate total number of integers to count the bits population from.
+  let bytes = new Array(65536);
   const bitAlloc = (bin) => {
-    var cnt = 0;
+    let counter = 0;
     while (bin > 0) {
-      cnt += bin & 1;
+      counter += bin & 1;
       bin >>=1;
       }
-  return cnt;
-  } //end of bitallocation
+  return counter;
+  }
 
-for (var k=0; k < 65536; k++) bytes[k] = bitAlloc(k);
+//count all 1-bits from entire bit set
+for (let k=0; k < 65536; k++)
+bytes[k] = bitAlloc(k);
 
-//perform leftmost shifting for integer value
+//perform bit shifting for integer values for bit-populated result
 return bytes[b & 0xFFFF] + bytes[b >> 16];
 }
 
