a1edd212be2ae982d649bebc4415f0d11835e104
maruf hasan <gamerid703@gmail.com>
2023-10-04 16:02:08 +0600

    Create Limit.test.js

---
+++ Maths/test/Limit.test.js
@@ -0,0 +1,6 @@
+import { limit } from "../limit.js";
+
+let a1 = limit(2, (x) => x ** 2); //returns 4
+let a2 = limit(Infinity, (x) => (2 * x - 1) / x); // returns 2
+let a3 = limit(1, Math.sin); // returns approximately 0.84147
+console.log(a1, a2, a3);
