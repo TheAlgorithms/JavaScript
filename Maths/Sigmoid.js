3430fab91cc70ed98c4db0d48d4734048132dc14
Kausthub Kannan <kausthubkannan961@gmail.com>
2023-10-05 12:23:12 +0000

    Added Sigmoid Function

---
+++ Maths/Sigmoid.js
@@ -0,0 +1,12 @@
+/**
+ * @see [Wikipedia](https://en.wikipedia.org/wiki/Sigmoid_function)
+ * Calculate the Sigmoid value of a given number.
+ * @param {number} x- The input number.
+ * @returns {number} The Sigmoid value of the input.
+ */
+
+const sigmoid = (x) => {
+  return 1 / (1 + Math.exp(-x))
+}
+
+export { sigmoid }
