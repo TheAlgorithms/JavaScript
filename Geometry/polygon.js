3b65d25474a69cebc288972111c113255bc6bb56
rtang09 <49603415+rtang09@users.noreply.github.com>
2023-10-05 19:14:51 -0700

    Update polygon.js

--- Geometry/polygon.js
+++ Geometry/polygon.js
@@ -1,1 +1,22 @@
+/*
+This program calculates the area, perimeter, and interior angle of a regular n-gon given the side length.
+*/
 
+class polygon{
+  constructor(sidelength, sides) {
+    this.sidelength = sidelength
+    this.sides = sides
+  }
+
+  area = () => {
+    return (this.sides*this.sidelength*this.sidelength*Math.tan(Math.PI*(this.sides-2)/(2*this.sides)))/4
+  }
+
+  perimeter = () => {
+    return this.sides*this.sidelength
+  }
+
+  angle = () => {
+    return (180*(this.sides-2)/(this.sides))
+  }
+}
