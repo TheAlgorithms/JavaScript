5258492c10d42e515cd788573261940bffbb6612
ChrDek <cdekatrr@hotmail.com>
2023-10-06 16:26:24 +0300

    Create TriangularNumberCheck.test.js

---
+++ Maths/test/TriangularNumberCheck.test.js
@@ -0,0 +1,39 @@
+import { IsTriangularNumber } from '../IsTriangularNumber'
+
+describe.each([
+  { inputVal:55, expectedVal:10 },
+  { inputVal:45, expectedVal:9 },
+  { inputVal:190, expectedVal:19 },
+  { inputVal:66, expectedVal:11 },
+  { inputVal:666, expectedVal:36 },
+  { inputVal:6, expectedVal:3 },
+  { inputVal:10, expectedVal:4 },
+  { inputVal:1, expectedVal:1 },
+])('If the number of $inputVal is triangular', ({inputVal, expectedVal}) => {
+  test(`should return ${expectedVal}`, () => {
+    expect(IsTriangularNumber(inputVal)).toBe(expectedVal)
+  })
+})
+
+describe.each([
+  { inputVal:-1, expectedVal:-1 },
+  { inputVal:-10, expectedVal:-1 },
+  { inputVal:-9, expectedVal:-1 },
+  { inputVal:"foo", expectedVal:-1 },
+  { inputVal:"fizz", expectedVal:-1 }, 
+])('If the number of $inputVal is completely wrong or negative', ({inputVal, expectedVal}) => {
+  test(`should return ${expectedVal}`, () => {
+    expect(IsTriangularNumber(inputVal)).not.toBeLessThan(-1)
+  })
+})
+
+describe.each([
+  { inputVal:4183059834009, expectedVal:0 },
+  { inputVal:69, expectedVal:0 },
+  { inputVal:0, expectedVal:0 },
+  { inputVal:23, expectedVal:0 }
+])('If the number of $inputVal is NOT triangular', ({inputVal, expectedVal}) => {
+  test(`should return ${expectedVal}`, () => {
+    expect(IsTriangularNumber(inputVal)).toBe(0)
+  })
+})
