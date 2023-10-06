3c3ba46c9cb3ccbd6eafeb75bd63daed6bd75efd
James Israel <yongeazi143@gmail.com>
2023-10-06 07:03:01 +0100

    feat: Added more function to the circle Class to improve it functionality.

--- Geometry/Circle.js
+++ Geometry/Circle.js
@@ -6,14 +6,37 @@
  */
 export default class Circle {
   constructor(radius) {
+    if (radius <= 0) {
+      throw new RangeError('Radius should be a positive number')
+    }
     this.radius = radius
   }
-
+  // Formular to Calculate the perimeter of a circle
   perimeter = () => {
     return this.radius * 2 * Math.PI
   }
 
+  // Formular to Calculate the area of a circle
   area = () => {
     return Math.pow(this.radius, 2) * Math.PI
+  }
+
+  // Formular to Calculate the diameter of a circle
+  diameter = () => {
+    return 2 * this.radius
+  }
+  // Function That Changes the value of the default radius
+  changeRadius = (newRadius) => {
+    if (newRadius <= 0) {
+      throw new RangeError('New radius should be a positive number')
+    }
+    this.radius = newRadius
+  }
+  // Function That Calculates the lenght of an arc
+  lenghtOfArc = (angle) => {
+    if (angle <= 0) {
+      throw new RangeError('Angle should be a positive number')
+    }
+    return this.radius * angle
   }
 }
--- Geometry/Test/Circle.test.js
+++ Geometry/Test/Circle.test.js
@@ -2,10 +2,28 @@ import Circle from '../Circle'
 
 const circle = new Circle(3)
 
+test('The radius of the circle should be initialized to 3', () => {
+  expect(circle.radius).toEqual(3)
+})
+
 test('The area of a circle with radius equal to 3', () => {
   expect(parseFloat(circle.area().toFixed(2))).toEqual(28.27)
 })
 
 test('The perimeter of a circle with radius equal to 3', () => {
   expect(parseFloat(circle.perimeter().toFixed(2))).toEqual(18.85)
+})
+
+test('The diameter of a circle with radius equal to 3', () => {
+  expect(parseFloat(circle.diameter().toFixed(2))).toEqual(6.0)
+})
+
+test('Change radius and check if it reflects in calculations', () => {
+  circle.changeRadius(4)
+  expect(circle.radius).toEqual(4)
+  expect(parseFloat(circle.area().toFixed(2))).toEqual(50.27)
+})
+
+test('The Lenght of arc in radian of the circle', () => {
+  expect(parseFloat(circle.lenghtOfArc(2.1).toFixed(2))).toEqual(8.4)
 })
