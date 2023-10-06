895bea8d0924298c74adb7ae5d90398aab343a5c
AbhineshJha <abhinesh_223002@saitm.org>
2023-10-07 01:59:54 +0530

    Updated

--- Cellular-Automata/LangtonAnt.js
+++ Cellular-Automata/LangtonAnt.js
@@ -8,47 +8,50 @@
  * Simulates Langton's Ant for a given number of steps.
  * @param {number} steps - The number of steps to simulate.
  */
-export function simulateLangtonsAnt(steps) {
-    const gridSize = 101; // Grid size (an odd number to have a central cell)
-    const grid = Array.from({ length: gridSize }, () => Array(gridSize).fill(false));
-    let antX = Math.floor(gridSize / 2);
-    let antY = Math.floor(gridSize / 2);
-    let antDirection = 0; // 0: Up, 1: Right, 2: Down, 3: Left
-  
-    // Function to turn the ant based on the current direction
-    function turnAnt() {
-      if (grid[antX][antY]) {
-        antDirection = (antDirection + 1) % 4; // Turn right
-      } else {
-        antDirection = (antDirection + 3) % 4; // Turn left
-      }
+
+export function simulateLangtonAnt(steps, gridSize) {
+  // Initialize the grid
+  const grid = Array.from({ length: gridSize }, () =>
+    Array(gridSize).fill(false)
+  );
+
+  // Initialize the ant's position and direction
+  let antX = Math.floor(gridSize / 2);
+  let antY = Math.floor(gridSize / 2);
+  let antDirection = 'up'; // Initial direction (up, down, left, or right)
+
+  // Define the Langton's Ant rules
+  const turnRight = { up: 'right', right: 'down', down: 'left', left: 'up' };
+  const turnLeft = { up: 'left', left: 'down', down: 'right', right: 'up' };
+
+  // Simulate Langton's Ant for the specified number of steps
+  for (let step = 0; step < steps; step++) {
+    // Check the current cell's color
+    const isWhite = grid[antY][antX];
+
+    // Flip the color of the current cell
+    grid[antY][antX] = !isWhite;
+
+    // Turn the ant based on the cell's color
+    antDirection = isWhite ? turnRight[antDirection] : turnLeft[antDirection];
+
+    // Move the ant forward
+    if (antDirection === 'up') {
+      antY--;
+    } else if (antDirection === 'down') {
+      antY++;
+    } else if (antDirection === 'left') {
+      antX--;
+    } else if (antDirection === 'right') {
+      antX++;
     }
-  
-    // Function to move the ant forward
-    function moveAnt() {
-      if (antDirection === 0) {
-        antX = (antX - 1 + gridSize) % gridSize; // Move up
-      } else if (antDirection === 1) {
-        antY = (antY + 1) % gridSize; // Move right
-      } else if (antDirection === 2) {
-        antX = (antX + 1) % gridSize; // Move down
-      } else if (antDirection === 3) {
-        antY = (antY - 1 + gridSize) % gridSize; // Move left
-      }
-    }
-  
-    // Function to toggle the color of the cell the ant is on
-    function toggleCell() {
-      grid[antX][antY] = !grid[antX][antY];
-    }
-  
-    // Simulate Langton's Ant for the specified number of steps
-    for (let step = 0; step < steps; step++) {
-      toggleCell();
-      turnAnt();
-      moveAnt();
-    }
-  
-    return grid;
+
+    // Handle ant's position wrapping around the grid edges
+    if (antX < 0) antX = gridSize - 1;
+    if (antX >= gridSize) antX = 0;
+    if (antY < 0) antY = gridSize - 1;
+    if (antY >= gridSize) antY = 0;
   }
-  
+
+  return grid;
+}
