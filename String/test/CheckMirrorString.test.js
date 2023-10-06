1ab6689bd5bfa1e7a379eb8722adc107447c1db4
Nikunj Bisht <nikkubisht12@gmail.com>
2023-10-06 17:43:40 +0530

    test: chenged test for CheckMirrorString

--- String/test/CheckMirrorString.test.js
+++ String/test/CheckMirrorString.test.js
@@ -1,26 +1,27 @@
 import { checkMirrorString } from '../CheckMirrorString'
 
-describe('checkMirrorString', () => {})
-it('CheckMirrorString -> ', () => {
-  const word = 'HOAMMAOH'
-  const res = checkMirrorString(word)
-  expect(res).toBe('Is a mirror string')
-})
+describe('checkMirrorString', () => {
+  it('CheckMirrorString -> ', () => {
+    const word = 'HOAMMAOH'
+    const res = checkMirrorString(word)
+    expect(res).toBe('Is a mirror string')
+  })
 
-it('check for npn mirror string -> ', () => {
-  const word = 'JAVASCRIPT'
-  const res = checkMirrorString(word)
-  expect(res).toBe('Not a mirror string')
-})
+  it('check for npn mirror string -> ', () => {
+    const word = 'JAVASCRIPT'
+    const res = checkMirrorString(word)
+    expect(res).toBe('Not a mirror string')
+  })
 
-it('string with numeric value -> ', () => {
-  const word = 'Kop12Po'
-  const res = checkMirrorString(word)
-  expect(res).toBe('String should only contains alphabet')
-})
+  it('string with numeric value -> ', () => {
+    const word = 'Kop12Po'
+    const res = checkMirrorString(word)
+    expect(res).toBe('String should only contains alphabet')
+  })
 
-it('number as a string -> ', () => {
-  const word = 1234
-  const res = checkMirrorString(word)
-  expect(res).toBe('Not a string')
+  it('number as a string -> ', () => {
+    const word = 1234
+    const res = checkMirrorString(word)
+    expect(res).toBe('Not a string')
+  })
 })
