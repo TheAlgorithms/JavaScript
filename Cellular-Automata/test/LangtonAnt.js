af6844a86fda7bcaa154bd19225e6d3e16d4e37a
AbhineshJha <abhinesh_223002@saitm.org>
2023-10-07 01:42:55 +0530

    updated

--- Cellular-Automata/test/LangtonAnt.js
+++ Cellular-Automata/test/LangtonAnt.js
@@ -1,27 +1,27 @@
-import { simulateLangtonsAnt } from './LangtonsAnt';
+import { simulateLangtonAnt } from './LangtonAnt';
 
-describe('Langton\'s Ant', () => {
-  it('Simulates Langton\'s Ant for 1 step', () => {
+describe('LangtonAnt', () => {
+  it('Simulates LangtonAnt for 1 step', () => {
     const gridSize = 11;
     const steps = 1;
-    const result = simulateLangtonsAnt(steps, gridSize);
+    const result = simulateLangtonAnt(steps, gridSize);
 
     // Add your assertions here to check the grid state after 1 step
     // For example, you can check the color of specific cells using result[row][col]
   });
 
-  it('Simulates Langton\'s Ant for 10 steps', () => {
+  it('Simulates LangtonAnt for 10 steps', () => {
     const gridSize = 21;
     const steps = 10;
-    const result = simulateLangtonsAnt(steps, gridSize);
+    const result = simulateLangtonAnt(steps, gridSize);
 
     // Add your assertions here to check the grid state after 10 steps
   });
 
-  it('Simulates Langton\'s Ant for 50 steps', () => {
+  it('Simulates LangtonAnt for 50 steps', () => {
     const gridSize = 31;
     const steps = 50;
-    const result = simulateLangtonsAnt(steps, gridSize);
+    const result = simulateLangtonAnt(steps, gridSize);
 
     // Add your assertions here to check the grid state after 50 steps
   });
