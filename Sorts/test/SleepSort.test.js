4b9cbafb0178cb15e1e1ec9fa797069a19f3ca86
sahilkumarpb <sahilkumar@policybazaar.com>
2023-10-07 15:32:29 +0530

    Fix test file

--- Sorts/test/SleepSort.test.js
+++ Sorts/test/SleepSort.test.js
@@ -7,55 +7,55 @@
                       output matches the expected sorted array. Finally, it runs the test 
                       using jest.runAllTimers() to allow the timeouts to execute.
 */
-function sleepSort(arr) {
-    const sortedArray = [];
-  
-    function sleepSortElement(element) {
-      setTimeout(function () {
-        sortedArray.push(element);
-      }, element);
+// Import the sleepSort function
+const { sleepSort } = require('../SleepSort');
+
+// Helper function to check if an array is sorted
+function isSorted(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
     }
-  
-    arr.forEach(function (element) {
-      sleepSortElement(element);
-    });
-  
-    // Sleep for an additional time to allow all elements to be sorted
-    const maxElement = Math.max(...arr);
-    setTimeout(function () {
-      console.log(sortedArray);
-    }, maxElement + 10); // Adding a little extra time for safety
   }
-  
-  // Test cases
-  describe('sleepSort', () => {
-    it('should correctly sort an array of integers', () => {
-      const unsortedArray = [5, 3, 1, 7, 2, 4];
-      const sortedArray = [];
-      const expectedArray = [1, 2, 3, 4, 5, 7];
-  
-      // Capture the console output
-      const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-  
-      // Call the sleepSort function
-      sleepSort(unsortedArray);
-  
-      // Wait for the console output
-      jest.runAllTimers();
-  
-      // Check if the sortedArray matches the expectedArray
-      expect(logSpy).toHaveBeenCalled();
-      const actualArray = logSpy.mock.calls[0][0];
-      expect(actualArray).toEqual(expectedArray);
-  
-      // Restore the console.log function
-      logSpy.mockRestore();
+  return true;
+}
+
+// Test cases for sleepSort
+function testSleepSort() {
+  const testCases = [
+    {
+      input: [1, 6, 4, 7, 2],
+      expected: [1, 2, 4, 6, 7],
+    },
+    {
+      input: [],
+      expected: [],
+    },
+    {
+      input: [46, 15, 49, 65, 23],
+      expected: [15, 23, 46, 49, 65],
+    },
+    {
+      input: [136, 459, 132, 566, 465],
+      expected: [132, 136, 459, 465, 566],
+    },
+    {
+      input: [45, 3, 156, 1, 56],
+      expected: [1, 3, 45, 56, 156],
+    },
+  ];
+
+  for (const testCase of testCases) {
+    const { input, expected } = testCase;
+    const result = [];
+    sleepSort([...input], (element) => {
+      result.push(element);
+      if (result.length === input.length && isSorted(result)) {
+        console.log(`Pass: ${JSON.stringify(input)} => ${JSON.stringify(expected)}`);
+      }
     });
-  });
-  
-  // Run the tests
-  jest.useFakeTimers();
-  test('Test sleepSort function', () => {
-    jest.runAllTimers();
-  });
-  
+  }
+}
+
+// Run the sleepSort tests
+testSleepSort();
