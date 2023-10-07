43695cc20061b6dc826d9504f3c6b0447069d4e8
Ridge Kimani <ridgekimani@gmail.com>
2023-10-07 14:21:14 +0300

    chore: update abs function to use Math utility

--- Maths/Abs.js
+++ Maths/Abs.js
@@ -10,13 +10,12 @@
  */
 
 const abs = (num) => {
-  const validNumber = +num // converted to number, also can use - Number(num)
+  const result = Math.abs(num)
 
-  if (Number.isNaN(validNumber)) {
-    throw new TypeError('Argument is NaN - Not a Number')
-  }
+  if(Number.isNaN(result))
+    throw new TypeError('Argument is NaN - Not a Number.')
 
-  return validNumber < 0 ? -validNumber : validNumber // if number is less then zero mean negative then it converted to positive. i.e -> n = -2 = -(-2) = 2
+  return result
 }
 
 export { abs }
