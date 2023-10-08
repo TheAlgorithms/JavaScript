5105fe989a56da099e4fc203161f603d9c7517a8
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-10-08 16:03:52 +0200

    Update tests

--- Data-Structures/Stack/test/EvaluateExpression.test.js
+++ Data-Structures/Stack/test/EvaluateExpression.test.js
@@ -1,21 +1,21 @@
-import { evaluateExpression } from '../EvaluateExpression.js';
+import { evaluatePostfixExpression } from '../EvaluateExpression.js';
 
-describe('evaluateExpression', () => {
+describe('evaluatePostfixExpression', () => {
   it('should evaluate a valid expression', () => {
     const expression = '3 4 * 2 / 5 +'; // (3 * 4) / 2 + 5 = 11
-    const result = evaluateExpression(expression);
+    const result = evaluatePostfixExpression(expression);
     expect(result).toBe(11);
   });
 
   it('should handle division by zero', () => {
     const expression = '3 0 /'; // Division by zero
-    const result = evaluateExpression(expression);
+    const result = evaluatePostfixExpression(expression);
     expect(result).toBe(null);
   });
 
   it('should handle an invalid expression', () => {
     const expression = '3 * 4 2 / +'; // Invalid expression
-    const result = evaluateExpression(expression);
+    const result = evaluatePostfixExpression(expression);
     expect(result).toBe(null);
   });
 
