73c53bc1557e37a10fe0e82d6490f860d9f04847
Ashwin V Kumar <122103046+github-ashwin@users.noreply.github.com>
2023-10-08 14:17:20 +0000

    Change added

--- Data-Structures/Tree/Skiplist.js
+++ Data-Structures/Tree/Skiplist.js
@@ -45,61 +45,11 @@ class SkipList {
         }
     }
 
-    search(target) {
-        let current = this.head;
-
-        for (let i = this.level - 1; i >= 0; i--) {
-            while (current.next[i] !== undefined && current.next[i].value < target) {
-                current = current.next[i];
-            }
-        }
-
-        if (current.next[0] !== undefined && current.next[0].value === target) {
-            return true;
-        }
-
-        return false;
-    }
-
-    delete(value) {
-        let current = this.head;
-        const update = new Array(this.level);
-
-        for (let i = this.level - 1; i >= 0; i--) {
-            while (current.next[i] !== undefined && current.next[i].value < value) {
-                current = current.next[i];
-            }
-            update[i] = current;
-        }
-
-        if (current.next[0] !== undefined && current.next[0].value === value) {
-            const deletedNode = current.next[0];
-            for (let i = 0; i < deletedNode.next.length; i++) {
-                update[i].next[i] = deletedNode.next[i];
-            }
-
-            // Update the skip list's level if necessary
-            while (this.level > 1 && this.head.next[this.level - 1] === undefined) {
-                this.level--;
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-    display() {
-        for (let i = this.level - 1; i >= 0; i--) {
-            let current = this.head.next[i];
-            let levelStr = `Level ${i}: Head -> `;
-
-            while (current !== undefined) {
-                levelStr += `${current.value} -> `;
-                current = current.next[i];
-            }
-
-            console.log(levelStr + "null");
+    *iterator() {
+        let current = this.head.next[0];
+        while (current !== undefined) {
+            yield current.value;
+            current = current.next[0];
         }
     }
 }
@@ -114,8 +64,8 @@ skipList.insert(12);
 skipList.insert(16);
 skipList.insert(24);
 
-console.log(skipList.search(8)); // Should print true
-console.log(skipList.search(5)); // Should print false
+const iterator = skipList.iterator();
+for (const value of iterator) {
+    console.log(value);
+}
 
-skipList.delete(8);
-skipList.display();
