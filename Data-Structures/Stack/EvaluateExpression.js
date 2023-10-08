9c790bc93d7f7e96b776d6be965f262be80c94e1
gaurovgiri <gaurovgiri@gmail.com>
2023-10-08 20:57:10 +0545

    fixes mentioned issues

--- Data-Structures/Stack/EvaluateExpression.js
+++ Data-Structures/Stack/EvaluateExpression.js
@@ -2,64 +2,58 @@
  * Evaluate a numeric operations string in postfix notation using a stack.
  * Supports basic arithmetic operations: +, -, *, /
  * @see https://www.geeksforgeeks.org/evaluation-of-postfix-expression/
- * @param {string} expression - Numeric operations expression to evaluate.
+ * @param {string} expression - Numeric operations expression to evaluate. Must be a valid postfix expression.
  * @returns {number|null} - Result of the expression evaluation, or null if the expression is invalid.
  */
 function evaluatePostfixExpression(expression) {
-    const stack = [];
-  
-    // Helper function to perform an operation and push the result to the stack
-    function performOperation(operator) {
-      const operand2 = stack.pop();
-      const operand1 = stack.pop();
-      if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
-        if (operand1 === undefined || operand2 === undefined) {
-          return null; // Invalid expression
-        }
-        switch (operator) {
-          case '+':
-            stack.push(operand1 + operand2);
-            break;
-          case '-':
-            stack.push(operand1 - operand2);
-            break;
-          case '*':
-            stack.push(operand1 * operand2);
-            break;
-          case '/':
-            if (operand2 === 0) {
-              return null; // Division by zero
-            }
-            stack.push(operand1 / operand2);
-            break;
-        }
-      } else {
-        return null; // Invalid operator
-      }
+  const stack = [];
+
+  // Helper function to perform an operation and push the result to the stack
+  function performOperation(operator) {
+    const rightOp = stack.pop(); // Right operand is the top of the stack
+    const leftOp = stack.pop(); // Left operand is the next item on the stack
+
+    if (leftOp === undefined || rightOp === undefined) {
+      return false; // Invalid expression
     }
-  
-    const tokens = expression.split(' ');
-  
-    for (const token of tokens) {
-      if (!isNaN(parseFloat(token))) {
-        // If the token is a number, push it to the stack
-        stack.push(parseFloat(token));
-      } else {
-        // If the token is an operator, perform the operation
-        const result = performOperation(token);
-        if (result === null) {
-          return null; // Invalid expression
+    switch (operator) {
+      case '+':
+        stack.push(leftOp + rightOp);
+        break;
+      case '-':
+        stack.push(leftOp - rightOp);
+        break;
+      case '*':
+        stack.push(leftOp * rightOp);
+        break;
+      case '/':
+        if (rightOp === 0) {
+          return false;
         }
-      }
+        stack.push(leftOp / rightOp);
+        break;
+      default:
+        break;
     }
-  
-    if (stack.length === 1) {
-      // The final result should be on the stack
-      return stack[0];
+
+  }
+
+  const tokens = expression.split(/\s+/);
+
+  for (const token of tokens) {
+    if (!isNaN(parseFloat(token))) {
+      // If the token is a number, push it to the stack
+      stack.push(parseFloat(token));
     } else {
-      return null; // Invalid expression
+      // If the token is an operator, perform the operation
+      const result = performOperation(token);
+      if (result === false) {
+        return null; // Invalid expression
+      }
     }
   }
-  
-  export { evaluatePostfixExpression };
-  
+
+  return (stack.length === 1) ? stack[0] : null;
+}
+
+export { evaluatePostfixExpression };
