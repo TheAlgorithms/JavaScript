3a2de532e2178427fbd2e53434ca957b21d6de5f
Pronay Debnath <pronayd4455@gmail.com>
2023-10-09 23:52:46 +0530

    Update CircularDoublyLinkedList.test.js

--- Data-Structures/Linked-List/test/CircularDoublyLinkedList.test.js
+++ Data-Structures/Linked-List/test/CircularDoublyLinkedList.test.js
@@ -1,4 +1,4 @@
-import { CircularDoublyLinkedList } from '../CircularDoublyLinkedList'
+import { CircularDoublyLinkedList } from '../CircularDoublyLinkedList';
 
 describe('CircularDoublyLinkedList', () => {
   /**
@@ -7,52 +7,52 @@ describe('CircularDoublyLinkedList', () => {
    * @returns {CircularDoublyLinkedList} - The created list.
    */
   function createAndAppend(elements) {
-    const list = new CircularDoublyLinkedList()
-    elements.forEach((element) => list.append(element))
-    return list
+    const list = new CircularDoublyLinkedList();
+    elements.forEach((element) => list.append(element));
+    return list;
   }
 
-  const expectList = (list, expected) => expect([...list.elements()]).toEqual(expected)
+  const expectList = (list, expected) => expect([...list.elements()]).toEqual(expected);
 
   it('append', () => {
-    const list = createAndAppend([1])
+    const list = createAndAppend([1]);
 
-    expectList(list, [1])
+    expectList(list, [1]);
 
-    list.append(2)
-    expectList(list, [1, 2])
-  })
+    list.append(2);
+    expectList(list, [1, 2]);
+  });
 
-  it('insert', () => {
-    const list = createAndAppend([1])
+  it('insertAt', () => {
+    const list = createAndAppend([1]);
 
-    list.insert(0, 20)
-    expectList(list, [20, 1])
+    list.insertAt(0, 20);
+    expectList(list, [20, 1]);
 
-    list.insert(1, 30)
-    expectList(list, [20, 30, 1])
-  })
+    list.insertAt(1, 30);
+    expectList(list, [20, 30, 1]);
+  });
 
   it('removeAt', () => {
-    const list = createAndAppend([10, 40, 30])
+    const list = createAndAppend([10, 40, 30]);
 
-    list.removeAt(0)
-    expectList(list, [40, 30])
+    list.removeAt(0);
+    expectList(list, [40, 30]);
 
-    list.removeAt(1)
-    expectList(list, [40])
-  })
+    list.removeAt(1);
+    expectList(list, [40]);
+  });
 
   it('elements', () => {
-    const list = createAndAppend([10, 20, 30])
-    expect([...list.elements()]).toEqual([10, 20, 30])
-  })
+    const list = createAndAppend([10, 20, 30]);
+    expect([...list.elements()]).toEqual([10, 20, 30]);
+  });
 
   it('isEmpty', () => {
-    const emptyList = new CircularDoublyLinkedList()
-    expect(emptyList.isEmpty()).toEqual(true)
+    const emptyList = new CircularDoublyLinkedList();
+    expect(emptyList.isEmpty()).toEqual(true);
 
-    const nonEmptyList = createAndAppend(['Hello'])
-    expect(nonEmptyList.isEmpty()).toEqual(false)
-  })
-})
+    const nonEmptyList = createAndAppend(['Hello']);
+    expect(nonEmptyList.isEmpty()).toEqual(false);
+  });
+});
