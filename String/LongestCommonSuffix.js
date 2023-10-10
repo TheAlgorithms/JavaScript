4937723412bc2ab55b01413d069c3fb6024fbea5
AmbrishRamachandiran <ambrish.r@infosys.com>
2023-10-10 13:51:59 +0530

    feat:longest common suffix in a string
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>

--- String/LongestCommonSuffix.js
+++ String/LongestCommonSuffix.js
@@ -1,24 +1,22 @@
-// @function findLongestCommonSuffix
-// @description Finds the longest common suffix in an array of strings.
-// @param {string[]} strings - An array of strings to find the common suffix from.
-// @returns {string} - The longest common suffix.
-
+/**
+ * Finds the longest common suffix in an array of strings.
+ * @param {string[]} strings - An array of strings to find the common suffix from.
+ * @returns {string} - The longest common suffix.
+ */
 export function findLongestCommonSuffix(strings) {
     if (strings.length === 0) return '';
   
-    const firstString = strings[0];
+    const sortedStrings = strings.slice().sort(); // Sort the strings lexicographically.
+    const firstString = sortedStrings[0];
+    const lastString = sortedStrings[sortedStrings.length - 1];
     let commonSuffix = '';
-  
+    
     for (let i = 0; i < firstString.length; i++) {
-      const char = firstString.charAt(i);
-  
-      for (let j = 1; j < strings.length; j++) {
-        if (i >= strings[j].length || strings[j].charAt(i) !== char) {
-          return commonSuffix;
-        }
+      if (firstString[i] === lastString[i]) {
+        commonSuffix += firstString[i];
+      } else {
+        break;
       }
-  
-      commonSuffix += char;
     }
   
     return commonSuffix;
