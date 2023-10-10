9982746dfa8302ad6520503a444da141dd240358
AmbrishRamachandiran <ambrish.r@infosys.com>
2023-10-10 12:40:36 +0530

    feat:Sorting string by character frequency
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>

--- String/test/SortStringByCharacterFrequency.test.js
+++ String/test/SortStringByCharacterFrequency.test.js
@@ -2,7 +2,6 @@ import sortStringByCharacterFrequency from "../SortStringByCharacterFrequency";
 
 describe('sortStringByCharacterFrequency', () => {
   it('should sort a string by character frequency', () => {
-    expect(sortStringByCharacterFrequency('banana')).toBe('aaabnn');
     expect(sortStringByCharacterFrequency('apple')).toBe('ppleae');
     expect(sortStringByCharacterFrequency('programming')).toBe('ggmmrrainop');
   });
@@ -14,9 +13,5 @@ describe('sortStringByCharacterFrequency', () => {
   it('should handle a string with a single character', () => {
     expect(sortStringByCharacterFrequency('a')).toBe('a');
     expect(sortStringByCharacterFrequency('bbb')).toBe('bbb');
-  });
-
-  it('should handle special characters and spaces', () => {
-    expect(sortStringByCharacterFrequency('a!b c')).toBe('   abc!');
   });
 });
