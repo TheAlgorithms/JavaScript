cf9ad50b343ccbd9d8bad59878b53ea6d85bd73d
Nikunj Bisht <nikkubisht12@gmail.com>
2023-10-11 21:40:23 +0530

    feat: added find subsets algorithm using bitmanipulation

---
+++ Bit-Manipulation/test/generateSubSets.test.js
@@ -0,0 +1,17 @@
+import { generateSubSets } from '../generateSubSets'
+
+describe('subSets', () => {
+  it('find the subsets', () => {
+    expect(generateSubSets([1, 2, 3])).toEqual([
+      [],
+      [1],
+      [2],
+      [1, 2],
+      [3],
+      [1, 3],
+      [2, 3],
+      [1, 2, 3]
+    ])
+    expect(generateSubSets([1, 2])).toEqual([[], [1], [2], [1, 2]])
+  })
+})
