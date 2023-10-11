ca9abeae5df9e25b6e1fa859c35579e2972d098f
AmbrishRamachandiran <ambrish.r@infosys.com>
2023-10-11 16:19:55 +0530

    feat:Longest common prefix
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>

--- String/LongestCommonPrefix.js
+++ String/LongestCommonPrefix.js
@@ -6,27 +6,26 @@
  * @returns {string} - The longest common prefix of the input strings.
  */
 const longestCommonPrefix = (strs) => {
-    if (!strs || strs.length === 0) {
-      return "";
+  if (!strs || strs.length === 0) {
+    return "";
+  }
+
+  // Find the shortest string in the array
+  const shortestStr = strs.reduce((shortest, current) => {
+    return current.length < shortest.length ? current : shortest;
+  }, strs[0]);
+
+  let maxCommonIndex = 0;
+  for (let i = 0; i < shortestStr.length; i++) {
+    const char = shortestStr[i];
+    if (strs.every((str) => str[i] === char)) {
+      maxCommonIndex = i;
+    } else {
+      break;
     }
-  
-    // Find the shortest string in the array
-    const shortestStr = strs.reduce((shortest, current) => {
-      return current.length < shortest.length ? current : shortest;
-    }, strs[0]);
-  
-    let prefix = "";
-    for (let i = 0; i < shortestStr.length; i++) {
-      const char = shortestStr[i];
-      if (strs.every((str) => str[i] === char)) {
-        prefix += char;
-      } else {
-        break;
-      }
-    }
-  
-    return prefix;
-  };
-  
-  export default longestCommonPrefix;
-  
+  }
+
+  return shortestStr.substring(0, maxCommonIndex + 1);
+};
+
+export default longestCommonPrefix;
