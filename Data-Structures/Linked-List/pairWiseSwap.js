4882828ab12f0e2820db222e5d2bc2f8624889bc
Pranav Ojha <ojhapranav14@gmail.com>
2023-10-12 12:02:17 +0530

    updated pairwiseswap

--- Data-Structures/Linked-List/pairWiseSwap.js
+++ Data-Structures/Linked-List/pairWiseSwap.js
@@ -1,5 +1,5 @@
-//problem link leetcode:https://leetcode.com/problems/swap-nodes-in-pairs/
 
+//problem link leetcode:https://leetcode.com/problems/swap-nodes-in-pairs/
 class Node {
     constructor(data) {
         this.data = data;
@@ -19,46 +19,6 @@ class LinkedList {
             }
         }
     }
-
-    size() {
-        return this.length;
-    }
-
-    head() {
-        return this.headNode?.data ?? null;
-    }
-
-    tail() {
-        return this.tailNode?.data ?? null;
-    }
-
-    isEmpty() {
-        return this.length === 0;
-    }
-
-    addLast(element) {
-        if (this.headNode === null) {
-            return this.addFirst(element);
-        }
-        const node = new Node(element);
-        this.tailNode.next = node;
-        this.tailNode = node;
-        this.length++;
-        return this.size();
-    }
-
-    addFirst(element) {
-        const node = new Node(element);
-        if (this.headNode === null) {
-            this.tailNode = node;
-        }
-        node.next = this.headNode;
-        this.headNode = node;
-        this.length++;
-        return this.size();
-    }
-
-    // ... (other methods from the original code)
 
     pairwiseSwap() {
         let current = this.headNode;
@@ -85,21 +45,6 @@ class LinkedList {
             this.tailNode = prev;
         }
     }
-
-    get() {
-        const list = [];
-        let currentNode = this.headNode;
-        while (currentNode) {
-            list.push(currentNode.data);
-            currentNode = currentNode.next;
-        }
-        return list;
-    }
 }
 
-// Example usage:
-const linkedList = new LinkedList([1, 2, 3, 4, 5]);
-console.log("Linked List before pairwise swapping:", linkedList.get());
-
-linkedList.pairwiseSwap();
-console.log("Linked List after pairwise swapping:", linkedList.get());
+export { Node, LinkedList };
