efd14646ae1140693424e4017bf93ef78a7f9204
AkshayChavan <akshay.chavan20@vit.edu>
2023-10-12 14:54:46 +0530

    changes made per review by appgurueu

--- Data-Structures/Linked-List/CycleDetectionII.js
+++ Data-Structures/Linked-List/CycleDetectionII.js
@@ -2,66 +2,76 @@
  * A LinkedList based solution for Detecting a start node of the Cycle in a list.
  * https://en.wikipedia.org/wiki/Cycle_detection
  */
-  
-  function detectCycleNode(head) {
-    /*
+
+function detectCycleNode(head) {
+  /*
     Problem Statement:
     Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
     Link for the Problem: https://leetcode.com/problems/linked-list-cycle-ii/
-    */
+  */
 
-    let length = 0;
-    let fast = head;
-    let slow = head;
-  
-    while (fast !== null && fast.next !== null) {
-      fast = fast.next.next;
-      slow = slow.next;
-      if (fast === slow) {
-        // Calculate length
-        length = lengthCycle(slow);
-        break; 
-      }
+  let length = 0
+  let fast = head
+  let slow = head
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next
+    slow = slow.next
+    if (fast === slow) {
+    // Calculate length of the cycle
+      length = lengthCycle(slow)
+      break
     }
-    
-    if (length === 0) {
-      return null;  //if length = 0, return null
-    }
-  
-    let f = head;
-    let s = head;
-    while (length > 0) {
-      s = s.next;
-      length--;
-    }
-    
-    while (f !== s) {
-      f = f.next;
-      s = s.next; 
-    }
-  
-    return s;
   }
 
-  function lengthCycle(head) {
-    let fast = head;
-    let slow = head;
-  
-    while (fast !== null && fast.next !== null) {
-      fast = fast.next.next;
-      slow = slow.next;
-      if (fast === slow) {
-        let temp = slow;
-        let length = 0;
-        do {
-          temp = temp.next;
-          length++;
-        } while (temp !== slow);
-        return length;
-      }
-    }
-  
-    return 0;
+  if (length === 0) {
+    // If length = 0, return null
+    return null
   }
 
-  export { detectCycleNode}
+  //Point the nodes at head 
+  fast = head
+  slow = head
+  // Move slow pointer ahead by 'length' of cycle times
+  while (length > 0) {
+    slow = slow.next
+    length--
+  }
+
+  // Now move both pointers until they meet - this will be the start of cycle
+  while (fast !== slow) {
+    fast = fast.next
+    slow = slow.next
+  }
+
+  // return the meeting node
+  return slow
+}
+
+function lengthCycle(head) {
+  let fast = head
+  let slow = head
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next
+    slow = slow.next
+
+    // if found a cycle, calculate the length
+    if (fast === slow) {
+      let temp = slow
+      let length = 0
+
+      // Traverse until we reach initial cycle pointer again
+      do {
+        temp = temp.next
+        length++
+      } while (temp !== slow)
+
+      return length
+    }
+  }
+
+  return 0
+}
+
+export { detectCycleNode }
--- Data-Structures/Linked-List/test/CycleDetectionII.test.js
+++ Data-Structures/Linked-List/test/CycleDetectionII.test.js
@@ -1,48 +1,39 @@
-import { detectCycleNode } from "../CycleDetectionII";
-import { Node } from "../SinglyLinkedList";
-
+import { detectCycleNode } from '../CycleDetectionII'
+import { Node } from '../SinglyLinkedList'
 
 describe('Detect Cycle', () => {
-    // Test case where list has no cycle 
   it('no cycle', () => {
-    const head = new Node(1);
-    head.next = new Node(2);
-  
-    expect(detectCycleNode(head)).toBeNull();
-  });
-  
-  // Test case with a simple cycle 
+    const head = new Node(1)
+    head.next = new Node(2)
+
+    expect(detectCycleNode(head)).toBeNull()
+  })
+
   it('simple cycle', () => {
-    const head = new Node(1);
-    head.next = new Node(2);
-    head.next.next = new Node(3);
-    head.next.next.next = head.next; // Creates a cycle
-  
-    expect(detectCycleNode(head)).toBe(head.next); 
-  });
-  
-  // Test case with longer list and cycle
+    const head = new Node(1)
+    head.next = new Node(2)
+    head.next.next = new Node(3)
+    head.next.next.next = head.next // Creates a cycle
+
+    expect(detectCycleNode(head)).toBe(head.next)
+  })
+
   it('long list with cycle', () => {
-    const head = new Node(1);
-    head.next = new Node(2);
-    head.next.next = new Node(3);
-    head.next.next.next = new Node(4);
-    head.next.next.next.next = new Node(5);
-    head.next.next.next.next.next = head.next.next; // Cycle
-  
-    expect(detectCycleNode(head)).toBe(head.next.next);
-  });
-  
-  // Test case with cycle on last node
+    const head = new Node(1)
+    head.next = new Node(2)
+    head.next.next = new Node(3)
+    head.next.next.next = new Node(4)
+    head.next.next.next.next = new Node(5)
+    head.next.next.next.next.next = head.next.next // Cycle
+
+    expect(detectCycleNode(head)).toBe(head.next.next)
+  })
+
   it('cycle on last node', () => {
-    const head = new Node(1);
-    head.next = new Node(2);
-    head.next.next = head; 
-  
-    expect(detectCycleNode(head)).toBe(head);
-  });
+    const head = new Node(1)
+    head.next = new Node(2)
+    head.next.next = head
 
+    expect(detectCycleNode(head)).toBe(head)
+  })
 })
-
-  
-  
