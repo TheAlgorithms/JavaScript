1bcfca32d91cacf1d11e4ecf1b83f1d121f6229c
Fahad <108647795+Fahadddddd@users.noreply.github.com>
2023-10-14 17:26:48 +0530

    Added Palidrome Code

---
+++ Data-Structures/Array/Palidrome.js
@@ -0,0 +1,12 @@
+// Given an integer x, return true if x is a palindrome,and false otherwise.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+
+ var isPalidrome = function y(){
+    let intt = x.toString().split("").reverse().join("");
+    return x = parseInt(intt)
+    }
