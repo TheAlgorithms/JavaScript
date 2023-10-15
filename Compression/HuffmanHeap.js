85df9122e0c0b19acafdd144f4073be303ff415c
aladin002dz <mahfoudh.arous@gmail.com>
2023-10-15 14:56:08 +0100

    Prettier Style

--- Compression/HuffmanHeap.js
+++ Compression/HuffmanHeap.js
@@ -1,90 +1,97 @@
-import { BinaryHeap, minHeapComparator } from '../Data-Structures/Heap/BinaryHeap';
+import {
+  BinaryHeap,
+  minHeapComparator
+} from '../Data-Structures/Heap/BinaryHeap'
 class Node {
-    constructor(symbol, frequency, left = null, right = null) {
-        this.symbol = symbol;
-        this.frequency = frequency;
-        this.left = left;
-        this.right = right;
-    }
+  constructor(symbol, frequency, left = null, right = null) {
+    this.symbol = symbol
+    this.frequency = frequency
+    this.left = left
+    this.right = right
+  }
 }
 
 class HuffmanCoder {
-    constructor(data) {
-        this.data = data;
-        this.codes = {};
-        this.buildHuffmanTree();
-        this.generateCodes(this.huffmanTree, "");
+  constructor(data) {
+    this.data = data
+    this.codes = {}
+    this.buildHuffmanTree()
+    this.generateCodes(this.huffmanTree, '')
+  }
+
+  buildFrequencyTable() {
+    const frequencyTable = {}
+    for (const char of this.data) {
+      if (char in frequencyTable) {
+        frequencyTable[char]++
+      } else {
+        frequencyTable[char] = 1
+      }
+    }
+    return frequencyTable
+  }
+
+  buildHuffmanTree() {
+    const frequencyTable = this.buildFrequencyTable()
+    const minHeap = new BinaryHeap(minHeapComparator)
+
+    for (const symbol in frequencyTable) {
+      minHeap.insert(new Node(symbol, frequencyTable[symbol]))
     }
 
-    buildFrequencyTable() {
-        const frequencyTable = {};
-        for (const char of this.data) {
-            if (char in frequencyTable) {
-                frequencyTable[char]++;
-            } else {
-                frequencyTable[char] = 1;
-            }
-        }
-        return frequencyTable;
+    while (minHeap.size() > 1) {
+      const left = minHeap.extractTop()
+      const right = minHeap.extractTop()
+      const combined = new Node(
+        null,
+        left.frequency + right.frequency,
+        left,
+        right
+      )
+      minHeap.insert(combined)
     }
 
-    buildHuffmanTree() {
-        const frequencyTable = this.buildFrequencyTable();
-        const minHeap = new BinaryHeap(minHeapComparator);
+    this.huffmanTree = minHeap.extractTop()
+  }
 
-        for (const symbol in frequencyTable) {
-            minHeap.insert(new Node(symbol, frequencyTable[symbol]));
-        }
+  generateCodes(node, code) {
+    if (!node) return
 
-        while (minHeap.size() > 1) {
-            const left = minHeap.extractTop();
-            const right = minHeap.extractTop();
-            const combined = new Node(null, left.frequency + right.frequency, left, right);
-            minHeap.insert(combined);
-        }
+    if (node.symbol) {
+      this.codes[node.symbol] = code
+    } else {
+      this.generateCodes(node.left, code + '0')
+      this.generateCodes(node.right, code + '1')
+    }
+  }
 
-        this.huffmanTree = minHeap.extractTop();
+  encode(data) {
+    let encodedString = ''
+    for (const char of data) {
+      encodedString += this.codes[char]
+    }
+    return encodedString
+  }
+
+  decode(encodedString) {
+    let decodedString = ''
+    let currentNode = this.huffmanTree
+
+    for (const bit of encodedString) {
+      if (bit === '0') {
+        currentNode = currentNode.left
+      } else {
+        currentNode = currentNode.right
+      }
+
+      if (currentNode.symbol) {
+        decodedString += currentNode.symbol
+        currentNode = this.huffmanTree
+      }
     }
 
-    generateCodes(node, code) {
-        if (!node) return;
-
-        if (node.symbol) {
-            this.codes[node.symbol] = code;
-        } else {
-            this.generateCodes(node.left, code + "0");
-            this.generateCodes(node.right, code + "1");
-        }
-    }
-
-    encode(data) {
-        let encodedString = "";
-        for (const char of data) {
-            encodedString += this.codes[char];
-        }
-        return encodedString;
-    }
-
-    decode(encodedString) {
-        let decodedString = "";
-        let currentNode = this.huffmanTree;
-
-        for (const bit of encodedString) {
-            if (bit === "0") {
-                currentNode = currentNode.left;
-            } else {
-                currentNode = currentNode.right;
-            }
-
-            if (currentNode.symbol) {
-                decodedString += currentNode.symbol;
-                currentNode = this.huffmanTree;
-            }
-        }
-
-        return decodedString;
-    }
+    return decodedString
+  }
 }
 
-
-export { HuffmanCoder };
+export { HuffmanCoder }
