07f36991d7ea4cd0d18e48cc105c4e5dbe0bd237
{Harshit Malpotra} <{malpotra.harshit@gmail.com}>
2023-10-20 21:43:09 +0530

    test: removed tests for recursive kept tests for iterative implementation of Eucledian GCD Algorithm

--- Recursive/test/EucledianGCD.test.js
+++ Recursive/test/EucledianGCD.test.js
@@ -1,16 +1,13 @@
-import { euclideanGCDIterative, euclideanGCDRecursive } from "../EucledianGCD";
+import { euclideanGCDIterative  } from "../EucledianGCD";
 
 describe('EucledianGCD', () => {
     it('the GCD of 2 and 3 should be 1', () => {
         expect(euclideanGCDIterative(2,3)).toBe(1);
-        expect(euclideanGCDRecursive(2,3)).toBe(1);
     })
     it('the GCD of 2 and 8 should be 2', () => {
         expect(euclideanGCDIterative(2,8)).toBe(2);
-        expect(euclideanGCDRecursive(2,8)).toBe(2);
     })
     it('the GCD of 1 and 100 should be 1', () => {
         expect(euclideanGCDIterative(1,100)).toBe(1);
-        expect(euclideanGCDRecursive(1,100)).toBe(1);
     })
 })
