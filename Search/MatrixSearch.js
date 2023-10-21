fe4075240d2ce33380a7fef680d7773fc7ddb41d
shahayush480 <shahayush480@gmail.com>
2023-10-21 08:39:58 +0530

    Feat: Created matrix search function with test cases

---
+++ Search/MatrixSearch.js
@@ -0,0 +1,15 @@
+/**
+ * @param {T} key - The element to be found.
+ * @param {T[][]} matrix - The matrix in which the element should be found.
+ * @template T
+ * @returns {number[]} - An array containing the first found coordinates of the element.
+ */
+const MatrixSearch = (key, matrix) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === key)   return [i, j] // Found the element, return its coordinates
+    }
+  }
+  return [-1, -1] // Element not found in the matrix
+}
+export { MatrixSearch }
