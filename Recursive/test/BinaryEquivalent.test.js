e3e48b412993e3ef62247882f3049b21de53f435
{Harshit Malpotra} <{malpotra.harshit@gmail.com}>
2023-10-22 13:01:46 +0530

    test: Refactored tests using .each()

--- Recursive/test/BinaryEquivalent.test.js
+++ Recursive/test/BinaryEquivalent.test.js
@@ -1,16 +1,29 @@
 import { binaryEquivalent } from "../BinaryEquivalent";
 
-describe("BinaryEquivalent", () => {
-    it('The binary equivalent of 2 should be "10"', () => {
-        expect(binaryEquivalent(2)).toBe("10");
-    })
-    it('The binary equivalent of 0 should be "0"', () => {
-        expect(binaryEquivalent(0)).toBe("0");
-    })
-    it('The binary equivalent of 543 should be "1000011111"', () => {
-        expect(binaryEquivalent(543)).toBe("1000011111");
-    })
-    it('The binary equivalent of 4697621023 should be "100011000000000000000001000011111"', () => {
-        expect(binaryEquivalent(4697621023)).toBe("100011000000000000000001000011111");
-    })
+const tests = [
+    {
+        test: 2,
+        expectedValue: "10"
+    },
+    {       
+        test: 0,
+        expectedValue: "0"
+    },
+    {
+        test: 543,
+        expectedValue: "1000011111"
+    },
+    {
+        test: 4697621023,
+        expectedValue: "100011000000000000000001000011111"
+    }
+]
+
+describe("Binary Equivalent Tests", () => {
+    test.each(tests)(
+        "Binary Equivalent of $test should be $expectedValue",
+        ({test, expectedValue}) => {
+            expect(binaryEquivalent(test)).toBe(expectedValue);
+        }
+    )
 })
