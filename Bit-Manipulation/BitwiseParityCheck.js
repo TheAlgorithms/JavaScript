cf539bbb1b24264d41db9b1c7a71df91b173533d
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-10-23 19:48:15 +0530

    Add comments

--- Bit-Manipulation/BitwiseParityCheck.js
+++ Bit-Manipulation/BitwiseParityCheck.js
@@ -1,3 +1,21 @@
+/*
+    author: Hardvan
+
+    This script will check whether the given
+    number is even parity or odd parity.
+
+    Even parity is a property of binary numbers,
+    where the total number of 1-bits in a number
+    is even or odd.
+
+    Reference link: https://en.wikipedia.org/wiki/Parity_bit
+
+    If we will right shift the number by 1 bit
+    and XOR it with the original number, the
+    result will be 0 if the number has even parity
+    and 1 if the number has odd parity.
+
+*/
 export const checkEvenParity = (number) => {
   let count = 0
   while (number > 0) {
