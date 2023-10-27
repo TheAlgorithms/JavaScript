25f908cd733722efffff8d04ffa9a745341a9a55
Lars Mueller <appgurulars@gmx.de>
2023-10-28 00:56:39 +0200

    chore: format using prettier

--- Backtracking/MColoringProblem.js
+++ Backtracking/MColoringProblem.js
@@ -6,44 +6,44 @@
  * @see https://en.wikipedia.org/wiki/Graph_coloring
  */
 function mColoring(graph, m) {
-  const colors = new Array(graph.length).fill(0);
+  const colors = new Array(graph.length).fill(0)
 
   // Check if it's safe to color a vertex with a given color.
   function isSafe(vertex, color) {
     for (let i = 0; i < graph.length; i++) {
       if (graph[vertex][i] && colors[i] === color) {
-        return false;
+        return false
       }
     }
-    return true;
+    return true
   }
 
   // Use backtracking to try and color the graph.
   function solveColoring(vertex = 0) {
     if (vertex === graph.length) {
-      return true;
+      return true
     }
 
     for (let color = 1; color <= m; color++) {
       if (isSafe(vertex, color)) {
-        colors[vertex] = color;
-        
+        colors[vertex] = color
+
         if (solveColoring(vertex + 1)) {
-          return true;
+          return true
         }
 
         // If no solution, backtrack.
-        colors[vertex] = 0;
+        colors[vertex] = 0
       }
     }
-    return false;
+    return false
   }
 
   // If coloring is possible, return the colors.
   if (solveColoring()) {
-    return colors;
+    return colors
   }
-  return null;
+  return null
 }
 
-export { mColoring };
+export { mColoring }
