62682cec765c5c4ac649f5604a42e82bcd0bc462
Manpreet Singh <63737630+ManpreetSingh2004@users.noreply.github.com>
2023-10-29 15:59:05 +0530

    Introduce hexMD5 function

--- Hashes/tests/MD5.test.js
+++ Hashes/tests/MD5.test.js
@@ -1,14 +1,23 @@
 import { MD5 } from '../MD5'
 
+/**
+ * Returns the MD5 hash of the given message as a hexadecimal string
+ *
+ * @param {Uint8Array} message - message to hash
+ * @return {string} - hash as a hexadecimal string
+ */
+function hexMD5(message) {
+  return Array.from(MD5(message), (byte) =>
+    byte.toString(16).padStart(2, '0')
+  ).join('')
+}
+
 describe('Testing MD5 function', () => {
   it('should return the correct hash for "The quick brown fox jumps over the lazy dog"', () => {
     const input = new TextEncoder().encode(
       'The quick brown fox jumps over the lazy dog'
     )
-
-    const hash = Array.from(MD5(input), (byte) =>
-      byte.toString(16).padStart(2, '0')
-    ).join('')
+    const hash = hexMD5(input)
 
     expect(hash).toBe('9e107d9d372bb6826bd81d3542a419d6')
   })
@@ -17,20 +26,14 @@ describe('Testing MD5 function', () => {
     const input = new TextEncoder().encode(
       'The quick brown fox jumps over the lazy dog.'
     )
-
-    const hash = Array.from(MD5(input), (byte) =>
-      byte.toString(16).padStart(2, '0')
-    ).join('')
+    const hash = hexMD5(input)
 
     expect(hash).toBe('e4d909c290d0fb1ca068ffaddf22cbd0')
   })
 
   it('should correctly hash an empty string', () => {
     const input = new TextEncoder().encode('')
-
-    const hash = Array.from(MD5(input), (byte) =>
-      byte.toString(16).padStart(2, '0')
-    ).join('')
+    const hash = hexMD5(input)
 
     expect(hash).toBe('d41d8cd98f00b204e9800998ecf8427e')
   })
