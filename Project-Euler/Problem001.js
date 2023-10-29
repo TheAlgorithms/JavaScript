f29ea97ac06011419f1e54cab7a78d792e5077b9
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-29 22:20:51 +0530

    Update Problem001.js

--- Project-Euler/Problem001.js
+++ Project-Euler/Problem001.js
@@ -5,19 +5,19 @@ Find the sum of all the multiples of 3 or 5 below the provided parameter value n
 */
 
 // This method uses the nSum function to add the nSum for 3 and 5. However, it needs to subtract the nSum for 15 once to avoid double counting.
-const multiplesThreeAndFive = num => {
-	if (num < 1) throw new Error('No natural numbers exist below 1')
-	num -= 1
-	let sum = 0
-	
-	// The nSum function calculates the sum of the first n numbers in the sequence with a common difference of num.
-	// Here, n is denoted as frequency.
-	const nSum = (num, frequency) => (frequency * (frequency + 1) * num) >> 1
-	
-	sum += nSum(3, Math.floor(num / 3))
-	sum += nSum(5, Math.floor(num / 5))
-	sum -= nSum(15, Math.floor(num / 15))
-	return sum
+const multiplesThreeAndFive = (num) => {
+  if (num < 1) throw new Error('No natural numbers exist below 1')
+  num -= 1
+  let sum = 0
+
+  // The nSum function calculates the sum of the first n numbers in the sequence with a common difference of num.
+  // Here, n is denoted as frequency.
+  const nSum = (num, frequency) => (frequency * (frequency + 1) * num) >> 1
+
+  sum += nSum(3, Math.floor(num / 3))
+  sum += nSum(5, Math.floor(num / 5))
+  sum -= nSum(15, Math.floor(num / 15))
+  return sum
 }
 
 export { multiplesThreeAndFive }
