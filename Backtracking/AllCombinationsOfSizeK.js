56f3199438b757efd56e7cb0e8345da6d48456c6
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-29 18:49:54 +0530

    Update AllCombinationsOfSizeK.js

--- Backtracking/AllCombinationsOfSizeK.js
+++ Backtracking/AllCombinationsOfSizeK.js
@@ -1,25 +1,3 @@
-/*
-  Problem: Given two numbers, n and k, make all unique combinations of k numbers from 1 to n and in sorted order
-
-  What is combinations?
-  - Combinations is selecting items from a collections without considering the order of selection
-
-  Example:
-  - We have an apple, a banana, and a jackfruit
-  - We have three objects, and need to choose two items, then combinations will be
-
-  1. Apple & Banana
-  2. Apple & Jackfruit
-  3. Banana & Jackfruit
-
-  To read more about combinations, you can visit the following link:
-  - https://betterexplained.com/articles/easy-permutations-and-combinations/
-
-  Solution:
-  - We will be using backtracking to solve this questions
-  - Take one element, and make all them combinations for k-1 elements
-  - Once we get all combinations of that element, pop it and do same for next element
-*/
 function generateCombinations(n, k) {
   let currentCombination = [];
   let allCombinations = []; // will be used for storing all combinations
