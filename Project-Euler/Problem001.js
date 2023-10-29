a77bd6d4784f64b5c92ad01d8507d4a69c15bd01
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-29 22:09:59 +0530

    Update Problem001.js

--- Project-Euler/Problem001.js
+++ Project-Euler/Problem001.js
@@ -1,27 +1,23 @@
 // https://projecteuler.net/problem=1
-/*    Multiples of 3 and 5
-    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+/* Multiples of 3 and 5
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 */
 
-//nSum function calculates the sum of first n numbers in the table of number num
-//here n is denoted as frequency
+// This method uses the nSum function to add the nSum for 3 and 5. However, it needs to subtract the nSum for 15 once to avoid double counting.
+const multiplesThreeAndFive = num => {
+	if (num < 1) throw new Error('No natural numbers exist below 1')
+	num -= 1
+	let sum = 0
 
+	// The nSum function calculates the sum of the first n numbers in the sequence with a common difference of num.
+	// Here, n is denoted as frequency.
+	const nSum = (num, frequency) => (frequency * (frequency + 1) * num) >> 1
 
-//this method uses the nSum function adds the nSum for 3 and 5 but due to this nSum for 15 has been automatically added twice so it needs to be removed once
-const multiplesThreeAndFive = (num) => {
-  if (num < 1) throw new Error('No natural numbers exist below 1')
-    num-=1;
-  let sum = 0
-
-    const nSum = (num,frequency)=>{
-        return frequency*(frequency+1)*num>>1
-    }
-  sum += nSum(3,Math.floor(num/3));
-  sum += nSum(5,Math.floor(num/5));
-  sum -= nSum(15,Math.floor(num/15));
-  return sum
+	sum += nSum(3, Math.floor(num / 3))
+	sum += nSum(5, Math.floor(num / 5))
+	sum -= nSum(15, Math.floor(num / 15))
+	return sum
 }
 
-
-export { multiplesThreeAndFive }
+export {multiplesThreeAndFive}
