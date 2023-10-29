a63bf463dad9caa755cec115094b17f0936514a9
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-29 19:08:06 +0530

    Update AllCombinationsOfSizeK.test.js

--- Backtracking/tests/AllCombinationsOfSizeK.test.js
+++ Backtracking/tests/AllCombinationsOfSizeK.test.js
@@ -2,7 +2,8 @@ import { generateCombinations } from '../AllCombinationsOfSizeK'
 
 describe('AllCombinationsOfSizeK', () => {
   it('should return 3x2 matrix solution for n = 3 and k = 2', () => {
-    expect(generateCombinations(3,2)).toEqual([
+    const res = generateCombinations(3,2)
+    expect(res).toEqual([
       [1, 2],
       [1, 3],
       [2, 3]
@@ -10,7 +11,8 @@ describe('AllCombinationsOfSizeK', () => {
   })
 
   it('should return 6x2 matrix solution for n = 4 and k = 2', () => {
-    expect(generateCombinations(4,2)).toEqual([
+    const res = generateCombinations(4,2)
+    expect(res).toEqual([
       [1, 2],
       [1, 3],
       [1, 4],
