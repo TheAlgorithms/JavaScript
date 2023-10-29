2ff146633dcf9d088420cb514cfb095f6c6e8f3e
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-29 22:15:05 +0530

    Update Problem001.js

--- Project-Euler/Problem001.js
+++ Project-Euler/Problem001.js
@@ -9,11 +9,11 @@ const multiplesThreeAndFive = num => {
 	if (num < 1) throw new Error('No natural numbers exist below 1')
 	num -= 1
 	let sum = 0
-
+	
 	// The nSum function calculates the sum of the first n numbers in the sequence with a common difference of num.
 	// Here, n is denoted as frequency.
 	const nSum = (num, frequency) => (frequency * (frequency + 1) * num) >> 1
-
+	
 	sum += nSum(3, Math.floor(num / 3))
 	sum += nSum(5, Math.floor(num / 5))
 	sum -= nSum(15, Math.floor(num / 15))
