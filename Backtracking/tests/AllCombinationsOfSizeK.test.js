de46071908c994ded9badafd8d1cda416a06f590
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-29 16:20:02 +0530

    Update AllCombinationsOfSizeK.test.js
    Changes made it the type of testing. 
    Instead of testing the class now the program will test the function

--- Backtracking/tests/AllCombinationsOfSizeK.test.js
+++ Backtracking/tests/AllCombinationsOfSizeK.test.js
@@ -1,9 +1,8 @@
-import { Combinations } from '../AllCombinationsOfSizeK'
+import { generateCombinations } from '../AllCombinationsOfSizeK'
 
 describe('AllCombinationsOfSizeK', () => {
   it('should return 3x2 matrix solution for n = 3 and k = 2', () => {
-    const test1 = new Combinations(3, 2)
-    expect(test1.findCombinations()).toEqual([
+    expect(generateCombinations(3,2)).toEqual([
       [1, 2],
       [1, 3],
       [2, 3]
@@ -11,8 +10,7 @@ describe('AllCombinationsOfSizeK', () => {
   })
 
   it('should return 6x2 matrix solution for n = 4 and k = 2', () => {
-    const test2 = new Combinations(4, 2)
-    expect(test2.findCombinations()).toEqual([
+    expect(generateCombinations(4,2)).toEqual([
       [1, 2],
       [1, 3],
       [1, 4],
