64cf5ae257a1866bda38b46d19bc3c0e7155d4aa
vedas-dixit <vedasdixit1006@gmail.com>
2023-11-05 21:55:16 +0530

    fixed code style

--- Recursive/Partition.js
+++ Recursive/Partition.js
@@ -10,30 +10,30 @@
 
 const canPartition = (nums, index = 0, target = 0) => {
   if (!Array.isArray(nums)) {
-    throw new TypeError('Invalid Input');
+    throw new TypeError('Invalid Input')
   }
 
-  const sum = nums.reduce((acc, num) => acc + num, 0);
+  const sum = nums.reduce((acc, num) => acc + num, 0)
 
   if (sum % 2 !== 0) {
-    return false;
+    return false
   }
 
   if (target === sum / 2) {
-    return true;
+    return true
   }
 
   if (index >= nums.length || target > sum / 2) {
-    return false;
+    return false
   }
 
   // Include the current number in the first subset and check if a solution is possible.
-  const withCurrent = canPartition(nums, index + 1, target + nums[index]);
+  const withCurrent = canPartition(nums, index + 1, target + nums[index])
 
   // Exclude the current number from the first subset and check if a solution is possible.
-  const withoutCurrent = canPartition(nums, index + 1, target);
+  const withoutCurrent = canPartition(nums, index + 1, target)
 
-  return withCurrent || withoutCurrent;
-};
+  return withCurrent || withoutCurrent
+}
 
-export { canPartition };
+export { canPartition }
