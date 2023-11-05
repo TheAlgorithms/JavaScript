9cfd3f224dbe2bb332be2c30748eaf3108f8a393
vedas-dixit <vedasdixit1006@gmail.com>
2023-11-05 20:52:34 +0530

    Updated test-case of TugOfWar

--- Recursive/test/TugOfWar.test.js
+++ Recursive/test/TugOfWar.test.js
@@ -1,12 +1,12 @@
-import { tugOfWar } from '../TugOfWar'
+import { tugOfWar } from '../TugOfWar';
 
 describe('tugOfWar', () => {
   it('should divide array into two subsets with minimized sum difference', () => {
-    const arr = [3, 4, 5, -3, 100, 1, 89, 54, 23, 20]
-    const [subset1, subset2] = tugOfWar(arr)
+    const arr = [3, 4, 5, -3, 100, 1, 89, 54, 23, 20];
+    const [subset1, subset2] = tugOfWar(arr);
     const diff = Math.abs(
       subset1.reduce((a, b) => a + b, 0) - subset2.reduce((a, b) => a + b, 0)
-    )
-    expect(diff).toBe(1)
-  })
-})
+    );
+    expect(diff).toBe(1);
+  });
+});
