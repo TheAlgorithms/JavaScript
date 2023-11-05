55307a06d935ab1879c43ea80e57ecf48bea5ac2
vedas-dixit <vedasdixit1006@gmail.com>
2023-11-05 20:47:03 +0530

    Code Modification: subsets should have sizes as close to n/2 as possible

--- Recursive/TugOfWar.js
+++ Recursive/TugOfWar.js
@@ -6,32 +6,34 @@
  */
 
 function tugOfWar(arr) {
-  const totalSum = arr.reduce((a, b) => a + b, 0)
-  let minimumDiff = totalSum
-  let result = [arr, []]
+  const totalSum = arr.reduce((a, b) => a + b, 0);
+  let minimumDiff = totalSum;
+  let result = [arr, []];
 
-  function recurse(subset, index) {
+  function recurse(subset, index, size1, size2) {
     if (index === arr.length) {
-      const currentSubsetSum = subset.reduce((a, b) => a + b, 0)
-      const otherSubsetSum = totalSum - currentSubsetSum
-      const currentDiff = Math.abs(currentSubsetSum - otherSubsetSum)
-
-      if (currentDiff < minimumDiff) {
-        minimumDiff = currentDiff
-        result = [subset, arr.filter((x) => !subset.includes(x))]
+      if (Math.abs(size1 - size2) < minimumDiff) {
+        minimumDiff = Math.abs(size1 - size2);
+        result = [subset.slice(), arr.filter((x) => !subset.includes(x))];
       }
-      return
+      return;
     }
 
-    // Include the current element.
-    recurse([...subset, arr[index]], index + 1)
+    // Include the current element in the first subset if it doesn't exceed the other subset's size.
+    if (size1 + 1 <= size2 + (arr.length - index)) {
+      subset.push(arr[index]);
+      recurse(subset, index + 1, size1 + 1, size2);
+      subset.pop();
+    }
 
-    // Exclude the current element.
-    recurse([...subset], index + 1)
+    // Include the current element in the second subset if it doesn't exceed the other subset's size.
+    if (size2 + 1 <= size1 + (arr.length - index)) {
+      recurse(subset, index + 1, size1, size2 + 1);
+    }
   }
 
-  recurse([], 0)
-  return result
+  recurse([], 0, 0, 0);
+  return result;
 }
 
-export { tugOfWar }
+export { tugOfWar };
