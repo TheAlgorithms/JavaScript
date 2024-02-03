0fa3514ed0ad6ea41c94e2c71da3e5bf5bd166fc
Piotr Idzik <vil02@o2.pl>
2024-02-03 12:21:10 +0000

    tests: add missing tests for `PascalTriangle`

--- Maths/test/PascalTriangle.test.js
+++ Maths/test/PascalTriangle.test.js
@@ -1,3 +1,4 @@
+import { expect } from 'vitest'
 import { generate } from '../PascalTriangle'
 
 describe('Pascals Triangle', () => {
@@ -16,5 +17,20 @@ describe('Pascals Triangle', () => {
     expect(pascalsTriangle).toEqual(
       expect.arrayContaining([[1], [1, 1], [1, 2, 1]])
     )
+  })
+  it.each([
+    [0, []],
+    [1, [[1]]],
+    [2, [[1], [1, 1]]],
+    [3, [[1], [1, 1], [1, 2, 1]]],
+    [4, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]],
+    [5, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]], [1, 4, 6, 4, 1]]
+  ])('check with %j', (input, expected) => {
+    const pascalsTriangle = generate(input)
+    expect(pascalsTriangle.length).toEqual(input)
+    pascalsTriangle.forEach((arr, index) => {
+      expect(arr.length).toEqual(index + 1)
+    })
+    expect(pascalsTriangle).toEqual(expect.arrayContaining(expected))
   })
 })
