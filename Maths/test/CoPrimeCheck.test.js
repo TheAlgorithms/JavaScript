1f88b1c954267b4b18723907025fd418b6745e79
Piotr Idzik <vil02@o2.pl>
2024-02-28 16:27:46 +0000

    fix: handle zeros in `CoPrimeCheck`

--- Maths/test/CoPrimeCheck.test.js
+++ Maths/test/CoPrimeCheck.test.js
@@ -8,7 +8,9 @@ describe('CoPrimeCheck', () => {
     [1, 7],
     [20, 21],
     [5, 7],
-    [-5, -7]
+    [-5, -7],
+    [1, 0],
+    [-1, 0]
   ])('returns true for %j and %i', (inputA, inputB) => {
     expect(CoPrimeCheck(inputA, inputB)).toBe(true)
     expect(CoPrimeCheck(inputB, inputA)).toBe(true)
@@ -16,7 +18,9 @@ describe('CoPrimeCheck', () => {
 
   it.each([
     [5, 15],
-    [13 * 17 * 19, 17 * 23 * 29]
+    [13 * 17 * 19, 17 * 23 * 29],
+    [2, 0],
+    [0, 0]
   ])('returns false for %j and %i', (inputA, inputB) => {
     expect(CoPrimeCheck(inputA, inputB)).toBe(false)
     expect(CoPrimeCheck(inputB, inputA)).toBe(false)
