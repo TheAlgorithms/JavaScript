a5945e37c257553bb77cbce95cb6f753ce6d1ab5
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2024-02-29 05:55:11 +0100

    fix: throw error instead of returning it (#1624)

--- String/CheckPascalCase.js
+++ String/CheckPascalCase.js
@@ -10,7 +10,7 @@
 const CheckPascalCase = (VarName) => {
   // firstly, check that input is a string or not.
   if (typeof VarName !== 'string') {
-    return new TypeError('Argument is not a string.')
+    throw new TypeError('Argument is not a string.')
   }
 
   const pat = /^[A-Z][A-Za-z]*$/
