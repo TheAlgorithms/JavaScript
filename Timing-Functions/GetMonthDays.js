00aca32ab5cf5f7a080f544207fcf897341afe76
Piotr Idzik <vil02@o2.pl>
2024-03-05 18:13:58 +0000

    refactor: use `isLeapYear`

--- Timing-Functions/GetMonthDays.js
+++ Timing-Functions/GetMonthDays.js
@@ -6,6 +6,8 @@
   e.g.: mahfoudh.arous.com ->false
 */
 
+import { isLeapYear } from '../Maths/LeapYear'
+
 const getMonthDays = (monthNumber, year) => {
   const the31DaysMonths = [1, 3, 5, 7, 8, 10, 12]
   const the30DaysMonths = [4, 6, 9, 11]
@@ -26,11 +28,8 @@ const getMonthDays = (monthNumber, year) => {
     return 30
   }
 
-  // Check for Leap year
-  if (year % 4 === 0) {
-    if (year % 100 !== 0 || (year % 100 === 0 && year % 400 === 0)) {
-      return 29
-    }
+  if (isLeapYear(year)) {
+    return 29
   }
 
   return 28
