fb5530fdbc85494323274895845c040850ccd355
mohmmadAyesh <mohmmadayesh22@gmail.com>
2024-03-28 13:30:02 +0200

    fixing JSDOCS for some files in Recursive folder

--- Recursive/TowerOfHanoi.js
+++ Recursive/TowerOfHanoi.js
@@ -1,6 +1,14 @@
 // wiki - https://en.wikipedia.org/wiki/Tower_of_Hanoi
 // Recursive Javascript function to solve tower of hanoi
-
+/**
+ * Solves the Tower of Hanoi problem recursively.
+ * @param {number} n - The number of disks to move.
+ * @param {string} from - The rod from which to move the disks.
+ * @param {string} to - The rod to which to move the disks.
+ * @param {string} aux - The auxiliary rod for moving disks.
+ * @param {string[]} [output=[]] - Optional array to store the sequence of moves.
+ * @returns {string[]} The sequence of moves to solve the Tower of Hanoi problem.
+ */
 export function TowerOfHanoi(n, from, to, aux, output = []) {
   if (n === 1) {
     output.push(`Move disk 1 from rod ${from} to rod ${to}`)
