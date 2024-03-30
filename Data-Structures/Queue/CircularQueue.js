7adf170811309df907db7e2ea9d73348ca89b775
Martin Beacham <martinlutherbeacham@gmail.com>
2024-03-30 16:55:16 -0400

    Update CircularQueue.js
    Taking comments into account for refactoring my change.

--- Data-Structures/Queue/CircularQueue.js
+++ Data-Structures/Queue/CircularQueue.js
@@ -77,11 +77,7 @@ class CircularQueue {
 
   // Displays the length of queue
   length() {
-    if (this.queue.length > 0) {
-      return this.queue.length - 1
-    } else {
-      return 0
-    }
+    return checkEmpty() ? 0 : this.queue.length - 1
   }
 
   // Display the top most value of queue
