584424241c61079940d94a5aed1bed583c555098
Daniel <67126972+ddaniel27@users.noreply.github.com>
2024-06-17 23:08:16 -0500

    [Solution] Project euler challenge 19 with tests (#1659)
    * [Solution] Project euler challenge 19 with tests
    
    * update leap year function
    
    * Remove unnecessary, confusingly placed comments
    
    ---------
    
    Co-authored-by: Lars Müller <34514239+appgurueu@users.noreply.github.com>

---
+++ Project-Euler/test/Problem019.test.js
@@ -0,0 +1,8 @@
+import { problem19 } from '../Problem019.js'
+
+describe('checking sundays during the twentieth century', () => {
+  // Project Euler Challenge Check
+  test('result should be 171', () => {
+    expect(problem19()).toBe(171)
+  })
+})
