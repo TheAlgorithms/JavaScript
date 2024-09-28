2863fa120279e201296f86f9c66390fac96a5bd7
Omkarnath Parida <omkarnath.parida@yocket.in>
2024-09-28 13:23:25 +0530

    📦 NEW: Project Euler Problem 5 improvement and added test cases

---
+++ Project-Euler/test/Problem005.test.js
@@ -0,0 +1,14 @@
+import { findSmallestMultiple } from '../Problem005.js'
+
+describe.concurrent('Find smallest multiple', () => {
+  test('if max divisor is 10', () => {
+    expect(findSmallestMultiple(10)).toBe(2520)
+  })
+  test('if max divisor is 15', () => {
+    expect(findSmallestMultiple(15)).toBe(360360)
+  })
+  // Project Euler Condition Check
+  test('if max divisor is 20', () => {
+    expect(findSmallestMultiple(20)).toBe(232792560)
+  })
+})
