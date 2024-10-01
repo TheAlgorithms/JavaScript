0b2b277e4e740ff6d88c8f560785a9ae8ebfec83
0APOCALYPSE0 <giriaakash00@gmail.com>
2024-10-01 15:10:57 +0530

    covered all lines in test cases

--- Conversions/test/CamelPascalToSnakeCase.test.js
+++ Conversions/test/CamelPascalToSnakeCase.test.js
@@ -4,6 +4,7 @@ describe('camelToSnakeCase', () => {
   it.each([
     ['someRandomString', 'some_random_string'],
     ['SomeRandomStr#ng', 'some_random_str_ng'],
+    ['_SomeRandomStr#ng', 'some_random_str_ng'],
     ['123someRandom123String123', '123some_random123_string123'],
     ['123SomeRandom123String123', '123_some_random123_string123'],
     ['123SomeRandom123String123', '123_some_random123_string123'],
