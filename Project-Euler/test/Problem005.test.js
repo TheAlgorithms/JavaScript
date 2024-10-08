a1117ef7789f21b6a9490c35d89ddbd370babe39
Omkarnath Parida <omkarnath.parida@yocket.in>
2024-10-08 18:27:17 +0530

    👌 IMPROVE: test cases writing imrovements

--- Project-Euler/test/Problem005.test.js
+++ Project-Euler/test/Problem005.test.js
@@ -1,15 +1,12 @@
+import { expect } from 'vitest'
 import { findSmallestMultiple } from '../Problem005.js'
 
 describe.concurrent('Find smallest multiple', () => {
-  test('if max divisor is 10', () => {
-    expect(findSmallestMultiple(10)).toBe(2520)
-  })
-  test('if max divisor is 15', () => {
-    expect(findSmallestMultiple(15)).toBe(360360)
-  })
-
-  // Project Euler Condition Check
-  test('if max divisor is 20', () => {
-    expect(findSmallestMultiple(20)).toBe(232792560)
+  test.each([
+    [10, 2520],
+    [15, 360360],
+    [20, 232792560]
+  ])('max divisor -> %i, smallest multiple -> %i', (a, expected) => {
+    expect(findSmallestMultiple(a)).toBe(expected)
   })
 })
