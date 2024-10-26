e6cda60c542fcf75ccf2bba42c424c8ef48c3dd4
Ankana Pari <143877643+ankana2113@users.noreply.github.com>
2024-10-26 10:39:33 +0530

    prettier fixes

--- Maths/DigitalRoot.js
+++ Maths/DigitalRoot.js
@@ -1,29 +1,27 @@
-export const digitalRoot = (num) => {
+const digitalRoot = (num) => {
   /**
    * Calculates the digital root of a number in constant time.
    * @param {number} num - The number to compute the digital root for.
    * @returns {number} The digital root of the given number.
    *
-   * @example
-   * digitalRoot(456) // returns 6
-   * digitalRoot(-999) // returns 9
-   * digitalRoot(0) // returns 0
+   * @example digitalRoot(456) // returns 6
+   * @example digitalRoot(-999) // returns 9
+   * @example digitalRoot(0) // returns 0
    */
   if (num < 0) num = -num;
   return num === 0 ? num : 1 + ((num - 1) % 9);
 };
 /*------------------------------------------------------------------------------------*/
 
-export const digitalRootRecursive = (num) => {
+const digitalRootRecursive = (num) => {
   /**
    * Calculates the digital root of a number using recursion.
    * @param {number} num - The number to compute the digital root for.
    * @returns {number} The digital root of the given number.
    *
-   * @example
-   * digitalRoot(456) // returns 6
-   * digitalRoot(999) // returns 9
-   * digitalRoot(0) // returns 0
+   * @example digitalRootRecursive(456) // returns 6
+   * @example digitalRootrecursive(999) // returns 9
+   * @example digitalRootRecursive(0) // returns 0
    */
   if (num < 0) num = -num; // Handle negative input by converting to positive
   if (num < 10) return num; // Base case for single-digit number
@@ -32,3 +30,8 @@ export const digitalRootRecursive = (num) => {
   const sum = (num % 10) + digitalRootRecursive(Math.floor(num / 10));
   return digitalRootRecursive(sum); // Call digitalRoot recursively to reduce to single digit
 };
+
+export {
+  digitalRoot,
+  digitalRootRecursive
+}
