a807c3657ddec1afd602cbd8481615d391483590
Ankana Pari <143877643+ankana2113@users.noreply.github.com>
2024-10-26 10:24:27 +0530

    feat: added digital root algorithm and its recursive implementation

--- Maths/DigitalRoot.js
+++ Maths/DigitalRoot.js
@@ -1,14 +1,14 @@
 export const digitalRoot = (num) => {
   /**
- * Calculates the digital root of a number in constant time.
- * @param {number} num - The number to compute the digital root for.
- * @returns {number} The digital root of the given number.
- *
- * @example
- * digitalRoot(456) // returns 6
- * digitalRoot(-999) // returns 9
- * digitalRoot(0) // returns 0
- */
+   * Calculates the digital root of a number in constant time.
+   * @param {number} num - The number to compute the digital root for.
+   * @returns {number} The digital root of the given number.
+   *
+   * @example
+   * digitalRoot(456) // returns 6
+   * digitalRoot(-999) // returns 9
+   * digitalRoot(0) // returns 0
+   */
   if (num < 0) num = -num;
   return num === 0 ? num : 1 + ((num - 1) % 9);
 };
@@ -16,15 +16,15 @@ export const digitalRoot = (num) => {
 
 export const digitalRootRecursive = (num) => {
   /**
- * Calculates the digital root of a number using recursion.
- * @param {number} num - The number to compute the digital root for.
- * @returns {number} The digital root of the given number.
- *
- * @example
- * digitalRoot(456) // returns 6
- * digitalRoot(999) // returns 9
- * digitalRoot(0) // returns 0
- */
+   * Calculates the digital root of a number using recursion.
+   * @param {number} num - The number to compute the digital root for.
+   * @returns {number} The digital root of the given number.
+   *
+   * @example
+   * digitalRoot(456) // returns 6
+   * digitalRoot(999) // returns 9
+   * digitalRoot(0) // returns 0
+   */
   if (num < 0) num = -num; // Handle negative input by converting to positive
   if (num < 10) return num; // Base case for single-digit number
 
