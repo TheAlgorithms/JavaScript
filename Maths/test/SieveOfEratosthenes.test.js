a62a46e732798622b520769c9c1131a052721ca2
Shankha Suvra Dam <71999854+SpiderMath@users.noreply.github.com>
2025-01-12 15:57:54 +0530

    Resolve duplicate entries for sieve of eratosthenes  (#1770)
    * remove intarr test
    
    * Remove main file oops
    
    * FIXES: #1666 , remove references to SieveOfEratosthenesIntArray
    
    * Finally fix the requirements, passes vitest
    
    * Updated Documentation in README.md
    
    * FIXES: #1666 and conform to alg comment standards
    
    ---------
    
    Co-authored-by: SpiderMath <SpiderMath@users.noreply.github.com>

--- Maths/test/SieveOfEratosthenes.test.js
+++ Maths/test/SieveOfEratosthenes.test.js
@@ -1,14 +1,29 @@
 import { sieveOfEratosthenes } from '../SieveOfEratosthenes'
-import { PrimeCheck } from '../PrimeCheck'
 
-describe('should return an array of prime booleans', () => {
-  it('should have each element in the array as a prime boolean', () => {
-    const n = 30
-    const primes = sieveOfEratosthenes(n)
-    primes.forEach((primeBool, index) => {
-      if (primeBool) {
-        expect(PrimeCheck(index)).toBeTruthy()
-      }
-    })
+describe('sieveOfEratosthenes', () => {
+  test('returns an empty array for max < 2', () => {
+    expect(sieveOfEratosthenes(1)).toEqual([])
+  })
+
+  test('returns [2] for max = 2', () => {
+    expect(sieveOfEratosthenes(2)).toEqual([2])
+  })
+
+  test('returns [2, 3] for max = 3', () => {
+    expect(sieveOfEratosthenes(3)).toEqual([2, 3])
+  })
+
+  test('returns [2, 3, 5, 7] for max = 10', () => {
+    expect(sieveOfEratosthenes(10)).toEqual([2, 3, 5, 7])
+  })
+
+  test('returns [2, 3, 5, 7, 11, 13, 17, 19] for max = 20', () => {
+    expect(sieveOfEratosthenes(20)).toEqual([2, 3, 5, 7, 11, 13, 17, 19])
+  })
+
+  test('returns [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for max = 30', () => {
+    expect(sieveOfEratosthenes(30)).toEqual([
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+    ])
   })
 })
