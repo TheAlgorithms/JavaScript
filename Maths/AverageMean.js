19ecb18f0af35a83542410e59c16975bd8ddd4c3
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2025-09-04 07:01:22 +0300

    fix: improve mean function with input validation and extended tests (#1785)

--- Maths/AverageMean.js
+++ Maths/AverageMean.js
@@ -1,19 +1,30 @@
 /**
  * @function mean
  * @description This script will find the mean value of a array of numbers.
- * @param {Integer[]} nums - Array of integer
- * @return {Integer} - mean of nums.
+ * @param {number[]} numbers - Array of integer
+ * @return {number} - mean of numbers.
+ * @throws {TypeError} If the input is not an array or contains non-number elements.
+ * @throws {Error} If the input array is empty.
  * @see [Mean](https://en.wikipedia.org/wiki/Mean)
  * @example mean([1, 2, 4, 5]) = 3
  * @example mean([10, 40, 100, 20]) = 42.5
  */
-
-const mean = (nums) => {
-  if (!Array.isArray(nums)) {
+const mean = (numbers) => {
+  if (!Array.isArray(numbers)) {
     throw new TypeError('Invalid Input')
+  } else if (numbers.length === 0) {
+    throw new Error('Array is empty')
   }
 
-  return nums.reduce((sum, cur) => sum + cur, 0) / nums.length
+  let total = 0
+  numbers.forEach((num) => {
+    if (typeof num !== 'number') {
+      throw new TypeError('Invalid Input')
+    }
+    total += num
+  })
+
+  return total / numbers.length
 }
 
 export { mean }
